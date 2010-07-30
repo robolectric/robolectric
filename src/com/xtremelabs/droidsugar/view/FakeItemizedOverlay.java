4a82145dfa6ae70d8effaaa0784cbe0204e73769
Ryan Richard & Tyler Schultz <pair+rrichard+tyler@pivotallabs.com>
2010-07-30 11:40:06 -0700

    Add hitTest functionality to FakeItemizedOverlay

--- src/com/xtremelabs/droidsugar/view/FakeItemizedOverlay.java
+++ src/com/xtremelabs/droidsugar/view/FakeItemizedOverlay.java
@@ -1,10 +1,17 @@
 package com.xtremelabs.droidsugar.view;
 
+import com.google.android.maps.OverlayItem;
+
 @SuppressWarnings({"UnusedDeclaration"})
 public class FakeItemizedOverlay {
     public boolean populated;
+    public boolean shouldHit;
 
     protected final void populate() {
         populated = true;
+    }
+
+    protected boolean hitTest(OverlayItem item, android.graphics.drawable.Drawable drawable, int i, int i1) {
+        return shouldHit;
     }
 }
