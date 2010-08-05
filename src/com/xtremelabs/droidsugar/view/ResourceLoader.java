c66756e164965158fe37ae73b9b056bd37bf71aa
Harry Ugol & Joe Moore <pair+harry+joe@pivotallabs.com>
2010-08-05 10:58:10 -0700

    Load resources from all layout* directories

--- src/com/xtremelabs/droidsugar/view/ResourceLoader.java
+++ src/com/xtremelabs/droidsugar/view/ResourceLoader.java
@@ -1,19 +1,1 @@
-package com.xtremelabs.droidsugar.view;
-
-import java.io.File;
-
-public class ResourceLoader {
-    public final ViewLoader viewLoader;
-    public final StringResourceLoader stringResourceLoader;
-
-    public ResourceLoader(Class rClass, File resourceDir) throws Exception {
-        ResourceExtractor resourceExtractor = new ResourceExtractor();
-        resourceExtractor.addRClass(rClass);
-
-        viewLoader = new ViewLoader(resourceExtractor);
-        viewLoader.addResourceXmlDir(new File(resourceDir, "layout"));
-
-        stringResourceLoader = new StringResourceLoader(resourceExtractor);
-        stringResourceLoader.addResourceXmlDir(new File(resourceDir, "values"));
-    }
-}
+package com.xtremelabs.droidsugar.view;import java.io.File;import java.io.FileFilter;public class ResourceLoader {    public final ViewLoader viewLoader;    public final StringResourceLoader stringResourceLoader;    public ResourceLoader(Class rClass, File resourceDir) throws Exception {        ResourceExtractor resourceExtractor = new ResourceExtractor();        resourceExtractor.addRClass(rClass);        viewLoader = new ViewLoader(resourceExtractor);        File[] layoutDirs = resourceDir.listFiles(new FileFilter() {            @Override            public boolean accept(File pathname) {                return pathname.getPath().contains("/layout");            }        });        for (File layoutDir : layoutDirs) {            viewLoader.addResourceXmlDir(layoutDir);        }        stringResourceLoader = new StringResourceLoader(resourceExtractor);        stringResourceLoader.addResourceXmlDir(new File(resourceDir, "values"));    }}
