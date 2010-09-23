628391564010a3858cc1998c2c7e0227eb3729fb
Christian Williams & Ryan Richard <pair+xian+rrichard@pivotallabs.com>
2010-09-22 17:58:28 -0700

    Shoddy attempt at nested test classes.

--- src/com/xtremelabs/droidsugar/AbstractAndroidTestRunner.java
+++ src/com/xtremelabs/droidsugar/AbstractAndroidTestRunner.java
@@ -1,11 +1,15 @@
 package com.xtremelabs.droidsugar;
 
+import org.junit.Test;
 import org.junit.runners.BlockJUnit4ClassRunner;
 import org.junit.runners.model.FrameworkMethod;
 import org.junit.runners.model.InitializationError;
 import org.junit.runners.model.Statement;
+import org.junit.runners.model.TestClass;
 
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AbstractAndroidTestRunner extends BlockJUnit4ClassRunner {
     private ClassHandler classHandler;
@@ -26,6 +30,8 @@ public class AbstractAndroidTestRunner extends BlockJUnit4ClassRunner {
         if (classHandler != null) classHandler.beforeTest();
         beforeTest(method.getMethod());
 
+        if (currentFrameworkMethod != null) throw new IllegalStateException("current framework method is already set!");
+        currentFrameworkMethod = method;
         final Statement statement = super.methodBlock(method);
         return new Statement() {
             @Override public void evaluate() throws Throwable {
@@ -35,6 +41,7 @@ public class AbstractAndroidTestRunner extends BlockJUnit4ClassRunner {
                 } finally {
                     afterTest(method.getMethod());
                     if (classHandler != null) classHandler.afterTest();
+                    currentFrameworkMethod = null;
                 }
             }
         };
@@ -45,4 +52,42 @@ public class AbstractAndroidTestRunner extends BlockJUnit4ClassRunner {
 
     protected void afterTest(Method method) {
     }
+
+    @Override protected Object createTest() throws Exception {
+        Class<?> declaringClass = AbstractAndroidTestRunner.currentFrameworkMethod.getMethod().getDeclaringClass();
+        Object test;
+        if (declaringClass.isMemberClass()) {
+            Object enclosingTest = declaringClass.getEnclosingClass().newInstance();
+            test = declaringClass.getConstructor(enclosingTest.getClass()).newInstance(enclosingTest);
+        } else {
+            test = declaringClass.newInstance();
+        }
+        System.out.println("created test = " + test);
+        return test;
+    }
+
+    @Override protected List<FrameworkMethod> getChildren() {
+        List<FrameworkMethod> children = super.getChildren();
+        ArrayList<FrameworkMethod> mine = new ArrayList<FrameworkMethod>();
+        mine.addAll(children);
+        Class<?>[] innerClasses = getTestClass().getJavaClass().getClasses();
+        for (final Class<?> innerClass : innerClasses) {
+            TestClass testClass = new TestClass(innerClass);
+            List<FrameworkMethod> methods = testClass.getAnnotatedMethods(Test.class);
+            for (FrameworkMethod method : methods) {
+                mine.add(new FrameworkMethod(method.getMethod()) {
+                    @Override public String getName() {
+                        return innerClass.getSimpleName() + "." + super.getName();
+                    }
+                });
+            }
+        }
+        return mine;
+    }
+
+//    @Override
+//    protected Description describeChild(FrameworkMethod method) {
+//        return Description.createTestDescription(getTestClass().getJavaClass(),
+//                testName(method), method.getAnnotations());
+//    }
 }
