d6d683b28a16e424e09029bfafc6deb22c06f007
Phil Goodwin <pair+phil@pivotallabs.com>
2010-11-02 09:39:00 -0700

    Added jar target, fixed ShadoContextThemeWrapper constructor

--- src/com/xtremelabs/robolectric/shadows/ShadowContextThemeWrapper.java
+++ src/com/xtremelabs/robolectric/shadows/ShadowContextThemeWrapper.java
@@ -7,11 +7,6 @@ import com.xtremelabs.robolectric.util.Implements;
 @SuppressWarnings({"UnusedDeclaration"})
 @Implements(ContextThemeWrapper.class)
 public class ShadowContextThemeWrapper extends ShadowContextWrapper {
-
-    public ShadowContextThemeWrapper(ContextThemeWrapper realContextThemeWrapper) {
-        super(realContextThemeWrapper);
-    }
-
     public void __constructor__(Context baseContext, int themesres) {
         super.__constructor__(baseContext);
     }
