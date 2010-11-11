47277624dc760bb2716ca43fa9302117cc591b63
Michael Portuesi <mikep@zoodles.com>
2010-11-11 14:50:11 -0800

    Add ShadowIntent#setClass

--- src/com/xtremelabs/robolectric/shadows/ShadowIntent.java
+++ src/com/xtremelabs/robolectric/shadows/ShadowIntent.java
@@ -28,6 +28,7 @@ public class ShadowIntent {
     private ComponentName componentName;
     private Uri data;
     private int flags;
+    private Class<?> cls;
 
     public void __constructor__(Context packageContext, Class cls) {
         componentName = new ComponentName(packageContext, cls);
@@ -56,6 +57,12 @@ public class ShadowIntent {
     @Implementation
     public Uri getData() {
         return data;
+    }
+    
+    @Implementation
+    public Intent setClass(Context packageContext, Class<?> cls ) {
+    	this.cls = cls;
+    	return realIntent;
     }
 
     @Implementation
@@ -206,8 +213,12 @@ public class ShadowIntent {
                 ) +
                 '}';
     }
+    
+    public Class<?> getIntentClass() {
+		return cls;
+	}
 
-    private String ifWeHave(Object o, String name) {
+	private String ifWeHave(Object o, String name) {
         if (o == null) return null;
         if (o instanceof Map && ((Map)o).isEmpty()) return null;
         return name + "=" + o;
