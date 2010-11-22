595a0c838d8e59d44d8aab3d468b71e3dbf43e52
Michael Portuesi <mportuesisf@gmail.com>
2010-11-21 17:53:32 -0800

    Add ShadowSQLiteDatabase#insert and SQLiteDatabase#delete.  Both accept a very limited where clause of the form 'colname=value'.  Also, implement ShadowAbstractCursor#moveToNext and ShadowAbstractCursor#getPosition.

--- src/com/xtremelabs/robolectric/shadows/ShadowAbstractCursor.java
+++ src/com/xtremelabs/robolectric/shadows/ShadowAbstractCursor.java
@@ -9,9 +9,26 @@ import com.xtremelabs.robolectric.util.RealObject;
 @Implements(AbstractCursor.class)
 public class ShadowAbstractCursor {
     @RealObject private AbstractCursor realAbstractCursor;
+    
+    private int currentRowNumber = 0;
 
     @Implementation
     public final boolean moveToFirst() {
+    	currentRowNumber = 0;
         return realAbstractCursor.getCount() > 0;
+    }
+    
+    @Implementation
+    public int getPosition() {
+    	return currentRowNumber;
+    }
+    
+    @Implementation
+    public final boolean moveToNext() {
+    	if (currentRowNumber == realAbstractCursor.getCount() - 1) {
+    		return false;
+    	}
+    	currentRowNumber++;
+    	return true;
     }
 }
