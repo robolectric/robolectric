ab0f6772ac0ff737f89d8fbf89ce3b29f13caf25
Macarse <macarse@gmail.com>
2010-12-01 12:41:16 -0300

    @Ignore in non test classes

--- test/com/xtremelabs/robolectric/TestApplication.java
+++ test/com/xtremelabs/robolectric/TestApplication.java
@@ -1,6 +1,9 @@
 package com.xtremelabs.robolectric;
 
+import org.junit.Ignore;
+
 import android.app.Application;
 
+@Ignore
 public class TestApplication extends Application {
 }
--- test/com/xtremelabs/robolectric/util/TestOnClickListener.java
+++ test/com/xtremelabs/robolectric/util/TestOnClickListener.java
@@ -1,7 +1,10 @@
 package com.xtremelabs.robolectric.util;
 
+import org.junit.Ignore;
+
 import android.view.View;
 
+@Ignore
 public class TestOnClickListener implements View.OnClickListener {
     public boolean clicked = false;
     @Override public void onClick(View v) {
