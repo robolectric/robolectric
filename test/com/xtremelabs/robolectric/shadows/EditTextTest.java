f709a20d50b3ec27f3c8da426899848d049bb967
Phil Goodwin <pair+phil@pivotallabs.com>
2010-12-06 12:24:22 -0800

    remove unneeded test setup

--- test/com/xtremelabs/robolectric/shadows/EditTextTest.java
+++ test/com/xtremelabs/robolectric/shadows/EditTextTest.java
@@ -1,9 +1,7 @@
 package com.xtremelabs.robolectric.shadows;
 
 import android.widget.EditText;
-import com.xtremelabs.robolectric.Robolectric;
 import com.xtremelabs.robolectric.WithTestDefaultsRunner;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -11,10 +9,6 @@ import static org.junit.Assert.assertTrue;
 
 @RunWith(WithTestDefaultsRunner.class)
 public class EditTextTest {
-    @Before public void setUp() throws Exception {
-        Robolectric.bindDefaultShadowClasses();
-    }
-
     @Test
     public void shouldBeFocusableByDefault() throws Exception {
         assertTrue(new EditText(null).isFocusable());
