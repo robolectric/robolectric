966bbed22d63d7eb0f481bfbd54b860c3607153e
Cristian <casidiablo@elhacker.net>
2010-12-22 17:23:19 -0500

    It is sometimes useful and necessary for custom activities to be notified when its content has changed, invoking the Activity#onContentChanged() method. Examples of that are ListActivity and TabActivity which force the developer to declare a view with an specific ID, which is checked in the Activity/onContentChanged() method. In the same way, custom activities which are not part of the SDK could require that method to be called.

--- src/com/xtremelabs/robolectric/shadows/ShadowActivity.java
+++ src/com/xtremelabs/robolectric/shadows/ShadowActivity.java
@@ -59,11 +59,13 @@ public class ShadowActivity extends ShadowContextWrapper {
     @Implementation
     public void setContentView(int layoutResID) {
         contentView = getLayoutInflater().inflate(layoutResID, null);
+        realActivity.onContentChanged();
     }
 
     @Implementation
     public void setContentView(View view) {
         contentView = view;
+        realActivity.onContentChanged();
     }
 
     @Implementation
