22ae9206e660fd187ff79a529452dbe2bab504fd
Rich Humphrey <rich@zoodles.com>
2011-01-11 13:25:31 -0800

    Implemented ShadowWebView.getSettings()

--- src/main/java/com/xtremelabs/robolectric/shadows/ShadowWebView.java
+++ src/main/java/com/xtremelabs/robolectric/shadows/ShadowWebView.java
@@ -2,7 +2,10 @@ package com.xtremelabs.robolectric.shadows;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+
+import com.xtremelabs.robolectric.Robolectric;
 import com.xtremelabs.robolectric.internal.Implementation;
 import com.xtremelabs.robolectric.internal.Implements;
 
@@ -11,6 +14,7 @@ import com.xtremelabs.robolectric.internal.Implements;
 public class ShadowWebView extends ShadowAbsoluteLayout {
 
     private String lastUrl;
+    private WebSettings webSettings = Robolectric.newInstanceOf(WebSettings.class);
 
     @Override public void __constructor__(Context context, AttributeSet attributeSet) {
         super.__constructor__(context, attributeSet);
@@ -19,6 +23,11 @@ public class ShadowWebView extends ShadowAbsoluteLayout {
     @Implementation
     public void loadUrl(String url) {
         lastUrl = url;
+    }
+    
+    @Implementation 
+    public WebSettings getSettings() {
+    	return webSettings;
     }
 
     /**
--- src/test/java/com/xtremelabs/robolectric/shadows/WebViewTest.java
+++ src/test/java/com/xtremelabs/robolectric/shadows/WebViewTest.java
@@ -1,12 +1,15 @@
 package com.xtremelabs.robolectric.shadows;
 
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+
+import com.xtremelabs.robolectric.Robolectric;
 import com.xtremelabs.robolectric.WithTestDefaultsRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static com.xtremelabs.robolectric.Robolectric.shadowOf;
-import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertThat;
 
 @RunWith(WithTestDefaultsRunner.class)
@@ -18,4 +21,14 @@ public class WebViewTest {
         webView.loadUrl("http://example.com");
         assertThat(shadowOf(webView).getLastLoadedUrl(), equalTo("http://example.com"));
     }
+    
+    @Test
+    public void testGetSettings() {
+        WebView webView = new WebView(null);
+        WebSettings webSettings = webView.getSettings();
+        ShadowWebSettings shadowWebSettings = Robolectric.shadowOf(webSettings);
+        
+        assertThat( webSettings, notNullValue());
+        assertThat( shadowWebSettings, notNullValue());
+   }
 }
