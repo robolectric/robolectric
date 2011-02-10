fd0f6795e94faca052c9d240bd1adf4fbd55c796
Neal Sanche <thorinside@gmail.com>
2011-02-10 14:45:21 -0800

    Improved ShadowNetworkInfo by adding support for isAvailable and isConnected as well.

--- src/main/java/com/xtremelabs/robolectric/shadows/ShadowNetworkInfo.java
+++ src/main/java/com/xtremelabs/robolectric/shadows/ShadowNetworkInfo.java
@@ -12,19 +12,41 @@ import com.xtremelabs.robolectric.internal.Implements;
 public class ShadowNetworkInfo {
 
     private boolean isConnected = true;
+    private boolean isAvailable = true;
+
+    @Implementation
+    public boolean isConnected() {
+        return isConnected;
+    }
 
     @Implementation
     public boolean isConnectedOrConnecting() {
         return isConnected;
     }
 
+    @Implementation
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
     /**
      * Non-Android accessor
-     * Sets up the return value of {@link #isConnectedOrConnecting()}.
+     * Sets up the return value of {@link #isConnectedOrConnecting()} and {@link @isConnected()}.
      *
-     * @param isConnected the value that {@link #isConnectedOrConnecting()} will return.
+     * @param isConnected the value that {@link #isConnectedOrConnecting()} and {@link #isConnected()} will return.
      */
     public void setConnectionStatus(boolean isConnected) {
         this.isConnected = isConnected;
     }
+
+    /**
+     * Non-Android accessor
+     * Sets up the return value of {@link #isAvailable()}.
+     *
+     * @param isConnected the value that {@link #isAvailable()} will return.
+     */
+    public void setAvailableStatus(boolean isAvailable) {
+        this.isAvailable = isAvailable;
+    }
+
 }
