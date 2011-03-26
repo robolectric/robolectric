5c64020aebafacaa7677f81c044ca22cb1694539
Michael Portuesi <mikep@zoodles.com>
2011-03-25 19:03:51 -0700

    Add methods to ShadowCursorAdapter, ShadowSQLiteCursor, ShadowAbstractCursor. Tests pending

--- src/main/java/com/xtremelabs/robolectric/shadows/ShadowCursorAdapter.java
+++ src/main/java/com/xtremelabs/robolectric/shadows/ShadowCursorAdapter.java
@@ -1,7 +1,11 @@
 package com.xtremelabs.robolectric.shadows;
 
+import java.util.List;
+
 import android.content.Context;
 import android.database.Cursor;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.CursorAdapter;
 
 import com.xtremelabs.robolectric.internal.Implementation;
@@ -12,6 +16,7 @@ import com.xtremelabs.robolectric.internal.Implements;
 public class ShadowCursorAdapter {
 
 	private Cursor cursor;
+	private List<View> views;
 	
     public void __constructor__( Context ctx, Cursor curs, boolean autoRequery ) {
     	cursor = curs;
@@ -36,5 +41,49 @@ public class ShadowCursorAdapter {
     		return 0;
     	}
     	return cursor.getCount();
+    }
+    
+    @Implementation
+    public Object getItem(int position) {
+    	if ( cursor == null ) {
+    		return null;
+    	}
+    	
+    	cursor.moveToPosition(position);
+    	return new Integer(position);
+    }
+    
+    @Implementation
+    public long getItemId(int position) {
+    	if ( cursor == null ) {
+    		return 0;
+    	}
+
+    	cursor.moveToPosition(position);
+    	int rowIdColumn = cursor.getColumnIndexOrThrow("_id");
+    	return cursor.getLong(rowIdColumn);
+    }
+    
+    @Implementation
+    public View getView(int position, View convertView, ViewGroup parent) {
+    	if ( cursor == null ) {
+    		return null;
+    	}
+    	
+    	if ( convertView != null ) {
+    		return convertView;
+    	}
+    	
+    	return views.get(position);
+    }
+    
+    /**
+     * Non-Android API.  Set a list of views to be returned for successive
+     * calls to getView().
+     * 
+     * @param views
+     */
+    public void setViews( List<View> views ) {
+    	this.views = views;
     }
 }
