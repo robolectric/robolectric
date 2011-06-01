f96b2c0575b8c3efcf05420815e7297c759bef24
Chris Heisterkamp & Lowell Kirsh <pair+cheister+lowell@pivotallabs.com>
2011-06-01 10:47:58 -0700

    extract code into ParamsParser helper; add getLastSendHttpRequestInfo method

--- src/main/java/com/xtremelabs/robolectric/tester/org/apache/http/FakeHttpLayer.java
+++ src/main/java/com/xtremelabs/robolectric/tester/org/apache/http/FakeHttpLayer.java
@@ -1,27 +1,21 @@
 package com.xtremelabs.robolectric.tester.org.apache.http;
 
+import com.xtremelabs.robolectric.Robolectric;
 import org.apache.http.Header;
 import org.apache.http.HttpException;
 import org.apache.http.HttpHost;
 import org.apache.http.HttpRequest;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.RequestDirector;
-import org.apache.http.params.BasicHttpParams;
-import org.apache.http.params.HttpParams;
 import org.apache.http.protocol.HttpContext;
 
 import javax.xml.ws.http.HTTPException;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.lang.reflect.Field;
 import java.net.URI;
-import java.net.URLDecoder;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.StringTokenizer;
 import java.util.regex.Pattern;
 
 public class FakeHttpLayer {
@@ -30,6 +24,14 @@ public class FakeHttpLayer {
     List<HttpEntityStub.ResponseRule> httpResponseRules = new ArrayList<HttpEntityStub.ResponseRule>();
     HttpResponse defaultHttpResponse;
     private HttpResponse defaultResponse;
+
+    public HttpRequestInfo getLastSentHttpRequestInfo() {
+        List<HttpRequestInfo> requestInfos = Robolectric.getFakeHttpLayer().getSentHttpRequestInfos();
+        if (requestInfos.isEmpty()) {
+            return null;
+        }
+        return requestInfos.get(requestInfos.size() - 1);
+    }
 
     public void addPendingHttpResponse(int statusCode, String responseBody) {
         addPendingHttpResponse(new TestHttpResponse(statusCode, responseBody));
@@ -238,63 +240,13 @@ public class FakeHttpLayer {
                 return false;
             }
             if (params.size() > 0) {
-                Map<String, String> requestParams = parseParams(request);
+                Map<String, String> requestParams = ParamsParser.parseParams(request);
                 return requestParams.equals(params);
             }
 
             return true;
         }
 
-        private Map<String, String> parseParams(HttpRequest request) {
-            URI uri = URI.create(request.getRequestLine().getUri());
-            String rawQuery = uri.getRawQuery();
-            Map<String, String> params;
-            if (rawQuery != null) {
-                params = new LinkedHashMap<String, String>();
-                StringTokenizer tok = new StringTokenizer(rawQuery, "&", false);
-                while (tok.hasMoreTokens()) {
-                    String name, value;
-                    String nextParam = tok.nextToken();
-                    if (nextParam.contains("=")) {
-                        String[] nameAndValue = nextParam.split("=");
-                        name = nameAndValue[0];
-                        try {
-                            value = URLDecoder.decode(nameAndValue[1], "UTF-8");
-                        } catch (UnsupportedEncodingException e) {
-                            throw new RuntimeException(e);
-                        }
-                    } else {
-                        name = nextParam;
-                        value = "";
-                    }
-                    params.put(name, value);
-                }
-            } else {
-                HttpParams httpParams = request.getParams();
-                if (httpParams instanceof BasicHttpParams) {
-                    Map<String, String> parameters = getPrivateMember(httpParams, "parameters");
-                    params = new LinkedHashMap<String, String>(parameters);
-                }
-                else {
-                    throw new RuntimeException("Was expecting a "+BasicHttpParams.class.getName());
-                }
-                return params;
-            }
-            return params;
-        }
-
-        private <T> T getPrivateMember(Object obj, String name) {
-            try {
-                Field f = obj.getClass().getDeclaredField(name);
-                f.setAccessible(true);
-                //noinspection unchecked
-                return (T) f.get(obj);
-            } catch (NoSuchFieldException e) {
-                throw new RuntimeException(e);
-            } catch (IllegalAccessException e) {
-                throw new RuntimeException(e);
-            }
-        }
     }
 
     // TODO: test this
