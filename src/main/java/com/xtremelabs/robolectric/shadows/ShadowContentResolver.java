e2099d676d57981179b7ac894e4d339716e50d5d
Patrick Forhan <pforhan@squareup.com>
2011-06-14 16:19:43 -0500

    Improve ShadowContentResolver

--- src/main/java/com/xtremelabs/robolectric/shadows/ShadowContentResolver.java
+++ src/main/java/com/xtremelabs/robolectric/shadows/ShadowContentResolver.java
@@ -1,5 +1,7 @@
 package com.xtremelabs.robolectric.shadows;
 
+import java.io.ByteArrayInputStream;
+
 import android.content.ContentResolver;
 import android.net.Uri;
 import com.xtremelabs.robolectric.internal.Implementation;
@@ -11,16 +13,27 @@ import java.io.InputStream;
 @SuppressWarnings({"UnusedDeclaration"})
 @Implements(ContentResolver.class)
 public class ShadowContentResolver {
+    private byte[] bytes;
+
+    public void setStreamData(byte[] bytes) {
+        this.bytes = bytes;
+    }
+    
     @Implementation
     public final InputStream openInputStream(final Uri uri) {
-        return new InputStream() {
-            @Override public int read() throws IOException {
-                throw new UnsupportedOperationException();
-            }
-
-            @Override public String toString() {
-                return "stream for " + uri;
-            }
-        };
+        if (bytes == null) {
+            return new InputStream() {
+                @Override public int read() throws IOException {
+                    throw new UnsupportedOperationException();
+                }
+    
+                @Override public String toString() {
+                    return "stream for " + uri;
+                }
+            };
+            
+        } else {
+          return new ByteArrayInputStream(bytes);
+        }
     }
 }
---
+++ src/test/java/com/xtremelabs/robolectric/shadows/ContentResolverTest.java
@@ -0,0 +1,32 @@
+package com.xtremelabs.robolectric.shadows;
+
+import com.xtremelabs.robolectric.WithTestDefaultsRunner;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(WithTestDefaultsRunner.class)
+public class ContentResolverTest {
+
+  private ShadowContentResolver contentResolver;
+
+  @Before public void setUp() throws Exception {
+    contentResolver = new ShadowContentResolver();
+  }
+
+  @Test(expected=UnsupportedOperationException.class)
+  public void testWhenNoBytesSet_readThrowsException() throws Exception {
+    contentResolver.openInputStream(null).read();
+  }
+
+  public void testWhenBytesSet_readWorks() throws Exception {
+    String streamData = "Hello, I am a stream";
+    byte[] sourceBytes = streamData.getBytes();
+    contentResolver.setStreamData(sourceBytes);
+    byte[] bytes = new byte[sourceBytes.length];
+    contentResolver.openInputStream(null).read(bytes);
+    assertEquals(sourceBytes, bytes);
+  }
+}
