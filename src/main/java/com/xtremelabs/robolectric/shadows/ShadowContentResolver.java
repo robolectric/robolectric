248cd26e9980ffe90f8dd1537f0fb75bf8e2659c
Scott Bale <scott@squareup.com>
2011-06-16 10:37:34 -0500

    remove old mock byte[]

--- src/main/java/com/xtremelabs/robolectric/shadows/ShadowContentResolver.java
+++ src/main/java/com/xtremelabs/robolectric/shadows/ShadowContentResolver.java
@@ -5,46 +5,37 @@ import android.net.Uri;
 import com.xtremelabs.robolectric.Robolectric;
 import com.xtremelabs.robolectric.internal.Implementation;
 import com.xtremelabs.robolectric.internal.Implements;
-import java.io.ByteArrayInputStream;
+
 import java.io.IOException;
 import java.io.InputStream;
 
 @SuppressWarnings({"UnusedDeclaration"})
 @Implements(ContentResolver.class)
 public class ShadowContentResolver {
-    private byte[] bytes;
-
-    public void setStreamData(byte[] bytes) {
-        this.bytes = bytes;
-    }
 
     @Implementation
     public final InputStream openInputStream(final Uri uri) {
-        if (bytes == null) {
 
-            if (uri != null && ContentResolver.SCHEME_ANDROID_RESOURCE.equals(uri.getScheme())) {
-                String path = uri.getPath();
-                // check that path is a numerical resource id
-                if (path != null && path.matches("/[0-9]+")) {
-                    int resourceId = Integer.parseInt(path.substring(1));
-                    return Robolectric.application.getResources().openRawResource(resourceId);
-                }
+        if (uri != null && ContentResolver.SCHEME_ANDROID_RESOURCE.equals(uri.getScheme())) {
+            String path = uri.getPath();
+            // check that path is a numerical resource id
+            if (path != null && path.matches("/[0-9]+")) {
+                int resourceId = Integer.parseInt(path.substring(1));
+                return Robolectric.application.getResources().openRawResource(resourceId);
+            }
+        }
+
+        return new InputStream() {
+            @Override
+            public int read() throws IOException {
+                throw new UnsupportedOperationException();
             }
 
-            return new InputStream() {
-                @Override
-                public int read() throws IOException {
-                    throw new UnsupportedOperationException();
-                }
+            @Override
+            public String toString() {
+                return "stream for " + uri;
+            }
+        };
 
-                @Override
-                public String toString() {
-                    return "stream for " + uri;
-                }
-            };
-
-        } else {
-            return new ByteArrayInputStream(bytes);
-        }
     }
 }
--- src/test/java/com/xtremelabs/robolectric/shadows/ContentResolverTest.java
+++ src/test/java/com/xtremelabs/robolectric/shadows/ContentResolverTest.java
@@ -10,15 +10,12 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static com.xtremelabs.robolectric.Robolectric.shadowOf;
-import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 @RunWith(WithTestDefaultsRunner.class)
 public class ContentResolverTest {
 
     private ContentResolver contentResolver;
-
 
     @Before
     public void setUp() throws Exception {
@@ -28,16 +25,6 @@ public class ContentResolverTest {
     @Test(expected = UnsupportedOperationException.class)
     public void whenNullUri_readThrowsException() throws Exception {
         contentResolver.openInputStream(null).read();
-    }
-
-    @Test
-    public void whenBytesSet_readWorks() throws Exception {
-        String streamData = "Hello, I am a stream";
-        byte[] sourceBytes = streamData.getBytes();
-        shadowOf(contentResolver).setStreamData(sourceBytes);
-        byte[] bytes = new byte[sourceBytes.length];
-        contentResolver.openInputStream(null).read(bytes);
-        assertArrayEquals(sourceBytes, bytes);
     }
 
     @Test
