ad36a593f2f497e0fa77e04e9cd597f42f64bbdb
Dave <d_spanton@hotmail.com>
2011-07-03 23:50:33 +0100

    added add/remove/has gps listener to shadow location manager. added tests for shadow location manager.

--- src/main/java/com/xtremelabs/robolectric/shadows/ShadowLocationManager.java
+++ src/main/java/com/xtremelabs/robolectric/shadows/ShadowLocationManager.java
@@ -1,9 +1,12 @@
 package com.xtremelabs.robolectric.shadows;
 
 import android.location.LocationManager;
+import android.location.GpsStatus.Listener;
+
 import com.xtremelabs.robolectric.internal.Implementation;
 import com.xtremelabs.robolectric.internal.Implements;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,7 +19,9 @@ import java.util.Map;
 @Implements(LocationManager.class)
 public class ShadowLocationManager {
     private final Map<String, Boolean> providersEnabled = new HashMap<String, Boolean>();
-
+    
+    private final ArrayList<Listener> listeners = new ArrayList<Listener>();
+    
     @Implementation
     public boolean isProviderEnabled(String provider) {
         Boolean isEnabled = providersEnabled.get(provider);
@@ -32,4 +37,22 @@ public class ShadowLocationManager {
     public void setProviderEnabled(String provider, boolean isEnabled) {
         providersEnabled.put(provider, isEnabled);
     }
+    	
+	@Implementation
+	public boolean addGpsStatusListener(Listener listener) {
+		
+		if(!listeners.contains(listener))
+			listeners.add(listener);
+		
+		return true;
+	}
+	
+	@Implementation
+	public void removeGpsStatusListener(Listener listener) {
+		listeners.remove(listener);
+	}
+	
+	public boolean hasListener(Listener listener) {
+		return listeners.contains(listener);
+	}
 }
