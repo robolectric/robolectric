84c8e78264cc160819f32a6006049f87d81c43de
David Farber & Joe Moore <pair+dfarber+joe@pivotallabs.com>
2011-07-07 09:51:35 -0600

    - Added WindowManager to TestWindow (to prevent FacebookConnect testing problem) - Added ability to track which RadioButton in a RadioGroup is currently checked.   This required implementing ShadowLinearLayout, ShadowRadioGroup, and ShadowRadioButton.

---
+++ src/main/java/com/xtremelabs/robolectric/shadows/ShadowLinearLayout.java
@@ -0,0 +1,10 @@
+package com.xtremelabs.robolectric.shadows;
+
+import android.widget.LinearLayout;
+import com.xtremelabs.robolectric.internal.Implements;
+
+@SuppressWarnings({"UnusedDeclaration"})
+@Implements(LinearLayout.class)
+public class ShadowLinearLayout extends ShadowViewGroup {
+
+}
