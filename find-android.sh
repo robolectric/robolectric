bcc3a6240915f64464c55f6c9d62a9d0d085f0a9
David Farber & Ryan Richard <pair+dfarber+rrichard@pivotallabs.com>
2011-07-14 12:01:15 -0700

    make find_android.sh work in Bourne sh

--- find-android.sh
+++ find-android.sh
@@ -3,8 +3,8 @@
 # TODO: make this script work with android installed via homebrew. Until then, you must manually make sure
 # ANDROID_HOME has been set before running ant (e.g. ANDROID_HOME=/usr/local/Cellar/android-sdk/r10 ant)
 
-if [ "$ANDROID_HOME" == "" ]; then
-	ANDROID_HOME=`which android | sed 's/\/tools\/android$//'`
+if [ -z $ANDROID_HOME ]; then
+  ANDROID_HOME=`which android | sed 's/\/tools\/android$//'`
 fi
 
 echo "sdk.dir=$ANDROID_HOME" >> local.properties
