robolectric/robolectric

commit f61111cc68263f33e6fe1d9b29de305e5004e834

TestHttpResponse takes an array of headers rather than just content type

src/main/java/com/xtremelabs/robolectric/tester/org/apache/http/TestHttpResponse.java
@@ -16,10 +16,10 @@ public class TestHttpResponse extends HttpResponseStub {
 
     private int statusCode;
     private String responseBody;
-    private Header contentType;
     private TestStatusLine statusLine = new TestStatusLine();
     private TestHttpEntity httpEntity = new TestHttpEntity();
     private int openEntityContentStreamCount = 0;
+    private Header[] headers;
 
     public TestHttpResponse() {
         this.statusCode = 200;
@@ -31,9 +31,9 @@ public class TestHttpResponse extends HttpResponseStub {
         this.responseBody = responseBody;
     }
 
-    public TestHttpResponse(int statusCode, String responseBody, Header contentType) {
+    public TestHttpResponse(int statusCode, String responseBody, Header[] headers) {
         this(statusCode, responseBody);
-        this.contentType = contentType;
+        this.headers = headers;
     }
 
     protected void setResponseBody(String responseBody) {
@@ -49,7 +49,7 @@ public class TestHttpResponse extends HttpResponseStub {
     }
 
     @Override public Header[] getAllHeaders() {
-        return new Header[] { contentType };
+        return headers;
     }
 
     public boolean entityContentStreamsHaveBeenClosed() {
@@ -65,7 +65,13 @@ public class TestHttpResponse extends HttpResponseStub {
         }
         
         @Override public Header getContentType() {
-            return contentType;
+            for (int i = 0; i < headers.length; i++) {
+                Header header = headers[i];
+                if (header.getName().equals("Content-Type")) {
+                    return header;
+                }
+            }
+            return null;
         }
         
         @Override public boolean isStreaming() {

.pairs
@@ -17,6 +17,7 @@ pairs:
   am: Alexander Murmann
   jt: Jesse Toth
   lt: Lenny Turetsky
+  mg: Mike Grafton
 
 email:
   prefix: pair

src/main/java/com/xtremelabs/robolectric/tester/org/apache/http/FakeHttpLayer.java
@@ -38,7 +38,7 @@ public class FakeHttpLayer {
     }
 
     public void addPendingHttpResponseWithContentType(int statusCode, String responseBody, Header contentType) {
-        addPendingHttpResponse(new TestHttpResponse(statusCode, responseBody, contentType));
+        addPendingHttpResponse(new TestHttpResponse(statusCode, responseBody, new Header[] {contentType}));
     }
 
     public void addPendingHttpResponse(HttpResponse httpResponse) {

commit 39f81d70bfc70857886cfffbd91aa2ea79322ba6

TestHttpResponse needed to initialize the new headers field to allow instances created by the first two constructors to avoid null-pointing in TestHttpResponse.TestHttpEntity.getContentType(). fixes bug caused by commit f61111cc68263f33e6fe1d9b29de305e5004e834

src/main/java/com/xtremelabs/robolectric/tester/org/apache/http/TestHttpResponse.java
@@ -17,7 +17,7 @@ public class TestHttpResponse extends HttpResponseStub {
     private TestStatusLine statusLine = new TestStatusLine();
     private TestHttpEntity httpEntity = new TestHttpEntity();
     private int openEntityContentStreamCount = 0;
-    private Header[] headers;
+    private Header[] headers = new Header[0];
     private HttpParams params = new BasicHttpParams();
 
     public TestHttpResponse() {
@@ -72,8 +72,7 @@ public class TestHttpResponse extends HttpResponseStub {
         }
         
         @Override public Header getContentType() {
-            for (int i = 0; i < headers.length; i++) {
-                Header header = headers[i];
+            for (Header header : headers) {
                 if (header.getName().equals("Content-Type")) {
                     return header;
                 }