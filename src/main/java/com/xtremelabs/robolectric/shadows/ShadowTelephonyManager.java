a1e0deb2012e243b0c4e037ce845794533029ad9
Phil Goodwin & Rick Kawala <pair+phil+rick@pivotallabs.com>
2011-07-18 11:59:05 -0700

    Add getNetworkOperatorName(), getNetworkCountryIso() and getNetworkOperator() to TelephonyManager.

--- src/main/java/com/xtremelabs/robolectric/shadows/ShadowTelephonyManager.java
+++ src/main/java/com/xtremelabs/robolectric/shadows/ShadowTelephonyManager.java
@@ -12,8 +12,11 @@ public class ShadowTelephonyManager {
 	private PhoneStateListener listener;
 	private int eventFlags;
     private static String deviceId;
-	
-	@Implementation
+    private String networkOperatorName;
+    private String networkCountryIso;
+    private String networkOperator;
+
+    @Implementation
 	public void listen(PhoneStateListener listener, int events) {
 		this.listener = listener;
 		this.eventFlags = events;
@@ -45,5 +48,32 @@ public class ShadowTelephonyManager {
 
     public static void setDeviceId(String newDeviceId) {
         deviceId = newDeviceId;
+    }
+
+    public void setNetworkOperatorName(String networkOperatorName) {
+        this.networkOperatorName = networkOperatorName;
+    }
+
+    @Implementation
+    public String getNetworkOperatorName() {
+        return networkOperatorName;
+    }
+
+    public void setNetworkCountryIso(String networkCountryIso) {
+        this.networkCountryIso = networkCountryIso;
+    }
+
+    @Implementation
+    public String getNetworkCountryIso() {
+        return networkCountryIso;
+    }
+
+    public void setNetworkOperator(String networkOperator) {
+        this.networkOperator = networkOperator;
+    }
+
+    @Implementation
+    public String getNetworkOperator() {
+        return networkOperator;
     }
 }
