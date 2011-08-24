1d9fdcbc65094ac463194f848aed4bb73bf81d78
Mike Mazur & Tyler Schultz <pair+mmazur+tyler@pivotallabs.com>
2011-08-23 17:53:49 -0700

    add support for LocatiomManager#getBestProvider

--- src/main/java/com/xtremelabs/robolectric/shadows/ShadowLocationManager.java
+++ src/main/java/com/xtremelabs/robolectric/shadows/ShadowLocationManager.java
@@ -1,8 +1,8 @@
 package com.xtremelabs.robolectric.shadows;
 
-import android.location.LocationManager;
+import android.location.Criteria;
 import android.location.GpsStatus.Listener;
-
+import android.location.LocationManager;
 import com.xtremelabs.robolectric.internal.Implementation;
 import com.xtremelabs.robolectric.internal.Implements;
 
@@ -21,7 +21,10 @@ public class ShadowLocationManager {
     private final Map<String, Boolean> providersEnabled = new HashMap<String, Boolean>();
     
     private final ArrayList<Listener> listeners = new ArrayList<Listener>();
-    
+    private Criteria lastBestProviderCriteria;
+    private boolean lastBestProviderEnabledOnly;
+    private String bestProvider;
+
     @Implementation
     public boolean isProviderEnabled(String provider) {
         Boolean isEnabled = providersEnabled.get(provider);
@@ -51,8 +54,27 @@ public class ShadowLocationManager {
 	public void removeGpsStatusListener(Listener listener) {
 		listeners.remove(listener);
 	}
-	
-	public boolean hasListener(Listener listener) {
+
+    @Implementation
+    public java.lang.String getBestProvider(android.location.Criteria criteria, boolean enabledOnly) {
+        lastBestProviderCriteria = criteria;
+        lastBestProviderEnabledOnly = enabledOnly;
+        return bestProvider;
+    }
+
+    public boolean hasListener(Listener listener) {
 		return listeners.contains(listener);
 	}
+
+    public Criteria getLastBestProviderCriteria() {
+        return lastBestProviderCriteria;
+    }
+
+    public boolean getLastBestProviderEnabledOnly() {
+        return lastBestProviderEnabledOnly;
+    }
+
+    public void setBestProvider(String provider) {
+        bestProvider = provider;
+    }
 }
--- src/test/java/com/xtremelabs/robolectric/shadows/LocationManagerTest.java
+++ src/test/java/com/xtremelabs/robolectric/shadows/LocationManagerTest.java
@@ -1,76 +1,99 @@
 package com.xtremelabs.robolectric.shadows;
 
+import android.content.Context;
+import android.location.Criteria;
+import android.location.GpsStatus.Listener;
+import android.location.LocationManager;
+import com.xtremelabs.robolectric.Robolectric;
+import com.xtremelabs.robolectric.WithTestDefaultsRunner;
 import junit.framework.Assert;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import android.content.Context;
-import android.location.LocationManager;
-import android.location.GpsStatus.Listener;
-
-import com.xtremelabs.robolectric.Robolectric;
-import com.xtremelabs.robolectric.WithTestDefaultsRunner;
-
 import static com.xtremelabs.robolectric.Robolectric.shadowOf;
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNull;
+import static junit.framework.Assert.assertSame;
+import static junit.framework.Assert.assertTrue;
 
 @RunWith(WithTestDefaultsRunner.class)
 public class LocationManagerTest {
-	private LocationManager locationManager;
-	
-	@Before
-	public void setUp() {
-		locationManager = (LocationManager) Robolectric.application.getSystemService(Context.LOCATION_SERVICE);
-	}
-	
-	@Test
-	public void shouldReturnProviderEnabledAsDefault() {
-		Boolean enabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
-		Assert.assertTrue(enabled);
-	}
-	
-	@Test
-	public void shouldDisableProvider() {
-		ShadowLocationManager shadowLocationManager = shadowOf(locationManager);
-		shadowLocationManager.setProviderEnabled(LocationManager.GPS_PROVIDER, false);
-		
-		Boolean enabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
-		Assert.assertFalse(enabled);
-	}
-	
-	@Test
-	public void shouldHaveListenerOnceAdded() {
-		Listener listener = addListenerToLocationManager();
-		
-		ShadowLocationManager shadowLocationManager = shadowOf(locationManager);
-		
-		Assert.assertTrue(shadowLocationManager.hasListener(listener));
-	}
-	
-	@Test
-	public void shouldNotHaveListenerOnceRemoved() {
-		Listener listener = addListenerToLocationManager();
-		
-		ShadowLocationManager shadowLocationManager = shadowOf(locationManager);
-		
-		locationManager.removeGpsStatusListener(listener);
-		
-		Assert.assertFalse(shadowLocationManager.hasListener(listener));
-	}
-	
-	private Listener addListenerToLocationManager() {
-		Listener listener = new TestListener();
-		locationManager.addGpsStatusListener(listener);
-		return listener;
-	}
-	
-	private class TestListener implements Listener {
+    private LocationManager locationManager;
 
-		@Override
-		public void onGpsStatusChanged(int event) {
-			
-		}
-		
-	}
+    @Before
+    public void setUp() {
+        locationManager = (LocationManager) Robolectric.application.getSystemService(Context.LOCATION_SERVICE);
+    }
+
+    @Test
+    public void shouldReturnProviderEnabledAsDefault() {
+        Boolean enabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        Assert.assertTrue(enabled);
+    }
+
+    @Test
+    public void shouldDisableProvider() {
+        ShadowLocationManager shadowLocationManager = shadowOf(locationManager);
+        shadowLocationManager.setProviderEnabled(LocationManager.GPS_PROVIDER, false);
+
+        Boolean enabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        Assert.assertFalse(enabled);
+    }
+
+    @Test
+    public void shouldHaveListenerOnceAdded() {
+        Listener listener = addListenerToLocationManager();
+
+        ShadowLocationManager shadowLocationManager = shadowOf(locationManager);
+
+        Assert.assertTrue(shadowLocationManager.hasListener(listener));
+    }
+
+    @Test
+    public void shouldNotHaveListenerOnceRemoved() {
+        Listener listener = addListenerToLocationManager();
+
+        ShadowLocationManager shadowLocationManager = shadowOf(locationManager);
+
+        locationManager.removeGpsStatusListener(listener);
+
+        Assert.assertFalse(shadowLocationManager.hasListener(listener));
+    }
+
+    @Test
+    public void shouldStoreBestProviderCriteriaAndEnabledOnlyFlag() throws Exception {
+        ShadowLocationManager shadowLocationManager = shadowOf(locationManager);
+        Criteria criteria = new Criteria();
+        assertNull(locationManager.getBestProvider(criteria, true));
+        assertSame(criteria, shadowLocationManager.getLastBestProviderCriteria());
+        assertTrue(shadowLocationManager.getLastBestProviderEnabledOnly());
+    }
+
+    @Test
+    public void shouldReturnBestProvider() throws Exception {
+        ShadowLocationManager shadowLocationManager = shadowOf(locationManager);
+        shadowLocationManager.setBestProvider("GNSS");
+        assertEquals("GNSS", locationManager.getBestProvider(new Criteria(), false));
+    }
+
+    @Test
+    public void shouldReturnNullWhenBestProviderIsNotSet() throws Exception {
+        assertNull(locationManager.getBestProvider(new Criteria(), true));
+    }
+
+    private Listener addListenerToLocationManager() {
+        Listener listener = new TestListener();
+        locationManager.addGpsStatusListener(listener);
+        return listener;
+    }
+
+    private class TestListener implements Listener {
+
+        @Override
+        public void onGpsStatusChanged(int event) {
+
+        }
+
+    }
 }
