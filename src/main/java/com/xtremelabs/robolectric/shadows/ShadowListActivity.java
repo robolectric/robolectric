383b51f42ad1f4e0bc855637c26d96ad36ec0ad6
Ronald Dehuysser <ronald.dehuysser@gmail.com>
2011-08-30 20:58:44 +0200

    support for Preferences

--- src/main/java/com/xtremelabs/robolectric/shadows/ShadowListActivity.java
+++ src/main/java/com/xtremelabs/robolectric/shadows/ShadowListActivity.java
@@ -2,7 +2,9 @@ package com.xtremelabs.robolectric.shadows;
 
 import android.app.ListActivity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ListView;
+
 import com.xtremelabs.robolectric.internal.Implementation;
 import com.xtremelabs.robolectric.internal.Implements;
 
@@ -12,36 +14,38 @@ import static com.xtremelabs.robolectric.Robolectric.shadowOf;
  * Shadow of {@code ListActivity} that supports the retrieval of {@code ListViews}
  */
 
-@SuppressWarnings({"UnusedDeclaration"})
+@SuppressWarnings({ "UnusedDeclaration" })
 @Implements(ListActivity.class)
 public class ShadowListActivity extends ShadowActivity {
-    private ListView listView;
+	private ListView listView;
 
-    @Implementation
-    public ListView getListView() {
-        if (listView == null) {
-            if ((listView = findListView(contentView)) == null) {
-                throw new RuntimeException("No ListView found under content view");
-            }
-        }
-        return listView;
-    }
+	@Implementation
+	public ListView getListView() {
+		if (listView == null) {
+			if ((listView = findListView(contentView)) == null) {
+				throw new RuntimeException("No ListView found under content view");
+			}
+		}
+		return listView;
+	}
 
-    public void setListView(ListView view) {
-    	listView = view;
-    }
-    
-    private ListView findListView(View parent) {
-        if (parent instanceof ListView) {
-            return (ListView) parent;
-        }
-        ShadowViewGroup shadowViewGroup = (ShadowViewGroup) shadowOf(parent);
-        for (int i = 0; i < shadowViewGroup.getChildCount(); i++) {
-            ListView listView = findListView(shadowViewGroup.getChildAt(i));
-            if (listView != null) {
-                return listView;
-            }
-        }
-        return null;
-    }
+	public void setListView(ListView view) {
+		listView = view;
+	}
+
+	private ListView findListView(View parent) {
+		if (parent instanceof ListView) {
+			return (ListView) parent;
+		}
+		if (parent instanceof ViewGroup) {
+			ShadowViewGroup shadowViewGroup = (ShadowViewGroup) shadowOf(parent);
+			for (int i = 0; i < shadowViewGroup.getChildCount(); i++) {
+				ListView listView = findListView(shadowViewGroup.getChildAt(i));
+				if (listView != null) {
+					return listView;
+				}
+			}
+		}
+		return null;
+	}
 }
--- src/main/java/com/xtremelabs/robolectric/shadows/ShadowPreferenceActivity.java
+++ src/main/java/com/xtremelabs/robolectric/shadows/ShadowPreferenceActivity.java
@@ -1,15 +1,16 @@
 package com.xtremelabs.robolectric.shadows;
 
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceGroup;
 import android.preference.PreferenceScreen;
-import android.widget.ListView;
 
 import com.xtremelabs.robolectric.internal.Implementation;
 import com.xtremelabs.robolectric.internal.Implements;
 
 @Implements(PreferenceActivity.class)
 public class ShadowPreferenceActivity extends ShadowListActivity {
-	
+
 	private int preferencesResId = -1;
 	private PreferenceScreen preferenceScreen;
 
@@ -18,13 +19,21 @@ public class ShadowPreferenceActivity extends ShadowListActivity {
 		this.preferencesResId = preferencesResId;
 		preferenceScreen = getResourceLoader().inflatePreferences(getApplicationContext(), preferencesResId);
 	}
-	
+
 	public int getPreferencesResId() {
 		return preferencesResId;
-	}	
-	
+	}
+
 	@Implementation
 	public PreferenceScreen getPreferenceScreen() {
 		return preferenceScreen;
+	}
+
+	@Implementation
+	public Preference findPreference(CharSequence key) {
+		if (preferenceScreen != null && preferenceScreen instanceof PreferenceGroup) {
+			return ((PreferenceGroup) preferenceScreen).findPreference(key);
+		}
+		return null;
 	}
 }
