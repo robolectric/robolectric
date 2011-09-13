f9922da1d4c6c0528e5b44699c0f90ec25e6a721
Nick Parker <nparker@zetetic.net>
2011-09-13 15:22:55 -0500

    Adding support for getting/setting the ListAdapter for the ListActivity.

---
+++ src/test/java/com/xtremelabs/robolectric/shadows/ListActivityTest.java
@@ -0,0 +1,25 @@
+package com.xtremelabs.robolectric.shadows;
+
+import android.app.ListActivity;
+import android.widget.ListAdapter;
+import com.xtremelabs.robolectric.WithTestDefaultsRunner;
+import junit.framework.TestCase;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@RunWith(WithTestDefaultsRunner.class)
+public class ListActivityTest extends TestCase {
+
+    @Test
+    public void shouldSupportSettingAndGettingListAdapter(){
+        ListActivity listActivity = new ListActivity();
+        ListAdapter adapter = new CountingAdapter(5);
+        listActivity.setListAdapter(adapter);
+
+        assertThat(listActivity.getListAdapter(), is(notNullValue()));
+    }
+}
