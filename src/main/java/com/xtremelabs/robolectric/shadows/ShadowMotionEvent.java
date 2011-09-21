ca99781d0778d4b64a6fa3858924fe31f593ba2c
Glenn Jahnke & Lenny Turetsky <pair+glennj+lenny@pivotallabs.com>
2011-09-21 11:37:38 -0700

    Provide some support of pointer IDs and pointer indices in MotionEvents.

--- src/main/java/com/xtremelabs/robolectric/shadows/ShadowMotionEvent.java
+++ src/main/java/com/xtremelabs/robolectric/shadows/ShadowMotionEvent.java
@@ -23,6 +23,8 @@ public class ShadowMotionEvent {
     private int pointerCount = 1;
     private long downTime;
     private long eventTime;
+    private int[] pointerIds = new int[2];
+    private int pointerIndex;
 
     @Implementation
     public static MotionEvent obtain(long downTime, long eventTime, int action, float x, float y, int metaState) {
@@ -49,7 +51,7 @@ public class ShadowMotionEvent {
 
     @Implementation
     public int getAction() {
-        return action;
+        return action | (pointerIndex << MotionEvent.ACTION_POINTER_ID_SHIFT);
     }
 
     @Implementation
@@ -87,10 +89,33 @@ public class ShadowMotionEvent {
         return downTime;
     }
 
+    @Implementation
+    public final int getPointerId(int index) {
+        return pointerIds[index];
+    }
+
+    @Implementation
+    public final int getActionMasked() {
+        return action;
+    }
+
+    @Implementation
+    public final int getActionIndex() {
+        return pointerIndex;
+    }
+
     public MotionEvent setPointer2(float x, float y) {
         this.x[1] = x;
         this.y[1] = y;
         pointerCount = 2;
         return realObject;
+    }
+
+    public void setPointerIdByIndex(int index, int id) {
+        pointerIds[index] = id;
+    }
+
+    public void setPointerIndex(int pointerIndex) {
+        this.pointerIndex = pointerIndex;
     }
 }
