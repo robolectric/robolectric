b766dfbe13c83896fe9f3f3121c1b7a50f5019a2
Patrick Forhan <pforhan@squareup.com>
2011-09-28 18:54:18 -0700

    fix dialog.setContentView(View) for tests

--- src/test/java/com/xtremelabs/robolectric/shadows/DialogTest.java
+++ src/test/java/com/xtremelabs/robolectric/shadows/DialogTest.java
@@ -1,13 +1,16 @@
 package com.xtremelabs.robolectric.shadows;
 
+import android.app.Activity;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.view.View;
 import com.xtremelabs.robolectric.WithTestDefaultsRunner;
 import com.xtremelabs.robolectric.util.Transcript;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.hamcrest.CoreMatchers.sameInstance;
+import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertThat;
 
 @RunWith(WithTestDefaultsRunner.class)
@@ -28,5 +31,17 @@ public class DialogTest {
         dialog.dismiss();
 
         transcript.assertEventsSoFar("onDismiss called!");
+    }
+
+    @Test
+    public void setContentViewWithViewAllowsFindById() throws Exception {
+        final int viewId = 1234;
+        Activity context = new Activity();
+        final Dialog dialog = new Dialog(context);
+        final View view = new View(context);
+        view.setId(viewId);
+        dialog.setContentView(view);
+
+        assertSame(view, dialog.findViewById(viewId));
     }
 }
