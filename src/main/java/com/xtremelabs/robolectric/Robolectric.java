2e2f68b7efbd1341cceaf9bb35507df88b9111a3
Diego Garcia <diego@xaethos.net>
2011-11-03 14:56:13 +0800

    ShadowContentProvider: has application context

--- src/main/java/com/xtremelabs/robolectric/Robolectric.java
+++ src/main/java/com/xtremelabs/robolectric/Robolectric.java
@@ -225,6 +225,7 @@ public class Robolectric {
                 ShadowCompoundButton.class,
                 ShadowConfiguration.class,
                 ShadowConnectivityManager.class,
+                ShadowContentProvider.class,
                 ShadowContentResolver.class,
                 ShadowContentUris.class,
                 ShadowContentValues.class,
