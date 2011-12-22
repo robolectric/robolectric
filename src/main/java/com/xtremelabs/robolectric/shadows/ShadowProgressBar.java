575bc4a6673262cbff1bd63f6fc3e9957bc83fa8
Robert Taylor <robert.taylor426@gmail.com>
2011-12-22 17:03:28 +0000

    Added OnRatingBarChangeListener support to ShadowRatingBar

--- src/main/java/com/xtremelabs/robolectric/shadows/ShadowProgressBar.java
+++ src/main/java/com/xtremelabs/robolectric/shadows/ShadowProgressBar.java
@@ -7,10 +7,19 @@ import com.xtremelabs.robolectric.internal.Implements;
 @Implements(ProgressBar.class)
 public class ShadowProgressBar extends ShadowView {
 
-  private int progress = 0;
-  private int secondaryProgress = 0;
+  private int progress;
+  private int secondaryProgress;
   private int max = 100;
 
+  @Override public void applyAttributes() {
+      super.applyAttributes();
+      
+      final int max = attributeSet.getAttributeIntValue("android", "max", this.max);
+      
+      if (max >= 0)
+          setMax(max);
+  }
+  
   @Implementation
   public void setMax(int max) {
     progress = 0;
