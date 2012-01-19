ed4e0e591a1e1c92521512f6c36007eb23bcb03a
Ray Ryan <ray@squareup.com>
2012-01-19 15:39:46 -0800

    Improves attribute resource parsing, and makes TestAttributeSet honor namespaces.

--- src/main/java/com/xtremelabs/robolectric/res/AttrResourceLoader.java
+++ src/main/java/com/xtremelabs/robolectric/res/AttrResourceLoader.java
@@ -7,6 +7,7 @@ import org.w3c.dom.NodeList;
 
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 import java.io.File;
 import java.util.HashMap;
@@ -15,40 +16,131 @@ import java.util.Map;
 import java.util.Set;
 
 public class AttrResourceLoader extends XmlLoader {
-    Map<String, String> classAttrEnumToValue = new HashMap<String, String>();
-    Set<String> knownClassAttrs = new HashSet<String>();
+    private static final String ANDROID_XML_NAMESPACE = "http://schemas.android.com/apk/res/android";
+    private static final String ANDROID_PREFIX = "android:";
 
+    Map<String, EnumDef> enums = new HashMap<String, EnumDef>();
+    Map<String, EnumRef> enumRefs = new HashMap<String, EnumRef>();
+    boolean resolved = false;
+
+    Map<String, String> classEnumToValue = new HashMap<String, String>();
+    Set<String> knownClassEnums = new HashSet<String>();
+  
     public AttrResourceLoader(ResourceExtractor resourceExtractor) {
         super(resourceExtractor);
     }
 
-    @Override protected void processResourceXml(File xmlFile, Document document, boolean ignored) throws Exception {
-        XPathExpression stringsXPath = XPathFactory.newInstance().newXPath().compile("/resources/declare-styleable/attr/enum");
-        NodeList stringNodes = (NodeList) stringsXPath.evaluate(document, XPathConstants.NODESET);
-        for (int i = 0; i < stringNodes.getLength(); i++) {
-            Node node = stringNodes.item(i);
-            String viewName = node.getParentNode().getParentNode().getAttributes().getNamedItem("name").getNodeValue();
-            String enumName = node.getParentNode().getAttributes().getNamedItem("name").getNodeValue();
-            String name = node.getAttributes().getNamedItem("name").getNodeValue();
-            String value = node.getAttributes().getNamedItem("value").getNodeValue();
+    static class EnumDef {
+        final String name;
+        final Map<String, String> values = new HashMap<String, String>();
 
-            classAttrEnumToValue.put(key(viewName, enumName, name), value);
-            knownClassAttrs.add(key(viewName, enumName));
+        EnumDef(String name) { this.name = name; }
+    }
+
+    static class EnumRef {
+        final String viewName;
+        final String enumName;
+
+        EnumRef(String viewName, String enumName) {
+            this.enumName = enumName;
+            this.viewName = viewName;
+        }
+    }
+
+    @Override protected void processResourceXml(File xmlFile, Document document, boolean system) throws Exception {
+
+        // Pick up inline enum definitions
+        {
+            NodeList nodeList = findNodes(document, "/resources/declare-styleable/attr/enum|/resources/declare-styleable/attr/flag");
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Node node = nodeList.item(i);
+                String viewName = node.getParentNode().getParentNode().getAttributes().getNamedItem("name").getNodeValue();
+                String enumName = enumName(node.getParentNode().getAttributes().getNamedItem("name").getNodeValue(), system);
+                String name = node.getAttributes().getNamedItem("name").getNodeValue();
+                String value = node.getAttributes().getNamedItem("value").getNodeValue();
+
+                classEnumToValue.put(key(viewName, enumName, name), value);
+                knownClassEnums.add(key(viewName, enumName));
+            }
+        }
+
+        // Look for any global enum definitions.
+        {
+            NodeList nodeList = findNodes(document, "/resources/attr/enum|/resources/attr/flag");
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Node node = nodeList.item(i);
+
+                String enumName = enumName(node.getParentNode().getAttributes().getNamedItem("name").getNodeValue(), system);
+                EnumDef enumDef = enums.get(enumName);
+                if (enumDef == null) {
+                    enumDef = new EnumDef(enumName);
+                    enums.put(enumName, enumDef);
+                }
+                enumDef.values.put(node.getAttributes().getNamedItem("name").getNodeValue(),
+                        node.getAttributes().getNamedItem("value").getNodeValue());
+            }
+        }
+
+        // Note uses of system enums and top level local enums by childless attr nodes
+        {
+            NodeList nodeList = findNodes(document, "/resources/declare-styleable/attr[not(node())]");
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Node node = nodeList.item(i);
+
+                String viewName = node.getParentNode().getAttributes().getNamedItem("name").getNodeValue();
+                String enumName = enumName(node.getAttributes().getNamedItem("name").getNodeValue(), system);
+
+                enumRefs.put(key(viewName, enumName), new EnumRef(viewName, enumName));
+            }
         }
     }
 
     public String convertValueToEnum(Class<? extends View> viewClass, String namespace, String attrName, String attrValue) {
-        String className = findKnownAttrClass(attrName, viewClass).getName();
-        return classAttrEnumToValue.get(key(className, attrName, attrValue));
+        resolveReferences();
+        if (ANDROID_XML_NAMESPACE.equals(namespace)) attrName = ANDROID_PREFIX + attrName;
+        String className = findKnownAttrClass(attrName, viewClass).getSimpleName();
+        return classEnumToValue.get(key(className, attrName, attrValue));
     }
 
     public boolean hasAttributeFor(Class<? extends View> viewClass, String namespace, String attrName) {
+        resolveReferences();
+        if (ANDROID_XML_NAMESPACE.equals(namespace)) attrName = ANDROID_PREFIX + attrName;
         return findKnownAttrClass(attrName, viewClass) != null;
+    }
+
+    private String enumName(String name, boolean system) {
+        String enumName = name;
+        enumName = system ? ANDROID_PREFIX + enumName : enumName;
+        return enumName;
+    }
+
+    private NodeList findNodes(Document document, String path) throws XPathExpressionException {
+        XPathExpression nestedEnumsXPath = XPathFactory.newInstance().newXPath().compile(path);
+        return (NodeList) nestedEnumsXPath.evaluate(document, XPathConstants.NODESET);
+    }
+
+    private void resolveReferences() {
+        if (!resolved) {
+            for (EnumRef enumRef : enumRefs.values()) {
+                noteEnumUses(enumRef.viewName, enumRef.enumName);
+            }
+            resolved = true;
+        }
+    }
+
+    private void noteEnumUses(String viewName, String enumName) {
+        EnumDef enumDef = enums.get(enumName);
+        if (enumDef == null) return;
+
+        for (Map.Entry<String, String> entry : enumDef.values.entrySet()) {
+            classEnumToValue.put(key(viewName, enumName, entry.getKey()), entry.getValue());
+        }
+        knownClassEnums.add(key(viewName, enumName));
     }
 
     private Class<?> findKnownAttrClass(String attrName, Class<?> clazz) {
         while (clazz != null) {
-            if (knownClassAttrs.contains(key(clazz.getName(), attrName))) {
+            if (knownClassEnums.contains(key(clazz.getSimpleName(), attrName))) {
                 return clazz;
             }
             clazz = clazz.getSuperclass();
@@ -56,11 +148,11 @@ public class AttrResourceLoader extends XmlLoader {
         return null;
     }
 
-    private String key(String viewName, String attrName, String name) {
-        return viewName + "#" + attrName + "#" + name;
+    private String key(String viewName, String enumName, String name) {
+        return viewName + "#" + enumName + "#" + name;
     }
 
-    private String key(String viewName, String attrName) {
-        return viewName + "#" + attrName;
+    private String key(String viewName, String enunName) {
+        return viewName + "#" + enunName;
     }
 }
--- src/test/java/com/xtremelabs/robolectric/res/AttrResourceLoaderTest.java
+++ src/test/java/com/xtremelabs/robolectric/res/AttrResourceLoaderTest.java
@@ -16,25 +16,71 @@ public class AttrResourceLoaderTest {
 
     @Before
     public void setUp() throws Exception {
-        ResourceExtractor resourceExtractor = new ResourceExtractor();
-        resourceExtractor.addLocalRClass(R.class);
-        attrResourceLoader = new AttrResourceLoader(resourceExtractor);
+        attrResourceLoader = makeResourceLoader();
         new DocumentLoader(attrResourceLoader).loadResourceXmlDir(resourceFile("res", "values"));
     }
 
+    private AttrResourceLoader makeResourceLoader() throws Exception {
+        ResourceExtractor resourceExtractor = new ResourceExtractor();
+        resourceExtractor.addLocalRClass(R.class);
+        return new AttrResourceLoader(resourceExtractor);
+    }
+
     @Test
-    public void testAttributesAreResolved() throws Exception {
+    public void testUnknownAttributesAreUnknown() throws Exception {
+        assertThat(attrResourceLoader.hasAttributeFor(CustomView.class, "xxx", "otherItemType"), equalTo(false));
+    }
+
+    @Test
+    public void testInlineEnumAttributesAreResolved() throws Exception {
         assertThat(attrResourceLoader.convertValueToEnum(CustomView.class, "xxx", "itemType", "integer"), equalTo("0"));
         assertThat(attrResourceLoader.hasAttributeFor(CustomView.class, "xxx", "itemType"), equalTo(true));
 
         assertThat(attrResourceLoader.convertValueToEnum(CustomView.class, "xxx", "itemType", "string"), equalTo("1"));
-        assertThat(attrResourceLoader.hasAttributeFor(CustomView.class, "xxx", "otherItemType"), equalTo(false));
+    }
+
+    @Test
+    public void testGlobalEnumAttributesAreResolved() throws Exception {
+        assertThat(attrResourceLoader.convertValueToEnum(CustomView.class, "xxx", "keycode", "KEYCODE_SOFT_RIGHT"), equalTo("2"));
+        assertThat(attrResourceLoader.hasAttributeFor(CustomView.class, "xxx", "keycode"), equalTo(true));
+
+        assertThat(attrResourceLoader.convertValueToEnum(CustomView.class, "xxx", "keycode", "KEYCODE_HOME"), equalTo("3"));
+    }
+
+    @Test
+    public void testInlineFlagAttributesAreResolved() throws Exception {
+        assertThat(attrResourceLoader.convertValueToEnum(CustomView.class, "xxx", "scrollbars", "horizontal"), equalTo("0x00000100"));
+        assertThat(attrResourceLoader.hasAttributeFor(CustomView.class, "xxx", "scrollbars"), equalTo(true));
+
+        assertThat(attrResourceLoader.convertValueToEnum(CustomView.class, "xxx", "scrollbars", "vertical"), equalTo("0x00000200"));
+    }
+
+    @Test
+    public void testGlobalFlagAttributesAreResolved() throws Exception {
+        assertThat(attrResourceLoader.convertValueToEnum(CustomView.class, "xxx", "gravity", "center"), equalTo("0x11"));
+        assertThat(attrResourceLoader.hasAttributeFor(CustomView.class, "xxx", "gravity"), equalTo(true));
+
+        assertThat(attrResourceLoader.convertValueToEnum(CustomView.class, "xxx", "gravity", "fill_vertical"), equalTo("0x70"));
     }
 
     @Test
     public void shouldResolveAttributesForSubClasses() throws Exception {
         assertThat(attrResourceLoader.convertValueToEnum(SubCustomView.class, "xxx", "itemType", "integer"), equalTo("0"));
         assertThat(attrResourceLoader.hasAttributeFor(SubCustomView.class, "xxx", "itemType"), equalTo(true));
+    }
+    
+    @Test
+    public void systemResourcesArePrefixedAndroid() throws Exception {
+        attrResourceLoader = makeResourceLoader();
+        new DocumentLoader(attrResourceLoader).loadSystemResourceXmlDir(resourceFile("res", "values"));
+
+        assertThat(attrResourceLoader.convertValueToEnum(CustomView.class, "http://schemas.android.com/apk/res/android",
+                "gravity", "center"), equalTo("0x11"));
+        assertThat(attrResourceLoader.hasAttributeFor(CustomView.class, "http://schemas.android.com/apk/res/android",
+                "gravity"), equalTo(true));
+
+        assertThat(attrResourceLoader.convertValueToEnum(CustomView.class, "http://schemas.android.com/apk/res/android",
+                "gravity", "fill_vertical"), equalTo("0x70"));
     }
 
     private class SubCustomView extends CustomView {
