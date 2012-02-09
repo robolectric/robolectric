053e79655d54be37606f46344fef68acf2eaf56b
Jessica Chen <jessicachen@Jessicas-MacBook-Air.local>
2012-02-09 09:59:08 -0800

    Support getExternalFilesDir(String type)

--- src/main/java/com/xtremelabs/robolectric/shadows/ShadowContext.java
+++ src/main/java/com/xtremelabs/robolectric/shadows/ShadowContext.java
@@ -26,6 +26,7 @@ abstract public class ShadowContext {
     public static final File CACHE_DIR = new File(System.getProperty("java.io.tmpdir"), "android-cache");
     public static final File EXTERNAL_CACHE_DIR = new File(System.getProperty("java.io.tmpdir"), "android-external-cache");
     public static final File FILES_DIR = new File(System.getProperty("java.io.tmpdir"), "android-tmp");
+    public static final File EXTERNAL_FILES_DIR = new File(System.getProperty("java.io.tmpdir"), "android-external-files");
 
     @RealObject private Context realContext;
 
@@ -90,6 +91,14 @@ abstract public class ShadowContext {
         return EXTERNAL_CACHE_DIR;
     }
 
+    @Implementation
+    public File getExternalFilesDir(String type) {
+    	File path = new File(EXTERNAL_FILES_DIR, type);
+    	if (!path.exists()) {
+    		path.mkdirs();
+    	}
+    	return path;
+    }
     @Implementation
     public FileInputStream openFileInput(String path) throws FileNotFoundException {
         return new FileInputStream(getFileStreamPath(path));
