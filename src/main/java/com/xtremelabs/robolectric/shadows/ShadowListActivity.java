6b92d0bcbd859dbd8f4d14b65f633d3b694a449c
Jan Berkel <jan.berkel@gmail.com>
2012-03-22 03:08:47 +0100

    ListActivity improvments

--- src/main/java/com/xtremelabs/robolectric/shadows/ShadowListActivity.java
+++ src/main/java/com/xtremelabs/robolectric/shadows/ShadowListActivity.java
@@ -2,12 +2,15 @@ package com.xtremelabs.robolectric.shadows;
 
 import android.app.ListActivity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import com.xtremelabs.robolectric.internal.Implementation;
 import com.xtremelabs.robolectric.internal.Implements;
 
-import static com.xtremelabs.robolectric.Robolectric.shadowOf;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 /**
  * Shadow of {@code ListActivity} that supports the retrieval of {@code ListViews}
@@ -36,6 +39,24 @@ public class ShadowListActivity extends ShadowActivity {
     @Implementation
     public void setListAdapter(ListAdapter listAdapter) {
         this.listAdapter = listAdapter;
+        ListView lv = findListView(contentView);
+        if (lv != null) {
+            lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    try {
+                        Method handler = realActivity.getClass().getDeclaredMethod("onListItemClick",
+                                ListView.class, View.class, int.class, long.class);
+                        handler.setAccessible(true);
+                        handler.invoke(realActivity, parent, view, position, id);
+                    } catch (NoSuchMethodException ignored) {
+                    } catch (InvocationTargetException ignored) {
+                    } catch (IllegalAccessException ignored) {
+                    }
+                }
+            });
+            lv.setAdapter(listAdapter);
+        }
     }
 
     @Implementation
@@ -46,12 +67,13 @@ public class ShadowListActivity extends ShadowActivity {
     private ListView findListView(View parent) {
         if (parent instanceof ListView) {
             return (ListView) parent;
-        }
-        ShadowViewGroup shadowViewGroup = (ShadowViewGroup) shadowOf(parent);
-        for (int i = 0; i < shadowViewGroup.getChildCount(); i++) {
-            ListView listView = findListView(shadowViewGroup.getChildAt(i));
-            if (listView != null) {
-                return listView;
+        } else if (parent instanceof ViewGroup) {
+            ViewGroup viewGroup = (ViewGroup) parent;
+            for (int i = 0; i < viewGroup.getChildCount(); i++) {
+                ListView listView = findListView(viewGroup.getChildAt(i));
+                if (listView != null) {
+                    return listView;
+                }
             }
         }
         return null;
