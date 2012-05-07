c3d22ae1f4114a835f1522ceebaaf358b5994454
Jon Boekenoogen <jboekeno@google.com>
2012-05-07 09:57:26 -0700

    Change Base64 decoder to Apache's since it handles cases properly.

--- src/main/java/com/xtremelabs/robolectric/shadows/ShadowBase64.java
+++ src/main/java/com/xtremelabs/robolectric/shadows/ShadowBase64.java
@@ -1,21 +1,22 @@
 package com.xtremelabs.robolectric.shadows;
 
-import android.util.Base64;
 import com.xtremelabs.robolectric.internal.Implementation;
 import com.xtremelabs.robolectric.internal.Implements;
 
-import javax.xml.bind.DatatypeConverter;
+import org.apache.commons.codec.binary.Base64;
 
-@Implements(Base64.class)
+@Implements(android.util.Base64.class)
 public class ShadowBase64 {
 
     @Implementation
     public static String encodeToString(byte[] bytes, int flags) {
-        return DatatypeConverter.printBase64Binary(bytes);
+        Base64 base64 = new Base64();
+        return base64.encodeBase64String(bytes);
     }
 
     @Implementation
     public static byte[] decode(String str, int flags) {
-        return DatatypeConverter.parseBase64Binary(str);
+        Base64 base64 = new Base64();
+        return base64.decodeBase64(str);
     }
 }
