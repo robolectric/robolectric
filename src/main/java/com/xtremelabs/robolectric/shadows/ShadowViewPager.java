8e2c6553251a68921c4cc345ca0aaa08a405bf61
Phil Goodwin & Ryan Richard <pair+phil+rrichard@pivotallabs.com>
2012-05-23 18:07:46 -0700

    simplify ShadowViewPager to make it less integrationy

--- src/main/java/com/xtremelabs/robolectric/shadows/ShadowViewPager.java
+++ src/main/java/com/xtremelabs/robolectric/shadows/ShadowViewPager.java
@@ -1,14 +1,10 @@
 package com.xtremelabs.robolectric.shadows;
 
-import android.database.DataSetObserver;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.view.View;
 import com.xtremelabs.robolectric.internal.Implementation;
 import com.xtremelabs.robolectric.internal.Implements;
 import com.xtremelabs.robolectric.internal.RealObject;
-
-import static com.xtremelabs.robolectric.Robolectric.shadowOf;
 
 @Implements(ViewPager.class)
 public class ShadowViewPager extends ShadowViewGroup {
@@ -21,28 +17,11 @@ public class ShadowViewPager extends ShadowViewGroup {
     @Implementation
     public void setAdapter(PagerAdapter adapter) {
         this.adapter = adapter;
-        shadowOf(adapter).registerDataSetObserver(new DataSetObserver() {
-            @Override
-            public void onChanged() {
-                dataSetChanged();
-            }
-        });
-        dataSetChanged();
     }
 
     @Implementation
     public PagerAdapter getAdapter() {
         return adapter;
-    }
-
-    @Implementation
-    public void dataSetChanged() {
-        for (int i = getChildCount(); i < adapter.getCount(); i++) {
-            adapter.startUpdate(realViewPager);
-            Object item = adapter.instantiateItem(realViewPager, i);
-            adapter.setPrimaryItem(realViewPager, 0, item);
-            adapter.finishUpdate(realViewPager);
-        }
     }
 
     @Implementation
@@ -53,9 +32,5 @@ public class ShadowViewPager extends ShadowViewGroup {
     @Implementation
     public void setCurrentItem(int item) {
         currentItem = item;
-    }
-
-    public View getCurrentView() {
-        return getChildAt(getCurrentItem());
     }
 }
