a16489f7f239bf6974e4f5607bb071670ae02245
Jon Boekenoogen <jboekeno@google.com>
2012-06-25 12:18:17 -0700

    Add isDigitsOnly to ShadowTextUtils

--- src/main/java/com/xtremelabs/robolectric/shadows/ShadowTextUtils.java
+++ src/main/java/com/xtremelabs/robolectric/shadows/ShadowTextUtils.java
@@ -35,4 +35,15 @@ public class ShadowTextUtils {
     public static String join(CharSequence delimiter, Object[] tokens) {
         return Join.join((String) delimiter, tokens);
     }
+
+    @Implementation
+    public static boolean isDigitsOnly(CharSequence str) {
+        final int len = str.length();
+        for (int i = 0; i < len; i++) {
+          if (!Character.isDigit(str.charAt(i))) {
+            return false;
+          }
+        }
+        return true;
+    }
 }
