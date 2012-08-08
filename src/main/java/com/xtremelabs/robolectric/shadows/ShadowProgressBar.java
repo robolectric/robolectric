a010f2f6552fe780d5fcad4bd58b96e1eba01cd9
Jan Berkel <jan.berkel@gmail.com>
2012-08-08 15:51:19 +0200

    WS

--- src/main/java/com/xtremelabs/robolectric/shadows/ShadowProgressBar.java
+++ src/main/java/com/xtremelabs/robolectric/shadows/ShadowProgressBar.java
@@ -1,53 +1,55 @@
 package com.xtremelabs.robolectric.shadows;
 
-import android.widget.ProgressBar;
 import com.xtremelabs.robolectric.internal.Implementation;
 import com.xtremelabs.robolectric.internal.Implements;
+
+import android.widget.ProgressBar;
 
 @Implements(ProgressBar.class)
 public class ShadowProgressBar extends ShadowView {
 
-  private int progress;
-  private int secondaryProgress;
-  private int max = 100;
+    private int progress;
+    private int secondaryProgress;
+    private int max = 100;
 
-  @Override public void applyAttributes() {
-      super.applyAttributes();
-      
-      final int max = attributeSet.getAttributeIntValue("android", "max", this.max);
-      
-      if (max >= 0)
-          setMax(max);
-  }
-  
-  @Implementation
-  public void setMax(int max) {
-    progress = 0;
-    this.max = max;
-  }
+    @Override
+    public void applyAttributes() {
+        super.applyAttributes();
 
-  @Implementation
-  public int getMax() {
-    return max;
-  }
+        final int max = attributeSet.getAttributeIntValue("android", "max", this.max);
 
-  @Implementation
-  public void setProgress(int progress) {
-    this.progress = progress;
-  }
+        if (max >= 0)
+            setMax(max);
+    }
 
-  @Implementation
-  public int getProgress() {
-    return progress;
-  }
-  
-  @Implementation
-  public void setSecondaryProgress(int secondaryProgress) {
-    this.secondaryProgress = secondaryProgress;
-  }
-  
-  @Implementation
-  public int getSecondaryProgress() {
-    return secondaryProgress;
-  }
+    @Implementation
+    public void setMax(int max) {
+        progress = 0;
+        this.max = max;
+    }
+
+    @Implementation
+    public int getMax() {
+        return max;
+    }
+
+    @Implementation
+    public void setProgress(int progress) {
+        this.progress = progress;
+    }
+
+    @Implementation
+    public int getProgress() {
+        return progress;
+    }
+
+    @Implementation
+    public void setSecondaryProgress(int secondaryProgress) {
+        this.secondaryProgress = secondaryProgress;
+    }
+
+    @Implementation
+    public int getSecondaryProgress() {
+        return secondaryProgress;
+    }
 }
