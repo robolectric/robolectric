06536cd500e51ccc8091e98e475d072b582952b8
Tobias Gesellchen <tobias@gesellix.de>
2012-09-20 21:02:52 +0200

    Changed ShadowBase64 to be a copy of the original Android source file android.util.Base64.

--- src/test/java/com/xtremelabs/robolectric/shadows/ShadowBase64Test.java
+++ src/test/java/com/xtremelabs/robolectric/shadows/ShadowBase64Test.java
@@ -2,27 +2,25 @@ package com.xtremelabs.robolectric.shadows;
 
 import org.junit.Test;
 
+import java.io.UnsupportedEncodingException;
+
+import static android.util.Base64.DEFAULT;
+import static android.util.Base64.NO_WRAP;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertThat;
 
 public class ShadowBase64Test {
 
-  @Test
-  public void testEncodeToString_alwaysReturnsEmptyString() throws Exception {
-      assertThat(ShadowBase64.encodeToString(null, -1), equalTo(""));
-      assertThat(ShadowBase64.encodeToString(null, -1, -1, -1), equalTo(""));
-  }
+    @Test
+    public void testEncode() throws UnsupportedEncodingException {
+        String inputString = "Some nice String";
+        String encodedString = ShadowBase64.encodeToString(inputString.getBytes("UTF-8"), DEFAULT);
+        assertThat(encodedString, equalTo("U29tZSBuaWNlIFN0cmluZw==\n"));
+    }
 
-  @Test
-  public void testEncodeAlwaysReturnsEmptyByteArray() throws Exception {
-      assertThat(ShadowBase64.encode(null, -1), equalTo(new byte[0]));
-      assertThat(ShadowBase64.encode(null, -1, -1, -1), equalTo(new byte[0]));
-  }
-
-  @Test
-  public void testDecodeAlwaysReturnsEmptyByteArray() throws Exception {
-      assertThat(ShadowBase64.decode((String) null, -1), equalTo(new byte[0]));
-      assertThat(ShadowBase64.decode((byte[]) null, -1), equalTo(new byte[0]));
-      assertThat(ShadowBase64.decode(null, -1, -1, -1), equalTo(new byte[0]));
-  }
+    @Test
+    public void testDecode() throws Exception {
+        byte[] decodedBytes = ShadowBase64.decode("U29tZSBuaWNlIFN0cmluZw==", DEFAULT);
+        assertThat(new String(decodedBytes), equalTo("Some nice String"));
+    }
 }
