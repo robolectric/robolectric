6ebc891d91a34c6a007ecbdd209342fad0a665b0
Rich Humphrey <rich@zoodles.com>
2012-09-26 16:10:27 -0700

    Fix formatting

--- src/test/java/com/xtremelabs/robolectric/annotation/WithConstantStringTest.java
+++ src/test/java/com/xtremelabs/robolectric/annotation/WithConstantStringTest.java
@@ -15,7 +15,7 @@ public class WithConstantStringTest {
 	private static final String NEW_VALUE = "HTC";
 
 	@Test
-	@WithConstantString(classWithField=android.os.Build.class, fieldName="MANUFACTURER", newValue=NEW_VALUE )
+	@WithConstantString(classWithField=android.os.Build.class, fieldName="MANUFACTURER", newValue=NEW_VALUE)
 	public void testWithConstantString() {
 		assertThat(android.os.Build.MANUFACTURER, equalTo(NEW_VALUE));
 	}
