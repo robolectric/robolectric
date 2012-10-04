7a96f386e9ec0c78ce145c1455b7ac1ed5ef7463
Chris Nix <chris.nix@gmail.com>
2012-10-05 00:36:02 +0100

    Added methods to ShadowSQLiteQueryBuilder

--- src/main/java/com/xtremelabs/robolectric/shadows/ShadowSQLiteQueryBuilder.java
+++ src/main/java/com/xtremelabs/robolectric/shadows/ShadowSQLiteQueryBuilder.java
@@ -1,50 +1,193 @@
 package com.xtremelabs.robolectric.shadows;
 
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.regex.Pattern;
+
 import android.database.sqlite.SQLiteQueryBuilder;
+import android.provider.BaseColumns;
+
 import com.xtremelabs.robolectric.internal.Implementation;
 import com.xtremelabs.robolectric.internal.Implements;
-import com.xtremelabs.robolectric.util.Join;
 
 /**
  * Shadow for {@code SQLiteQueryBuilder}.
  */
 @Implements(SQLiteQueryBuilder.class)
 public class ShadowSQLiteQueryBuilder {
+  private static final Pattern sLimitPattern = Pattern.compile("\\s*\\d+\\s*(,\\s*\\d+\\s*)?");
+  
+  private boolean mStrict;
+  private String mTables = "";
+  private StringBuilder mWhereClause = null; // lazily created
+  private Map<String, String> mProjectionMap = null;
+  private boolean mDistinct;
 
-    @Implementation
-    public static String buildQueryString(boolean distinct, String tables,
-                                          String[] columns, String where, String groupBy, String having,
-                                          String orderBy, String limit) {
+  @Implementation
+  public void setTables(String inTables) {
+    mTables = inTables;
+  }
 
-        StringBuilder sb = new StringBuilder("SELECT ");
+  @Implementation
+  public void setStrict(boolean flag) {
+    mStrict = flag;
+  }
 
-        if (distinct) {
-            sb.append("DISTINCT ");
-        }
+  @Implementation
+  public void setDistinct(boolean distinct) {
+    mDistinct = distinct;
+  }
 
-        if (columns != null) {
-            sb.append(Join.join(", ", (Object[]) columns));
-        } else {
-            sb.append("*");
-        }
+  @Implementation
+  public void appendWhere(CharSequence inWhere) {
+    if (mWhereClause == null) {
+      mWhereClause = new StringBuilder(inWhere.length() + 16);
+    }
+    if (mWhereClause.length() == 0) {
+      mWhereClause.append('(');
+    }
+    mWhereClause.append(inWhere);
+  }
 
-        sb.append(" FROM ");
-        sb.append(tables);
+  @Implementation
+  public void setProjectionMap(Map<String, String> columnMap) {
+    mProjectionMap = columnMap;
+  }
 
-        conditionallyAppend(sb, " WHERE ", where);
-        conditionallyAppend(sb, " GROUP BY ", groupBy);
-        conditionallyAppend(sb, " HAVING ", having);
-        conditionallyAppend(sb, " ORDER BY ", orderBy);
-        conditionallyAppend(sb, " LIMIT ", limit);
+  @Implementation
+  public String buildQuery(String[] projectionIn, String selection, String groupBy, String having, String sortOrder, String limit) {
+    String[] projection = computeProjection(projectionIn);
 
-        return sb.toString();
+    StringBuilder where = new StringBuilder();
+    boolean hasBaseWhereClause = mWhereClause != null && mWhereClause.length() > 0;
+
+    if (hasBaseWhereClause) {
+      where.append(mWhereClause.toString());
+      where.append(')');
     }
 
-    private static void conditionallyAppend(StringBuilder sb, String keyword, String value) {
-        if (value != null) {
-            sb.append(keyword);
-            sb.append(value);
-        }
+    // Tack on the user's selection, if present.
+    if (selection != null && selection.length() > 0) {
+      if (hasBaseWhereClause) {
+        where.append(" AND ");
+      }
+
+      where.append('(');
+      where.append(selection);
+      where.append(')');
     }
+
+    return buildQueryString(mDistinct, mTables, projection, where.toString(), groupBy, having, sortOrder, limit);
+  }
+
+  @Implementation
+  public static String buildQueryString(boolean distinct, String tables, String[] columns, String where, String groupBy, String having, String orderBy,
+      String limit) {
+    if (isEmpty(groupBy) && !isEmpty(having)) {
+      throw new IllegalArgumentException("HAVING clauses are only permitted when using a GROUP BY clause");
+    }
+    if (!isEmpty(limit) && !sLimitPattern.matcher(limit).matches()) {
+      throw new IllegalArgumentException("invalid LIMIT clauses:" + limit);
+    }
+
+    StringBuilder query = new StringBuilder(120);
+
+    query.append("SELECT ");
+    if (distinct) {
+      query.append("DISTINCT ");
+    }
+    if (columns != null && columns.length != 0) {
+      appendColumns(query, columns);
+    } else {
+      query.append("* ");
+    }
+    query.append("FROM ");
+    query.append(tables);
+    appendClause(query, " WHERE ", where);
+    appendClause(query, " GROUP BY ", groupBy);
+    appendClause(query, " HAVING ", having);
+    appendClause(query, " ORDER BY ", orderBy);
+    appendClause(query, " LIMIT ", limit);
+
+    return query.toString();
+  }
+
+  private static void appendClause(StringBuilder s, String name, String clause) {
+    if (!isEmpty(clause)) {
+      s.append(name);
+      s.append(clause);
+    }
+  }
+
+  @Implementation
+  public static void appendColumns(StringBuilder s, String[] columns) {
+    int n = columns.length;
+
+    for (int i = 0; i < n; i++) {
+      String column = columns[i];
+
+      if (column != null) {
+        if (i > 0) {
+          s.append(", ");
+        }
+        s.append(column);
+      }
+    }
+    s.append(' ');
+  }
+
+  private String[] computeProjection(String[] projectionIn) {
+    if (projectionIn != null && projectionIn.length > 0) {
+      if (mProjectionMap != null) {
+        String[] projection = new String[projectionIn.length];
+        int length = projectionIn.length;
+
+        for (int i = 0; i < length; i++) {
+          String userColumn = projectionIn[i];
+          String column = mProjectionMap.get(userColumn);
+
+          if (column != null) {
+            projection[i] = column;
+            continue;
+          }
+
+          if (!mStrict && (userColumn.contains(" AS ") || userColumn.contains(" as "))) {
+            /* A column alias already exist */
+            projection[i] = userColumn;
+            continue;
+          }
+
+          throw new IllegalArgumentException("Invalid column " + projectionIn[i]);
+        }
+        return projection;
+      } else {
+        return projectionIn;
+      }
+    } else if (mProjectionMap != null) {
+      // Return all columns in projection map.
+      Set<Entry<String, String>> entrySet = mProjectionMap.entrySet();
+      String[] projection = new String[entrySet.size()];
+      Iterator<Entry<String, String>> entryIter = entrySet.iterator();
+      int i = 0;
+
+      while (entryIter.hasNext()) {
+        Entry<String, String> entry = entryIter.next();
+
+        // Don't include the _count column when people ask for no projection.
+        if (entry.getKey().equals(BaseColumns._COUNT)) {
+          continue;
+        }
+        projection[i++] = entry.getValue();
+      }
+      return projection;
+    }
+    return null;
+  }
+
+  private static boolean isEmpty(String s) {
+    return s == null || s.length() == 0;
+  }
 
 }
--- src/test/java/com/xtremelabs/robolectric/shadows/SQLiteQueryBuilderTest.java
+++ src/test/java/com/xtremelabs/robolectric/shadows/SQLiteQueryBuilderTest.java
@@ -1,152 +1,275 @@
 package com.xtremelabs.robolectric.shadows;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
-import android.database.sqlite.SQLiteQueryBuilder;
-import com.xtremelabs.robolectric.WithTestDefaultsRunner;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.junit.Assert.assertThat;
+import android.database.sqlite.SQLiteQueryBuilder;
+
+import com.xtremelabs.robolectric.WithTestDefaultsRunner;
 
 @RunWith(WithTestDefaultsRunner.class)
 public class SQLiteQueryBuilderTest {
 
-    SQLiteQueryBuilder builder;
+  SQLiteQueryBuilder builder;
 
-    @Before
-    public void setUp() throws Exception {
-        builder = new SQLiteQueryBuilder();
+  @Before
+  public void setUp() throws Exception {
+    builder = new SQLiteQueryBuilder();
+  }
+
+  @Test
+  public void testDistinct() {
+    String s = "SELECT DISTINCT id, name FROM table_name";
+    
+    String tables = "table_name";
+    String[] columns = new String[] { "id", "name" };
+    
+    String sql1 = SQLiteQueryBuilder.buildQueryString(true, tables, columns, null, null, null, null, null);
+    assertThat(sql1, equalTo(s));
+    
+    builder.setTables(tables);
+    builder.setDistinct(true);
+    String sql2 = builder.buildQuery(columns, null, null, null, null, null);
+    assertThat(sql2, equalTo(s));
+  }
+
+  @Test
+  public void testSelectColumn() {
+    String s = "SELECT id FROM table_name";
+
+    String tables = "table_name";
+    String[] columns = new String[] { "id" };
+    
+    String sql1 = SQLiteQueryBuilder.buildQueryString(false, tables, columns, null, null, null, null, null);
+    assertThat(sql1, equalTo(s));
+
+    builder.setTables(tables);
+    String sql2 = builder.buildQuery(columns, null, null, null, null, null);
+    assertThat(sql2, equalTo(s));
+  }
+
+  @Test
+  public void testSelectColumns() {
+    String s = "SELECT id, name FROM table_name";
+
+    String tables = "table_name";
+    String[] columns = new String[] { "id", "name" };
+    
+    String sql1 = SQLiteQueryBuilder.buildQueryString(false, tables, columns, null, null, null, null, null);
+    assertThat(sql1, equalTo(s));
+
+    builder.setTables(tables);
+    String sql2 = builder.buildQuery(columns, null, null, null, null, null);
+    assertThat(sql2, equalTo(s));
+  }
+
+  @Test
+  public void testSelectAllColumns() {
+    String s = "SELECT * FROM table_name";    
+
+    String tables = "table_name";
+    
+    String sql1 = SQLiteQueryBuilder.buildQueryString(false, tables, null, null, null, null, null, null);
+    assertThat(sql1, equalTo(s));
+
+    builder.setTables(tables);
+    String sql2 = builder.buildQuery(null, null, null, null, null, null);
+    assertThat(sql2, equalTo(s));
+  }
+
+  @Test
+  public void testWhereClause() {
+    String s = "SELECT person, department, division FROM table_name WHERE \\(id = 2 AND name = 'Chuck'\\s?\\)";    
+
+    String tables = "table_name";
+    String[] columns = new String[] { "person", "department", "division" };
+    String where = "id = 2 AND name = 'Chuck'";
+    
+    String sql1 = SQLiteQueryBuilder.buildQueryString(false, tables, columns, "(" + where + " )", null, null, null, null);
+    assertTrue("'" + sql1 + "' doesn't match '" + s + "'", sql1.matches(s));
+
+    builder.setTables(tables);
+    String sql2 = builder.buildQuery(columns, where, null, null, null, null);
+    assertTrue("'" + sql2 + "' doesn't match '" + s + "'", sql2.matches(s));
+
+    builder.appendWhere(where);
+    String sql3 = builder.buildQuery(columns, null, null, null, null, null);
+    assertTrue("'" + sql3 + "' doesn't match '" + s + "'", sql3.matches(s));
+  }
+
+  @Test
+  public void testEmptyWhereClause() {
+    String s = "SELECT person, department, division FROM table_name GROUP BY person";    
+
+    String tables = "table_name";
+    String groupBy = "person";
+    String[] columns = new String[] { "person", "department", "division" };
+    
+    String sql1 = SQLiteQueryBuilder.buildQueryString(false, tables, columns, null, groupBy, null, null, null);
+    assertThat(sql1, equalTo(s));
+
+    builder.setTables(tables);
+    String sql2 = builder.buildQuery(columns, null, groupBy, null, null, null);
+    assertThat(sql2, equalTo(s));
+  }
+
+  @Test
+  public void testGroupBy() {
+    String s = "SELECT person, department, division FROM table_name WHERE \\(id = 2 AND name = 'Chuck'\\s?\\) GROUP BY person";    
+
+    String tables = "table_name";
+    String groupBy = "person";
+    String[] columns = new String[] { "person", "department", "division" };    
+    String where = "id = 2 AND name = 'Chuck'";
+    
+    String sql1 = SQLiteQueryBuilder.buildQueryString(false, tables, columns, "(" + where + " )", groupBy, null, null, null);
+    assertTrue("'" + sql1 + "' doesn't match '" + s + "'", sql1.matches(s));
+
+    builder.setTables(tables);
+    String sql2 = builder.buildQuery(columns, where, groupBy, null, null, null);
+    assertTrue("'" + sql2 + "' doesn't match '" + s + "'", sql2.matches(s));
+  }
+
+  @Test
+  public void testHavingWithEmptyGroupByBuildQueryString() {
+    String tables = "table_name";
+    String[] columns = new String[] { "person", "department", "division" };
+    String where = "id = 2 AND name = 'Chuck'";
+    String having = "SUM(hours) < 20";
+    
+    try {
+      SQLiteQueryBuilder.buildQueryString(false, tables, columns, "(" + where + " )", null, having, null, null);
+      assertTrue(false);
+    } catch (IllegalArgumentException e) {
+      assertTrue(true);
     }
 
-    @Test
-    public void testDistinct() {
-        String sql = SQLiteQueryBuilder.buildQueryString(
-                true,
-                "table_name",
-                new String[]{"id", "name"},
-                null, null, null, null, null);
-        assertThat(sql, equalTo("SELECT DISTINCT id, name FROM table_name"));
-
+    try {
+      builder.setTables(tables);
+      builder.buildQuery(columns, where, null, having, null, null);
+      assertTrue(false);
+    } catch (IllegalArgumentException e) {
+      assertTrue(true);
     }
+  }
 
-    @Test
-    public void testSelectColumn() {
-        String sql = SQLiteQueryBuilder.buildQueryString(
-                false,
-                "table_name",
-                new String[]{"id"},
-                null, null, null, null, null);
-        assertThat(sql, equalTo("SELECT id FROM table_name"));
-    }
+  @Test
+  public void testHaving() {
+    String s = "SELECT person, department, division FROM table_name WHERE \\(id = 2 AND name = 'Chuck'\\s?\\) GROUP BY person HAVING SUM\\(hours\\) < 20";
+    
+    String tables = "table_name";
+    String groupBy = "person";
+    String[] columns = new String[] { "person", "department", "division" };
+    String where = "id = 2 AND name = 'Chuck'";
+    String having = "SUM(hours) < 20";
+    
+    String sql1 = SQLiteQueryBuilder.buildQueryString(false, tables, columns, "(" + where + " )", groupBy, having, null, null);
+    assertTrue("'" + sql1 + "' doesn't match '" + s + "'", sql1.matches(s));
 
-    @Test
-    public void testSelectColumns() {
-        String sql = SQLiteQueryBuilder.buildQueryString(
-                false,
-                "table_name",
-                new String[]{"id", "name"},
-                null, null, null, null, null);
-        assertThat(sql, equalTo("SELECT id, name FROM table_name"));
-    }
+    builder.setTables(tables);
+    String sql2 = builder.buildQuery(columns, where, groupBy, having, null, null);
+    assertTrue("'" + sql2 + "' doesn't match '" + s + "'", sql2.matches(s));
+  }
 
-    @Test
-    public void testSelectAllColumns() {
-        String sql = SQLiteQueryBuilder.buildQueryString(
-                false,
-                "table_name",
-                null, null, null, null, null, null);
-        assertThat(sql, equalTo("SELECT * FROM table_name"));
-    }
+  @Test
+  public void testEmptyHaving() {
+    String s = "SELECT person, department, division FROM table_name WHERE \\(id = 2 AND name = 'Chuck'\\s?\\) GROUP BY person ORDER BY id ASC";
 
-    @Test
-    public void testWhereClause() {
-        String sql = SQLiteQueryBuilder.buildQueryString(
-                false,
-                "table_name",
-                new String[]{"person", "department", "division"},
-                "(id = 2 AND name = 'Chuck')", null, null, null, null);
-        assertThat(sql, equalTo("SELECT person, department, division FROM table_name WHERE (id = 2 AND name = 'Chuck')"));
-    }
+    String tables = "table_name";
+    String groupBy = "person";
+    String[] columns = new String[] { "person", "department", "division" };
+    String where = "id = 2 AND name = 'Chuck'";
+    String orderBy = "id ASC";
+    
+    String sql1 = SQLiteQueryBuilder.buildQueryString(false, tables, columns, "(" + where + " )", groupBy, null, orderBy, null);
+    assertTrue("'" + sql1 + "' doesn't match '" + s + "'", sql1.matches(s));
 
-    @Test
-    public void testEmptyWhereClause() {
-        String sql = SQLiteQueryBuilder.buildQueryString(
-                false,
-                "table_name",
-                new String[]{"person", "department", "division"},
-                null, "person", null, null, null);
-        assertThat(sql, equalTo("SELECT person, department, division FROM table_name GROUP BY person"));
-    }
+    builder.setTables(tables);
+    String sql2 = builder.buildQuery(columns, where, groupBy, null, orderBy, null);
+    assertTrue("'" + sql2 + "' doesn't match '" + s + "'", sql2.matches(s));
+  }
 
-    @Test
-    public void testGroupBy() {
-        String sql = SQLiteQueryBuilder.buildQueryString(
-                false,
-                "table_name",
-                new String[]{"person", "department", "division"},
-                "(id = 2 AND name = 'Chuck')", "person", null, null, null);
-        assertThat(sql, equalTo("SELECT person, department, division FROM table_name WHERE (id = 2 AND name = 'Chuck') GROUP BY person"));
-    }
+  @Test
+  public void testSortOrder() {
+    String s = "SELECT person, department, division FROM table_name WHERE \\(id = 2 AND name = 'Chuck'\\s?\\) GROUP BY person HAVING SUM\\(hours\\) < 20 ORDER BY id ASC";
 
-    @Test
-    public void testEmptyGroupBy() {
-        String sql = SQLiteQueryBuilder.buildQueryString(
-                false,
-                "table_name",
-                new String[]{"person", "department", "division"},
-                "(id = 2 AND name = 'Chuck')", null, "SUM(hours) < 20", null, null);
-        assertThat(sql, equalTo("SELECT person, department, division FROM table_name WHERE (id = 2 AND name = 'Chuck') HAVING SUM(hours) < 20"));
-    }
+    String tables = "table_name";
+    String groupBy = "person";
+    String[] columns = new String[] { "person", "department", "division" };
+    String where = "id = 2 AND name = 'Chuck'";
+    String having = "SUM(hours) < 20";
+    String orderBy = "id ASC";
+    
+    String sql1 = SQLiteQueryBuilder.buildQueryString(false, tables, columns, "(" + where + " )", groupBy, having, orderBy, null);
+    assertTrue("'" + sql1 + "' doesn't match '" + s + "'", sql1.matches(s));
 
-    @Test
-    public void testHaving() {
-        String sql = SQLiteQueryBuilder.buildQueryString(
-                false,
-                "table_name",
-                new String[]{"person", "department", "division"},
-                "(id = 2 AND name = 'Chuck')", "person", "SUM(hours) < 20", null, null);
-        assertThat(sql, equalTo("SELECT person, department, division FROM table_name WHERE (id = 2 AND name = 'Chuck') GROUP BY person HAVING SUM(hours) < 20"));
-    }
+    builder.setTables(tables);
+    String sql2 = builder.buildQuery(columns, where, groupBy, having, orderBy, null);
+    assertTrue("'" + sql2 + "' doesn't match '" + s + "'", sql2.matches(s));
+  }
 
-    @Test
-    public void testEmptyHaving() {
-        String sql = SQLiteQueryBuilder.buildQueryString(
-                false,
-                "table_name",
-                new String[]{"person", "department", "division"},
-                "(id = 2 AND name = 'Chuck')", "person", null, "id ASC", null);
-        assertThat(sql, equalTo("SELECT person, department, division FROM table_name WHERE (id = 2 AND name = 'Chuck') GROUP BY person ORDER BY id ASC"));
-    }
+  @Test
+  public void testEmptySortOrder() {
+    String s = "SELECT person, department, division FROM table_name WHERE \\(id = 2 AND name = 'Chuck'\\s?\\) GROUP BY person HAVING SUM\\(hours\\) < 20 LIMIT 10";
+    
+    String tables = "table_name";
+    String groupBy = "person";
+    String[] columns = new String[] { "person", "department", "division" };
+    String where = "id = 2 AND name = 'Chuck'";
+    String having = "SUM(hours) < 20";
+    String limit = "10";
+    
+    String sql1 = SQLiteQueryBuilder.buildQueryString(false, tables, columns, "(" + where + " )", groupBy, having, null, limit);
+    assertTrue("'" + sql1 + "' doesn't match '" + s + "'", sql1.matches(s));
+    
+    builder.setTables(tables);
+    String sql2 = builder.buildQuery(columns, where, groupBy, having, null, limit);
+    assertTrue("'" + sql2 + "' doesn't match '" + s + "'", sql2.matches(s));
+  }
 
-    @Test
-    public void testSortOrder() {
-        String sql = SQLiteQueryBuilder.buildQueryString(
-                false,
-                "table_name",
-                new String[]{"person", "department", "division"},
-                "(id = 2 AND name = 'Chuck')", "person", "SUM(hours) < 20", "id ASC", null);
-        assertThat(sql, equalTo("SELECT person, department, division FROM table_name WHERE (id = 2 AND name = 'Chuck') GROUP BY person HAVING SUM(hours) < 20 ORDER BY id ASC"));
-    }
+  @Test
+  public void testLimit() {
+    String s = "SELECT person, department, division FROM table_name WHERE \\(id = 2 AND name = 'Chuck'\\s?\\) GROUP BY person HAVING SUM\\(hours\\) < 20 ORDER BY id ASC LIMIT 10";
+    
+    String tables = "table_name";
+    String groupBy = "person";
+    String[] columns = new String[] { "person", "department", "division" };
+    String where = "id = 2 AND name = 'Chuck'";
+    String having = "SUM(hours) < 20";
+    String orderBy = "id ASC";
+    String limit = "10";
+    
+    String sql1 = SQLiteQueryBuilder.buildQueryString(false, tables, columns, "(" + where + " )", groupBy, having, orderBy, limit);
+    assertTrue("'" + sql1 + "' doesn't match '" + s + "'",sql1.matches(s));
+    
+    builder.setTables(tables);
+    String sql2 = builder.buildQuery(columns, where, groupBy, having, orderBy, limit);
+    assertTrue("'" + sql2 + "' doesn't match '" + s + "'", sql2.matches(s));
+  }
+  
+  @Test
+  public void testProjectionMap() {
+    String s = "SELECT person, department, division FROM table_name";
+    
+    String tables = "table_name";
+    String[] columns = new String[] { "pn", "dt", "dn" };
+    Map<String, String> map = new HashMap<String, String>(3);
+    map.put("pn", "person");
+    map.put("dt", "department");
+    map.put("dn", "division");
+    
+    builder.setTables(tables);
+    builder.setProjectionMap(map);
+    String sql = builder.buildQuery(columns, null, null, null, null, null);
+    assertThat(sql, equalTo(s));
+  }
 
-    @Test
-    public void testEmptySortOrder() {
-        String sql = SQLiteQueryBuilder.buildQueryString(
-                false,
-                "table_name",
-                new String[]{"person", "department", "division"},
-                "(id = 2 AND name = 'Chuck')", "person", "SUM(hours) < 20", null, "10");
-        assertThat(sql, equalTo("SELECT person, department, division FROM table_name WHERE (id = 2 AND name = 'Chuck') GROUP BY person HAVING SUM(hours) < 20 LIMIT 10"));
-    }
-
-    @Test
-    public void testLimit() {
-        String sql = SQLiteQueryBuilder.buildQueryString(
-                false,
-                "table_name",
-                new String[]{"person", "department", "division"},
-                "(id = 2 AND name = 'Chuck')", "person", "SUM(hours) < 20", "id ASC", "10");
-        assertThat(sql, equalTo("SELECT person, department, division FROM table_name WHERE (id = 2 AND name = 'Chuck') GROUP BY person HAVING SUM(hours) < 20 ORDER BY id ASC LIMIT 10"));
-    }
 }
