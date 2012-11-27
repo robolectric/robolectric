e494b46424503a686753cc3165cb456554f784c4
Blundell <paul@novoda.com>
2012-11-27 16:42:19 +0000

    Added implementation of static method DatabaseUtils.appendEscapedSQLString with test coverage

--- src/test/java/com/xtremelabs/robolectric/shadows/DatabaseUtilsTest.java
+++ src/test/java/com/xtremelabs/robolectric/shadows/DatabaseUtilsTest.java
@@ -3,10 +3,10 @@ package com.xtremelabs.robolectric.shadows;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 
+import com.xtremelabs.robolectric.WithTestDefaultsRunner;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import com.xtremelabs.robolectric.WithTestDefaultsRunner;
 
 @RunWith(WithTestDefaultsRunner.class)
 public class DatabaseUtilsTest {
@@ -15,5 +15,16 @@ public class DatabaseUtilsTest {
 	public void testQuote() {
 		assertThat( ShadowDatabaseUtils.sqlEscapeString( "foobar" ), equalTo( "'foobar'" ) );
 		assertThat( ShadowDatabaseUtils.sqlEscapeString( "Rich's" ), equalTo( "'Rich''s'" ) );
+	}
+	
+	@Test
+	public void testQuoteWithBuilder() {
+		StringBuilder builder = new StringBuilder();
+		ShadowDatabaseUtils.appendEscapedSQLString( builder , "foobar" );
+		assertThat( builder.toString(), equalTo( "'foobar'" ) );
+		
+		builder = new StringBuilder();
+		ShadowDatabaseUtils.appendEscapedSQLString( builder, "Rich's" );
+		assertThat( builder.toString(), equalTo( "'Rich''s'" ) );
 	}
 }
