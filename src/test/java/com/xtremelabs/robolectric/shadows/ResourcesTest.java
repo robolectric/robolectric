a88dea2ddc651cc9922e232483756f845d077d13
Rick Kawala and Taichi Noro <pair+rick+taichi@pivotallabs.com>
2013-01-04 14:17:42 -0800

    When changing the density, update existing DisplayMetrics instance.

--- src/test/java/com/xtremelabs/robolectric/shadows/ResourcesTest.java
+++ src/test/java/com/xtremelabs/robolectric/shadows/ResourcesTest.java
@@ -8,6 +8,7 @@ import android.graphics.drawable.AnimationDrawable;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.NinePatchDrawable;
+import android.util.DisplayMetrics;
 import com.xtremelabs.robolectric.R;
 import com.xtremelabs.robolectric.Robolectric;
 import com.xtremelabs.robolectric.WithTestDefaultsRunner;
@@ -36,9 +37,9 @@ public class ResourcesTest {
 	@Before
 	public void setup() {
 		resources = new Activity().getResources();
-		shadowApp = shadowOf( Robolectric.application );		
+		shadowApp = shadowOf( Robolectric.application );
 	}
-	
+
     @Test(expected = Resources.NotFoundException.class)
     public void getStringArray_shouldThrowExceptionIfNotFound() throws Exception {
         resources.getStringArray(-1);
@@ -70,7 +71,7 @@ public class ResourcesTest {
     	shadowApp.getResourceLoader().setLocalRClass( null );
     	assertThat( resources.getDrawable( TestR.anim.test_anim_1 ), instanceOf( BitmapDrawable.class ) );
     }
-    
+
     /**
      * given an R.anim.id value, will return an AnimationDrawable
      */
@@ -79,21 +80,21 @@ public class ResourcesTest {
     	shadowApp.getResourceLoader().setLocalRClass( TestR.class );
     	assertThat( resources.getDrawable( TestR.anim.test_anim_1 ), instanceOf( AnimationDrawable.class ) );
     }
-    
+
     @Test
     @Values( qualifiers="fr" )
     public void testGetValuesResFromSpecifiecQualifiers(){
     	String hello=resources.getString( R.string.hello );
     	assertThat( hello, equalTo( "Bonjour" ) );
     }
-    
+
     /**
      * given an R.color.id value, will return a ColorDrawable
      */
     @Test
     public void testGetColorDrawable() {
     	shadowApp.getResourceLoader().setLocalRClass( TestR.class );
-    	assertThat( resources.getDrawable( TestR.color.test_color_1 ), instanceOf( ColorDrawable.class ) );    	
+    	assertThat( resources.getDrawable( TestR.color.test_color_1 ), instanceOf( ColorDrawable.class ) );
     }
 
     /**
@@ -102,24 +103,24 @@ public class ResourcesTest {
     @Test
     public void testGetBitmapDrawable() {
     	shadowApp.getResourceLoader().setLocalRClass( TestR.class );
-    	assertThat( resources.getDrawable( TestR.drawable.test_drawable_1 ), instanceOf( BitmapDrawable.class ) );    	    	    	
+    	assertThat( resources.getDrawable( TestR.drawable.test_drawable_1 ), instanceOf( BitmapDrawable.class ) );
     }
-    
+
     /**
      * given an R.drawable.id value, will return a NinePatchDrawable for .9.png file
      */
     @Test
     public void testGetNinePatchDrawable() {
-    	assertThat(resources.getDrawable(R.drawable.nine_patch_drawable ), instanceOf(NinePatchDrawable.class ) );  
+    	assertThat(resources.getDrawable(R.drawable.nine_patch_drawable ), instanceOf(NinePatchDrawable.class ) );
     }
-    
+
     /**
-     * given a value that doesn't in one of R's inner classes, will return a BitmapDrawable 
+     * given a value that doesn't in one of R's inner classes, will return a BitmapDrawable
      */
     @Test
     public void testGetBitmapDrawableForUnknownId() {
     	shadowApp.getResourceLoader().setLocalRClass( TestR.class );
-    	assertThat(resources.getDrawable( Integer.MAX_VALUE ), instanceOf( BitmapDrawable.class ));    	    	
+    	assertThat(resources.getDrawable( Integer.MAX_VALUE ), instanceOf( BitmapDrawable.class ));
     }
     @Test
     public void testDensity() {
@@ -139,14 +140,14 @@ public class ResourcesTest {
         assertThat(activity.getResources().getDisplayMetrics().heightPixels, equalTo(800));
         assertThat(activity.getResources().getDisplayMetrics().widthPixels, equalTo(480));
     }
-    
+
     @Test
     public void testGetXml() throws Exception {
-    	int resId = R.xml.preferences;    	
+    	int resId = R.xml.preferences;
     	XmlResourceParser parser = resources.getXml(resId);
     	// Assert that a resource file is returned
     	assertThat(parser, notNullValue());
-    	
+
     	// Assert that the resource file is the preference screen
     	int event = -1;
     	do {
@@ -154,7 +155,7 @@ public class ResourcesTest {
     	} while (event != XmlPullParser.START_TAG);
     	assertThat(parser.getName(), equalTo("PreferenceScreen"));
     }
-    
+
     @Test
     public void testGetXml_unexistentResource() {
     	try {
@@ -164,5 +165,12 @@ public class ResourcesTest {
     		// pass
     	}
     }
-    
+
+    @Test
+    public void settingDensityWillChangeValueInExistingDisplayMetricsInstance() throws Exception {
+        DisplayMetrics displayMetrics = resources.getDisplayMetrics();
+        assertThat(displayMetrics.density, equalTo(1.0f));
+        Robolectric.setDisplayMetricsDensity(2f);
+        assertThat(displayMetrics.density, equalTo(2f));
+    }
 }
