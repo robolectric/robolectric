ca09f2afcb1ee940cb22cb2f2ed700fc573fbde4
buddy <buddy@ouya.tv>
2013-01-22 11:59:41 -0800

    Add some auth token support for ShadowAccountManager

--- src/main/java/com/xtremelabs/robolectric/shadows/ShadowAccountManager.java
+++ src/main/java/com/xtremelabs/robolectric/shadows/ShadowAccountManager.java
@@ -1,18 +1,17 @@
 package com.xtremelabs.robolectric.shadows;
 
-import android.accounts.Account;
-import android.accounts.AccountManager;
-import android.accounts.AccountManagerCallback;
-import android.accounts.AccountManagerFuture;
+import android.accounts.*;
 import android.content.Context;
 import android.os.Handler;
 import com.xtremelabs.robolectric.Robolectric;
 import com.xtremelabs.robolectric.internal.Implementation;
 import com.xtremelabs.robolectric.internal.Implements;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Shadow implementation for the Android {@code AccountManager } class.
@@ -23,6 +22,7 @@ public class ShadowAccountManager {
 	private static HashMap<Context,AccountManager> instances = new HashMap<Context,AccountManager>();
 
 	private List<Account> accounts = new ArrayList<Account>();
+    private Map<String, String> authTokenMap = new HashMap<String, String>();
 	
 	@Implementation
 	public static AccountManager get(Context context) {
@@ -55,6 +55,27 @@ public class ShadowAccountManager {
                                                        AccountManagerCallback<Boolean> callback, Handler handler) {
         accounts.remove(account);
         return null;
+    }
+
+    @Implementation
+    public String blockingGetAuthToken(Account account, String authTokenType,
+                                       boolean notifyAuthFailure)
+            throws OperationCanceledException, IOException, AuthenticatorException {
+        if (account == null) throw new IllegalArgumentException("account is null");
+        if (authTokenType == null) throw new IllegalArgumentException("authTokenType is null");
+
+        return authTokenMap.get(authTokenType);
+    }
+
+    @Implementation
+    public void invalidateAuthToken(final String accountType, final String authToken) {
+        if (authTokenMap.get(accountType) == authToken) {
+            authTokenMap.remove(accountType);
+        }
+    }
+
+    public void setAuthTokenForAccountType(String accountType, String authToken) {
+        authTokenMap.put(accountType, authToken);
     }
 
 	/**
