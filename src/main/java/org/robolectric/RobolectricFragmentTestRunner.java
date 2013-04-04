b30540268cc221433c4dbc4033337b7b3117bf40
Myeongseok Baek <msbaek@daumcorp.com>
2013-04-04 16:44:03 +0900

    make it work
    - implement RobolectricFragmentTestRunner#startFragment

--- src/main/java/org/robolectric/RobolectricFragmentTestRunner.java
+++ src/main/java/org/robolectric/RobolectricFragmentTestRunner.java
@@ -1,7 +1,11 @@
 package org.robolectric;
 
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import org.junit.runners.model.InitializationError;
+
+import static org.robolectric.Robolectric.shadowOf;
 
 public class RobolectricFragmentTestRunner extends RobolectricTestRunner {
     public RobolectricFragmentTestRunner(Class<?> testClass) throws InitializationError {
@@ -9,5 +13,23 @@ public class RobolectricFragmentTestRunner extends RobolectricTestRunner {
     }
 
     public static void startFragment(Fragment fragment) {
+        FragmentActivity activity = createActivity();
+
+        FragmentManager fragmentManager = activity.getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .add(fragment, null)
+                .commit();
+    }
+
+    private static FragmentActivity createActivity() {
+        FragmentActivity activity = new FragmentActivity();
+        initActivity(activity);
+        return activity;
+    }
+
+    private static void initActivity(FragmentActivity activity) {
+        shadowOf(activity).callOnCreate(null);
+        shadowOf(activity).callOnStart();
+        shadowOf(activity).callOnResume();
     }
 }
