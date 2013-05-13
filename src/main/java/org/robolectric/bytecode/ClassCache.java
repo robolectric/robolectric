9f297ef2c45282091e860be30fc99f14149afdb3
Christian Williams <xian@squareup.com>
2013-05-13 11:53:33 -0700

    Remove unused ZipClassCache.

--- src/main/java/org/robolectric/bytecode/ClassCache.java
+++
@@ -1,9 +0,0 @@
-package org.robolectric.bytecode;
-
-public interface ClassCache {
-    byte[] getClassBytesFor(String name);
-
-    boolean isWriting();
-
-    void addClass(String className, byte[] classBytes);
-}
