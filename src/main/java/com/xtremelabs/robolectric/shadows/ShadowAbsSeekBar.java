8cc185959b0643b93dea4e4ccb21c9023f1c9ff9
Corey Downing and Rick Kawala <pair+cdowning+rick@pivotallabs.com>
2013-06-18 17:14:53 -0700

    Add setThumbOffset to AbsSeekBar

--- src/main/java/com/xtremelabs/robolectric/shadows/ShadowAbsSeekBar.java
+++ src/main/java/com/xtremelabs/robolectric/shadows/ShadowAbsSeekBar.java
@@ -2,11 +2,21 @@ package com.xtremelabs.robolectric.shadows;
 
 import android.widget.AbsSeekBar;
 
+import com.xtremelabs.robolectric.internal.Implementation;
 import com.xtremelabs.robolectric.internal.Implements;
 
 @Implements(AbsSeekBar.class)
 public class ShadowAbsSeekBar extends ShadowProgressBar {
-    
     boolean mIsUserSeekable = true;
-    
+    private int thumbOffset;
+
+    @Implementation
+    public int getThumbOffset() {
+        return thumbOffset;
+    }
+
+    @Implementation
+    public void setThumbOffset(int thumbOffset) {
+        this.thumbOffset = thumbOffset;
+    }
 }
--- src/test/java/com/xtremelabs/robolectric/shadows/AbsSeekBarTest.java
+++ src/test/java/com/xtremelabs/robolectric/shadows/AbsSeekBarTest.java
@@ -8,7 +8,9 @@ import com.xtremelabs.robolectric.WithTestDefaultsRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static com.xtremelabs.robolectric.Robolectric.shadowOf;
 import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 
 @RunWith(WithTestDefaultsRunner.class)
@@ -17,10 +19,17 @@ public class AbsSeekBarTest {
 	@Test
 	public void testInheritance() {
 		TestAbsSeekBar seekBar = new TestAbsSeekBar(new Activity());
-		ShadowAbsSeekBar shadow = Robolectric.shadowOf(seekBar);
+		ShadowAbsSeekBar shadow = shadowOf(seekBar);
 		assertThat(shadow, instanceOf(ShadowProgressBar.class));
 	}
-	
+
+    @Test
+    public void canSetThumbOffset() throws Exception {
+        AbsSeekBar absSeekBar = new TestAbsSeekBar(new Activity());
+        absSeekBar.setThumbOffset(97);
+        assertThat(shadowOf(absSeekBar).getThumbOffset(), equalTo(97));
+    }
+
 	private static class TestAbsSeekBar extends AbsSeekBar {
 		
 		public TestAbsSeekBar(Context context) {
