738df879d116e324c7c2a6afc649d9b8c8fa12d2
Dandre' Allison <dandre.allison@gmail.com>
2013-09-25 15:27:48 -0700

    Extends FragmentTestUtil to work with non-support Fragments.
    Closes #717.

--- src/test/java/org/robolectric/util/FragmentTestUtilTest.java
+++ src/test/java/org/robolectric/util/FragmentTestUtilTest.java
@@ -1,39 +1,48 @@
 package org.robolectric.util;
 
+import android.app.Fragment;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import org.junit.Before;
+import android.view.LayoutInflater;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.R;
 import org.robolectric.TestRunners;
-
-import static org.hamcrest.core.IsNull.notNullValue;
-import static org.junit.Assert.assertThat;
+import static org.fest.assertions.api.Assertions.assertThat;
 import static org.robolectric.util.FragmentTestUtil.startFragment;
 
 @RunWith(TestRunners.WithDefaults.class)
 public class FragmentTestUtilTest {
-  private LoginFragment fragment;
-
-  @Before
-  public void setUp() {
-    fragment = new LoginFragment();
+  @Test
+  public void shouldStartFragment() {
+    final LoginFragment fragment = new LoginFragment();
     startFragment(fragment);
 
-    assertThat(fragment.getActivity(), notNullValue());
-    assertThat(fragment.getView(), notNullValue());
+    assertThat(fragment.getView()).isNotNull();
+    assertThat(fragment.getActivity()).isNotNull();
+    assertThat(fragment.getView().findViewById(R.id.tacos)).isNotNull();
   }
 
   @Test
-  public void tacosShouldBeFound() {
-    assertThat(fragment.getView().findViewById(R.id.tacos), notNullValue());
+  public void shouldStartSupportFragment() {
+    final LoginSupportFragment fragment = new LoginSupportFragment();
+    startFragment(fragment);
+
+    assertThat(fragment.getView()).isNotNull();
+    assertThat(fragment.getActivity()).isNotNull();
+    assertThat(fragment.getView().findViewById(R.id.tacos)).isNotNull();
   }
 
-  static class LoginFragment extends Fragment {
+  private static class LoginFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+      return inflater.inflate(R.layout.fragment_contents, container, false);
+    }
+  }
+
+  private static class LoginSupportFragment extends android.support.v4.app.Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
       return inflater.inflate(R.layout.fragment_contents, container, false);
