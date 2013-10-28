590034fb3f1532d0337ddce9ec21bb78847e365c
Jon Boekenoogen <jboekeno@google.com>
2013-10-27 20:36:54 -0700

    Add a method for setting Binder calling uid and pid.

--- src/main/java/org/robolectric/shadows/ShadowBinder.java
+++ src/main/java/org/robolectric/shadows/ShadowBinder.java
@@ -13,8 +13,40 @@ public class ShadowBinder {
   @RealObject
   Binder realObject;
 
+  private static Integer callingUid;
+  private static Integer callingPid;
+
   @Implementation
   public boolean transact(int code, Parcel data, Parcel reply, int flags) throws RemoteException {
     return new ShadowBinderBridge(realObject).onTransact(code, data, reply, flags);
+  }
+
+  @Implementation
+  public static final int getCallingPid() {
+    if (callingPid != null) {
+      return callingPid;
+    }
+    return android.os.Process.myPid();
+  }
+
+  @Implementation
+  public static final int getCallingUid() {
+    if (callingUid != null) {
+      return callingUid;
+    }
+    return android.os.Process.myUid();
+  }
+
+  public static void setCallingPid(int pid) {
+    ShadowBinder.callingPid = pid;
+  }
+
+  public static void setCallingUid(int uid) {
+    ShadowBinder.callingUid = uid;
+  }
+
+  public static void reset() {
+    ShadowBinder.callingPid = null;
+    ShadowBinder.callingUid = null;
   }
 }
