5d44ad9bc9276c56b11b5f97bfab77c35034e6b2
Erich Douglass <pair+erich@pivotallabs.com>
2013-11-11 11:02:09 -0800

    Fix yet another POSIX shell issue.

--- scripts/install-snapshot.sh
+++ scripts/install-snapshot.sh
@@ -3,6 +3,6 @@
 # Deploy a snapshot build to Sonatype.  Only non-pull requests will be deployed.
 #
 
-if [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
     mvn --settings scripts/mvn_settings.xml -DskipTests=true source:jar javadoc:jar deploy
 fi
