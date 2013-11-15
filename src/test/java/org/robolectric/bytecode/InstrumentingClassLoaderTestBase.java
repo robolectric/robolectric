d92bad48662937724c8a8b88e506b28daebc51ea
Erich Douglass and Luke Winikates <pair+erich+lwinikates@pivotallabs.com>
2013-11-15 15:23:34 -0800

    Generate stack map frames when producing new bytecode.
    This eliminates the need for the -XX:-UseSplitVerifier when running tests.

--- src/test/java/org/robolectric/bytecode/InstrumentingClassLoaderTestBase.java
+++ src/test/java/org/robolectric/bytecode/InstrumentingClassLoaderTestBase.java
@@ -630,8 +630,9 @@ abstract public class InstrumentingClassLoaderTestBase { // don't end in "Test" 
 
     @Override
     public boolean shouldAcquire(String name) {
-      if (name.equals(AClassToForget.class.getName()))
+      if (name.equals(AClassToForget.class.getName())) {
         throw new RuntimeException(name + " not found (for pretend)!");
+      }
       return super.shouldAcquire(name);
     }
   }
