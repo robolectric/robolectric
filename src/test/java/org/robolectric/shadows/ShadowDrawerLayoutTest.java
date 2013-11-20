77e957cb73cf42ca457f0d73cd500d68dfe9e718
Chris Kramp and Corey Downing <pair+ckramp+cdowning@pivotallabs.com>
2013-11-20 11:07:49 -0700

    Add ShadowDrawerLayout and implement get/set DrawerListener

---
+++ src/test/java/org/robolectric/shadows/ShadowDrawerLayoutTest.java
@@ -0,0 +1,24 @@
+package org.robolectric.shadows;
+
+import android.app.Activity;
+import android.support.v4.widget.DrawerLayout;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.robolectric.Robolectric.buildActivity;
+import static org.robolectric.Robolectric.shadowOf;
+
+@RunWith(RobolectricTestRunner.class)
+public class ShadowDrawerLayoutTest {
+  @Test
+  public void canGetAndSetDrawerListener() throws Exception {
+    DrawerLayout drawerLayout = new DrawerLayout(buildActivity(Activity.class).create().get());
+    DrawerLayout.DrawerListener mockDrawerListener = mock(DrawerLayout.DrawerListener.class);
+    drawerLayout.setDrawerListener(mockDrawerListener);
+    assertThat(shadowOf(drawerLayout).getDrawerListener()).isSameAs(mockDrawerListener);
+  }
+}
