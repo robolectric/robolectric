bccf96354e969903e11294736f39ac682643884e
Chris Kramp and Corey Downing <pair+ckramp+cdowning@pivotallabs.com>
2013-11-21 08:32:59 -0700

    Change maven path for support-v4 jar

--- scripts/install-support-jar.sh
+++ scripts/install-support-jar.sh
@@ -10,8 +10,8 @@
 #  2. Your ANDROID_HOME environment variable points to the Android SDK install dir.
 #  3. You have installed the Android Support (compatibility) libraries from the SDK installer.
 
-echo "Installing android.support.compatibility-v4 from $ANDROID_HOME/extras"
-mvn -q install:install-file -DgroupId=android.support -DartifactId=compatibility-v4 \
-  -Dversion=19 -Dpackaging=jar -Dfile=$ANDROID_HOME/extras/android/support/v4/android-support-v4.jar
+echo "Installing com.android.support:support-v4 from $ANDROID_HOME/extras"
+mvn -q install:install-file -DgroupId=com.android.support -DartifactId=support-v4 \
+  -Dversion=19.0.0 -Dpackaging=jar -Dfile="$ANDROID_HOME/extras/android/support/v4/android-support-v4.jar"
 
 echo "Done!"
