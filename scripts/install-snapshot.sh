0002b897a3317eadd43e4ba864d9b07f231f41a8
Erich Douglass and Joseph Rodriguez <pair+erich+jrodriguez@pivotallabs.com>
2013-11-21 13:25:38 -0700

    Add logging to Travis install script

--- scripts/install-snapshot.sh
+++ scripts/install-snapshot.sh
@@ -4,6 +4,7 @@
 #
 
 BRANCH=`git rev-parse --abbrev-ref HEAD`
+echo "Pull request: '${TRAVIS_PULL_REQUEST}' on branch '${BRANCH}'"
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${BRANCH}" = "master" ]; then
     mvn --settings scripts/mvn_settings.xml -DskipTests=true source:jar javadoc:jar deploy
 fi
