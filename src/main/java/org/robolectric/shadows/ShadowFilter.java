edb57bb0c0ad7751e66b3c28087b6a173a4d83c7
Erich Douglass <pair+erich@pivotallabs.com>
2013-12-19 12:24:35 -0800

    Support AutoCompleteTextView filtering.

--- src/main/java/org/robolectric/shadows/ShadowFilter.java
+++ src/main/java/org/robolectric/shadows/ShadowFilter.java
@@ -1,10 +1,23 @@
 package org.robolectric.shadows;
 
 import android.widget.Filter;
+import org.robolectric.annotation.RealObject;
 import org.robolectric.annotation.Implements;
+import org.robolectric.annotation.Implementation;
+import static org.fest.reflect.core.Reflection.*;
 
-@SuppressWarnings({"UnusedDeclaration"})
 @Implements(Filter.class)
 public class ShadowFilter {
+  @RealObject private Filter realObject;
 
+  @Implementation
+  public void filter(CharSequence constraint, Filter.FilterListener listener) {
+    try {
+      Class<?> forName = Class.forName("android.widget.Filter$FilterResults");
+      Object filtering = method("performFiltering").withReturnType(forName).withParameterTypes(CharSequence.class).in(realObject).invoke(constraint);
+      method("publishResults").withParameterTypes(CharSequence.class, forName).in(realObject).invoke(constraint, filtering);
+    } catch (ClassNotFoundException e) {
+      throw new RuntimeException("Cannot load android.widget.Filter$FilterResults");
+    }
+  }
 }
