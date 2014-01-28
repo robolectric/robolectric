0a9ed1f6048fa13df8b3077509a98923896bf742
Mladen Bajic and Phil Kuryloski <pair+mbajic+pkuryloski@pivotallabs.com>
2014-01-27 17:48:23 -0800

    ShadowLocaleData defaults to original behavior for locales other than US.

--- src/main/java/org/robolectric/shadows/ShadowLocaleData.java
+++ src/main/java/org/robolectric/shadows/ShadowLocaleData.java
@@ -17,64 +17,70 @@ public class ShadowLocaleData {
   public static Object get(Locale locale) {
     LocaleData localeData = (LocaleData) newInstanceOf(REAL_CLASS_NAME);
 
-    localeData.amPm = new String[]{"AM", "PM"};
-    localeData.eras = new String[]{"BC", "AD"};
+    if (locale == null) {
+      locale = Locale.getDefault();
+    }
 
-    localeData.firstDayOfWeek = 1;
-    localeData.minimalDaysInFirstWeek = 1;
+    if (locale.equals(Locale.US)) {
+      localeData.amPm = new String[]{"AM", "PM"};
+      localeData.eras = new String[]{"BC", "AD"};
 
-    localeData.longMonthNames = new String[]{"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
-    localeData.shortMonthNames = new String[]{"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
-    localeData.tinyMonthNames = new String[]{"J", "F", "M", "A", "M", "J", "J", "A", "S", "O", "N", "D"};
+      localeData.firstDayOfWeek = 1;
+      localeData.minimalDaysInFirstWeek = 1;
 
-    localeData.longStandAloneMonthNames = localeData.longMonthNames;
-    localeData.shortStandAloneMonthNames = localeData.shortMonthNames;
-    localeData.tinyStandAloneMonthNames = localeData.tinyMonthNames;
+      localeData.longMonthNames = new String[]{"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+      localeData.shortMonthNames = new String[]{"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+      localeData.tinyMonthNames = new String[]{"J", "F", "M", "A", "M", "J", "J", "A", "S", "O", "N", "D"};
 
-    localeData.longWeekdayNames = new String[]{"", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-    localeData.shortWeekdayNames = new String[]{"", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
-    localeData.tinyWeekdayNames = new String[]{"", "S", "M", "T", "W", "T", "F", "S"};
+      localeData.longStandAloneMonthNames = localeData.longMonthNames;
+      localeData.shortStandAloneMonthNames = localeData.shortMonthNames;
+      localeData.tinyStandAloneMonthNames = localeData.tinyMonthNames;
 
-    localeData.longStandAloneWeekdayNames = localeData.longWeekdayNames;
-    localeData.shortStandAloneWeekdayNames = localeData.shortWeekdayNames;
-    localeData.tinyStandAloneWeekdayNames = localeData.tinyWeekdayNames;
+      localeData.longWeekdayNames = new String[]{"", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+      localeData.shortWeekdayNames = new String[]{"", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+      localeData.tinyWeekdayNames = new String[]{"", "S", "M", "T", "W", "T", "F", "S"};
 
-    localeData.yesterday = "Yesterday";
-    localeData.today = "Today";
-    localeData.tomorrow = "Tomorrow";
+      localeData.longStandAloneWeekdayNames = localeData.longWeekdayNames;
+      localeData.shortStandAloneWeekdayNames = localeData.shortWeekdayNames;
+      localeData.tinyStandAloneWeekdayNames = localeData.tinyWeekdayNames;
 
-    localeData.fullTimeFormat = "h:mm:ss a zzzz";
-    localeData.longTimeFormat = "h:mm:ss a z";
-    localeData.mediumTimeFormat = "h:mm:ss a";
-    localeData.shortTimeFormat = "h:mm a";
-    localeData.timeFormat12 = "h:mm a";
-    localeData.timeFormat24 = "HH:mm";
+      localeData.yesterday = "Yesterday";
+      localeData.today = "Today";
+      localeData.tomorrow = "Tomorrow";
 
-    localeData.fullDateFormat = "EEEE, MMMM d, y";
-    localeData.longDateFormat = "MMMM d, y";
-    localeData.mediumDateFormat = "MMM d, y";
-    localeData.shortDateFormat = "M/d/yy";
+      localeData.fullTimeFormat = "h:mm:ss a zzzz";
+      localeData.longTimeFormat = "h:mm:ss a z";
+      localeData.mediumTimeFormat = "h:mm:ss a";
+      localeData.shortTimeFormat = "h:mm a";
+      localeData.timeFormat12 = "h:mm a";
+      localeData.timeFormat24 = "HH:mm";
 
-    localeData.zeroDigit = '0';
-    localeData.decimalSeparator = '.';
-    localeData.groupingSeparator = ',';
-    localeData.patternSeparator = ';';
-    localeData.percent = '%';
-    localeData.perMill = '‰';
-    localeData.monetarySeparator = '.';
-    localeData.minusSign = '-';
+      localeData.fullDateFormat = "EEEE, MMMM d, y";
+      localeData.longDateFormat = "MMMM d, y";
+      localeData.mediumDateFormat = "MMM d, y";
+      localeData.shortDateFormat = "M/d/yy";
 
-    localeData.exponentSeparator = "E";
-    localeData.infinity = "∞";
-    localeData.NaN = "NaN";
+      localeData.zeroDigit = '0';
+      localeData.decimalSeparator = '.';
+      localeData.groupingSeparator = ',';
+      localeData.patternSeparator = ';';
+      localeData.percent = '%';
+      localeData.perMill = '‰';
+      localeData.monetarySeparator = '.';
+      localeData.minusSign = '-';
 
-    localeData.currencySymbol = "$";
-    localeData.internationalCurrencySymbol = "USD";
+      localeData.exponentSeparator = "E";
+      localeData.infinity = "∞";
+      localeData.NaN = "NaN";
 
-    localeData.numberPattern = "#,##0.###";
-    localeData.integerPattern = "#,##0";
-    localeData.currencyPattern = "¤#,##0.00;(¤#,##0.00)";
-    localeData.percentPattern = "#,##0%";
+      localeData.currencySymbol = "$";
+      localeData.internationalCurrencySymbol = "USD";
+
+      localeData.numberPattern = "#,##0.###";
+      localeData.integerPattern = "#,##0";
+      localeData.currencyPattern = "¤#,##0.00;(¤#,##0.00)";
+      localeData.percentPattern = "#,##0%";
+    }
 
     return localeData;
   }
