52d7ee7fbe259ac0d20d853302fcd0b5e34b9b26
David Kirchner <dpk@dpk.net>
2014-02-03 18:04:58 -0600

    Implement OnSharedPreferenceChangeListener callbacks on commit.
    Closes #946.

--- src/main/java/org/robolectric/tester/android/content/TestSharedPreferences.java
+++ src/main/java/org/robolectric/tester/android/content/TestSharedPreferences.java
@@ -5,6 +5,7 @@ import android.content.SharedPreferences;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -160,19 +161,28 @@ public class TestSharedPreferences implements SharedPreferences {
     @Override
     public boolean commit() {
       Map<String, Object> previousContent = content.get(filename);
+      List<String> keysToPassToListeners = new ArrayList<String>();
+
       if (shouldClearOnCommit) {
         previousContent.clear();
       } else {
-        for (String key : editsThatNeedCommit.keySet()) {
-          previousContent.put(key, editsThatNeedCommit.get(key));
-        }
         for (String key : editsThatNeedRemove) {
           previousContent.remove(key);
+          keysToPassToListeners.add(key);
         }
       }
 
       for (String key : editsThatNeedCommit.keySet()) {
-        previousContent.put(key, editsThatNeedCommit.get(key));
+        if (!editsThatNeedCommit.get(key).equals(previousContent.get(key))) {
+          previousContent.put(key, editsThatNeedCommit.get(key));
+          keysToPassToListeners.add(key);
+        }
+      }
+
+      for (OnSharedPreferenceChangeListener listener : listeners) {
+        for (String key : keysToPassToListeners) {
+          listener.onSharedPreferenceChanged(TestSharedPreferences.this, key);
+        }
       }
 
       return true;
