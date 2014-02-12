9f0bb39a2548d48d719337be8dd7e49f1ea18979
Rick Carragher <rcarragher@gmail.com>
2014-02-12 09:04:24 -0500

    Implement Service.stopSelf(id).
    Fixes #959, Closes #960.

--- src/test/java/org/robolectric/shadows/ServiceTest.java
+++ src/test/java/org/robolectric/shadows/ServiceTest.java
@@ -12,54 +12,58 @@ import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.TestRunners;
 
-import static org.fest.assertions.api.Assertions.assertThat;
-import static org.robolectric.Robolectric.newInstanceOf;
-import static org.robolectric.Robolectric.shadowOf;
+import static org.robolectric.Robolectric.*;
+import static org.fest.assertions.api.Assertions.*;
 
 @RunWith(TestRunners.WithDefaults.class)
 public class ServiceTest {
+  private final MyService service = new MyService();
+  private final ShadowService shadow = shadowOf(service);
 
   @Test(expected = IllegalStateException.class)
   public void shouldComplainIfServiceIsDestroyedWithRegisteredBroadcastReceivers() throws Exception {
-    MyService service = new MyService();
     service.registerReceiver(new AppWidgetProvider(), new IntentFilter());
     service.onDestroy();
   }
 
   @Test
   public void shouldNotComplainIfServiceIsDestroyedWhileAnotherServiceHasRegisteredBroadcastReceivers() throws Exception {
-    MyService service = new MyService();
-
+    MyService service1 = new MyService();
     MyService service2 = new MyService();
     service2.registerReceiver(new AppWidgetProvider(), new IntentFilter());
-
-    service.onDestroy(); // should not throw exception
+    service1.onDestroy(); // should not throw exception
   }
 
   @Test
   public void shouldUnbindServiceSuccessfully() {
-    MyService service = new MyService();
     ServiceConnection conn = Robolectric.newInstanceOf(MediaScannerConnection.class);
     service.unbindService(conn);
   }
 
   @Test (expected=IllegalArgumentException.class)
   public void shouldUnbindServiceWithExceptionWhenRequested() {
-    MyService service = new MyService();
-    shadowOf(service).setUnbindServiceShouldThrowIllegalArgument(true);
-
+    shadow.setUnbindServiceShouldThrowIllegalArgument(true);
     ServiceConnection conn = newInstanceOf(MediaScannerConnection.class);
     service.unbindService(conn);
   }
 
   @Test
   public void stopForeground() {
-    MyService service = new MyService();
     service.stopForeground(true);
+    assertThat(shadow.isForegroundStopped()).isTrue();
+    assertThat(shadow.getNotificationShouldRemoved()).isTrue();
+  }
 
-    ShadowService shadowService = shadowOf(service);
-    assertThat(shadowService.isForegroundStopped()).isTrue();
-    assertThat(shadowService.getNotificationShouldRemoved()).isTrue();
+  @Test
+  public void shouldStopSelf() {
+    service.stopSelf();
+    assertThat(shadow.isStoppedBySelf()).isTrue();
+  }
+
+  @Test
+  public void shouldStopSelfWithId() {
+    service.stopSelf(1);
+    assertThat(shadow.isStoppedBySelf()).isTrue();
   }
 
   private static class MyService extends Service {
