3961be06e852ef80a4ae865061cc60bf23bf08f8
zofrex <zofrex@gmail.com>
2014-04-24 22:39:42 +0100

    Add method to ShadowPhoneWindow to get the indeterminate progress bar.
    Closes #1048.

--- src/test/java/org/robolectric/shadows/WindowTest.java
+++ src/test/java/org/robolectric/shadows/WindowTest.java
@@ -2,21 +2,19 @@ package org.robolectric.shadows;
 
 import android.R;
 import android.app.Activity;
-import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.TestRunners;
 
 import static org.fest.assertions.api.Assertions.assertThat;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 import static org.robolectric.Robolectric.shadowOf;
 
 @RunWith(TestRunners.WithDefaults.class)
@@ -26,11 +24,11 @@ public class WindowTest {
     Activity activity = Robolectric.buildActivity(Activity.class).create().get();
     Window window = activity.getWindow();
 
-    assertFalse(shadowOf(window).getFlag(WindowManager.LayoutParams.FLAG_FULLSCREEN));
+    assertThat(shadowOf(window).getFlag(WindowManager.LayoutParams.FLAG_FULLSCREEN)).isFalse();
     window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-    assertTrue(shadowOf(window).getFlag(WindowManager.LayoutParams.FLAG_FULLSCREEN));
+    assertThat(shadowOf(window).getFlag(WindowManager.LayoutParams.FLAG_FULLSCREEN)).isTrue();
     window.setFlags(WindowManager.LayoutParams.FLAG_ALLOW_LOCK_WHILE_SCREEN_ON, WindowManager.LayoutParams.FLAG_ALLOW_LOCK_WHILE_SCREEN_ON);
-    assertTrue(shadowOf(window).getFlag(WindowManager.LayoutParams.FLAG_FULLSCREEN));
+    assertThat(shadowOf(window).getFlag(WindowManager.LayoutParams.FLAG_FULLSCREEN)).isTrue();
   }
 
   @Test
@@ -38,7 +36,7 @@ public class WindowTest {
     Activity activity = Robolectric.buildActivity(Activity.class).create().get();
     Window window = activity.getWindow();
     window.setTitle("My Window Title");
-    assertEquals("My Window Title", shadowOf(window).getTitle());
+    assertThat(shadowOf(window).getTitle()).isEqualTo("My Window Title");
   }
 
   @Test
@@ -77,12 +75,25 @@ public class WindowTest {
     assertThat(shadowWindow.getSoftInputMode()).isEqualTo(7);
   }
 
+  @Test
+  public void getIndeterminateProgressBar_returnsTheIndeterminateProgressBar() {
+    Activity activity = Robolectric.buildActivity(TestActivity.class).create().get();
+    ProgressBar indeterminate = shadowOf(activity.getWindow()).getIndeterminateProgressBar();
+
+    assertThat(indeterminate.getVisibility()).isEqualTo(View.INVISIBLE);
+    activity.setProgressBarIndeterminateVisibility(true);
+    assertThat(indeterminate.getVisibility()).isEqualTo(View.VISIBLE);
+    activity.setProgressBarIndeterminateVisibility(false);
+    assertThat(indeterminate.getVisibility()).isEqualTo(View.GONE);
+  }
+
   public static class TestActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       setTheme(R.style.Theme_Holo_Light);
+      getWindow().requestFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
       setContentView(new LinearLayout(this));
       getActionBar().setIcon(R.drawable.ic_lock_power_off);
     }
