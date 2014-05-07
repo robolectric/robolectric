1cfca1574b1bea64f6e24b0bc69125c67b4486c8
Christian Becker <christian.becker@qnips.com>
2014-05-07 11:15:32 +0200

    Implement ClipboardManager.getPrimaryClip.
    Fixes #1060. Closes #1066.

--- src/test/java/org/robolectric/shadows/ClipboardManagerTest.java
+++ src/test/java/org/robolectric/shadows/ClipboardManagerTest.java
@@ -1,5 +1,6 @@
 package org.robolectric.shadows;
 
+import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
 import org.junit.Before;
@@ -48,6 +49,37 @@ public class ClipboardManagerTest {
   @Test
   public void shouldHaveTextIfString() throws Exception {
     clipboardManager.setText("BLARG");
+    assertTrue(clipboardManager.hasText());
+  }
+
+  @Test
+  public void shouldStorePrimaryClip() {
+    ClipData clip = ClipData.newPlainText(null, "BLARG?");
+    clipboardManager.setPrimaryClip(clip);
+    assertThat(clipboardManager.getPrimaryClip()).isEqualTo(clip);
+  }
+
+  @Test
+  public void shouldNotHaveTextIfPrimaryClipIsNull() throws Exception {
+    clipboardManager.setPrimaryClip(null);
+    assertFalse(clipboardManager.hasText());
+  }
+
+  @Test
+  public void shouldNotHaveTextIfPrimaryClipIsEmpty() throws Exception {
+    clipboardManager.setPrimaryClip(ClipData.newPlainText(null, ""));
+    assertFalse(clipboardManager.hasText());
+  }
+
+  @Test
+  public void shouldHaveTextIfEmptyPrimaryClip() throws Exception {
+    clipboardManager.setPrimaryClip(ClipData.newPlainText(null, " "));
+    assertTrue(clipboardManager.hasText());
+  }
+
+  @Test
+  public void shouldHaveTextIfPrimaryClip() {
+    clipboardManager.setPrimaryClip(ClipData.newPlainText(null, "BLARG?"));
     assertTrue(clipboardManager.hasText());
   }
 }
