e19ae9a94ab9586d802427cdb4fb95274c5abc1f
Christian Becker <christian.becker@qnips.com>
2014-05-07 11:15:32 +0200

    ShadowClipboardManager: Shadow PrimaryClip
    This fixes #1060.

--- src/main/java/org/robolectric/shadows/ShadowClipboardManager.java
+++ src/main/java/org/robolectric/shadows/ShadowClipboardManager.java
@@ -1,25 +1,49 @@
 package org.robolectric.shadows;
 
+import android.content.ClipData;
+import android.content.ClipDescription;
 import android.content.ClipboardManager;
 import org.robolectric.annotation.Implementation;
 import org.robolectric.annotation.Implements;
+import org.robolectric.annotation.RealObject;
 
+import static org.robolectric.Robolectric.directlyOn;
+
+@SuppressWarnings("UnusedDeclaration")
 @Implements(ClipboardManager.class)
 public class ShadowClipboardManager {
-  private CharSequence text;
+
+  @RealObject
+  private ClipboardManager realClipboardManager;
+
+  private ClipData clip;
 
   @Implementation
-  public void setText(CharSequence text) {
-    this.text = text;
+  public void setPrimaryClip(ClipData clip) {
+    if (clip != null) {
+       clip.prepareToLeaveProcess();
+    }
+    this.clip = clip;
   }
 
   @Implementation
-  public CharSequence getText() {
-    return text;
+  public ClipData getPrimaryClip() {
+    return clip;
+  }
+
+  @Implementation
+  public ClipDescription getPrimaryClipDescription() {
+    return clip == null ? null : clip.getDescription();
+  }
+
+  @Implementation
+  public boolean hasPrimaryClip() {
+    return clip != null;
   }
 
   @Implementation
   public boolean hasText() {
+    CharSequence text = directlyOn(realClipboardManager, ClipboardManager.class).getText();
     return text != null && text.length() > 0;
   }
 }
