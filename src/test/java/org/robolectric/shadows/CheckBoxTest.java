ce69d6651db2acab7f6aa980ee9f9078cadb46d2
Eric Haugh <erichaugh@gmail.com>
2014-06-03 13:40:38 -0700

    Revert commit 7742bba482e514f5abcec066c6336dbd00ecd916.

--- src/test/java/org/robolectric/shadows/CheckBoxTest.java
+++ src/test/java/org/robolectric/shadows/CheckBoxTest.java
@@ -21,10 +21,7 @@ public class CheckBoxTest {
     checkBox.toggle();
     assertThat(checkBox.isChecked()).isFalse();
 
-    checkBox.performClick();
-    assertThat(checkBox.isChecked()).isTrue();
-
-    checkBox.performClick();
-    assertThat(checkBox.isChecked()).isFalse();
+    checkBox.performClick();  // Used to support performClick(), but Android doesn't. Sigh.
+//        assertThat(checkBox.isChecked()).isFalse();
   }
 }
