e48f0ac5dd2a270e02b2763e9f8e492ad5014d7f
Fr Jeremy Krieg <fr.jeremy@stnectarios.org.au>
2014-06-12 13:41:01 +0930

    ShadowNotification enhancements.
    Enhanced ShadowNotification to capture style, progress and usesChronometer
    setting. Added ShadowBigTextStyle subclass to capture the bigText,
    bigContentTitle and summaryText data. Imported Notification's inner classes
    to make code a bit more compact and readable.
    
    Closes #1146.

--- src/test/java/org/robolectric/shadows/NotificationTest.java
+++ src/test/java/org/robolectric/shadows/NotificationTest.java
@@ -1,22 +1,24 @@
 package org.robolectric.shadows;
 
-import android.app.Activity;
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.robolectric.Robolectric.application;
+
 import android.app.Notification;
 import android.app.PendingIntent;
 import android.content.Intent;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.TestRunners;
 
-import static org.junit.Assert.assertSame;
-
 @RunWith(TestRunners.WithDefaults.class)
 public class NotificationTest {
+
   @Test
   public void setLatestEventInfo__shouldCaptureContentIntent() throws Exception {
-    PendingIntent pendingIntent = PendingIntent.getActivity(new Activity(), 0, new Intent(), 0);
+    PendingIntent pendingIntent = PendingIntent.getActivity(application, 0, new Intent(), 0);
     Notification notification = new Notification();
-    notification.setLatestEventInfo(new Activity(), "title", "content", pendingIntent);
-    assertSame(pendingIntent, notification.contentIntent);
+    notification.setLatestEventInfo(application, "title", "content", pendingIntent);
+    assertThat(notification.contentIntent).isSameAs(pendingIntent);
   }
 }
