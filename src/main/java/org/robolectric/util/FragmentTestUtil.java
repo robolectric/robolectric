a429827497faa6dbcb7839908da1ed2241fe274e
Callum Stott <callum@seadowg.com>
2014-06-30 17:25:00 -0700

    Clean up duplication

--- src/main/java/org/robolectric/util/FragmentTestUtil.java
+++ src/main/java/org/robolectric/util/FragmentTestUtil.java
@@ -12,42 +12,32 @@ import org.robolectric.Robolectric;
 public final class FragmentTestUtil {
   
   public static void startFragment(Fragment fragment) {
-    Activity activity = Robolectric.setupActivity(Activity.class);
-    FragmentManager fragmentManager = activity.getFragmentManager();
-    fragmentManager.beginTransaction().add(fragment, null).commit();
+    buildFragmentManager().beginTransaction().add(fragment, null).commit();
   }
   
   public static void startFragment(android.support.v4.app.Fragment fragment) {
-    FragmentActivity activity = Robolectric.setupActivity(FragmentActivity.class);
-
-    android.support.v4.app.FragmentManager fragmentManager = activity.getSupportFragmentManager();
-    fragmentManager.beginTransaction().add(fragment, null).commit();
+    buildSupportFragmentManager().beginTransaction().add(fragment, null).commit();
   }
 
   public static void startVisibleFragment(Fragment fragment) {
-    Activity activity = Robolectric.setupActivity(VisibleFragmentActivity.class);
-    FragmentManager fragmentManager = activity.getFragmentManager();
-    fragmentManager.beginTransaction().add(1, fragment, null).commit();
+    buildFragmentManager().beginTransaction().add(1, fragment, null).commit();
   }
 
   public static void startVisibleFragment(android.support.v4.app.Fragment fragment) {
-    VisibleSupportFragmentActivity activity = Robolectric.setupActivity(VisibleSupportFragmentActivity.class);
-    android.support.v4.app.FragmentManager fragmentManager = activity.getSupportFragmentManager();
-    fragmentManager.beginTransaction().add(1, fragment, null).commit();
+    buildSupportFragmentManager().beginTransaction().add(1, fragment, null).commit();
   }
 
-  private static class VisibleFragmentActivity extends Activity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-      super.onCreate(savedInstanceState);
-      LinearLayout view = new LinearLayout(this);
-      view.setId(1);
-
-      setContentView(view);
-    }
+  private static FragmentManager buildFragmentManager() {
+    Activity activity = Robolectric.setupActivity(FragmentUtilActivity.class);
+    return activity.getFragmentManager();
   }
 
-  private static class VisibleSupportFragmentActivity extends FragmentActivity {
+  private static android.support.v4.app.FragmentManager buildSupportFragmentManager() {
+    FragmentActivity activity = Robolectric.setupActivity(FragmentUtilActivity.class);
+    return activity.getSupportFragmentManager();
+  }
+
+  private static class FragmentUtilActivity extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
