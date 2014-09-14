0d6c034d266f52dd9646a73260c643ad34306abb
jacob-tabak <jacob@tabak.me>
2014-09-13 20:26:06 -0500

    Adds getFrameAtTime() support to ShadowMediametaDataRetriever

--- src/main/java/org/robolectric/shadows/ShadowMediaMetadataRetriever.java
+++ src/main/java/org/robolectric/shadows/ShadowMediaMetadataRetriever.java
@@ -1,28 +1,56 @@
 package org.robolectric.shadows;
 
-import java.util.Map;
-import java.util.HashMap;
-import com.google.android.collect.Maps;
+import android.content.Context;
+import android.graphics.Bitmap;
 import android.media.MediaMetadataRetriever;
-import org.robolectric.annotation.Implements;
+import android.net.Uri;
+
+import com.google.android.collect.Maps;
+
 import org.robolectric.annotation.Implementation;
+import org.robolectric.annotation.Implements;
+
+import java.io.FileDescriptor;
+import java.util.HashMap;
+import java.util.Map;
 
 @Implements(value = MediaMetadataRetriever.class, resetStaticState = true)
 public class ShadowMediaMetadataRetriever {
-  private String path;
+  private String dataSource;
   private static final Map<String, HashMap<Integer, String>> metadata = Maps.newHashMap();
+  private static final Map<String, HashMap<Long, Bitmap>> frames= Maps.newHashMap();
 
   @Implementation
   public void setDataSource(String path) {
-    this.path = path;
+    this.dataSource = path;
+  }
+
+  @Implementation
+  public void setDataSource(Context context, Uri uri) {
+    dataSource = uri.toString();
+  }
+
+  @Implementation
+  public void setDataSource(String uri, Map<String, String> headers) {
+    dataSource = uri;
+  }
+
+  @Implementation
+  public void setDataSource(FileDescriptor fd, long offset, long length) {
+    dataSource = fd.toString() + offset;
   }
 
   @Implementation
   public String extractMetadata(int keyCode) {
-    if (metadata.containsKey(path)) {
-      return metadata.get(path).get(keyCode);
+    if (metadata.containsKey(dataSource)) {
+      return metadata.get(dataSource).get(keyCode);
     }
     return null;
+  }
+
+  @Implementation
+  public Bitmap getFrameAtTime(long timeUs, int option) {
+    return frames.get(dataSource).get(timeUs);
   }
 
   public static void addMetadata(String path, int keyCode, String value) {
@@ -32,7 +60,40 @@ public class ShadowMediaMetadataRetriever {
     metadata.get(path).put(keyCode, value);
   }
 
+  public static void addFrame(String path, long time, Bitmap bitmap) {
+    if (!frames.containsKey(path)) {
+      frames.put(path, new HashMap<Long, Bitmap>());
+    }
+    frames.get(path).put(time, bitmap);
+  }
+
+  public static void addFrame(Context context, Uri uri, long time, Bitmap bitmap) {
+    String uriString = uri.toString();
+    if (!frames.containsKey(uriString)) {
+      frames.put(uriString, new HashMap<Long, Bitmap>());
+    }
+    frames.get(uriString).put(time, bitmap);
+  }
+
+  public static void addFrame(String uri, Map<String, String> headers, long time, Bitmap bitmap) {
+    addFrame(null, Uri.parse(uri), time, bitmap);
+  }
+
+  public static void addFrame(FileDescriptor fd, long time, Bitmap bitmap) {
+    addFrame(fd, 0, 0, time, bitmap);
+  }
+
+  public static void addFrame(FileDescriptor fd, long offset, long length,
+                              long time, Bitmap bitmap) {
+    String dataSource = fd.toString() + offset;
+    if (!frames.containsKey(dataSource)) {
+      frames.put(dataSource, new HashMap<Long, Bitmap>());
+    }
+    frames.get(dataSource).put(time, bitmap);
+  }
+
   public static void reset() {
     metadata.clear();
+    frames.clear();
   }
 }
--- src/test/java/org/robolectric/shadows/MediaMetadataRetrieverTest.java
+++ src/test/java/org/robolectric/shadows/MediaMetadataRetrieverTest.java
@@ -1,17 +1,35 @@
 package org.robolectric.shadows;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.media.MediaMetadataRetriever;
+import android.net.Uri;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
 import org.robolectric.TestRunners;
-import android.media.MediaMetadataRetriever;
-import static org.fest.assertions.api.Assertions.*;
-import static android.media.MediaMetadataRetriever.*;
-import static org.robolectric.shadows.ShadowMediaMetadataRetriever.*;
+
+import java.io.FileDescriptor;
+import java.util.HashMap;
+import java.util.Map;
+
+import static android.media.MediaMetadataRetriever.METADATA_KEY_ALBUM;
+import static android.media.MediaMetadataRetriever.METADATA_KEY_ARTIST;
+import static android.media.MediaMetadataRetriever.METADATA_KEY_TITLE;
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.robolectric.shadows.ShadowMediaMetadataRetriever.addFrame;
+import static org.robolectric.shadows.ShadowMediaMetadataRetriever.addMetadata;
 
 @RunWith(TestRunners.WithDefaults.class)
 public class MediaMetadataRetrieverTest {
   private final String path = "/media/foo.mp3";
+  private final String path2 = "/media/foo2.mp3";
   private final MediaMetadataRetriever retriever = new MediaMetadataRetriever();
+  private final MediaMetadataRetriever retriever2 = new MediaMetadataRetriever();
+  private final Bitmap bitmap = Bitmap.createBitmap(10, 10, Bitmap.Config.ARGB_8888);
+  private final Bitmap bitmap2 = Bitmap.createBitmap(11, 11, Bitmap.Config.ARGB_8888);
+  private FileDescriptor fd = new FileDescriptor();
 
   @Test
   public void extractMetadata_shouldReturnValue() {
@@ -23,6 +41,67 @@ public class MediaMetadataRetrieverTest {
     assertThat(retriever.extractMetadata(METADATA_KEY_ARTIST)).isEqualTo("The Rolling Stones");
     assertThat(retriever.extractMetadata(METADATA_KEY_ALBUM)).isEqualTo("Sticky Fingers");
     assertThat(retriever.extractMetadata(METADATA_KEY_TITLE)).isEqualTo("Brown Sugar");
+  }
+
+  @Test
+  public void getFrameAtTime_shouldDependOnDataSource() {
+    addFrame(path, 1, bitmap);
+    addFrame(path2, 1, bitmap2);
+    retriever.setDataSource(path);
+    retriever2.setDataSource(path2);
+    assertThat(retriever.getFrameAtTime(1)).isEqualTo(bitmap);
+    assertThat(retriever.getFrameAtTime(1)).isNotEqualTo(bitmap2);
+    assertThat(retriever2.getFrameAtTime(1)).isEqualTo(bitmap2);
+    assertThat(retriever2.getFrameAtTime(1)).isNotEqualTo(bitmap);
+  }
+
+  @Test
+  public void setDataSource_usersSameDataSourceForFileDescriptors() {
+    addFrame(fd, 1, bitmap);
+    addFrame(fd, 0, 0, 1, bitmap2);
+    retriever.setDataSource(fd);
+    assertThat(retriever.getFrameAtTime(1)).isEqualTo(bitmap2);
+  }
+
+  @Test
+  public void setDataSource_fdsWithDifferentOffsetsAreDifferentDataSources() {
+    addFrame(fd, 1, bitmap);
+    addFrame(fd, 1, 0, 1, bitmap2);
+    retriever.setDataSource(fd);
+    retriever2.setDataSource(fd, 1, 0);
+    assertThat(retriever.getFrameAtTime(1)).isEqualTo(bitmap);
+    assertThat(retriever.getFrameAtTime(1)).isNotEqualTo(bitmap2);
+    assertThat(retriever2.getFrameAtTime(1)).isEqualTo(bitmap2);
+    assertThat(retriever2.getFrameAtTime(1)).isNotEqualTo(bitmap);
+  }
+
+  @Test
+  public void getFrameAtTime_shouldDependOnTime() {
+    Context context = Robolectric.application;
+    Uri uri = Uri.parse(path);
+    addFrame(context, uri, 12, bitmap);
+    addFrame(context, uri, 13, bitmap2);
+    retriever.setDataSource(context, uri);
+    assertThat(retriever.getFrameAtTime(12)).isEqualTo(bitmap);
+    assertThat(retriever.getFrameAtTime(13)).isNotEqualTo(bitmap);
+    assertThat(retriever.getFrameAtTime(12)).isNotEqualTo(bitmap2);
+    assertThat(retriever.getFrameAtTime(13)).isEqualTo(bitmap2);
+  }
+
+  @Test
+  public void setDataSource_ignoresHeadersWhenShadowed() {
+    Context context = Robolectric.application;
+    Uri uri = Uri.parse(path);
+    Map<String, String> headers = new HashMap<String, String>();
+    headers.put("cookie", "nomnomnom");
+    retriever.setDataSource(context, uri);
+    retriever2.setDataSource(uri.toString(), headers);
+    addFrame(context, uri, 10, bitmap);
+    addFrame(uri.toString(), headers, 13, bitmap2);
+    assertThat(retriever.getFrameAtTime(10)).isEqualTo(bitmap);
+    assertThat(retriever.getFrameAtTime(13)).isEqualTo(bitmap2);
+    assertThat(retriever2.getFrameAtTime(13)).isEqualTo(bitmap2);
+    assertThat(retriever2.getFrameAtTime(10)).isEqualTo(bitmap);
   }
 
   @Test
