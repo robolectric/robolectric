563264262e7578308e1e8666a87d033ebabca469
Jonathan Gerrish <jonathan@indiekid.org>
2014-10-09 11:13:27 -0700

    Remove unused import.

--- src/main/java/org/robolectric/AndroidManifest.java
+++ src/main/java/org/robolectric/AndroidManifest.java
@@ -19,7 +19,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
