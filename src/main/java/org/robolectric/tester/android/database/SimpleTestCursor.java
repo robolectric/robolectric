0bb1e285dd0b32064d4f4526ccd7773fbca03403
jacob-tabak <jacob@tabak.me>
2014-10-10 22:22:40 -0500

    Added missing functionality to SimpleTestCursor

--- src/main/java/org/robolectric/tester/android/database/SimpleTestCursor.java
+++ src/main/java/org/robolectric/tester/android/database/SimpleTestCursor.java
@@ -40,13 +40,63 @@ public class SimpleTestCursor extends TestCursor {
   }
 
   @Override
+  public int getColumnCount() {
+    return columnNames.size();
+  }
+
+  @Override
+  public String getColumnName(int columnIndex) {
+    return columnNames.get(columnIndex);
+  }
+
+  @Override
   public String getString(int columnIndex) {
     return (String) results[resultsIndex][columnIndex];
   }
 
   @Override
+  public int getInt(int columnIndex) {
+    return (Integer) results[resultsIndex][columnIndex];
+  }
+
+  @Override
   public long getLong(int columnIndex) {
     return (Long) results[resultsIndex][columnIndex];
+  }
+
+  @Override
+  public short getShort(int columnIndex) {
+    return (Short) results[resultsIndex][columnIndex];
+  }
+
+  @Override
+  public float getFloat(int columnIndex) {
+    return (Float) results[resultsIndex][columnIndex];
+  }
+
+  @Override
+  public double getDouble(int columnIndex) {
+    return (Double) results[resultsIndex][columnIndex];
+  }
+
+  @Override
+  public byte[] getBlob(int columnIndex) {
+    return (byte[]) results[resultsIndex][columnIndex];
+  }
+
+  @Override
+  public int getCount() {
+    return results.length;
+  }
+
+  @Override
+  public int getPosition() {
+    return resultsIndex;
+  }
+
+  @Override
+  public String[] getColumnNames() {
+    return columnNames.toArray(new String[columnNames.size()]);
   }
 
   @Override
@@ -60,11 +110,39 @@ public class SimpleTestCursor extends TestCursor {
     closeWasCalled = true;
   }
 
-  public void setColumnNames(ArrayList<String> columnNames) {
+  /**
+   * Use {@link #setResults(java.util.List, Object[][])} to set the column names and the data
+   * in an atomic operation.
+   * @param columnNames list of column names
+   */
+  @Deprecated
+  public void setColumnNames(List<String> columnNames) {
     this.columnNames = columnNames;
   }
 
+  /**
+   * Use {@link #setResults(java.util.List, Object[][])} to set the column names and the data
+   * in an atomic operation.
+   * @param results matrix of results
+   */
+  @Deprecated
   public void setResults(Object[][] results) {
+    this.results = results;
+  }
+
+  /**
+   * Sets the results and column names in an atomic operation, verifies columns count
+   * aligns with the data.
+   * @param columnNames list of column names
+   * @param results matrix of results
+   */
+  public void setResults(List<String> columnNames, Object[][] results) {
+    for (Object[] result : results) {
+      if (columnNames.size() != result.length) {
+        throw new IllegalArgumentException("Each row must have the correct number of columns");
+      }
+    }
+    this.columnNames = columnNames;
     this.results = results;
   }
 
