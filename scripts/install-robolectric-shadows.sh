5b8aa93c48a80e5b3417049118f6fe4a272a93ff
Aaron VonderHaar <gruen0aermel@gmail.com>
2014-11-26 10:19:55 -0800

    Fix build order for install-robolectric-shadows.sh
    You no longer have to run the script twice to get correct results

--- scripts/install-robolectric-shadows.sh
+++ scripts/install-robolectric-shadows.sh
@@ -2,38 +2,17 @@
 
 set -e
 
-mvn clean
+PROJECT=$(cd $(dirname "$0")/..; pwd)
 
-#(
-#set -e
-#cd robolectric-shadows
-#mvn clean velocity:velocity javadoc:javadoc source:jar install -Pandroid-15  -DskipTests
-#)
-#
-#(
-#set -e
-#cd robolectric-shadows
-#mvn clean velocity:velocity javadoc:javadoc source:jar install -Pandroid-16  -DskipTests
-#)
-#
-#(
-#set -e
-#cd robolectric-shadows
-#mvn clean velocity:velocity javadoc:javadoc source:jar install -Pandroid-17  -DskipTests
-#)
-#
-#(
-#set -e
-#cd robolectric-shadows
-#mvn clean velocity:velocity javadoc:javadoc source:jar install -Pandroid-18  -DskipTests
-#)
-#
-#(
-#set -e
-#cd robolectric-shadows
-#mvn clean velocity:velocity javadoc:javadoc source:jar install -Pandroid-19  -DskipTests
-#)
+cd "$PROJECT"/robolectric-annotations; mvn clean install
+cd "$PROJECT"/robolectric-utils; mvn clean install
+cd "$PROJECT"/robolectric-resources; mvn clean install
 
-cd robolectric-shadows; mvn clean velocity:velocity javadoc:javadoc source:jar install -Pandroid-21 -DskipTests; cd -
+#cd "$PROJECT"/robolectric-shadows; mvn clean velocity:velocity javadoc:javadoc source:jar install -Pandroid-15 -DskipTests
+#cd "$PROJECT"/robolectric-shadows; mvn clean velocity:velocity javadoc:javadoc source:jar install -Pandroid-16 -DskipTests
+#cd "$PROJECT"/robolectric-shadows; mvn clean velocity:velocity javadoc:javadoc source:jar install -Pandroid-17 -DskipTests
+#cd "$PROJECT"/robolectric-shadows; mvn clean velocity:velocity javadoc:javadoc source:jar install -Pandroid-18 -DskipTests
+cd "$PROJECT"/robolectric-shadows; mvn clean velocity:velocity javadoc:javadoc source:jar install -Pandroid-19 -DskipTests
+cd "$PROJECT"/robolectric-shadows; mvn clean velocity:velocity javadoc:javadoc source:jar install -Pandroid-21 -DskipTests
 
-mvn javadoc:javadoc source:jar install
+#cd "$PROJECT"; mvn javadoc:javadoc source:jar install
