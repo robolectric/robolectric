752a5464b15ac23a8d15f066b53aa2dd4b0e84c4
Erich Douglass <erich.douglass@gmail.com>
2014-12-02 13:51:04 -0800

    Fix the build, yet again.

--- scripts/install-robolectric.sh
+++ scripts/install-robolectric.sh
@@ -4,25 +4,14 @@ set -e
 
 PROJECT=$(cd $(dirname "$0")/..; pwd)
 
-cd "$PROJECT"; mvn clean
-cd "$PROJECT"/robolectric-utils; mvn clean install
-cd "$PROJECT"/robolectric-annotations; mvn clean install
-cd "$PROJECT"/robolectric-processor; mvn clean install
-cd "$PROJECT"/robolectric-resources; mvn clean install
+# Build everything
+cd "$PROJECT"; mvn clean install -DskipTests
 
-# Build core shadows
+# Build older shadow packages
 cd "$PROJECT"/robolectric-shadows/shadows-core; mvn clean velocity:velocity install -Pandroid-15
 cd "$PROJECT"/robolectric-shadows/shadows-core; mvn clean velocity:velocity install -Pandroid-16
 cd "$PROJECT"/robolectric-shadows/shadows-core; mvn clean velocity:velocity install -Pandroid-17
 cd "$PROJECT"/robolectric-shadows/shadows-core; mvn clean velocity:velocity install -Pandroid-18
-cd "$PROJECT"/robolectric-shadows/shadows-core; mvn clean velocity:velocity install -Pandroid-19
 
-# Build robolectric (tests depend on shadows-core)
-cd "$PROJECT"/robolectric; mvn install -DskipTests
-
-# Build add-on shadows
-cd "$PROJECT"/robolectric-shadows/shadows-httpclient; mvn install
-cd "$PROJECT"/robolectric-shadows/shadows-support-v4; mvn install
-
-# Run the robolectric tests (and build docs / sources)
+# Build everything with tests (tests require the shadows)
 cd "$PROJECT"; mvn javadoc:javadoc source:jar install
