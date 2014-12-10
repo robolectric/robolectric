42ad425a9efec9d3b0fce7ad554ceb50b6c00ae8
Erich Douglass <erich.douglass@gmail.com>
2014-12-10 10:27:05 -0800

    Update the deploy snapshot script to deploy core shadows.

--- scripts/install-snapshot.sh
+++ scripts/install-snapshot.sh
@@ -1,9 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 #
 # Deploy a snapshot build to Sonatype.  Only non-pull requests will be deployed.
 #
 
+PROJECT=$(cd $(dirname "$0")/..; pwd)
+ARGUMENTS="--settings $PROJECT/scripts/mvn_settings.xml -DskipTests"
+
 echo "Pull request: '${TRAVIS_PULL_REQUEST}' on branch '${TRAVIS_BRANCH}'"
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]; then
-    mvn --settings scripts/mvn_settings.xml -DskipTests=true source:jar javadoc:jar deploy
+
+    # Install everything
+    cd "$PROJECT"; mvn ${ARGUMENTS} deploy
+
+    # Install older shadow packages
+    cd "$PROJECT"/robolectric-shadows/shadows-core; mvn ${ARGUMENTS} clean velocity:velocity deploy -Pandroid-15
+    cd "$PROJECT"/robolectric-shadows/shadows-core; mvn ${ARGUMENTS} clean velocity:velocity deploy -Pandroid-16
+    cd "$PROJECT"/robolectric-shadows/shadows-core; mvn ${ARGUMENTS} clean velocity:velocity deploy -Pandroid-17
+    cd "$PROJECT"/robolectric-shadows/shadows-core; mvn ${ARGUMENTS} clean velocity:velocity deploy -Pandroid-18
 fi
