3fa8f0582a44660d601ddede4a75fecbb1c9e4ec
Erich Douglass <erich.douglass@gmail.com>
2015-01-20 17:37:04 -0800

    Only deploy one of the SNAPSHOTS built by Travis.

--- scripts/install-snapshot.sh
+++ scripts/install-snapshot.sh
@@ -6,8 +6,8 @@
 PROJECT=$(cd $(dirname "$0")/..; pwd)
 ARGUMENTS="--settings $PROJECT/scripts/mvn_settings.xml -DskipTests"
 
-echo "Pull request: '${TRAVIS_PULL_REQUEST}' on branch '${TRAVIS_BRANCH}'"
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]; then
+echo "Pull request: '${TRAVIS_PULL_REQUEST}' on branch '${TRAVIS_BRANCH}' with JDK '${TRAVIS_JDK_VERSION}'"
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ] && [ "${TRAVIS_JDK_VERSION}" = "oraclejdk8" ]; then
 
     echo "Cleaning dist directories..."
     cd "$PROJECT"; mvn ${ARGUMENTS} clean -Pdist
