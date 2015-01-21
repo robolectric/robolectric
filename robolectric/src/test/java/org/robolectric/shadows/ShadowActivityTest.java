d1a50e242e439805ff778434bd7643c646c3fade
Erich Douglass <erich.douglass@gmail.com>
2015-01-20 16:11:32 -0800

    Revert "Merge pull request #1309 from jongerrish/android_manifest_no_application"
    This reverts commit 31e720d87f012dc7b2a153ca87efe2e9810c3540, reversing
    changes made to d4f1c6c70ac0f79080575d7ce6505d1c5851eb5c.
    
    Conflicts:
    	robolectric-resources/src/main/java/org/robolectric/manifest/AndroidManifest.java
    	robolectric/src/test/java/org/robolectric/AndroidManifestTest.java
    	robolectric/src/test/resources/TestAndroidManifestNoApplicationElement.xml

--- robolectric/src/test/java/org/robolectric/shadows/ShadowActivityTest.java
+++ robolectric/src/test/java/org/robolectric/shadows/ShadowActivityTest.java
@@ -795,7 +795,6 @@ public class ShadowActivityTest {
             "<manifest xmlns:android=\"http://schemas.android.com/apk/res/android\"\n" +
             "          package=\"" + packageName + "\">\n" +
             "    " + contents + "\n" +
-            "<application/>\n" +
             "</manifest>\n");
     return new AndroidManifest(Fs.newFile(f), null, null);
   }
