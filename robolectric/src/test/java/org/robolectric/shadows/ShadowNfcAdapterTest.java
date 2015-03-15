robolectric/robolectric

commit df981893683f704f4b8ea86a23cb4acdf4cc245b

Remove the ShadowNfcAdapter.getDefaultAdapter method. It is no longer needed as getSystemService can correctly instantiate the NfcManager (used within the real getDefaultAdapter method).

robolectric-shadows/shadows-core/src/main/java/org/robolectric/shadows/ShadowNfcAdapter.java
@@ -2,13 +2,11 @@ package org.robolectric.shadows;
 
 import android.app.Activity;
 import android.app.PendingIntent;
-import android.content.Context;
 import android.content.IntentFilter;
 import android.nfc.NfcAdapter;
 import org.robolectric.annotation.Implementation;
 import org.robolectric.annotation.Implements;
 import org.robolectric.annotation.RealObject;
-import org.robolectric.util.ReflectionHelpers;
 
 @Implements(NfcAdapter.class)
 public class ShadowNfcAdapter {
@@ -18,11 +16,6 @@ public class ShadowNfcAdapter {
   private IntentFilter[] filters;
   private String[][] techLists;
   private Activity disabledActivity;
-
-  @Implementation
-  public static NfcAdapter getDefaultAdapter(Context context) {
-    return ReflectionHelpers.callConstructor(NfcAdapter.class);
-  }
 
   @Implementation
   public void enableForegroundDispatch(Activity activity, PendingIntent intent, IntentFilter[] filters, String[][] techLists) {

robolectric-shadows/shadows-core/src/main/resources/org/robolectric/shadows/ShadowContextImpl.java.vm
@@ -58,6 +58,7 @@ public class ShadowContextImpl extends ShadowContext {
 #end
     SYSTEM_SERVICE_MAP.put(Context.ACCESSIBILITY_SERVICE, "android.view.accessibility.AccessibilityManager");
     SYSTEM_SERVICE_MAP.put(Context.ACCOUNT_SERVICE, "android.accounts.AccountManager");
+    SYSTEM_SERVICE_MAP.put(Context.NFC_SERVICE, "android.nfc.NfcManager");
   }
 
   private Map<String, Object> systemServices = new HashMap<String, Object>();

robolectric/src/test/java/org/robolectric/shadows/ShadowNfcAdapterTest.java
@@ -1,16 +1,0 @@
-package org.robolectric.shadows;
-
-import android.nfc.NfcAdapter;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.robolectric.TestRunners;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
-@RunWith(TestRunners.WithDefaults.class)
-public class ShadowNfcAdapterTest {
-  @Test
-  public void getDefaultAdapter_shouldReturnAnAdapter() throws Exception {
-    assertThat(NfcAdapter.getDefaultAdapter(null)).isInstanceOf(NfcAdapter.class);
-  }
-}

commit a402bc6a0c4444161da510f05d9f21e927f5f434

Fix bug introduced in df98189.
Add shadow for NfcAdapter.getNfcAdapter(). NfcManager needs calling
with a context as its constructor parameter otherwise it is not
initialized correctly.

Add support for NfcAdapter.setNdefPushMessageCallback() and a non-android
accessor so the callback can be executed in tests.

Closes #1614.

robolectric-shadows/shadows-core/src/main/java/org/robolectric/shadows/ShadowNfcAdapter.java
@@ -2,11 +2,13 @@ package org.robolectric.shadows;
 
 import android.app.Activity;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.IntentFilter;
 import android.nfc.NfcAdapter;
 import org.robolectric.annotation.Implementation;
 import org.robolectric.annotation.Implements;
 import org.robolectric.annotation.RealObject;
+import org.robolectric.util.ReflectionHelpers;
 
 @Implements(NfcAdapter.class)
 public class ShadowNfcAdapter {
@@ -16,6 +18,12 @@ public class ShadowNfcAdapter {
   private IntentFilter[] filters;
   private String[][] techLists;
   private Activity disabledActivity;
+  private NfcAdapter.CreateNdefMessageCallback callback;
+
+  @Implementation
+  public static NfcAdapter getNfcAdapter(Context context) {
+    return ReflectionHelpers.callConstructor(NfcAdapter.class);
+  }
 
   @Implementation
   public void enableForegroundDispatch(Activity activity, PendingIntent intent, IntentFilter[] filters, String[][] techLists) {
@@ -28,6 +36,11 @@ public class ShadowNfcAdapter {
   @Implementation
   public void disableForegroundDispatch(Activity activity) {
     disabledActivity = activity;
+  }
+
+  @Implementation
+  public void setNdefPushMessageCallback(NfcAdapter.CreateNdefMessageCallback callback, Activity activity, Activity... activities) {
+    this.callback = callback;
   }
 
   public Activity getEnabledActivity() {
@@ -48,5 +61,9 @@ public class ShadowNfcAdapter {
 
   public Activity getDisabledActivity() {
     return disabledActivity;
+  }
+
+  public NfcAdapter.CreateNdefMessageCallback getNdefPushMessageCallback() {
+    return callback;
   }
 }

robolectric-shadows/shadows-core/src/main/resources/org/robolectric/shadows/ShadowContextImpl.java.vm
@@ -100,6 +100,10 @@ public class ShadowContextImpl extends ShadowContext {
         } else if (serviceClassName.equals("android.os.storage.StorageManager")) {
           service = ReflectionHelpers.callConstructor(clazz);
 
+        } else if (serviceClassName.equals("android.nfc.NfcManager")) {
+          service = ReflectionHelpers.callConstructor(clazz,
+              ClassParameter.from(Context.class, RuntimeEnvironment.application));
+
         } else if (serviceClassName.equals("android.hardware.display.DisplayManager")) {
           service = ReflectionHelpers.callConstructor(clazz, ClassParameter.from(Context.class, RuntimeEnvironment.application));
 

robolectric/src/test/java/org/robolectric/shadows/ShadowNfcAdapterTest.java
@@ -1,0 +1,29 @@
+package org.robolectric.shadows;
+
+import android.app.Activity;
+import android.nfc.NfcAdapter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.TestRunners;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.robolectric.Shadows.shadowOf;
+
+@RunWith(TestRunners.WithDefaults.class)
+public class ShadowNfcAdapterTest {
+
+  @Test
+  public void testNdefPushMessageCallback() {
+    final NfcAdapter.CreateNdefMessageCallback callback = mock(NfcAdapter.CreateNdefMessageCallback.class);
+
+    final Activity activty = Robolectric.setupActivity(Activity.class);
+    NfcAdapter adapter = NfcAdapter.getDefaultAdapter(activty);
+    adapter.setNdefPushMessageCallback(callback, activty);
+
+    final ShadowNfcAdapter shadowNfcAdapter = shadowOf(adapter);
+    assertThat(shadowNfcAdapter.getNdefPushMessageCallback()).isSameAs(callback);
+  }
+}