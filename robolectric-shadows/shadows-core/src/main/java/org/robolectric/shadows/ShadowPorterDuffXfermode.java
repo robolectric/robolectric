dee6667eef43caeafce91986244f0d38da93a76d
Erich Douglass <erich.douglass@gmail.com>
2015-04-17 11:11:10 -0700

    Fixed NPE caused by PorterDuffColorFilter.setColor.
    Closes #1649

--- robolectric-shadows/shadows-core/src/main/java/org/robolectric/shadows/ShadowPorterDuffXfermode.java
+++
@@ -1,8 +0,0 @@
-package org.robolectric.shadows;
-
-import android.graphics.PorterDuffXfermode;
-import org.robolectric.annotation.Implements;
-
-@Implements(PorterDuffXfermode.class)
-public class ShadowPorterDuffXfermode {
-}
