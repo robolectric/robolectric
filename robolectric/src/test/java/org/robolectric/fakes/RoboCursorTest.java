c7fea1f4b6b2b5f27f31ec9fbba50a5d7577d724
Erich Douglass <erich.douglass@gmail.com>
2015-07-06 19:42:30 -0700

    Implement additional RoboCursor methods.
    Closes #1828.

--- robolectric/src/test/java/org/robolectric/fakes/RoboCursorTest.java
+++ robolectric/src/test/java/org/robolectric/fakes/RoboCursorTest.java
@@ -8,29 +8,24 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RuntimeEnvironment;
 import org.robolectric.TestRunners;
-import org.robolectric.fakes.RoboCursor;
-import org.robolectric.shadows.ShadowContentResolver;
 
 import java.util.ArrayList;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.fail;
 import static org.robolectric.Shadows.shadowOf;
 
 @RunWith(TestRunners.WithDefaults.class)
 public class RoboCursorTest {
-  private Uri uri;
-  private RoboCursor cursor;
+  private final Uri uri = Uri.parse("http://foo");
+  private final RoboCursor cursor = new RoboCursor();
   private ContentResolver contentResolver;
 
   @Before
   public void setup() throws Exception {
     contentResolver = RuntimeEnvironment.application.getContentResolver();
-    ShadowContentResolver shadowContentResolver = shadowOf(contentResolver);
-    uri = Uri.parse("http://foo");
-    cursor = new RoboCursor();
-    shadowContentResolver.setCursor(uri, cursor);
-    ArrayList<String> columnNames = new ArrayList<>();
+    shadowOf(contentResolver).setCursor(uri, cursor);
+
+    final ArrayList<String> columnNames = new ArrayList<>();
     columnNames.add("stringColumn");
     columnNames.add("longColumn");
     columnNames.add("intColumn");
@@ -38,7 +33,7 @@ public class RoboCursorTest {
   }
 
   @Test
-  public void doingQueryShouldMakeQueryParamsAvailable() throws Exception {
+  public void query_shouldMakeQueryParamsAvailable() throws Exception {
     contentResolver.query(uri, new String[]{"projection"}, "selection", new String[]{"selection"}, "sortOrder");
     assertThat(cursor.uri).isEqualTo(uri);
     assertThat(cursor.projection[0]).isEqualTo("projection");
@@ -48,7 +43,7 @@ public class RoboCursorTest {
   }
 
   @Test
-  public void canGetStringsAndLongs() throws Exception {
+  public void get_shouldReturnColumnValue() throws Exception {
     cursor.setResults(new Object[][]{new Object[]{"aString", 1234L, 42}});
     assertThat(cursor.getCount()).isEqualTo(1);
     assertThat(cursor.getColumnCount()).isEqualTo(3);
@@ -63,9 +58,9 @@ public class RoboCursorTest {
   }
 
   @Test
-  public void moveToNextAdvancesToNextRow() throws Exception {
-    cursor.setResults(new Object[][] { new Object[] { "aString", 1234L, 41 },
-            new Object[] { "anotherString", 5678L, 42 }});
+  public void moveToNext_advancesToNextRow() throws Exception {
+    cursor.setResults(new Object[][]{new Object[]{"aString", 1234L, 41}, new Object[]{"anotherString", 5678L, 42}});
+
     assertThat(cursor.getCount()).isEqualTo(2);
     assertThat(cursor.getColumnCount()).isEqualTo(3);
     assertThat(cursor.getType(0)).isEqualTo(Cursor.FIELD_TYPE_STRING);
@@ -80,9 +75,9 @@ public class RoboCursorTest {
   }
 
   @Test
-  public void moveToPositionMovesToAppropriateRow() throws Exception {
-    cursor.setResults(new Object[][] { new Object[] { "aString", 1234L, 41 },
-            new Object[] { "anotherString", 5678L, 42 }});
+  public void moveToPosition_movesToAppropriateRow() throws Exception {
+    cursor.setResults(new Object[][]{new Object[]{"aString", 1234L, 41}, new Object[]{"anotherString", 5678L, 42}});
+
     assertThat(cursor.moveToPosition(1)).isTrue();
     assertThat(cursor.getString(cursor.getColumnIndex("stringColumn"))).isEqualTo("anotherString");
     assertThat(cursor.getLong(cursor.getColumnIndex("longColumn"))).isEqualTo(5678L);
@@ -95,34 +90,73 @@ public class RoboCursorTest {
   }
 
   @Test
-  public void moveToPositionBoundsChecks() {
-    cursor.setResults(new Object[][] { new Object[] { "aString", 1234L, 41 },
-            new Object[] { "anotherString", 5678L, 42 }});
+  public void moveToPosition_checksBounds() {
+    cursor.setResults(new Object[][]{new Object[]{"aString", 1234L, 41}, new Object[]{"anotherString", 5678L, 42}});
     assertThat(cursor.moveToPosition(2)).isFalse();
     assertThat(cursor.moveToPosition(-1)).isFalse();
   }
 
   @Test
-  public void getCount() {
-    cursor.setResults(new Object[][] { new Object[] { "aString", 1234L, 41 },
-            new Object[] { "anotherString", 5678L, 42 }});
+  public void getCount_shouldReturnNumberOfRows() {
+    cursor.setResults(new Object[][]{new Object[]{"aString", 1234L, 41}, new Object[]{"anotherString", 5678L, 42}});
     assertThat(cursor.getCount()).isEqualTo(2);
   }
 
   @Test
-  public void closeIsRemembered() throws Exception {
+  public void close_isRemembered() throws Exception {
     cursor.close();
     assertThat(cursor.getCloseWasCalled()).isTrue();
   }
 
   @Test
-  public void getColumnIndex(){
+  public void getColumnIndex_shouldReturnColumnIndex() {
     assertThat(cursor.getColumnIndex("invalidColumn")).isEqualTo(-1);
     assertThat(cursor.getColumnIndex("stringColumn")).isEqualTo(0);
     assertThat(cursor.getColumnIndexOrThrow("stringColumn")).isEqualTo(0);
-    try{
-      cursor.getColumnIndexOrThrow("invalidColumn");
-      fail();
-    }catch (IllegalArgumentException ex){}
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void getColumnIndexOrThrow_shouldThrowException() {
+    cursor.getColumnIndexOrThrow("invalidColumn");
+  }
+
+  @Test
+  public void isBeforeFirst_shouldReturnTrueForPosition() {
+    cursor.setResults(new Object[][]{new Object[]{"Foo"}});
+
+    assertThat(cursor.isBeforeFirst()).isTrue();
+    cursor.moveToPosition(0);
+    assertThat(cursor.isBeforeFirst()).isFalse();
+  }
+
+  @Test
+  public void isAfterLast_shouldReturnTrueForPosition() {
+    cursor.setResults(new Object[][]{new Object[]{"Foo"}});
+
+    assertThat(cursor.isAfterLast()).isFalse();
+    cursor.moveToPosition(1);
+    assertThat(cursor.isAfterLast()).isTrue();
+  }
+
+  @Test
+  public void isFirst_shouldReturnTrueForPosition() {
+    cursor.setResults(new Object[][]{new Object[]{"Foo"}, new Object[]{"Bar"}});
+
+    cursor.moveToPosition(0);
+    assertThat(cursor.isFirst()).isTrue();
+
+    cursor.moveToPosition(1);
+    assertThat(cursor.isFirst()).isFalse();
+  }
+
+  @Test
+  public void isLast_shouldReturnTrueForPosition() {
+    cursor.setResults(new Object[][]{new Object[]{"Foo"}, new Object[]{"Bar"}});
+
+    cursor.moveToPosition(0);
+    assertThat(cursor.isLast()).isFalse();
+
+    cursor.moveToPosition(1);
+    assertThat(cursor.isLast()).isTrue();
   }
 }
