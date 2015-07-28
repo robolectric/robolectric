74546b0dab63ce7a8013130b37a7e78741eeb922
Andrew Lord <andrew.lord@ocado.com>
2015-07-28 10:36:19 +0100

    1959: Reformatted with 2 space indent.

--- robolectric-shadows/shadows-support-v4/src/main/java/org/robolectric/util/SupportFragmentController.java
+++ robolectric-shadows/shadows-support-v4/src/main/java/org/robolectric/util/SupportFragmentController.java
@@ -14,104 +14,104 @@ import org.robolectric.ShadowsAdapter;
  * should be sufficient.
  */
 public class SupportFragmentController<F extends Fragment> extends ComponentController<SupportFragmentController<F>, F> {
-    private final F fragment;
-    private final ActivityController<? extends FragmentActivity> activityController;
+  private final F fragment;
+  private final ActivityController<? extends FragmentActivity> activityController;
 
-    private SupportFragmentController(ShadowsAdapter shadowsAdapter, F fragment, Class<? extends FragmentActivity> activityClass) {
-        super(shadowsAdapter, fragment);
-        this.fragment = fragment;
-        this.activityController = Robolectric.buildActivity(activityClass);
-    }
+  private SupportFragmentController(ShadowsAdapter shadowsAdapter, F fragment, Class<? extends FragmentActivity> activityClass) {
+    super(shadowsAdapter, fragment);
+    this.fragment = fragment;
+    this.activityController = Robolectric.buildActivity(activityClass);
+  }
 
-    public static <F extends Fragment> SupportFragmentController<F> of(F fragment) {
-        return new SupportFragmentController<>(Robolectric.getShadowsAdapter(), fragment, FragmentControllerActivity.class);
-    }
+  public static <F extends Fragment> SupportFragmentController<F> of(F fragment) {
+    return new SupportFragmentController<>(Robolectric.getShadowsAdapter(), fragment, FragmentControllerActivity.class);
+  }
 
-    public static <F extends Fragment> SupportFragmentController<F> of(F fragment, Class<? extends FragmentActivity> activityClass) {
-        return new SupportFragmentController<>(Robolectric.getShadowsAdapter(), fragment, activityClass);
-    }
+  public static <F extends Fragment> SupportFragmentController<F> of(F fragment, Class<? extends FragmentActivity> activityClass) {
+    return new SupportFragmentController<>(Robolectric.getShadowsAdapter(), fragment, activityClass);
+  }
 
+  @Override
+  public SupportFragmentController<F> attach() {
+    activityController.attach();
+    return this;
+  }
+
+  public SupportFragmentController<F> create(final Bundle bundle) {
+    shadowMainLooper.runPaused(new Runnable() {
+      @Override
+      public void run() {
+        if (!attached) attach();
+        activityController.create(bundle).get().getSupportFragmentManager().beginTransaction().add(fragment, null).commit();
+      }
+    });
+    return this;
+  }
+
+  @Override
+  public SupportFragmentController<F> create() {
+    return create(null);
+  }
+
+  @Override
+  public SupportFragmentController<F> destroy() {
+    shadowMainLooper.runPaused(new Runnable() {
+      @Override
+      public void run() {
+        activityController.destroy();
+      }
+    });
+    return this;
+  }
+
+  public SupportFragmentController<F> start() {
+    shadowMainLooper.runPaused(new Runnable() {
+      @Override
+      public void run() {
+        activityController.start();
+      }
+    });
+    return this;
+  }
+
+  public SupportFragmentController<F> resume() {
+    shadowMainLooper.runPaused(new Runnable() {
+      @Override
+      public void run() {
+        activityController.resume();
+      }
+    });
+    return this;
+  }
+
+  public SupportFragmentController<F> pause() {
+    shadowMainLooper.runPaused(new Runnable() {
+      @Override
+      public void run() {
+        activityController.pause();
+      }
+    });
+    return this;
+  }
+
+  public SupportFragmentController<F> stop() {
+    shadowMainLooper.runPaused(new Runnable() {
+      @Override
+      public void run() {
+        activityController.stop();
+      }
+    });
+    return this;
+  }
+
+  private static class FragmentControllerActivity extends FragmentActivity {
     @Override
-    public SupportFragmentController<F> attach() {
-        activityController.attach();
-        return this;
-    }
+    protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      LinearLayout view = new LinearLayout(this);
+      view.setId(1);
 
-    public SupportFragmentController<F> create(final Bundle bundle) {
-        shadowMainLooper.runPaused(new Runnable() {
-            @Override
-            public void run() {
-                if (!attached) attach();
-                activityController.create(bundle).get().getSupportFragmentManager().beginTransaction().add(fragment, null).commit();
-            }
-        });
-        return this;
+      setContentView(view);
     }
-
-    @Override
-    public SupportFragmentController<F> create() {
-        return create(null);
-    }
-
-    @Override
-    public SupportFragmentController<F> destroy() {
-        shadowMainLooper.runPaused(new Runnable() {
-            @Override
-            public void run() {
-                activityController.destroy();
-            }
-        });
-        return this;
-    }
-
-    public SupportFragmentController<F> start() {
-        shadowMainLooper.runPaused(new Runnable() {
-            @Override
-            public void run() {
-                activityController.start();
-            }
-        });
-        return this;
-    }
-
-    public SupportFragmentController<F> resume() {
-        shadowMainLooper.runPaused(new Runnable() {
-            @Override
-            public void run() {
-                activityController.resume();
-            }
-        });
-        return this;
-    }
-
-    public SupportFragmentController<F> pause() {
-        shadowMainLooper.runPaused(new Runnable() {
-            @Override
-            public void run() {
-                activityController.pause();
-            }
-        });
-        return this;
-    }
-
-    public SupportFragmentController<F> stop() {
-        shadowMainLooper.runPaused(new Runnable() {
-            @Override
-            public void run() {
-                activityController.stop();
-            }
-        });
-        return this;
-    }
-
-    private static class FragmentControllerActivity extends FragmentActivity {
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            LinearLayout view = new LinearLayout(this);
-            view.setId(1);
-
-            setContentView(view);
-        }
-    }
+  }
 }
--- robolectric-shadows/shadows-support-v4/src/test/java/org/robolectric/util/SupportFragmentControllerTest.java
+++ robolectric-shadows/shadows-support-v4/src/test/java/org/robolectric/util/SupportFragmentControllerTest.java
@@ -18,104 +18,104 @@ import static org.mockito.Mockito.verify;
 
 @RunWith(TestRunnerWithManifest.class)
 public class SupportFragmentControllerTest {
-    @Test
-    public void initialNotAttached() {
-        final LoginFragment fragment = new LoginFragment();
-        SupportFragmentController.of(fragment);
+  @Test
+  public void initialNotAttached() {
+    final LoginFragment fragment = new LoginFragment();
+    SupportFragmentController.of(fragment);
 
-        assertThat(fragment.getView()).isNull();
-        assertThat(fragment.getActivity()).isNull();
-        assertThat(fragment.isAdded()).isFalse();
+    assertThat(fragment.getView()).isNull();
+    assertThat(fragment.getActivity()).isNull();
+    assertThat(fragment.isAdded()).isFalse();
+  }
+
+  @Test
+  public void initialNotAttached_customActivity() {
+    final LoginFragment fragment = new LoginFragment();
+    SupportFragmentController.of(fragment, LoginActivity.class);
+
+    assertThat(fragment.getView()).isNull();
+    assertThat(fragment.getActivity()).isNull();
+    assertThat(fragment.isAdded()).isFalse();
+  }
+
+  @Test
+  public void attachedAfterCreate() {
+    final LoginFragment fragment = new LoginFragment();
+    SupportFragmentController.of(fragment).create();
+
+    assertThat(fragment.getActivity()).isNotNull();
+    assertThat(fragment.isAdded()).isTrue();
+    assertThat(fragment.isResumed()).isFalse();
+  }
+
+  @Test
+  public void attachedAfterCreate_customActivity() {
+    final LoginFragment fragment = new LoginFragment();
+    SupportFragmentController.of(fragment, LoginActivity.class).create();
+
+    assertThat(fragment.getActivity()).isNotNull();
+    assertThat(fragment.getActivity()).isInstanceOf(LoginActivity.class);
+    assertThat(fragment.isAdded()).isTrue();
+    assertThat(fragment.isResumed()).isFalse();
+  }
+
+  @Test
+  public void isResumed() {
+    final LoginFragment fragment = new LoginFragment();
+    SupportFragmentController.of(fragment, LoginActivity.class).create().start().resume();
+
+    assertThat(fragment.getView()).isNotNull();
+    assertThat(fragment.getActivity()).isNotNull();
+    assertThat(fragment.isAdded()).isTrue();
+    assertThat(fragment.isResumed()).isTrue();
+    assertThat(fragment.getView().findViewById(R.id.tacos)).isNotNull();
+  }
+
+  @Test
+  public void isPaused() {
+    final LoginFragment fragment = spy(new LoginFragment());
+    SupportFragmentController.of(fragment, LoginActivity.class).create().start().resume().pause();
+
+    assertThat(fragment.getView()).isNotNull();
+    assertThat(fragment.getActivity()).isNotNull();
+    assertThat(fragment.isAdded()).isTrue();
+    assertThat(fragment.isResumed()).isFalse();
+
+    verify(fragment).onResume();
+    verify(fragment).onPause();
+  }
+
+  @Test
+  public void isStopped() {
+    final LoginFragment fragment = spy(new LoginFragment());
+    SupportFragmentController.of(fragment, LoginActivity.class).create().start().resume().pause().stop();
+
+    assertThat(fragment.getView()).isNotNull();
+    assertThat(fragment.getActivity()).isNotNull();
+    assertThat(fragment.isAdded()).isTrue();
+    assertThat(fragment.isResumed()).isFalse();
+
+    verify(fragment).onStart();
+    verify(fragment).onResume();
+    verify(fragment).onPause();
+    verify(fragment).onStop();
+  }
+
+  private static class LoginFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+      return inflater.inflate(R.layout.fragment_contents, container, false);
     }
+  }
 
-    @Test
-    public void initialNotAttached_customActivity() {
-        final LoginFragment fragment = new LoginFragment();
-        SupportFragmentController.of(fragment, LoginActivity.class);
+  private static class LoginActivity extends FragmentActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      LinearLayout view = new LinearLayout(this);
+      view.setId(1);
 
-        assertThat(fragment.getView()).isNull();
-        assertThat(fragment.getActivity()).isNull();
-        assertThat(fragment.isAdded()).isFalse();
+      setContentView(view);
     }
-
-    @Test
-    public void attachedAfterCreate() {
-        final LoginFragment fragment = new LoginFragment();
-        SupportFragmentController.of(fragment).create();
-
-        assertThat(fragment.getActivity()).isNotNull();
-        assertThat(fragment.isAdded()).isTrue();
-        assertThat(fragment.isResumed()).isFalse();
-    }
-
-    @Test
-    public void attachedAfterCreate_customActivity() {
-        final LoginFragment fragment = new LoginFragment();
-        SupportFragmentController.of(fragment, LoginActivity.class).create();
-
-        assertThat(fragment.getActivity()).isNotNull();
-        assertThat(fragment.getActivity()).isInstanceOf(LoginActivity.class);
-        assertThat(fragment.isAdded()).isTrue();
-        assertThat(fragment.isResumed()).isFalse();
-    }
-
-    @Test
-    public void isResumed() {
-        final LoginFragment fragment = new LoginFragment();
-        SupportFragmentController.of(fragment, LoginActivity.class).create().start().resume();
-
-        assertThat(fragment.getView()).isNotNull();
-        assertThat(fragment.getActivity()).isNotNull();
-        assertThat(fragment.isAdded()).isTrue();
-        assertThat(fragment.isResumed()).isTrue();
-        assertThat(fragment.getView().findViewById(R.id.tacos)).isNotNull();
-    }
-
-    @Test
-    public void isPaused() {
-        final LoginFragment fragment = spy(new LoginFragment());
-        SupportFragmentController.of(fragment, LoginActivity.class).create().start().resume().pause();
-
-        assertThat(fragment.getView()).isNotNull();
-        assertThat(fragment.getActivity()).isNotNull();
-        assertThat(fragment.isAdded()).isTrue();
-        assertThat(fragment.isResumed()).isFalse();
-
-        verify(fragment).onResume();
-        verify(fragment).onPause();
-    }
-
-    @Test
-    public void isStopped() {
-        final LoginFragment fragment = spy(new LoginFragment());
-        SupportFragmentController.of(fragment, LoginActivity.class).create().start().resume().pause().stop();
-
-        assertThat(fragment.getView()).isNotNull();
-        assertThat(fragment.getActivity()).isNotNull();
-        assertThat(fragment.isAdded()).isTrue();
-        assertThat(fragment.isResumed()).isFalse();
-
-        verify(fragment).onStart();
-        verify(fragment).onResume();
-        verify(fragment).onPause();
-        verify(fragment).onStop();
-    }
-
-    private static class LoginFragment extends Fragment {
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            return inflater.inflate(R.layout.fragment_contents, container, false);
-        }
-    }
-
-    private static class LoginActivity extends FragmentActivity {
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            LinearLayout view = new LinearLayout(this);
-            view.setId(1);
-
-            setContentView(view);
-        }
-    }
+  }
 }
