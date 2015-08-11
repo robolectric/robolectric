f3d8f58eb16b4f3c3f4d7a5c813ac23456fe6609
Yu Zhong <yuzhong@google.com>
2015-08-11 11:37:39 -0700

    Use sdk level in config to choose rumtimeadapter instand of sdk_int

--- robolectric/src/main/java/org/robolectric/internal/runtime/RuntimeAdapterFactory.java
+++ robolectric/src/main/java/org/robolectric/internal/runtime/RuntimeAdapterFactory.java
@@ -2,18 +2,21 @@ package org.robolectric.internal.runtime;
 
 import android.os.Build;
 
+import org.robolectric.RuntimeEnvironment;
+
 public class RuntimeAdapterFactory {
   public static RuntimeAdapter getInstance() {
-    if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.JELLY_BEAN) {
+    int apiLevel = RuntimeEnvironment.getApiLevel();
+    if (apiLevel <= Build.VERSION_CODES.JELLY_BEAN) {
       return new Api16RuntimeAdapter();
-    } else if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+    } else if (apiLevel <= Build.VERSION_CODES.JELLY_BEAN_MR1) {
       return new Api17RuntimeAdapter();
-    } else if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.KITKAT) {
+    } else if (apiLevel <= Build.VERSION_CODES.KITKAT) {
       return new Api19RuntimeAdapter();
-    } else if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.LOLLIPOP) {
+    } else if (apiLevel <= Build.VERSION_CODES.LOLLIPOP) {
       return new Api21RuntimeAdapter();
     } else {
-      throw new RuntimeException("Could not find AndroidRuntimeAdapter for API level: " + Build.VERSION.SDK_INT);
+      throw new RuntimeException("Could not find AndroidRuntimeAdapter for API level: " + apiLevel);
     }
   }
 }
