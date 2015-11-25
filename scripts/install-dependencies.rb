robolectric/robolectric

commit bf4f3aad9b7a53902dce17e1d6cc826cf59ee673

Make install-dependencies more robust.
Closes #1968.

scripts/install-dependencies.rb
@@ -1,0 +1,88 @@
+#!/usr/bin/env ruby
+#
+# This script mavenizes all dependencies from the Android SDK required to build Robolectric.
+#
+# Usage:
+#   install-dependencies.rb
+#
+# Assumptions:
+#  1. You've got one or more Android SDKs and Google APIs installed locally.
+#  2. Your ANDROID_HOME environment variable points to the Android SDK install directory.
+#  3. You have installed the Android Repository and Google Repository libraries from the SDK installer.
+#
+require 'tmpdir'
+
+def install(group_id, artifact_id, version, archive)
+  system("mvn -q install:install-file -DgroupId='#{group_id}' -DartifactId='#{artifact_id}' -Dversion='#{version}' -Dfile='#{archive}' -Dpackaging=jar")
+end
+
+def install_jar(group_id, artifact_id, version, archive, &block)
+  unless File.exists?(archive)
+    puts "#{group_id}:#{artifact_id} not found!"
+    puts "Make sure that the 'Android Support Repository' and 'Google Repository' is up to date in the SDK manager."
+    exit 1
+  end
+
+  puts "Installing #{group_id}:#{artifact_id}"
+  install(group_id, artifact_id, version, archive)
+  block.call(dir) if block_given?
+end
+
+def install_aar(group_id, artifact_id, version, archive, &block)
+  unless File.exists?(archive)
+    puts "#{group_id}:#{artifact_id} not found!"
+    puts "Make sure that the 'Android Support Repository' and 'Google Repository' is up to date in the SDK manager."
+    exit 1
+  end
+
+  puts "Installing #{group_id}:#{artifact_id}"
+  Dir.mktmpdir('robolectric-dependencies') do |dir|
+    system("cd #{dir}; jar xvf #{archive} > /dev/null")
+    install(group_id, artifact_id, version, "#{dir}/classes.jar")
+    block.call(dir) if block_given?
+  end
+end
+
+def install_map(group_id, artifact_id, api, revision)
+  dir  = "#{ADDONS}/addon-google_apis-google-#{api}"
+  path = "#{dir}/libs/maps.jar"
+
+  unless File.exists?(path)
+    puts "#{group_id}:#{artifact_id} not found!"
+    puts "Make sure that 'Google APIs' is up to date in the SDK manager for API #{api}."
+    exit 1
+  end
+
+  version = `grep --color=never ^revision= "#{dir}/manifest.ini" | cut -d= -f2`
+  if version.strip != revision
+    puts "#{group_id}:#{artifact_id} is an incompatible revision!"
+    puts "Make sure that 'Google APIs' is up to date in the SDK manager for API #{api}. Expected revision #{revision} but was #{version}."
+    exit 1
+  end
+
+  puts "Installing #{group_id}:#{artifact_id}"
+  install(group_id, artifact_id, version, path)
+end
+
+# Local repository paths
+ADDONS = "#{ENV['ANDROID_HOME']}/add-ons"
+GOOGLE_REPO  = "#{ENV['ANDROID_HOME']}/extras/google/m2repository"
+ANDROID_REPO = "#{ENV['ANDROID_HOME']}/extras/android/m2repository"
+
+# Mavenize all dependencies
+install_map("com.google.android.maps", "maps", "18", "4")
+
+install_aar("com.android.support", "multidex", "1.0.0",
+  "#{ANDROID_REPO}/com/android/support/multidex/1.0.0/multidex-1.0.0.aar")
+
+install_aar("com.android.support", "appcompat-v7", "22.2.0",
+  "#{ANDROID_REPO}/com/android/support/appcompat-v7/22.2.0/appcompat-v7-22.2.0.aar")
+
+install_aar("com.google.android.gms", "play-services", "6.5.87",
+  "#{GOOGLE_REPO}/com/google/android/gms/play-services/6.5.87/play-services-6.5.87.aar")
+
+install_aar("com.android.support", "support-v4", "22.2.0",
+  "#{ANDROID_REPO}/com/android/support/support-v4/22.2.0/support-v4-22.2.0.aar") do |dir|
+
+  install_jar("com.android.support", "internal_impl", "22.2.0", "#{dir}/libs/internal_impl-22.2.0.jar")
+end

.travis.yml
@@ -14,7 +14,7 @@ before_install:
     - echo "y" | android update sdk --no-ui --filter build-tools-20.0.0
     - echo "y" | android update sdk --no-ui --filter android-18
     - echo "y" | android update sdk --no-ui --filter addon-google_apis-google-18,extra-android-m2repository,extra-google-m2repository
-    - ./scripts/install-dependencies.sh
+    - ./scripts/install-dependencies.rb
 
 script:
     - ./scripts/install-robolectric.sh

scripts/install-dependencies.sh
@@ -1,90 +1,0 @@
-#!/bin/bash
-#
-# This script mavenizes all dependencies from the Android SDK required to build Robolectric.
-#
-# Usage:
-#   install-dependencies.sh
-#
-# Assumptions:
-#  1. You've got one or more Android SDKs and Google APIs installed locally.
-#  2. Your ANDROID_HOME environment variable points to the Android SDK install directory.
-#  3. You have installed the Android Repository and Google Repository libraries from the SDK installer.
-
-set -e
-
-ANDROID_REPOSITORY=${ANDROID_HOME}/extras/android/m2repository
-GOOGLE_REPOSITORY=${ANDROID_HOME}/extras/google/m2repository
-ADDONS=${ANDROID_HOME}/add-ons
-
-function install_jar() {
-  groupId=$1; artifactId=$2; version=$3; archive=$4
-
-  if [ ! -f "$archive" ]; then
-      echo "${groupId}:${artifactId} not found! Make sure that the 'Android Support Library' and 'Google Repository' is up to date in the SDK Manager."
-      exit 1;
-  fi
-
-  echo "Installing ${groupId}:${artifactId} from ${archive}"
-  mvn -q install:install-file -DgroupId=${groupId} -DartifactId=${artifactId} -Dversion=${version} -Dpackaging=jar -Dfile="${archive}"
-}
-
-function install_aar() {
-  groupId=$1; artifactId=$2; version=$3; archive=$4
-
-  if [ ! -f "$archive" ]; then
-    echo "${groupId}:${artifactId} not found! Make sure that the 'Android Support Repository' and 'Google Repository' is up to date in the SDK manager."
-    exit 1
-  fi
-
-  tempdir=`mktemp -qd /tmp/robolectric-dependencies.XXXXXX`
-  ( cd ${tempdir}; jar xvf "${archive}" > /dev/null )
-
-  echo "Installing ${groupId}:${artifactId} from ${archive}"
-  mvn -q install:install-file -DgroupId=${groupId} -DartifactId=${artifactId} -Dversion=${version} -Dpackaging=jar -Dfile="${tempdir}/classes.jar"
-  rm -rf ${tempdir}
-}
-
-function install_maps() {
-  groupId=$1; artifactId=$2 api=$3; revision=$4
-
-  dir="${ADDONS}/addon-google_apis-google-${api}"
-
-  if [ ! -d "$dir" ]; then
-    echo "${groupId}:${artifactId} not found! Make sure that 'Google APIs' is up to date in the SDK manager for API ${api}."
-    exit 1
-  fi
-
-  version=`grep --color=never ^revision= "${dir}/manifest.ini" | cut -d= -f2`
-
-  if [ "$version" != "$revision" ]; then
-    echo "${groupId}:${artifactId} is an incompatible revision! Make sure that 'Google APIs' is up to date in the SDK manager for API ${api}. Expected revision ${revision} but was ${version}."
-    exit 1
-  fi
-
-  echo "Installing ${groupId}:${artifactId} from ${dir}/libs/maps.jar"
-  mvn -q install:install-file -DgroupId=${groupId} -DartifactId=${artifactId} -Dversion=${api}_r${version} -Dpackaging=jar -Dfile="${dir}/libs/maps.jar"
-}
-
-if [ -z "${ANDROID_HOME}" ]; then
-  echo "You must set \$ANDROID_HOME"
-  exit 1
-fi
-
-# Unzip support-v4 aar
-jar xf $ANDROID_REPOSITORY/com/android/support/support-v4/22.2.0/support-v4-22.2.0.aar libs/internal_impl-22.2.0.jar
-
-# Install jar lib for support-v4
-install_jar "com.android.support" "internal_impl" "22.2.0" "./libs/internal_impl-22.2.0.jar"
-rm -rf ./libs
-
-# Install support-v4
-install_aar "com.android.support" "support-v4" "22.2.0" "${ANDROID_REPOSITORY}/com/android/support/support-v4/22.2.0/support-v4-22.2.0.aar"
-
-# Install multidex
-install_aar "com.android.support" "multidex" "1.0.0" "${ANDROID_REPOSITORY}/com/android/support/multidex/1.0.0/multidex-1.0.0.aar"
-
-# Install Play Services
-install_aar "com.google.android.gms" "play-services" "6.5.87" "${GOOGLE_REPOSITORY}/com/google/android/gms/play-services/6.5.87/play-services-6.5.87.aar"
-
-# Install maps
-install_maps "com.google.android.maps" "maps" "18" "4"

commit 5013859eb877d84fce87c97136b8adfae4bd24f5

Fix install-dependencies.rb maps version
The correct version string should look like "18_r4", not just "4".

This bug was introduced in bf4f3aa (issue #1968 / PR #2092) by accident.

Note that this also intentionally uses #{revision} instead of
the shell version did. (This resulted in the previous version actually
trying to install to filenames with "4?" instead of "4".)

scripts/install-dependencies.rb
@@ -59,9 +59,8 @@ def install_map(group_id, artifact_id, api, revision)
     puts "Make sure that 'Google APIs' is up to date in the SDK manager for API #{api}. Expected revision #{revision} but was #{version}."
     exit 1
   end
-
   puts "Installing #{group_id}:#{artifact_id}"
-  install(group_id, artifact_id, version, path)
+  install(group_id, artifact_id, "#{api}_r#{revision}", path)
 end
 
 # Local repository paths