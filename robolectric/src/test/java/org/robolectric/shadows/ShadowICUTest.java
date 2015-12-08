66d5f329cdfa1a86b0c60ce3b02b19fba7ecc193
Simon Marco Janic <simon.marco.janic@systematic.com>
2015-12-08 19:08:03 +0100

    Added imports

--- robolectric/src/test/java/org/robolectric/shadows/ShadowICUTest.java
+++ robolectric/src/test/java/org/robolectric/shadows/ShadowICUTest.java
@@ -1,12 +1,18 @@
 package org.robolectric.shadows;
 
+import android.app.Activity;
 import android.os.Build;
+import android.os.Bundle;
+import android.widget.DatePicker;
+import android.widget.LinearLayout;
 
 import libcore.icu.ICU;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
 import org.robolectric.TestRunners;
 import org.robolectric.annotation.Config;
+import org.robolectric.util.ActivityController;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
