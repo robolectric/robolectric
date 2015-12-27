c0336ebe3c43e1038cdf9a65a71d77f8d6efea68
Zhang-Yao Cheng <zhangyaocheng24@gmail.com>
2015-12-27 14:16:02 -0500

    Implement ShadowProgressDialog.getMessage for horizontal progress style Fixes #1327

--- robolectric-shadows/shadows-core/src/main/java/org/robolectric/shadows/ShadowProgressDialog.java
+++ robolectric-shadows/shadows-core/src/main/java/org/robolectric/shadows/ShadowProgressDialog.java
@@ -1,11 +1,54 @@
 package org.robolectric.shadows;
 
 import android.app.ProgressDialog;
+import android.view.View;
+import android.widget.TextView;
+import com.android.internal.R;
+import org.robolectric.annotation.Implementation;
 import org.robolectric.annotation.Implements;
+import org.robolectric.annotation.RealObject;
+import org.robolectric.util.ReflectionHelpers;
+
+import static org.robolectric.internal.Shadow.directlyOn;
 
 /**
  * Shadow for {@link android.app.ProgressDialog}.
  */
+@SuppressWarnings({"UnusedDeclaration"})
 @Implements(ProgressDialog.class)
 public class ShadowProgressDialog extends ShadowAlertDialog {
+  @RealObject
+  private ProgressDialog realProgressDialog;
+
+  private int mProgressStyle;
+
+  /**
+   * Non-Android accessor.
+   *
+   * @return the message displayed in the dialog
+   */
+  @Override
+  public CharSequence getMessage() {
+    if (mProgressStyle == ProgressDialog.STYLE_HORIZONTAL) {
+      return super.getMessage();
+    } else {
+      TextView message = ReflectionHelpers.getField(realProgressDialog, "mMessageView");
+      return message.getText();
+    }
+  }
+
+  @Implementation
+  public void setProgressStyle(int style) {
+    mProgressStyle = style;
+    directlyOn(realProgressDialog, ProgressDialog.class).setProgressStyle(style);
+  }
+
+  /**
+   * Non-Android accessor.
+   *
+   * @return the style of the progress dialog
+   */
+  public int getProgressStyle() {
+    return mProgressStyle;
+  }
 }
