d093abe79fbabb78154a9dfec41a1bd7d77a0c78
Diego Tori <diego@gpshopper.com>
2015-12-27 16:09:49 -0500

    Cleaned up install-dependencies.rb. In particular, added constants for existing dependency Group IDs, Artifact IDs, and versions, which makes it very trivial to upgrade for future use. Also added preliminary support for instaliing Google Play Services versions beyond 6.5.87.

--- scripts/install-dependencies.rb
+++ scripts/install-dependencies.rb
@@ -13,7 +13,7 @@
 require 'tmpdir'
 
 def install(group_id, artifact_id, version, archive)
-  system("mvn -q install:install-file -DgroupId='#{group_id}' -DartifactId='#{artifact_id}' -Dversion='#{version}' -Dfile='#{archive}' -Dpackaging=jar")
+  system("mvn -q -e install:install-file -DgroupId='#{group_id}' -DartifactId='#{artifact_id}' -Dversion='#{version}' -Dfile='#{archive}' -Dpackaging=jar")
 end
 
 def install_jar(group_id, artifact_id, version, archive, &block)
@@ -23,7 +23,7 @@ def install_jar(group_id, artifact_id, version, archive, &block)
     exit 1
   end
 
-  puts "Installing #{group_id}:#{artifact_id}"
+  puts "Installing JAR #{group_id}:#{artifact_id}, version #{version} from \'#{archive}\'."
   install(group_id, artifact_id, version, archive)
   block.call(dir) if block_given?
 end
@@ -35,8 +35,9 @@ def install_aar(group_id, artifact_id, version, archive, &block)
     exit 1
   end
 
-  puts "Installing #{group_id}:#{artifact_id}"
+  puts "Installing AAR #{group_id}:#{artifact_id}, version #{version} from \'#{archive}\'."
   Dir.mktmpdir('robolectric-dependencies') do |dir|
+    puts "cd'ing into #{dir}, then jarring from #{archive} into /dev/null"
     system("cd #{dir}; jar xvf #{archive} > /dev/null")
     install(group_id, artifact_id, version, "#{dir}/classes.jar")
     block.call(dir) if block_given?
@@ -59,7 +60,8 @@ def install_map(group_id, artifact_id, api, revision)
     puts "Make sure that 'Google APIs' is up to date in the SDK manager for API #{api}. Expected revision #{revision} but was #{version}."
     exit 1
   end
-  puts "Installing #{group_id}:#{artifact_id}"
+
+  puts "Installing Maps API #{group_id}:#{artifact_id}, API #{api}, version #{version}, revision #{revision}."
   install(group_id, artifact_id, "#{api}_r#{revision}", path)
 end
 
@@ -68,20 +70,51 @@ ADDONS = "#{ENV['ANDROID_HOME']}/add-ons"
 GOOGLE_REPO  = "#{ENV['ANDROID_HOME']}/extras/google/m2repository"
 ANDROID_REPO = "#{ENV['ANDROID_HOME']}/extras/android/m2repository"
 
+# Library version constants
+SUPPORT_LIBRARY_VERSION = "22.2.1"
+MULTIDEX_VERSION = "1.0.0"
+
+PLAY_SERVICES_GROUP_ID = "com.google.android.gms"
+
+# Play Services 6.5.87 version constants, which pulls in all of the play-services
+# submodules in classes.jar
+PLAY_SERVICES_VERSION_6_5_87 = "6.5.87"
+PLAY_SERVICES_LEGACY = "play-services"
+
+# Play Services Base and Basement modules, version 8.3.0
+# Current "play-services" artifact no longer references each sub-module directly. When you
+# Extract its AAR, it only contains a manifest and blank res folder.
+# 
+# As a result, we now have to install "play-services-base" and "play-services-basement"
+# separately and use those versions instead.
+PLAY_SERVICES_VERSION = "8.3.0"
+PLAY_SERVICES_BASE = "play-services-base"
+PLAY_SERVICES_BASEMENT = "play-services-basement"
+
+# Maps API version constants
+MAPS_API_VERSION = "18"
+MAPS_REVISION_VERSION = "4"
+
 # Mavenize all dependencies
-install_map("com.google.android.maps", "maps", "18", "4")
+install_map("com.google.android.maps", "maps", MAPS_API_VERSION, MAPS_REVISION_VERSION)
 
-install_aar("com.android.support", "multidex", "1.0.0",
-  "#{ANDROID_REPO}/com/android/support/multidex/1.0.0/multidex-1.0.0.aar")
+install_aar("com.android.support", "multidex", MULTIDEX_VERSION,
+  "#{ANDROID_REPO}/com/android/support/multidex/#{MULTIDEX_VERSION}/multidex-#{MULTIDEX_VERSION}.aar")
 
-install_aar("com.android.support", "appcompat-v7", "22.2.0",
-  "#{ANDROID_REPO}/com/android/support/appcompat-v7/22.2.0/appcompat-v7-22.2.0.aar")
+install_aar("com.android.support", "appcompat-v7", SUPPORT_LIBRARY_VERSION,
+  "#{ANDROID_REPO}/com/android/support/appcompat-v7/#{SUPPORT_LIBRARY_VERSION}/appcompat-v7-#{SUPPORT_LIBRARY_VERSION}.aar")
 
-install_aar("com.google.android.gms", "play-services", "6.5.87",
-  "#{GOOGLE_REPO}/com/google/android/gms/play-services/6.5.87/play-services-6.5.87.aar")
+install_aar(PLAY_SERVICES_GROUP_ID, PLAY_SERVICES_LEGACY, PLAY_SERVICES_VERSION_6_5_87,
+  "#{GOOGLE_REPO}/com/google/android/gms/#{PLAY_SERVICES_LEGACY}/#{PLAY_SERVICES_VERSION_6_5_87}/#{PLAY_SERVICES_LEGACY}-#{PLAY_SERVICES_VERSION_6_5_87}.aar")
 
-install_aar("com.android.support", "support-v4", "22.2.0",
-  "#{ANDROID_REPO}/com/android/support/support-v4/22.2.0/support-v4-22.2.0.aar") do |dir|
+install_aar(PLAY_SERVICES_GROUP_ID, PLAY_SERVICES_BASEMENT, PLAY_SERVICES_VERSION,
+  "#{GOOGLE_REPO}/com/google/android/gms/#{PLAY_SERVICES_BASEMENT}/#{PLAY_SERVICES_VERSION}/#{PLAY_SERVICES_BASEMENT}-#{PLAY_SERVICES_VERSION}.aar")
 
-  install_jar("com.android.support", "internal_impl", "22.2.0", "#{dir}/libs/internal_impl-22.2.0.jar")
+install_aar(PLAY_SERVICES_GROUP_ID, PLAY_SERVICES_BASE, PLAY_SERVICES_VERSION,
+  "#{GOOGLE_REPO}/com/google/android/gms/#{PLAY_SERVICES_BASE}/#{PLAY_SERVICES_VERSION}/#{PLAY_SERVICES_BASE}-#{PLAY_SERVICES_VERSION}.aar")
+
+install_aar("com.android.support", "support-v4", SUPPORT_LIBRARY_VERSION,
+  "#{ANDROID_REPO}/com/android/support/support-v4/#{SUPPORT_LIBRARY_VERSION}/support-v4-#{SUPPORT_LIBRARY_VERSION}.aar") do |dir|
+
+install_jar("com.android.support", "internal_impl", SUPPORT_LIBRARY_VERSION, "#{dir}/libs/internal_impl-#{SUPPORT_LIBRARY_VERSION}.jar")
 end
