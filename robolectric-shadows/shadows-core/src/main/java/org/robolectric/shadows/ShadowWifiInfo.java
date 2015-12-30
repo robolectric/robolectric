977f5b0609f971305f56c8e46f661df093121d33
Jason Neufeld <jneufeld@google.com>
2015-12-30 11:00:45 -0800

    Exposes setRssi in ShadowWifiInfo for code that depends on the RSSI value for testing.

--- robolectric-shadows/shadows-core/src/main/java/org/robolectric/shadows/ShadowWifiInfo.java
+++ robolectric-shadows/shadows-core/src/main/java/org/robolectric/shadows/ShadowWifiInfo.java
@@ -4,12 +4,15 @@ import android.net.wifi.WifiInfo;
 import org.robolectric.annotation.Implementation;
 import org.robolectric.annotation.Implements;
 import org.robolectric.annotation.HiddenApi;
+import org.robolectric.annotation.RealObject;
 
 /**
  * Shadow for {@link android.net.wifi.WifiInfo}.
  */
 @Implements(WifiInfo.class)
 public class ShadowWifiInfo {
+  @RealObject private WifiInfo realObject;
+
   public static void __staticInitializer__() {
   }
 
@@ -23,5 +26,10 @@ public class ShadowWifiInfo {
   @HiddenApi @Implementation
   public void setMacAddress(String newMacAddress) {
     macAddress = newMacAddress;
+  }
+
+  @HiddenApi @Implementation
+  public void setRssi(int rssi) {
+    realObject.setRssi(rssi);
   }
 }
