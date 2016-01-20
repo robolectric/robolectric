8878ff85c7afc38f777db66d96815af908a8b683
Amin Shaikh <ashaikh@google.com>
2016-01-20 14:03:05 -0800

    Augmented a few shadow classes.
    Added methods to Shadow{TelephonyManager,WifiInfo,WifiManager}.
    
    Tested: install-robolectric.sh. Verified that all tests pass.

--- robolectric-shadows/shadows-core/src/main/java/org/robolectric/shadows/ShadowWifiInfo.java
+++ robolectric-shadows/shadows-core/src/main/java/org/robolectric/shadows/ShadowWifiInfo.java
@@ -13,15 +13,48 @@ public class ShadowWifiInfo {
   public static void __staticInitializer__() {
   }
 
-  private String macAddress;
+  private String macAddress = "02:00:00:00:00:00"; // WifiInfo.DEFAULT_MAC_ADDRESS (@hide)
+  private String ssid = "<unknown ssid>"; // WifiSsid.NONE (@hide)
+  private String bssid;
+  private int rssi = -127; // WifiInfo.INVALID_RSSI (@hide)
 
   @Implementation
   public String getMacAddress() {
     return macAddress;
   }
 
+  @Implementation
+  public String getSSID() {
+    return ssid;
+  }
+
+  @Implementation
+  public String getBSSID() {
+    return bssid;
+  }
+
+  @Implementation
+  public int getRssi() {
+    return rssi;
+  }
+
   @HiddenApi @Implementation
   public void setMacAddress(String newMacAddress) {
     macAddress = newMacAddress;
+  }
+
+  @HiddenApi @Implementation
+  public void setSSID(String ssid) {
+    this.ssid = ssid;
+  }
+
+  @HiddenApi @Implementation
+  public void setBSSID(String bssid) {
+    this.bssid = bssid;
+  }
+
+  @HiddenApi @Implementation
+  public void setRssi(int rssi) {
+    this.rssi = rssi;
   }
 }
