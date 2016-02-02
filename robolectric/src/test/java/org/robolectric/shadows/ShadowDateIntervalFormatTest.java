9fa7c7d43fea789fc8142a6205b9f11ff68856dd
Nikolas Bravo <nikolasr@nikolasr.sfo.corp.google.com>
2016-02-02 09:44:49 -0800

    Patched locale problem with testDateInterval_FormatDateRange()

--- robolectric/src/test/java/org/robolectric/shadows/ShadowDateIntervalFormatTest.java
+++ robolectric/src/test/java/org/robolectric/shadows/ShadowDateIntervalFormatTest.java
@@ -2,37 +2,47 @@
 
 package org.robolectric.shadows;
 
+import android.icu.text.DateFormat;
+import android.icu.text.SimpleDateFormat;
+import android.icu.util.TimeZone;
+import android.icu.util.ULocale;
 import android.os.Build;
 import android.text.format.DateUtils;
 import libcore.icu.DateIntervalFormat;
-import android.icu.util.TimeZone;
-import android.icu.util.ULocale;
-
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.TestRunners;
 import org.robolectric.annotation.Config;
 
+import java.text.ParseException;
 import java.util.Calendar;
-
-import static org.junit.Assert.assertEquals;
+import java.util.Date;
 
 @RunWith(TestRunners.MultiApiWithDefaults.class)
 @Config(sdk = {
     Build.VERSION_CODES.M })
 public class ShadowDateIntervalFormatTest {
 
-  @Test
-  public void testDateInterval_FormatDateRange() {
+    @Test
+    public void testDateInterval_FormatDateRange() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, 2013);
+        calendar.set(Calendar.MONTH, Calendar.JANUARY);
+        calendar.set(Calendar.DAY_OF_MONTH, 20);
 
-    Calendar calendar = Calendar.getInstance();
-    calendar.set(Calendar.YEAR, 2013);
-    calendar.set(Calendar.MONTH, Calendar.JANUARY);
-    calendar.set(Calendar.DAY_OF_MONTH, 20);
+        long timeInMillis = calendar.getTimeInMillis();
+        String actual = DateIntervalFormat.formatDateRange(ULocale.getDefault(), TimeZone.getDefault(), timeInMillis, timeInMillis, DateUtils.FORMAT_NUMERIC_DATE);
 
-    long timeInMillis = calendar.getTimeInMillis();
-    String actual = DateIntervalFormat.formatDateRange(ULocale.getDefault(), TimeZone.getDefault(), timeInMillis, timeInMillis, DateUtils.FORMAT_NUMERIC_DATE);
+        DateFormat format = new SimpleDateFormat("MM/dd/yyyy", ULocale.getDefault());
+        Date date = null;
+        try {
+            date = format.parse(actual);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        Assert.assertNotNull(date);
 
-    assertEquals("1/20/2013", actual);
-  }
+    }
+
 }
