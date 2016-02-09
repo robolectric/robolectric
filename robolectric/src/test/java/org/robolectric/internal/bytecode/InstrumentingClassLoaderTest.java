370adce9870d311d8084a3449f9970f3299ae5cb
Nikolas Bravo <nikolasbra@gmail.com>
2016-02-09 15:20:21 -0800

    does not instrument super final methods for equals() hashcode() and toString()

--- robolectric/src/test/java/org/robolectric/internal/bytecode/InstrumentingClassLoaderTest.java
+++ robolectric/src/test/java/org/robolectric/internal/bytecode/InstrumentingClassLoaderTest.java
@@ -1,66 +1,26 @@
 package org.robolectric.internal.bytecode;
 
 import android.os.Build;
-
-import java.lang.invoke.MethodHandle;
-import java.lang.invoke.MethodHandles;
-import java.lang.invoke.MethodType;
-import java.lang.invoke.SwitchPoint;
-import java.util.Arrays;
 import org.junit.Test;
 import org.mockito.Mockito;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.internal.Shadow;
 import org.robolectric.internal.ShadowConstants;
 import org.robolectric.internal.ShadowExtractor;
-import org.robolectric.internal.bytecode.testing.AChild;
-import org.robolectric.internal.bytecode.testing.AClassThatCallsAMethodReturningAForgettableClass;
-import org.robolectric.internal.bytecode.testing.AClassThatRefersToAForgettableClass;
-import org.robolectric.internal.bytecode.testing.AClassThatRefersToAForgettableClassInItsConstructor;
-import org.robolectric.internal.bytecode.testing.AClassThatRefersToAForgettableClassInMethodCalls;
-import org.robolectric.internal.bytecode.testing.AClassThatRefersToAForgettableClassInMethodCallsReturningPrimitive;
-import org.robolectric.internal.bytecode.testing.AClassToForget;
-import org.robolectric.internal.bytecode.testing.AClassToRemember;
-import org.robolectric.internal.bytecode.testing.AClassWithEqualsHashCodeToString;
-import org.robolectric.internal.bytecode.testing.AClassWithFunnyConstructors;
-import org.robolectric.internal.bytecode.testing.AClassWithMethodReturningArray;
-import org.robolectric.internal.bytecode.testing.AClassWithMethodReturningBoolean;
-import org.robolectric.internal.bytecode.testing.AClassWithMethodReturningDouble;
-import org.robolectric.internal.bytecode.testing.AClassWithMethodReturningInteger;
-import org.robolectric.internal.bytecode.testing.AClassWithNativeMethod;
-import org.robolectric.internal.bytecode.testing.AClassWithNativeMethodReturningPrimitive;
-import org.robolectric.internal.bytecode.testing.AClassWithNoDefaultConstructor;
-import org.robolectric.internal.bytecode.testing.AClassWithStaticMethod;
-import org.robolectric.internal.bytecode.testing.AClassWithoutEqualsHashCodeToString;
-import org.robolectric.internal.bytecode.testing.AFinalClass;
-import org.robolectric.internal.bytecode.testing.AnEnum;
-import org.robolectric.internal.bytecode.testing.AnExampleClass;
-import org.robolectric.internal.bytecode.testing.AnInstrumentedChild;
-import org.robolectric.internal.bytecode.testing.AnInstrumentedClassWithoutToStringWithSuperToString;
-import org.robolectric.internal.bytecode.testing.AnUninstrumentedClass;
-import org.robolectric.internal.bytecode.testing.AnUninstrumentedParent;
+import org.robolectric.internal.bytecode.testing.*;
 import org.robolectric.util.Transcript;
 import org.robolectric.util.Util;
 
-import java.lang.reflect.Array;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+import java.lang.invoke.SwitchPoint;
+import java.lang.reflect.*;
 
-import static java.lang.invoke.MethodHandles.constant;
-import static java.lang.invoke.MethodHandles.dropArguments;
-import static java.lang.invoke.MethodHandles.insertArguments;
+import static java.lang.invoke.MethodHandles.*;
 import static java.lang.invoke.MethodType.methodType;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertSame;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.mock;
@@ -329,6 +289,11 @@ public class InstrumentingClassLoaderTest {
     Method directMethod = declaringClass.getDeclaredMethod(directMethodName, argClasses);
     directMethod.setAccessible(true);
     return directMethod;
+  }
+
+  @Test
+  public void shouldNotInstrumentFinalEqualsHashcode() throws ClassNotFoundException {
+    Class<?> theClass = loadClass(AClassThatExtendsAClassWithFinalEqualsHashCode.class);
   }
 
   @Test
---
+++ robolectric/src/test/java/org/robolectric/internal/bytecode/testing/AClassThatExtendsAClassWithFinalEqualsHashCode.java
@@ -0,0 +1,10 @@
+package org.robolectric.internal.bytecode.testing;
+
+import org.robolectric.annotation.internal.Instrument;
+
+/**
+ * Created by nikolasr on 2/9/16.
+ */
+@Instrument
+public class AClassThatExtendsAClassWithFinalEqualsHashCode extends AClassWithFinalEqualsHashCode {
+}
