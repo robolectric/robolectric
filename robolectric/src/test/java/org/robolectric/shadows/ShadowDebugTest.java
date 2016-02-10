5930a4987e4a19e5d3dcce839688f661f9adc30c
Jonathan Gerrish <jongerrish@google.com>
2016-02-10 08:59:46 -0800

    Add implementation of Debug.getRuntimeStats() that does not NPE (Method added in API 23).

--- robolectric/src/test/java/org/robolectric/shadows/ShadowDebugTest.java
+++ robolectric/src/test/java/org/robolectric/shadows/ShadowDebugTest.java
@@ -1,9 +1,11 @@
 package org.robolectric.shadows;
 
+import android.os.Build;
 import android.os.Debug;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.TestRunners;
+import org.robolectric.annotation.Config;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -12,5 +14,11 @@ public class ShadowDebugTest {
   @Test
   public void initNoCrash() {
     assertThat(Debug.getNativeHeapAllocatedSize()).isNotNegative();
+  }
+
+  @Test
+  @Config(sdk = Build.VERSION_CODES.M)
+  public void getRuntimeStats() {
+    assertThat(Debug.getRuntimeStats()).isNotNull();
   }
 }
