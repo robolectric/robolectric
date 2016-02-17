f92ce44a969bdb800bdc7335326613f4219aecd6
Jonathan Gerrish <jongerrish@google.com>
2016-02-17 14:16:31 -0800

    Restore ShadowImageView.getImageResourceId()

--- robolectric-shadows/shadows-core/src/main/java/org/robolectric/shadows/ShadowImageView.java
+++ robolectric-shadows/shadows-core/src/main/java/org/robolectric/shadows/ShadowImageView.java
@@ -1,10 +1,13 @@
 package org.robolectric.shadows;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.widget.ImageView;
 
 import org.robolectric.annotation.Implements;
 import org.robolectric.annotation.RealObject;
-import org.robolectric.util.ReflectionHelpers;
+
+import static org.robolectric.Shadows.shadowOf;
 
 /**
  * Shadow for {@link android.widget.ImageView}.
@@ -15,7 +18,21 @@ public class ShadowImageView extends ShadowView {
   @RealObject
   private ImageView realImageView;
 
+  /**
+   * @deprecated Prefer shadowOf(realImageView.getDrawable()).getCreatedFromResId()
+   * - this method will be removed in the future.
+   */
+  @Deprecated
   public int getImageResourceId() {
-    return ReflectionHelpers.getField(realImageView, "mResource");
+    return shadowOf(realImageView.getDrawable()).getCreatedFromResId();
+  }
+
+  /**
+   * @deprecated Prefer ((BitmapDrawable)ImageView.getDrawable()).getBitmap() - this method will
+   * be removed in the future.
+   */
+  @Deprecated
+  public Bitmap getImageBitmap() {
+    return ((BitmapDrawable)realImageView.getDrawable()).getBitmap();
   }
 }
