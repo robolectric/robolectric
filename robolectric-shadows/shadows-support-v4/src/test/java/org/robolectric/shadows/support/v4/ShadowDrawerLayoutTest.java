b6547600f3552cc5233d119acbe5c0e1fef148f2
Ha Duy Trung <haduytrung@gmail.com>
2016-02-26 11:22:15 +0800

    Deprecate ShadowDrawerLayout.setDrawerListener()

--- robolectric-shadows/shadows-support-v4/src/test/java/org/robolectric/shadows/support/v4/ShadowDrawerLayoutTest.java
+++ robolectric-shadows/shadows-support-v4/src/test/java/org/robolectric/shadows/support/v4/ShadowDrawerLayoutTest.java
@@ -9,6 +9,7 @@ import org.robolectric.internal.ShadowExtractor;
 import org.robolectric.shadows.support.v4.ShadowDrawerLayout;
 import org.robolectric.util.TestRunnerWithManifest;
 
+import static junit.framework.Assert.assertNull;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 
@@ -21,6 +22,27 @@ public class ShadowDrawerLayoutTest {
     DrawerLayout.DrawerListener mockDrawerListener = mock(DrawerLayout.DrawerListener.class);
     drawerLayout.setDrawerListener(mockDrawerListener);
     assertThat(shadowOf(drawerLayout).getDrawerListener()).isSameAs(mockDrawerListener);
+  }
+
+  @Test
+  public void canAddAndRemoveDrawerListener() throws Exception {
+    DrawerLayout drawerLayout = new DrawerLayout(Robolectric.buildActivity(Activity.class).create().get());
+    DrawerLayout.DrawerListener mockDrawerListener1 = mock(DrawerLayout.DrawerListener.class);
+    DrawerLayout.DrawerListener mockDrawerListener2 = mock(DrawerLayout.DrawerListener.class);
+    drawerLayout.addDrawerListener(mockDrawerListener1);
+    drawerLayout.addDrawerListener(mockDrawerListener2);
+    assertThat(shadowOf(drawerLayout).getDrawerListeners())
+            .containsExactly(mockDrawerListener1, mockDrawerListener2);
+    assertThat(shadowOf(drawerLayout).getDrawerListener()).isSameAs(mockDrawerListener1);
+
+    drawerLayout.removeDrawerListener(mockDrawerListener1);
+    assertThat(shadowOf(drawerLayout).getDrawerListeners())
+            .containsExactly(mockDrawerListener2);
+    assertThat(shadowOf(drawerLayout).getDrawerListener()).isSameAs(mockDrawerListener2);
+
+    drawerLayout.removeDrawerListener(mockDrawerListener2);
+    assertThat(shadowOf(drawerLayout).getDrawerListeners()).isEmpty();
+    assertNull(shadowOf(drawerLayout).getDrawerListener());
   }
 
   private ShadowDrawerLayout shadowOf(DrawerLayout drawerLayout) {
