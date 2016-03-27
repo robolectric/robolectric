69e8cc79aa6d36e823c5ea844e0798c33dd29b28
Dima Gerasimov <karlicos@yandex-team.ru>
2016-03-27 22:26:55 +0300

    Fix for Powermock integration

--- robolectric/src/test/java/org/robolectric/util/FragmentControllerTest.java
+++ robolectric/src/test/java/org/robolectric/util/FragmentControllerTest.java
@@ -148,7 +148,7 @@ public class FragmentControllerTest {
     assertThat(fragment.isVisible()).isTrue();
   }
 
-  private static class LoginFragment extends Fragment {
+  public static class LoginFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
       return inflater.inflate(R.layout.fragment_contents, container, false);
