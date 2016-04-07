e2a8b02c6a42d39ce130bebe2403467f4f50e5a7
Jonathan Gerrish <jongerrish@google.com>
2016-04-07 09:48:06 -0700

    Make ShadowNotification a standard Java class rather than a velocity template since API specific code is not required now that actions work on the real notification.
    Remove shadows for Style + BigTextStyle and provide accessors on ShadowNotification for extracting the fields directly from the content view.
    
    Consolidate BigTextStyle tests into ShadowNotificationBuilderTest.

--- robolectric/src/test/java/org/robolectric/shadows/ShadowNotificationBuilderTest.java
+++ robolectric/src/test/java/org/robolectric/shadows/ShadowNotificationBuilderTest.java
@@ -1,200 +1,205 @@
 package org.robolectric.shadows;
 
 import android.app.Notification;
-import android.app.Notification.Style;
 import android.app.PendingIntent;
 import android.os.Build;
+import android.widget.ProgressBar;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.robolectric.R;
 import org.robolectric.RuntimeEnvironment;
 import org.robolectric.TestRunners;
 import org.robolectric.annotation.Config;
-import org.robolectric.shadows.ShadowNotification.Progress;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.robolectric.Shadows.shadowOf;
 
 @RunWith(TestRunners.MultiApiWithDefaults.class)
 public class ShadowNotificationBuilderTest {
-  private Notification notification;
-  private ShadowNotification s;
   private final Notification.Builder builder = new Notification.Builder(RuntimeEnvironment.application);
 
   @Test
   public void build_setsContentTitleOnNotification() throws Exception {
-    builder.setContentTitle("Hello");
-    build();
-    assertThat(s.getContentTitle().toString()).isEqualTo("Hello");
+    Notification notification = builder.setContentTitle("Hello").build();
+    assertThat(shadowOf(notification).getContentTitle().toString()).isEqualTo("Hello");
   }
 
   @Test
   public void build_whenSetOngoingNotSet_leavesSetOngoingAsFalse() {
-    build();
-    assertThat(s.isOngoing()).isFalse();
+    Notification notification = builder.build();
+    assertThat(shadowOf(notification).isOngoing()).isFalse();
   }
 
   @Test
   public void build_whenSetOngoing_setsOngoingToTrue() {
-    builder.setOngoing(true);
-    build();
-    assertThat(s.isOngoing()).isTrue();
+    Notification notification = builder.setOngoing(true).build();
+    assertThat(shadowOf(notification).isOngoing()).isTrue();
   }
 
-  @Test
-  public void build_whenShowWhenNotSet_setsShowWhenOnNotificationToTrue() {
-    build();
-    assertThat(s.isWhenShown()).isTrue();
-  }
-
-  @Test
   @Config(sdk = {
       Build.VERSION_CODES.JELLY_BEAN_MR1,
       Build.VERSION_CODES.JELLY_BEAN_MR2,
       Build.VERSION_CODES.KITKAT,
-      Build.VERSION_CODES.LOLLIPOP })
+      Build.VERSION_CODES.LOLLIPOP,
+      Build.VERSION_CODES.LOLLIPOP_MR1,
+      Build.VERSION_CODES.M})
+  @Test
+  public void build_whenShowWhenNotSet_setsShowWhenOnNotificationToTrue() {
+    Notification notification = builder.setWhen(100).setShowWhen(true).build();
+
+    assertThat(shadowOf(notification).isWhenShown()).isTrue();
+  }
+
+  @Config(sdk = {
+      Build.VERSION_CODES.JELLY_BEAN_MR1,
+      Build.VERSION_CODES.JELLY_BEAN_MR2,
+      Build.VERSION_CODES.KITKAT,
+      Build.VERSION_CODES.LOLLIPOP,
+      Build.VERSION_CODES.LOLLIPOP_MR1,
+      Build.VERSION_CODES.M})
+  @Test
   public void build_setShowWhenOnNotification() {
-    builder.setShowWhen(false);
-    build();
-    assertThat(s.isWhenShown()).isFalse();
+    Notification notification = builder.setShowWhen(false).build();
+
+    assertThat(shadowOf(notification).isWhenShown()).isFalse();
   }
 
   @Test
   public void build_setsContentTextOnNotification() throws Exception {
-    builder.setContentText("Hello Text");
-    build();
-    assertThat(s.getContentText().toString()).isEqualTo("Hello Text");
+    Notification notification = builder.setContentText("Hello Text").build();
+
+    assertThat(shadowOf(notification).getContentText().toString()).isEqualTo("Hello Text");
   }
 
   @Test
   public void build_setsTickerOnNotification() throws Exception {
-    builder.setTicker("My ticker");
-    build();
-    assertThat(s.getTicker().toString()).isEqualTo("My ticker");
+    Notification notification = builder.setTicker("My ticker").build();
+
+    assertThat(notification.tickerText).isEqualTo("My ticker");
   }
 
   @Test
   public void build_setsContentInfoOnNotification() throws Exception {
     builder.setContentInfo("11");
-    build();
-    assertThat(s.getContentInfo().toString()).isEqualTo("11");
+    Notification notification = builder.build();
+    assertThat(shadowOf(notification).getContentInfo().toString()).isEqualTo("11");
   }
 
+  @Config(sdk = Build.VERSION_CODES.M)
   @Test
   public void build_setsIconOnNotification() throws Exception {
-    builder.setSmallIcon(9001);
-    build();
-    assertThat(s.getSmallIcon()).isEqualTo(9001);
+    Notification notification = builder.setSmallIcon(R.drawable.an_image).build();
+
+    assertThat(notification.getSmallIcon().getResId()).isEqualTo(R.drawable.an_image);
   }
 
   @Test
   public void build_setsWhenOnNotification() throws Exception {
-    builder.setWhen(11L);
-    build();
-    assertThat(s.getWhen()).isEqualTo(11L);
+    Notification notification = builder.setWhen(11L).build();
+
+    assertThat(notification.when).isEqualTo(11L);
   }
 
   @Test
   public void build_setsProgressOnNotification_true() throws Exception {
-    builder.setProgress(36, 57, true);
-    build();
-    Progress p = s.getProgress();
-    assertThat(p.max).isEqualTo(36);
-    assertThat(p.progress).isEqualTo(57);
-    assertThat(p.indeterminate).isTrue();
+    Notification notification = builder.setProgress(36, 57, true).build();
+
+    ProgressBar progressBar = shadowOf(notification).getProgressBar();
+    // If indeterminate then max and progress values are ignored.
+    assertThat(progressBar.isIndeterminate()).isTrue();
   }
 
   @Test
   public void build_setsProgressOnNotification_false() throws Exception {
-    builder.setProgress(34, 56, false);
-    build();
-    Progress p = s.getProgress();
-    assertThat(p.max).isEqualTo(34);
-    assertThat(p.progress).isEqualTo(56);
-    assertThat(p.indeterminate).isFalse();
+    Notification notification = builder.setProgress(50, 10, false).build();
+
+    ProgressBar progressBar = shadowOf(notification).getProgressBar();
+    assertThat(progressBar.getMax()).isEqualTo(50);
+    assertThat(progressBar.getProgress()).isEqualTo(10);
+    assertThat(progressBar.isIndeterminate()).isFalse();
   }
 
-  @Test
-  public void build_setsStyleOnNotification() throws Exception {
-    Style style = new Style() {
-      @Override
-      public Notification buildStyled(Notification notification) {
-        return notification;
-      }
-
-      @Override
-      public Notification build() {
-        return new Notification();
-      }
-    };
-    builder.setStyle(style);
-    build();
-    assertThat(s.getStyle()).isSameAs(style);
-  }
-
+  @Config(sdk = {
+      Build.VERSION_CODES.JELLY_BEAN_MR1,
+      Build.VERSION_CODES.JELLY_BEAN_MR2,
+      Build.VERSION_CODES.KITKAT,
+      Build.VERSION_CODES.LOLLIPOP,
+      Build.VERSION_CODES.LOLLIPOP_MR1,
+      Build.VERSION_CODES.M})
   @Test
   public void build_setsUsesChronometerOnNotification_true() throws Exception {
-    builder.setUsesChronometer(true);
-    build();
-    assertThat(s.usesChronometer()).isTrue();
+    Notification notification = builder.setUsesChronometer(true).setWhen(10).setShowWhen(true).build();
+
+    assertThat(shadowOf(notification).usesChronometer()).isTrue();
   }
 
+  @Config(sdk = {
+      Build.VERSION_CODES.JELLY_BEAN_MR1,
+      Build.VERSION_CODES.JELLY_BEAN_MR2,
+      Build.VERSION_CODES.KITKAT,
+      Build.VERSION_CODES.LOLLIPOP,
+      Build.VERSION_CODES.LOLLIPOP_MR1,
+      Build.VERSION_CODES.M})
   @Test
   public void build_setsUsesChronometerOnNotification_false() throws Exception {
-    builder.setUsesChronometer(false);
-    build();
-    assertThat(s.usesChronometer()).isFalse();
+    Notification notification = builder.setUsesChronometer(false).setWhen(10).setShowWhen(true).build();
+
+    assertThat(shadowOf(notification).usesChronometer()).isFalse();
   }
 
   @Test
   public void build_handlesNullContentTitle() {
-    builder.setContentTitle(null);
-    build();
-    assertThat(s.getContentTitle()).isNull();
+    Notification notification = builder.setContentTitle(null).build();
+
+    assertThat(shadowOf(notification).getContentTitle()).isEmpty();
   }
 
   @Test
   public void build_handlesNullContentText() {
-    builder.setContentText(null);
-    build();
-    assertThat(s.getContentText()).isNull();
+    Notification notification = builder.setContentText(null).build();
+
+    assertThat(shadowOf(notification).getContentText()).isEmpty();
   }
 
   @Test
   public void build_handlesNullTicker() {
-    builder.setTicker(null);
-    build();
-    assertThat(s.getTicker()).isNull();
+    Notification notification = builder.setTicker(null).build();
+
+    assertThat(notification.tickerText).isNull();
   }
 
   @Test
   public void build_handlesNullContentInfo() {
-    builder.setContentInfo(null);
-    build();
-    assertThat(s.getContentInfo()).isNull();
-  }
+    Notification notification = builder.setContentInfo(null).build();
 
-  @Test
-  public void build_handlesNullStyle() {
-    builder.setStyle(null);
-    build();
-    assertThat(s.getStyle()).isNull();
+    assertThat(shadowOf(notification).getContentInfo()).isEmpty();
   }
 
   @Test
   @Config(sdk = {
       Build.VERSION_CODES.JELLY_BEAN_MR2,
       Build.VERSION_CODES.KITKAT,
-      Build.VERSION_CODES.LOLLIPOP })
+      Build.VERSION_CODES.LOLLIPOP,
+      Build.VERSION_CODES.LOLLIPOP_MR1,
+      Build.VERSION_CODES.M})
   public void build_addsActionToNotification() throws Exception {
     PendingIntent action = PendingIntent.getBroadcast(RuntimeEnvironment.application, 0, null, 0);
-    builder.addAction(0, "Action", action);
-    build();
-    assertThat(s.getActions().get(0).actionIntent).isEqualToComparingFieldByField(action);
+    Notification notification = builder.addAction(0, "Action", action).build();
+
+    assertThat(notification.actions[0].actionIntent).isEqualToComparingFieldByField(action);
   }
 
-  private void build() {
-    notification = builder.build();
-    s = shadowOf(notification);
+  @Test
+  public void withBigTextStyle() {
+    Notification notification = builder.setStyle(new Notification.BigTextStyle(builder)
+        .bigText("BigText")
+        .setBigContentTitle("Title")
+        .setSummaryText("Summary"))
+        .build();
+
+    assertThat(shadowOf(notification).getBigText()).isEqualTo("BigText");
+    assertThat(shadowOf(notification).getBigContentTitle()).isEqualTo("Title");
+    assertThat(shadowOf(notification).getBigContentText()).isEqualTo("Summary");
   }
 }
