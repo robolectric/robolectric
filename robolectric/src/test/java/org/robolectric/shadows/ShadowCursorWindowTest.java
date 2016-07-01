9fa41fbce01c4f3f333548205ac80968ec337ac3
Grant Oakley <groakley@gmail.com>
2016-07-01 12:30:39 -0700

    Make ShadowCursorWindow return an empty byte array instead of null when querying an empty blob. This matches Android's behavior. (#2541)

--- robolectric/src/test/java/org/robolectric/shadows/ShadowCursorWindowTest.java
+++ robolectric/src/test/java/org/robolectric/shadows/ShadowCursorWindowTest.java
@@ -24,18 +24,22 @@ public class ShadowCursorWindowTest {
     MatrixCursor testCursor = new MatrixCursor(new String[] { "a", "b", "c", "d"});
     testCursor.addRow(new Object[] { 12, "hello", null, new byte[] {(byte) 0xba, (byte) 0xdc, (byte) 0xaf, (byte) 0xfe} });
     testCursor.addRow(new Object[] { 34, "baz",   1.2,  null  });
+    testCursor.addRow(new Object[] { 46, "foo",   2.4,  new byte[]{}  });
 
     DatabaseUtils.cursorFillWindow(testCursor, 0, window);
 
-    assertThat(window.getNumRows()).isEqualTo(2);
+    assertThat(window.getNumRows()).isEqualTo(3);
 
-    assertThat(window.getString(0, 1)).isEqualTo("hello");
     assertThat(window.getInt(0, 0)).isEqualTo(12);
+    assertThat(window.getString(0, 1)).isEqualTo("hello");
     assertThat(window.getString(0, 2)).isNull();
     assertThat(window.getBlob(0, 3)).isEqualTo(new byte[] {(byte) 0xba, (byte) 0xdc, (byte) 0xaf, (byte) 0xfe});
 
-    assertThat(window.getString(1, 1)).isEqualTo("baz");
     assertThat(window.getInt(1, 0)).isEqualTo(34);
+    assertThat(window.getString(1, 1)).isEqualTo("baz");
     assertThat(window.getFloat(1, 2)).isEqualTo(1.2f);
+    assertThat(window.getBlob(1, 3)).isEqualTo(null);
+
+    assertThat(window.getBlob(2, 3)).isEqualTo(new byte[]{});
   }
 }
