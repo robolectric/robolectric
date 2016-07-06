836ec3f9e271e39e86461e0be06024fa74679fd7
Christian Williams <christianw@google.com>
2016-07-06 15:59:29 -0700

    Use gradle in `install-robolectric.sh`. Update `CONTRIBUTING.md`.

--- scripts/install-robolectric.sh
+++ scripts/install-robolectric.sh
@@ -4,39 +4,20 @@
 #
 set -e
 
-APIS=(16 17 18 19 20 21 22 23)
-PARALLEL_BUILD_CONFIG="0.5C"
-
 PROJECT=$(cd $(dirname "$0")/..; pwd)
-if [ -z ${INCLUDE_SOURCE+x} ]; then SOURCE_ARG=""; else SOURCE_ARG="source:jar"; fi
-if [ -z ${INCLUDE_JAVADOC+x} ]; then JAVADOC_ARG=""; else JAVADOC_ARG="javadoc:jar"; fi
 
 build_robolectric() {
   echo "Building Robolectric..."
   cd "$PROJECT"
-  mvn -T ${PARALLEL_BUILD_CONFIG} -D skipTests clean ${SOURCE_ARG} ${JAVADOC_ARG} install
-}
-
-build_shadows() {
-  for api in "${APIS[@]}"
-  do
-     build_shadows_for_api ${api}
-  done
-}
-
-build_shadows_for_api() {
-  echo "Building shadows for API ${1}..."
-  cd "$PROJECT"/robolectric-shadows/shadows-core
-  mvn -T ${PARALLEL_BUILD_CONFIG} -P android-${1} clean ${SOURCE_ARG} ${JAVADOC_ARG} install
+  ./gradlew clean assemble publishToMavenLocal compileTest --info --stacktrace
 }
 
 run_tests() {
   echo "Running Tests..."
   cd "$PROJECT"
-  mvn -T ${PARALLEL_BUILD_CONFIG} test
+  ./gradlew --continue test --info --stacktrace
 }
 
 build_robolectric
-build_shadows
 run_tests
 echo "Installation successful!"
