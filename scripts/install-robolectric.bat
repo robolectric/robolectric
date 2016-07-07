d904486cb3c8a97a7e79c051ea245f5702ba9a10
Jonathan Gerrish <jonathan@indiekid.org>
2016-07-07 13:34:28 -0700

    Clean up docs and windows installer.

--- scripts/install-robolectric.bat
+++ scripts/install-robolectric.bat
@@ -7,39 +7,9 @@ del "%TMPPATH%"
 rem Build everything
 echo Installing base installation (skipping tests)...
 cd "%PROJECT%"
-call mvn -D skipTests clean install
-
-rem Build older shadow packages
-
-echo Installing shadows for API 16...
-cd "%PROJECT%\robolectric-shadows\shadows-core"
-call mvn -D skipTests -P android-16 clean install
-
-echo Installing shadows for API 17...
-cd "%PROJECT%\robolectric-shadows\shadows-core"
-call mvn -D skipTests -P android-17 clean install
-
-echo Installing shadows for API 18...
-cd "%PROJECT%\robolectric-shadows\shadows-core"
-call mvn -D skipTests -P android-18 clean install
-
-echo Installing shadows for API 19...
-cd "%PROJECT%\robolectric-shadows\shadows-core"
-call mvn -D skipTests -P android-19 clean install
-
-echo Installing shadows for API 21...
-cd "%PROJECT%\robolectric-shadows\shadows-core"
-call mvn -D skipTests -P android-21 clean install
-
-echo Installing shadows for API 22s...
-cd "%PROJECT%\robolectric-shadows\shadows-core"
-call mvn -D skipTests -P android-22 clean install
-
-echo Installing shadows for API 22s...
-cd "%PROJECT%\robolectric-shadows\shadows-core"
-call mvn -D skipTests -P android-23 clean install
+call gradlew clean assemble publishToMavenLocal compileTest --info --stacktrace
 
 rem Build everything with tests (tests require the shadows)
 echo Installing base installation (with tests)...
 cd "%PROJECT%"
-call mvn -P android-latest install
+call gradlew --continue test --info --stacktrace
