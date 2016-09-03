8c2a42227e584a7d9c519e453ce715d4ce5172ba
Or Bar <orbar1@gmail.com>
2016-09-03 11:55:22 -0400

    switch to 2 space indents

--- robolectric/src/main/java/org/robolectric/util/IntentServiceController.java
+++ robolectric/src/main/java/org/robolectric/util/IntentServiceController.java
@@ -6,19 +6,20 @@ import android.app.IntentService;
 import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
+
 import org.robolectric.RuntimeEnvironment;
 import org.robolectric.ShadowsAdapter;
 import org.robolectric.util.ReflectionHelpers.ClassParameter;
 
 public class IntentServiceController<T extends IntentService> extends ComponentController<IntentServiceController<T>, T> {
-    private final  String shadowActivityThreadClassName;
+  private final String shadowActivityThreadClassName;
 
-    public static <T extends IntentService> IntentServiceController<T> of(final ShadowsAdapter shadowsAdapter,
-                                                                          final T service,
-                                                                          final Intent intent) {
-        final IntentServiceController<T> controller = new IntentServiceController<>(shadowsAdapter, service, intent);
-        controller.attach();
-        return controller;
+  public static <T extends IntentService> IntentServiceController<T> of(final ShadowsAdapter shadowsAdapter,
+                                                                        final T service,
+                                                                        final Intent intent) {
+    final IntentServiceController<T> controller = new IntentServiceController<>(shadowsAdapter, service, intent);
+      controller.attach();
+      return controller;
     }
 
     private IntentServiceController(final ShadowsAdapter shadowsAdapter, final T service, final Intent intent) {
@@ -27,66 +28,66 @@ public class IntentServiceController<T extends IntentService> extends ComponentC
     }
 
     public IntentServiceController<T> attach() {
-        if (attached) {
-            return this;
-        }
-
-        final Context baseContext = RuntimeEnvironment.application.getBaseContext();
-
-        final ClassLoader cl = baseContext.getClassLoader();
-        final Class<?> activityThreadClass;
-        try {
-            activityThreadClass = cl.loadClass(shadowActivityThreadClassName);
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
-        }
-
-        ReflectionHelpers.callInstanceMethod(Service.class, component, "attach",
-                ReflectionHelpers.ClassParameter.from(Context.class, baseContext),
-                ReflectionHelpers.ClassParameter.from(activityThreadClass, null),
-                ReflectionHelpers.ClassParameter.from(String.class, component.getClass().getSimpleName()),
-                ReflectionHelpers.ClassParameter.from(IBinder.class, null),
-                ReflectionHelpers.ClassParameter.from(Application.class, RuntimeEnvironment.application),
-                ReflectionHelpers.ClassParameter.from(Object.class, null));
-
-        attached = true;
+      if (attached) {
         return this;
+      }
+
+      final Context baseContext = RuntimeEnvironment.application.getBaseContext();
+
+      final ClassLoader cl = baseContext.getClassLoader();
+      final Class<?> activityThreadClass;
+        try {
+          activityThreadClass = cl.loadClass(shadowActivityThreadClassName);
+        } catch (ClassNotFoundException e) {
+          e.printStackTrace();
+          throw new RuntimeException(e);
+        }
+
+      ReflectionHelpers.callInstanceMethod(Service.class, component, "attach",
+         ReflectionHelpers.ClassParameter.from(Context.class, baseContext),
+         ReflectionHelpers.ClassParameter.from(activityThreadClass, null),
+         ReflectionHelpers.ClassParameter.from(String.class, component.getClass().getSimpleName()),
+         ReflectionHelpers.ClassParameter.from(IBinder.class, null),
+         ReflectionHelpers.ClassParameter.from(Application.class, RuntimeEnvironment.application),
+         ReflectionHelpers.ClassParameter.from(Object.class, null));
+
+      attached = true;
+      return this;
     }
 
     public IntentServiceController<T> bind() {
-        invokeWhilePaused("onBind", getIntent());
-        return this;
+      invokeWhilePaused("onBind", getIntent());
+      return this;
     }
 
     public IntentServiceController<T> create() {
-        invokeWhilePaused("onCreate");
-        return this;
+      invokeWhilePaused("onCreate");
+      return this;
     }
 
     public IntentServiceController<T> destroy() {
-        invokeWhilePaused("onDestroy");
-        return this;
+      invokeWhilePaused("onDestroy");
+      return this;
     }
 
     public IntentServiceController<T> rebind() {
-        invokeWhilePaused("onRebind", getIntent());
-        return this;
+      invokeWhilePaused("onRebind", getIntent());
+      return this;
     }
 
     public IntentServiceController<T> startCommand(final int flags, final int startId) {
-        final IntentServiceController<T> intentServiceController = handleIntent();
-        get().stopSelf(startId);
-        return intentServiceController;
+      final IntentServiceController<T> intentServiceController = handleIntent();
+      get().stopSelf(startId);
+      return intentServiceController;
     }
 
     public IntentServiceController<T> unbind() {
-        invokeWhilePaused("onUnbind", getIntent());
-        return this;
+      invokeWhilePaused("onUnbind", getIntent());
+      return this;
     }
 
     public IntentServiceController<T> handleIntent() {
-        invokeWhilePaused("onHandleIntent", getIntent());
-        return this;
+      invokeWhilePaused("onHandleIntent", getIntent());
+      return this;
     }
 }
