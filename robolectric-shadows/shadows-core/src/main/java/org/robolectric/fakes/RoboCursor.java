67b0ad99d5a6bbccbf846f23d8c645a592ff5f9d
Jesse Weinstein <jesse.weinstein@zonarsystems.com>
2016-11-07 14:03:46 -0800

    Add some missing methods

--- robolectric-shadows/shadows-core/src/main/java/org/robolectric/fakes/RoboCursor.java
+++ robolectric-shadows/shadows-core/src/main/java/org/robolectric/fakes/RoboCursor.java
@@ -65,7 +65,7 @@ public class RoboCursor extends BaseCursor {
 
   @Override
   public boolean moveToNext() {
-    return moveToPosition(resultsIndex + 1);
+    return move(1);
   }
 
   @Override
@@ -117,6 +117,50 @@ public class RoboCursor extends BaseCursor {
   @Override
   public boolean isLast() {
     return resultsIndex == results.length - 1;
+  }
+
+  @Override public int getPosition() {
+    return resultsIndex;
+  }
+
+  @Override public boolean move(int offset) {
+    return moveToPosition(resultsIndex + offset);
+  }
+
+  @Override public boolean moveToLast() {
+    return moveToPosition(results.length - 1);
+  }
+
+  @Override public boolean moveToPrevious() {
+    return move(-1);
+  }
+
+  @Override public String[] getColumnNames() {
+    return columnNames.toArray(new String[columnNames.size()]);
+  }
+
+  @Override public byte[] getBlob(int columnIndex) {
+    return (byte[]) results[resultsIndex][columnIndex];
+  }
+
+  @Override public short getShort(int columnIndex) {
+    return (Short) results[resultsIndex][columnIndex];
+  }
+
+  @Override public float getFloat(int columnIndex) {
+    return (Float) results[resultsIndex][columnIndex];
+  }
+
+  @Override public double getDouble(int columnIndex) {
+    return (Double) results[resultsIndex][columnIndex];
+  }
+
+  @Override public boolean isNull(int columnIndex) {
+    return results[resultsIndex][columnIndex] == null;
+  }
+
+  @Override public boolean isClosed() {
+    return closeWasCalled;
   }
 
   public void setColumnNames(List<String> columnNames) {
