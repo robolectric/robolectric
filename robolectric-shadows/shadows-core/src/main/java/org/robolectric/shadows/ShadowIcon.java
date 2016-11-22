1f61c41f743f419dddeab3632cb7398183070c2c
Michael Hoisie <hoisie@google.com>
2016-11-21 23:09:03 -0800

    Add ShadowIcon
    There are some hidden public methods on the Icon class that are useful
    for test-related assertions.
    
    For example, if a test displays a notification, it may be useful to
    examine the icons in the notification or the icons that belong to
    actions in the notification.
    
    Currently tests for this class require Android L+ due to the reference
    to Drawable.DEFAULT_TINT_MODE, which is only available in Android L and
    above.

---
+++ robolectric-shadows/shadows-core/src/main/java/org/robolectric/shadows/ShadowIcon.java
@@ -0,0 +1,56 @@
+package org.robolectric.shadows;
+
+import android.graphics.Bitmap;
+import android.graphics.drawable.Icon;
+import android.net.Uri;
+import org.robolectric.annotation.Implementation;
+import org.robolectric.annotation.Implements;
+import org.robolectric.annotation.RealObject;
+
+import static org.robolectric.internal.Shadow.directlyOn;
+
+/**
+ * Shadow for {@link android.graphics.drawable.Icon}.
+ */
+@SuppressWarnings({"UnusedDeclaration"})
+@Implements(Icon.class)
+public class ShadowIcon {
+
+  @RealObject
+  private Icon realIcon;
+
+  @Implementation
+  public int getType() {
+    return directlyOn(realIcon, Icon.class, "getType");
+  }
+
+  @Implementation
+  public int getResId() {
+    return directlyOn(realIcon, Icon.class, "getResId");
+  }
+
+  @Implementation
+  public Bitmap getBitmap() {
+    return directlyOn(realIcon, Icon.class, "getBitmap");
+  }
+
+  @Implementation
+  public Uri getUri() {
+    return directlyOn(realIcon, Icon.class, "getUri");
+  }
+
+  @Implementation
+  public int getDataLength() {
+    return directlyOn(realIcon, Icon.class, "getDataLength");
+  }
+
+  @Implementation
+  public int getDataOffset() {
+    return directlyOn(realIcon, Icon.class, "getDataOffset");
+  }
+
+  @Implementation
+  public byte[] getDataBytes() {
+    return directlyOn(realIcon, Icon.class, "getDataBytes");
+  }
+}
