3e9edbfb0df45f6372f691d04d86d0adbb6d6e56
Jonathan Gerrish <jonathan@indiekid.rg>
2016-12-28 11:27:48 -0800

    * Pull up PackageResourceIndex into ResourceIndex

--- robolectric/src/test/java/org/robolectric/res/ResourceIndexTest.java
+++ robolectric/src/test/java/org/robolectric/res/ResourceIndexTest.java
@@ -7,11 +7,11 @@ import org.junit.runners.JUnit4;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(JUnit4.class)
-public class PackageResourceIndexTest {
+public class ResourceIndexTest {
 
   @Test
   public void getPackageName_shouldReturnPackageNameOfItsResources() {
-    PackageResourceIndex resourceIndex = new PackageResourceIndex("myPackage");
+    ResourceIndex resourceIndex = new ResourceIndex("myPackage");
     resourceIndex.addResource(0x02999999, "type", "name");
 
     assertThat(resourceIndex.getPackageName()).isEqualTo("myPackage");
@@ -19,7 +19,7 @@ public class PackageResourceIndexTest {
 
   @Test
   public void getPackageIdentifier_shouldReturnPackageIdentiferOfItsResources() {
-    PackageResourceIndex resourceIndex = new PackageResourceIndex("myPackage");
+    ResourceIndex resourceIndex = new ResourceIndex("myPackage");
     resourceIndex.addResource(0x02999999, "type", "name");
 
     assertThat(resourceIndex.getPackageIdentifier()).isEqualTo(0x02);
@@ -27,21 +27,21 @@ public class PackageResourceIndexTest {
 
   @Test(expected = IllegalArgumentException.class)
   public void addResource_shouldPreventMixedPackageIdentifiers() {
-    PackageResourceIndex resourceIndex = new PackageResourceIndex("myPackage");
+    ResourceIndex resourceIndex = new ResourceIndex("myPackage");
     resourceIndex.addResource(0x02999999, "type", "name");
     resourceIndex.addResource(0x03999999, "type", "name");
   }
 
   @Test(expected = IllegalArgumentException.class)
   public void shouldForbidIdClashes() {
-    PackageResourceIndex resourceIndex = new PackageResourceIndex("myPackage");
+    ResourceIndex resourceIndex = new ResourceIndex("myPackage");
     resourceIndex.addResource(0x02888888, "type", "name");
     resourceIndex.addResource(0x02999999, "type", "name");
   }
 
   @Test(expected = IllegalArgumentException.class)
   public void shouldForbidDuplicateNames() {
-    PackageResourceIndex resourceIndex = new PackageResourceIndex("myPackage");
+    ResourceIndex resourceIndex = new ResourceIndex("myPackage");
     resourceIndex.addResource(0x02999999, "type", "name");
     resourceIndex.addResource(0x02999999, "type", "anotherName");
   }
