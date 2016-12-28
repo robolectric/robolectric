97e5e1bb6e4229d9be2c966df843a478320a561a
Jonathan Gerrish <jonathan@indiekid.rg>
2016-12-28 13:22:45 -0800

    * ResourceTableFactory now encapsulates the calls to ResourceParser. This gives a single entry point for building a ResourceTable meaning it will be easy to switch out ResourceTableFactories for different strategies of building ResourceTables.

--- robolectric-resources/src/main/java/org/robolectric/res/ResourceTableFactory.java
+++ robolectric-resources/src/main/java/org/robolectric/res/ResourceTableFactory.java
@@ -5,32 +5,43 @@ import java.lang.reflect.Modifier;
 
 public class ResourceTableFactory {
 
-  public static ResourceTable newResourceTable(String packageName, Class<?>... rClasses) {
+  public static ResourceTable newResourceTable(String packageName, ResourcePath... resourcePaths) {
     ResourceTable resourceTable = new ResourceTable(packageName);
 
-    for (Class<?> rClass : rClasses) {
-      if (rClass != null) {
-        for (Class innerClass : rClass.getClasses()) {
-          for (Field field : innerClass.getDeclaredFields()) {
-            if (field.getType().equals(Integer.TYPE) && Modifier.isStatic(field.getModifiers())) {
-              String resourceType = innerClass.getSimpleName();
-              int id;
-              try {
-                id = field.getInt(null);
-              } catch (IllegalAccessException e) {
-                throw new RuntimeException(e);
-              }
+    for (ResourcePath resourcePath : resourcePaths) {
+      if (resourcePath.getRClass() != null) {
+        addRClassValues(resourceTable, resourcePath.getRClass());
+      }
+      if (resourcePath.getInternalRClass() != null) {
+        addRClassValues(resourceTable, resourcePath.getInternalRClass());
+      }
+    }
 
-              if (!resourceType.equals("styleable")) {
-                String resourceName = field.getName();
-                resourceTable.addResource(id, resourceType, resourceName);
-              }
-            }
+    for (ResourcePath resourcePath : resourcePaths) {
+      ResourceParser.load(packageName, resourcePath, resourceTable);
+    }
+
+    return resourceTable;
+  }
+
+  private static void addRClassValues(ResourceTable resourceTable, Class<?> rClass) {
+    for (Class innerClass : rClass.getClasses()) {
+      for (Field field : innerClass.getDeclaredFields()) {
+        if (field.getType().equals(Integer.TYPE) && Modifier.isStatic(field.getModifiers())) {
+          String resourceType = innerClass.getSimpleName();
+          int id;
+          try {
+            id = field.getInt(null);
+          } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+          }
+
+          if (!resourceType.equals("styleable")) {
+            String resourceName = field.getName();
+            resourceTable.addResource(id, resourceType, resourceName);
           }
         }
       }
     }
-
-    return resourceTable;
   }
 }
