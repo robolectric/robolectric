597db9a7a7cb0cd3c06b577694f6ea03eb27a99f
Christian Williams <christianw@google.com>
2017-01-05 18:06:18 -0800

    Fix snapshot uploads.
    Don't upload javadoc with snapshots, the `javadoc` task is really slow.

--- scripts/deploy-snapshot.sh
+++ scripts/deploy-snapshot.sh
@@ -9,5 +9,5 @@ PROJECT=$(cd $(dirname "$0")/..; pwd)
 
 echo "Pull request: '${TRAVIS_PULL_REQUEST}' on branch '${TRAVIS_BRANCH}' with JDK '${TRAVIS_JDK_VERSION}'"
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ] && [ "${TRAVIS_JDK_VERSION}" = "oraclejdk8" ]; then
-    ./gradlew publish
+    SKIP_JAVADOC=true ./gradlew upload --info --stacktrace
 fi
