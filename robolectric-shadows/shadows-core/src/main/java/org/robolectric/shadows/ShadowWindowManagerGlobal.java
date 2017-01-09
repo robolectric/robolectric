118fb370a3deaa3862a84fdb7fae31d5df076c4b
Christian Williams <christianw@google.com>
2017-01-09 14:05:45 -0800

    Fix memory leak from WindowManagerGlobal.
    Reset static instance so we don't hold on to views between tests.

--- robolectric-shadows/shadows-core/src/main/java/org/robolectric/shadows/ShadowWindowManagerGlobal.java
+++ robolectric-shadows/shadows-core/src/main/java/org/robolectric/shadows/ShadowWindowManagerGlobal.java
@@ -4,6 +4,8 @@ import android.os.Looper;
 import android.view.WindowManagerGlobal;
 import org.robolectric.annotation.Implementation;
 import org.robolectric.annotation.Implements;
+import org.robolectric.annotation.Resetter;
+import org.robolectric.util.ReflectionHelpers;
 
 import static android.os.Build.VERSION_CODES.JELLY_BEAN_MR1;
 
@@ -12,6 +14,11 @@ import static android.os.Build.VERSION_CODES.JELLY_BEAN_MR1;
  */
 @Implements(value = WindowManagerGlobal.class, isInAndroidSdk = false, minSdk = JELLY_BEAN_MR1)
 public class ShadowWindowManagerGlobal {
+
+  @Resetter
+  public static void reset() {
+    ReflectionHelpers.setStaticField(WindowManagerGlobal.class, "sDefaultWindowManager", null);
+  }
 
   @Implementation
   public static Object getWindowSession() {
