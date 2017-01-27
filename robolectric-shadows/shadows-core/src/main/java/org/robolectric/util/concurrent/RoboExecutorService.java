973324615b1366dfeee0b46e024f95e09b022caa
Christian Williams <christianw@google.com>
2017-01-26 18:46:19 -0800

    Restore and @deprecate RoboExecutorService at old location.

---
+++ robolectric-shadows/shadows-core/src/main/java/org/robolectric/util/concurrent/RoboExecutorService.java
@@ -0,0 +1,8 @@
+package org.robolectric.util.concurrent;
+
+/**
+ * @deprecated Use {@link org.robolectric.android.util.concurrent.RoboExecutorService} instead.
+ */
+@Deprecated
+public class RoboExecutorService extends org.robolectric.android.util.concurrent.RoboExecutorService {
+}
