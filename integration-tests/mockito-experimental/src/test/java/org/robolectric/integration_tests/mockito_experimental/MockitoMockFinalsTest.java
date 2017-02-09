28be66b1e275541990c0be4bb57635c313b76272
Christian Williams <christianw@google.com>
2017-02-08 18:07:57 -0800

    Better test for mockito final mocking.

--- integration-tests/mockito-experimental/src/test/java/org/robolectric/integration_tests/mockito_experimental/MockitoMockFinalsTest.java
+++ integration-tests/mockito-experimental/src/test/java/org/robolectric/integration_tests/mockito_experimental/MockitoMockFinalsTest.java
@@ -1,17 +1,18 @@
 package org.robolectric.integration_tests.mockito_experimental;
 
-import android.app.Activity;
-import android.support.v4.app.FragmentActivity;
+import android.text.Layout;
 import android.widget.TextView;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
-import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @RunWith(RobolectricTestRunner.class)
 public class MockitoMockFinalsTest {
@@ -21,11 +22,24 @@ public class MockitoMockFinalsTest {
   @Mock
   TextView textView;
 
-  @InjectMocks
-  Activity activity = Robolectric.setupActivity(FragmentActivity.class);
-
   @Test
   public void testInjection() {
-    activity.finish();
+    Layout layout = mock(Layout.class);
+    when(textView.getLayout()).thenReturn(layout);
+    assertThat(textView.getLayout()).isSameAs(layout);
+  }
+
+  @Test
+  public void canMockUserId() {
+    User user = mock(User.class);
+    when(user.getId()).thenReturn(1);
+    assertThat(user.getId()).isEqualTo(1);
+  }
+
+
+  final static class User {
+    final int getId() {
+      return -1;
+    }
   }
 }
