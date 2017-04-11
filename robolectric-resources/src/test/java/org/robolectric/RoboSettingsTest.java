5d9d92887db95581de9cfc96f992e518c2354794
jongerrish <jongerrish@google.com>
2017-04-11 23:22:30 +0000

    Added @RunWith(JUnit4.class) to all tests
    PiperOrigin-RevId: 152876012

--- robolectric-resources/src/test/java/org/robolectric/RoboSettingsTest.java
+++ robolectric-resources/src/test/java/org/robolectric/RoboSettingsTest.java
@@ -3,9 +3,12 @@ package org.robolectric;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
 import static org.junit.Assert.*;
 
+@RunWith(JUnit4.class)
 public class RoboSettingsTest {
 
   private String originalMavenRepositoryId;
