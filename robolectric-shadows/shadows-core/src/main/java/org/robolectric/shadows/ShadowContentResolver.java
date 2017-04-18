b18061570a96ae665b625aa89e2a8e600eb71bed
Christian Williams <christianw@google.com>
2017-04-18 14:10:45 -0700

    ShadowContentResolver improvements.
    Add #getStatements() including insert, bulkInsert, update, and delete.
    All insert, bulkInsert, update, and delete calls cause statements to be
    	recorded for verification.
    ShadowContentResolver#setNextDatabaseIdForUpdates is deprecated; #update
    	now returns 1 (row) by default.
    Better javadoc.

--- robolectric-shadows/shadows-core/src/main/java/org/robolectric/shadows/ShadowContentResolver.java
+++ robolectric-shadows/shadows-core/src/main/java/org/robolectric/shadows/ShadowContentResolver.java
@@ -23,9 +23,9 @@ import org.robolectric.annotation.Implements;
 import org.robolectric.annotation.RealObject;
 import org.robolectric.annotation.Resetter;
 import org.robolectric.fakes.BaseCursor;
-import org.robolectric.shadow.api.Shadow;
 import org.robolectric.manifest.AndroidManifest;
 import org.robolectric.manifest.ContentProviderData;
+import org.robolectric.shadow.api.Shadow;
 import org.robolectric.util.NamedStream;
 
 import java.io.FileNotFoundException;
@@ -48,11 +48,12 @@ import static org.robolectric.Shadows.shadowOf;
 @Implements(ContentResolver.class)
 public class ShadowContentResolver {
   private int nextDatabaseIdForInserts;
-  private int nextDatabaseIdForUpdates;
+  private int nextDatabaseIdForUpdates = -1;
 
   @RealObject ContentResolver realContentResolver;
 
   private BaseCursor cursor;
+  private final List<Statement> statements = new ArrayList<>();
   private final List<InsertStatement> insertStatements = new ArrayList<>();
   private final List<UpdateStatement> updateStatements = new ArrayList<>();
   private final List<DeleteStatement> deleteStatements = new ArrayList<>();
@@ -125,27 +126,54 @@ public class ShadowContentResolver {
     };
   }
 
+  /**
+   * If a {@link ContentProvider} is registered for the given {@link Uri}, its
+   * {@link ContentProvider#insert(Uri, ContentValues)} method will be invoked.
+   *
+   * Tests can verify that this method was called using {@link #getStatements()} or
+   * {@link #getInsertStatements()}.
+   *
+   * If no appropriate {@link ContentProvider} is found, no action will be taken and
+   * a {@link Uri} including the incremented value set with {@link #setNextDatabaseIdForInserts(int)} will returned.
+   */
   @Implementation
   public final Uri insert(Uri url, ContentValues values) {
     ContentProvider provider = getProvider(url);
+
+    InsertStatement insertStatement = new InsertStatement(url, provider, new ContentValues(values));
+    statements.add(insertStatement);
+    insertStatements.add(insertStatement);
+
     if (provider != null) {
       return provider.insert(url, values);
     } else {
-      InsertStatement insertStatement = new InsertStatement(url, new ContentValues(values));
-      insertStatements.add(insertStatement);
       return Uri.parse(url.toString() + "/" + ++nextDatabaseIdForInserts);
     }
   }
 
+  /**
+   * If a {@link ContentProvider} is registered for the given {@link Uri}, its
+   * {@link ContentProvider#update(Uri, ContentValues, String, String[])} method will be invoked.
+   *
+   * Tests can verify that this method was called using {@link #getStatements()} or
+   * {@link #getUpdateStatements()}.
+   *
+   * If no appropriate {@link ContentProvider} is found, no action will be taken and
+   * the value set with {@link #setNextDatabaseIdForUpdates(int)} will be incremented and returned.
+   * *Note:* the return value in this case will be changed to 1 in a future release of Robolectric.
+   */
   @Implementation
   public int update(Uri uri, ContentValues values, String where, String[] selectionArgs) {
     ContentProvider provider = getProvider(uri);
+
+    UpdateStatement updateStatement = new UpdateStatement(uri, provider, new ContentValues(values), where, selectionArgs);
+    statements.add(updateStatement);
+    updateStatements.add(updateStatement);
+
     if (provider != null) {
       return provider.update(uri, values, where, selectionArgs);
     } else {
-      UpdateStatement updateStatement = new UpdateStatement(uri, new ContentValues(values), where, selectionArgs);
-      updateStatements.add(updateStatement);
-      return ++nextDatabaseIdForUpdates;
+      return nextDatabaseIdForUpdates == -1 ? 1 : ++nextDatabaseIdForUpdates;
     }
   }
 
@@ -268,28 +296,62 @@ public class ShadowContentResolver {
     return null;
   }
 
+  /**
+   * If a {@link ContentProvider} is registered for the given {@link Uri}, its
+   * {@link ContentProvider#delete(Uri, String, String[])} method will be invoked.
+   *
+   * Tests can verify that this method was called using {@link #getDeleteStatements()}
+   * or {@link #getDeletedUris()}.
+   *
+   * If no appropriate {@link ContentProvider} is found, no action will be taken and
+   * <code>1</code> will be returned.
+   */
   @Implementation
   public final int delete(Uri url, String where, String[] selectionArgs) {
     ContentProvider provider = getProvider(url);
+
+    DeleteStatement deleteStatement = new DeleteStatement(url, provider, where, selectionArgs);
+    statements.add(deleteStatement);
+    deleteStatements.add(deleteStatement);
+
     if (provider != null) {
       return provider.delete(url, where, selectionArgs);
     } else {
-      DeleteStatement deleteStatement = new DeleteStatement(url, where, selectionArgs);
-      deleteStatements.add(deleteStatement);
       return 1;
     }
   }
 
+  /**
+   * If a {@link ContentProvider} is registered for the given {@link Uri}, its
+   * {@link ContentProvider#bulkInsert(Uri, ContentValues[])} method will be invoked.
+   *
+   * Tests can verify that this method was called using {@link #getStatements()} or
+   * {@link #getInsertStatements()}.
+   *
+   * If no appropriate {@link ContentProvider} is found, no action will be taken and
+   * the number of rows in <code>values</code> will be returned.
+   */
   @Implementation
   public final int bulkInsert(Uri url, ContentValues[] values) {
     ContentProvider provider = getProvider(url);
+
+    InsertStatement insertStatement = new InsertStatement(url, provider, values);
+    statements.add(insertStatement);
+    insertStatements.add(insertStatement);
+
     if (provider != null) {
       return provider.bulkInsert(url, values);
     } else {
-      return 0;
+      return values.length;
     }
   }
 
+  /**
+   *
+   * @param uri
+   * @param observer
+   * @param syncToNetwork
+   */
   @Implementation
   public void notifyChange(Uri uri, ContentObserver observer, boolean syncToNetwork) {
     notifiedUris.add(new NotifiedUri(uri, observer, syncToNetwork));
@@ -493,22 +555,59 @@ public class ShadowContentResolver {
     this.uriCursorMap.put(uri, cursorForUri);
   }
 
+  @SuppressWarnings({"unused", "WeakerAccess"})
   public void setNextDatabaseIdForInserts(int nextId) {
     nextDatabaseIdForInserts = nextId;
   }
 
+  /**
+   * Set the value to be returned by {@link ContentResolver#update(Uri, ContentValues, String, String[])}
+   * when no appropriate {@link ContentProvider} can be found.
+   *
+   * @deprecated This method will be removed in Robolectric 3.5. Instead, <code>1</code> will be returned.
+   *
+   * @param nextId the number of rows to return
+   */
+  @SuppressWarnings({"unused", "WeakerAccess"})
   public void setNextDatabaseIdForUpdates(int nextId) {
     nextDatabaseIdForUpdates = nextId;
   }
 
+  /**
+   * Returns the list of {@link InsertStatement}s, {@link UpdateStatement}s, and
+   * {@link DeleteStatement}s invoked on this {@link ContentResolver}.
+   *
+   * @return a list of statements
+   */
+  @SuppressWarnings({"unused", "WeakerAccess"})
+  public List<Statement> getStatements() {
+    return statements;
+  }
+
+  /**
+   * Returns the list of {@link InsertStatement}s for corresponding calls to
+   * {@link ContentResolver#insert(Uri, ContentValues)} or
+   * {@link ContentResolver#bulkInsert(Uri, ContentValues[])}.
+   *
+   * @return a list of insert statements
+   */
+  @SuppressWarnings({"unused", "WeakerAccess"})
   public List<InsertStatement> getInsertStatements() {
     return insertStatements;
   }
 
+  /**
+   * Returns the list of {@link UpdateStatement}s for corresponding calls to
+   * {@link ContentResolver#update(Uri, ContentValues, String, String[])}.
+   *
+   * @return a list of update statements
+   */
+  @SuppressWarnings({"unused", "WeakerAccess"})
   public List<UpdateStatement> getUpdateStatements() {
     return updateStatements;
   }
 
+  @SuppressWarnings({"unused", "WeakerAccess"})
   public List<Uri> getDeletedUris() {
     List<Uri> uris = new ArrayList<>();
     for (DeleteStatement deleteStatement : deleteStatements) {
@@ -517,10 +616,18 @@ public class ShadowContentResolver {
     return uris;
   }
 
+  /**
+   * Returns the list of {@link DeleteStatement}s for corresponding calls to
+   * {@link ContentResolver#delete(Uri, String, String[])}.
+   *
+   * @return a list of delete statements
+   */
+  @SuppressWarnings({"unused", "WeakerAccess"})
   public List<DeleteStatement> getDeleteStatements() {
     return deleteStatements;
   }
 
+  @SuppressWarnings({"unused", "WeakerAccess"})
   public List<NotifiedUri> getNotifiedUris() {
     return notifiedUris;
   }
@@ -565,18 +672,23 @@ public class ShadowContentResolver {
   }
 
   /**
-   * Non-Android accessor.  Clears the list of registered content observers.
-   * Commonly used in test case setup.
+   * Clears the list of registered {@link ContentObserver}s.
+   *
+   * Since a new {@link ContentResolver} is created for each test case,
+   * this method generally need not be called explicitly.
    */
+  @SuppressWarnings({"unused", "WeakerAccess"})
   synchronized public void clearContentObservers() {
     contentObservers.clear();
   }
 
   /**
-   * Non-Android accessor. Returns the content observers registered with
-   * the given URI, will be empty if no observer is registered.
+   * Returns the content observers registered with the given URI.
+   *
+   * Will be empty if no observer is registered.
+   *
    * @param uri Given URI
-   * @return The content observers
+   * @return The content observers, or null.
    */
   synchronized public Collection<ContentObserver> getContentObservers( Uri uri ) {
     CopyOnWriteArraySet<ContentObserver> observers = contentObservers.get(uri);
@@ -635,73 +747,96 @@ public class ShadowContentResolver {
     return true;
   }
 
-  public static class InsertStatement {
+  public static class Statement {
     private final Uri uri;
-    private final ContentValues contentValues;
+    private final ContentProvider contentProvider;
 
-    public InsertStatement(Uri uri, ContentValues contentValues) {
+    Statement(Uri uri, ContentProvider contentProvider) {
       this.uri = uri;
-      this.contentValues = contentValues;
+      this.contentProvider = contentProvider;
     }
 
     public Uri getUri() {
       return uri;
     }
 
-    public ContentValues getContentValues() {
-      return contentValues;
+    @SuppressWarnings({"unused", "WeakerAccess"})
+    public ContentProvider getContentProvider() {
+      return contentProvider;
     }
   }
 
-  public static class UpdateStatement {
-    private final Uri uri;
+  public static class InsertStatement extends Statement {
+    private final ContentValues[] bulkContentValues;
+
+    InsertStatement(Uri uri, ContentProvider contentProvider, ContentValues contentValues) {
+      super(uri, contentProvider);
+      this.bulkContentValues = new ContentValues[]{contentValues};
+    }
+
+    InsertStatement(Uri uri, ContentProvider contentProvider, ContentValues[] bulkContentValues) {
+      super(uri, contentProvider);
+      this.bulkContentValues = bulkContentValues;
+    }
+
+    @SuppressWarnings({"unused", "WeakerAccess"})
+    public ContentValues getContentValues() {
+      if (bulkContentValues.length != 1) {
+        throw new ArrayIndexOutOfBoundsException("bulk insert, use getBulkContentValues() instead");
+      }
+      return bulkContentValues[0];
+    }
+
+    @SuppressWarnings({"unused", "WeakerAccess"})
+    public ContentValues[] getBulkContentValues() {
+      return bulkContentValues;
+    }
+  }
+
+  public static class UpdateStatement extends Statement {
     private final ContentValues values;
     private final String where;
     private final String[] selectionArgs;
 
-    public UpdateStatement(Uri uri, ContentValues values, String where, String[] selectionArgs) {
-      this.uri = uri;
+    UpdateStatement(Uri uri, ContentProvider contentProvider, ContentValues values, String where, String[] selectionArgs) {
+      super(uri, contentProvider);
       this.values = values;
       this.where = where;
       this.selectionArgs = selectionArgs;
     }
 
-    public Uri getUri() {
-      return uri;
-    }
-
+    @SuppressWarnings({"unused", "WeakerAccess"})
     public ContentValues getContentValues() {
       return values;
     }
 
+    @SuppressWarnings({"unused", "WeakerAccess"})
     public String getWhere() {
       return where;
     }
 
+    @SuppressWarnings({"unused", "WeakerAccess"})
     public String[] getSelectionArgs() {
       return selectionArgs;
     }
   }
 
-  public static class DeleteStatement {
-    private final Uri uri;
+  public static class DeleteStatement extends Statement {
     private final String where;
     private final String[] selectionArgs;
 
-    public DeleteStatement(Uri uri, String where, String[] selectionArgs) {
-      this.uri = uri;
+    DeleteStatement(Uri uri, ContentProvider contentProvider, String where, String[] selectionArgs) {
+      super(uri, contentProvider);
       this.where = where;
       this.selectionArgs = selectionArgs;
     }
 
-    public Uri getUri() {
-      return uri;
-    }
-
+    @SuppressWarnings({"unused", "WeakerAccess"})
     public String getWhere() {
       return where;
     }
 
+    @SuppressWarnings({"unused", "WeakerAccess"})
     public String[] getSelectionArgs() {
       return selectionArgs;
     }
@@ -710,7 +845,7 @@ public class ShadowContentResolver {
   private static class UnregisteredInputStream extends InputStream implements NamedStream {
     private final Uri uri;
 
-    public UnregisteredInputStream(Uri uri) {
+    UnregisteredInputStream(Uri uri) {
       this.uri = uri;
     }
 
