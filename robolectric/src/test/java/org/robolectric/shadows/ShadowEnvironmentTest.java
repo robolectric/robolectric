7365a5e33183c7c25e50fa91a5c3b0232b8df683
Christian Williams <christianw@google.com>
2017-06-01 17:35:06 -0700

    Revert "Add improved support for external directories in ShadowEnvironment. (#3140)"
    This reverts commit 8c5c450bb3d4cf5034e299e4fd2b1176227862b3.

--- robolectric/src/test/java/org/robolectric/shadows/ShadowEnvironmentTest.java
+++ robolectric/src/test/java/org/robolectric/shadows/ShadowEnvironmentTest.java
@@ -2,19 +2,14 @@ package org.robolectric.shadows;
 
 import android.os.Environment;
 import org.junit.After;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.robolectric.RuntimeEnvironment;
 import org.robolectric.TestRunners;
 import org.robolectric.annotation.Config;
 
 import java.io.File;
 
-import static android.os.Build.VERSION_CODES.JELLY_BEAN_MR1;
-import static android.os.Build.VERSION_CODES.KITKAT;
 import static android.os.Build.VERSION_CODES.LOLLIPOP;
-import static android.os.Build.VERSION_CODES.LOLLIPOP_MR1;
 import static android.os.Build.VERSION_CODES.M;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertNotNull;
@@ -135,85 +130,6 @@ public class ShadowEnvironmentTest {
     ShadowEnvironment.setIsExternalStorageEmulated(true);
     assertThat(Environment.isExternalStorageEmulated()).isTrue();
     ShadowEnvironment.reset();
-    assertThat(Environment.isExternalStorageEmulated()).isFalse();
-  }
-
-  // TODO: failing test
-  @Ignore
-  @Test
-  @Config(minSdk = KITKAT)
-  public void getExternalFilesDirs() throws Exception {
-    ShadowEnvironment.addExternalDir("external_dir_1");
-    ShadowEnvironment.addExternalDir("external_dir_2");
-
-    File[] externalFilesDirs =
-        RuntimeEnvironment.application.getExternalFilesDirs(Environment.DIRECTORY_MOVIES);
-
-    assertThat(externalFilesDirs).isNotEmpty();
-    assertThat(externalFilesDirs[0].getCanonicalPath()).contains("external_dir_1");
-    assertThat(externalFilesDirs[1].getCanonicalPath()).contains("external_dir_2");
-
-    // TODO(jongerrish): This fails because ShadowContext overwrites getExternalFilesDir.
-//     assertThat(RuntimeEnvironment.application.getExternalFilesDir(Environment.DIRECTORY_MOVIES)
-//         .getCanonicalPath()).contains("external_dir_1");
-  }
-
-  @Test
-  @Config(sdk = JELLY_BEAN_MR1)
-  public void getExternalStorageStateJB() throws Exception {
-    ShadowEnvironment.setExternalStorageState("blah");
-    assertThat(ShadowEnvironment.getExternalStorageState()).isEqualTo("blah");
-  }
-
-  @Test
-  @Config(minSdk = KITKAT, maxSdk = LOLLIPOP)
-  public void getExternalStorageStatePreLollipopMR1() throws Exception {
-    File storageDir1 = ShadowEnvironment.addExternalDir("dir1");
-    File storageDir2 = ShadowEnvironment.addExternalDir("dir2");
-    ShadowEnvironment.setExternalStorageState(storageDir1, Environment.MEDIA_MOUNTED);
-    ShadowEnvironment.setExternalStorageState(storageDir2, Environment.MEDIA_REMOVED);
-    ShadowEnvironment.setExternalStorageState("blah");
-
-    assertThat(ShadowEnvironment.getStorageState(storageDir1))
-        .isEqualTo(Environment.MEDIA_MOUNTED);
-    assertThat(ShadowEnvironment.getStorageState(storageDir2))
-        .isEqualTo(Environment.MEDIA_REMOVED);
-    assertThat(ShadowEnvironment.getStorageState(new File(storageDir1, "subpath")))
-        .isEqualTo(Environment.MEDIA_MOUNTED);
-    assertThat(ShadowEnvironment.getExternalStorageState()).isEqualTo("blah");
-  }
-
-  @Test
-  @Config(minSdk = LOLLIPOP_MR1)
-  public void getExternalStorageState() throws Exception {
-    File storageDir1 = ShadowEnvironment.addExternalDir("dir1");
-    File storageDir2 = ShadowEnvironment.addExternalDir("dir2");
-    ShadowEnvironment.setExternalStorageState(storageDir1, Environment.MEDIA_MOUNTED);
-    ShadowEnvironment.setExternalStorageState(storageDir2, Environment.MEDIA_REMOVED);
-    ShadowEnvironment.setExternalStorageState("blah");
-
-    assertThat(ShadowEnvironment.getExternalStorageState(storageDir1))
-        .isEqualTo(Environment.MEDIA_MOUNTED);
-    assertThat(ShadowEnvironment.getStorageState(storageDir1))
-        .isEqualTo(Environment.MEDIA_MOUNTED);
-    assertThat(ShadowEnvironment.getExternalStorageState(storageDir2))
-        .isEqualTo(Environment.MEDIA_REMOVED);
-    assertThat(ShadowEnvironment.getStorageState(storageDir2))
-        .isEqualTo(Environment.MEDIA_REMOVED);
-    assertThat(ShadowEnvironment.getExternalStorageState(new File(storageDir1, "subpath")))
-        .isEqualTo(Environment.MEDIA_MOUNTED);
-    assertThat(ShadowEnvironment.getStorageState(new File(storageDir1, "subpath")))
-        .isEqualTo(Environment.MEDIA_MOUNTED);
-    assertThat(ShadowEnvironment.getExternalStorageState()).isEqualTo("blah");
-  }
-
-  @Test
-  @Config(minSdk = M)
-  public void isExternalStorageEmulated() {
-    ShadowEnvironment.setIsExternalStorageEmulated(true);
-    assertThat(Environment.isExternalStorageEmulated()).isTrue();
-
-    ShadowEnvironment.setIsExternalStorageEmulated(false);
     assertThat(Environment.isExternalStorageEmulated()).isFalse();
   }
 }
