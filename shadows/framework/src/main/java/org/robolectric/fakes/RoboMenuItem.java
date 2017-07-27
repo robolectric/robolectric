789922678e7b370da16e84d8b04a0e3e1377f8c8
Jorge Gil <jorgegilcavazos@gmail.com>
2017-07-26 23:14:47 -0700

    Fix RoboMenuItem not setting title from resId
    Fix #3085

--- shadows/framework/src/main/java/org/robolectric/fakes/RoboMenuItem.java
+++ shadows/framework/src/main/java/org/robolectric/fakes/RoboMenuItem.java
@@ -78,6 +78,7 @@ public class RoboMenuItem implements MenuItem {
 
   @Override
   public MenuItem setTitle(int title) {
+    this.title = title == 0 ? null : context.getResources().getString(title);
     return this;
   }
 
