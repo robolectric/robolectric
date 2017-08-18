1b43b3cdca0361521e70dc688ddd6576a5c45c28
James Lemieux <jplemieux@google.com>
2017-08-17 17:37:11 -0700

    Add support for AlarmClockInfo to ShadowAlarmManager

--- shadows/framework/src/main/java/org/robolectric/shadows/ShadowAlarmManager.java
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowAlarmManager.java
@@ -1,77 +1,89 @@
 package org.robolectric.shadows;
 
-import static android.os.Build.VERSION_CODES.KITKAT;
-import static android.os.Build.VERSION_CODES.M;
-
+import android.annotation.TargetApi;
 import android.app.AlarmManager;
+import android.app.AlarmManager.AlarmClockInfo;
 import android.app.PendingIntent;
 import android.content.Intent;
-import java.util.ArrayList;
-import java.util.List;
+
 import org.robolectric.Shadows;
 import org.robolectric.annotation.Implementation;
 import org.robolectric.annotation.Implements;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static android.app.AlarmManager.RTC_WAKEUP;
+import static android.os.Build.VERSION_CODES.KITKAT;
+import static android.os.Build.VERSION_CODES.LOLLIPOP;
+import static android.os.Build.VERSION_CODES.M;
 
 @SuppressWarnings({"UnusedDeclaration"})
 @Implements(AlarmManager.class)
 public class ShadowAlarmManager {
 
-  private List<ScheduledAlarm> scheduledAlarms = new ArrayList<ScheduledAlarm>();
+  private final List<ScheduledAlarm> scheduledAlarms = new ArrayList<>();
 
   @Implementation
   public void set(int type, long triggerAtTime, PendingIntent operation) {
-    internalSet(type, triggerAtTime, 0L, operation);
+    internalSet(type, triggerAtTime, 0L, operation, null);
   }
 
   @Implementation(minSdk = KITKAT)
   public void setExact(int type, long triggerAtTime, PendingIntent operation) {
-    internalSet(type, triggerAtTime, 0L, operation);
+    internalSet(type, triggerAtTime, 0L, operation, null);
   }
 
   @Implementation(minSdk = KITKAT)
-  public void setWindow(
-          int type, long windowStartMillis, long windowLengthMillis, PendingIntent operation) {
-    internalSet(type, windowStartMillis, 0L, operation);
+  public void setWindow(int type, long windowStartMillis, long windowLengthMillis, PendingIntent operation) {
+    internalSet(type, windowStartMillis, 0L, operation, null);
   }
 
   @Implementation(minSdk = M)
   public void setAndAllowWhileIdle(int type, long triggerAtTime, PendingIntent operation) {
-    internalSet(type, triggerAtTime, 0L, operation);
+    internalSet(type, triggerAtTime, 0L, operation, null);
   }
 
   @Implementation(minSdk = M)
   public void setExactAndAllowWhileIdle(int type, long triggerAtTime, PendingIntent operation) {
-    internalSet(type, triggerAtTime, 0L, operation);
+    internalSet(type, triggerAtTime, 0L, operation, null);
   }
 
   @Implementation
   public void setRepeating(int type, long triggerAtTime, long interval, PendingIntent operation) {
-    internalSet(type, triggerAtTime, interval, operation);
+    internalSet(type, triggerAtTime, interval, operation, null);
   }
 
   @Implementation
   public void setInexactRepeating(int type, long triggerAtMillis, long intervalMillis, PendingIntent operation) {
-    internalSet(type, triggerAtMillis, intervalMillis, operation);
+    internalSet(type, triggerAtMillis, intervalMillis, operation, null);
   }
 
-  private void internalSet(int type, long triggerAtTime, long interval, PendingIntent operation) {
-    Intent intent = Shadows.shadowOf(operation).getSavedIntent();
+  @Implementation(minSdk = LOLLIPOP)
+  public void setAlarmClock(AlarmClockInfo info, PendingIntent operation) {
+    internalSet(RTC_WAKEUP, info.getTriggerTime(), 0L, operation, info.getShowIntent());
+  }
+
+  @Implementation(minSdk = LOLLIPOP)
+  public AlarmClockInfo getNextAlarmClock() {
     for (ScheduledAlarm scheduledAlarm : scheduledAlarms) {
-      Intent scheduledIntent = Shadows.shadowOf(scheduledAlarm.operation).getSavedIntent();
-      if (scheduledIntent.filterEquals(intent)) {
-        scheduledAlarms.remove(scheduledAlarm);
-        break;
+      AlarmClockInfo alarmClockInfo = scheduledAlarm.getAlarmClockInfo();
+      if (alarmClockInfo != null) {
+        return alarmClockInfo;
       }
     }
-    scheduledAlarms.add(new ScheduledAlarm(type, triggerAtTime, interval, operation));
+    return null;
+  }
+
+  private void internalSet(int type, long triggerAtTime, long interval, PendingIntent operation, PendingIntent showIntent) {
+    cancel(operation);
+    scheduledAlarms.add(new ScheduledAlarm(type, triggerAtTime, interval, operation, showIntent));
+    Collections.sort(scheduledAlarms);
   }
 
   /**
-   * Consumes and returns the next scheduled alarm on the
-   * AlarmManager's stack.
-   *
-   * @return the next scheduled alarm, wrapped in a
-   *         {@link ShadowAlarmManager.ScheduledAlarm} object
+   * @return the next scheduled alarm after consuming it
    */
   public ScheduledAlarm getNextScheduledAlarm() {
     if (scheduledAlarms.isEmpty()) {
@@ -82,10 +94,7 @@ public class ShadowAlarmManager {
   }
 
   /**
-   * Returns the most recent scheduled alarm without consuming it.
-   *
-   * @return the most recently scheduled alarm, wrapped in a
-   *         {@link ShadowAlarmManager.ScheduledAlarm} object
+   * @return the most recently scheduled alarm without consuming it
    */
   public ScheduledAlarm peekNextScheduledAlarm() {
     if (scheduledAlarms.isEmpty()) {
@@ -95,40 +104,58 @@ public class ShadowAlarmManager {
     }
   }
 
+  /**
+   * @return all scheduled alarms
+   */
   public List<ScheduledAlarm> getScheduledAlarms() {
     return scheduledAlarms;
   }
 
   @Implementation
-  public void cancel(PendingIntent pendingIntent) {
-    final Intent intentTypeToRemove = Shadows.shadowOf(pendingIntent).getSavedIntent();
-    for (ScheduledAlarm scheduledAlarm : new ArrayList<ScheduledAlarm>(scheduledAlarms)) {
-      final Intent alarmIntent = Shadows.shadowOf(scheduledAlarm.operation).getSavedIntent();
-      if (intentTypeToRemove.filterEquals(alarmIntent)) {
+  public void cancel(PendingIntent operation) {
+    Intent toRemove = Shadows.shadowOf(operation).getSavedIntent();
+    for (ScheduledAlarm scheduledAlarm : scheduledAlarms) {
+      Intent scheduledIntent = Shadows.shadowOf(scheduledAlarm.operation).getSavedIntent();
+      if (scheduledIntent.filterEquals(toRemove)) {
         scheduledAlarms.remove(scheduledAlarm);
+        break;
       }
     }
   }
 
   /**
-   * Container object to hold an PendingIntent, together with the alarm
-   * parameters used in a call to {@code AlarmManager}
+   * Container object to hold a PendingIntent and parameters describing when to send it.
    */
-  public class ScheduledAlarm {
-    public int type;
-    public long triggerAtTime;
-    public long interval;
-    public PendingIntent operation;
+  public static class ScheduledAlarm implements Comparable<ScheduledAlarm> {
 
-    public ScheduledAlarm(int type, long triggerAtTime, PendingIntent operation) {
-      this(type, triggerAtTime, 0, operation);
+    public final int type;
+    public final long triggerAtTime;
+    public final long interval;
+    public final PendingIntent operation;
+
+    // A non-null showIntent implies this alarm has a user interface. (i.e. in an alarm clock app)
+    public final PendingIntent showIntent;
+
+    public ScheduledAlarm(int type, long triggerAtTime, PendingIntent operation, PendingIntent showIntent) {
+      this(type, triggerAtTime, 0, operation, showIntent);
     }
 
-    public ScheduledAlarm(int type, long triggerAtTime, long interval, PendingIntent operation) {
+    public ScheduledAlarm(int type, long triggerAtTime, long interval, PendingIntent operation, PendingIntent showIntent) {
       this.type = type;
       this.triggerAtTime = triggerAtTime;
       this.operation = operation;
       this.interval = interval;
+      this.showIntent = showIntent;
+    }
+
+    @TargetApi(LOLLIPOP)
+    public AlarmClockInfo getAlarmClockInfo() {
+      return showIntent == null ? null : new AlarmClockInfo(triggerAtTime, showIntent);
+    }
+
+    @Override
+    public int compareTo(ScheduledAlarm scheduledAlarm) {
+      return Long.compare(triggerAtTime, scheduledAlarm.triggerAtTime);
     }
   }
 }
