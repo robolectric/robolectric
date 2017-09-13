481a81b8bc6d04c4b1f2784cb05c5af170dd9b2b
Brett Chabot <brettchabot@google.com>
2017-09-13 11:02:36 -0700

    Add ResXMLParser

---
+++ resources/src/main/java/org/robolectric/res/android/ResXMLParser.java
@@ -0,0 +1,16 @@
+package org.robolectric.res.android;
+
+public class ResXMLParser {
+
+  public int indexOfStyle() {
+    return -1;
+  }
+
+  public int getAttributeValue(int idx, Ref<ResValue> value) {
+    return 0;
+  }
+
+  public int getAttributeCount() {
+    return -1;
+  }
+}
