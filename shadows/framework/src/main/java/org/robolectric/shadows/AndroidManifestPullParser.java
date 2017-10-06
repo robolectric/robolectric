7b4a13658f7241f63977ca41d85bccea2c0f2546
Jonathan Gerrish <jonathan@indiekid.org>
2017-10-06 14:34:20 -0700

    Manifest parsing proof of concept

--- shadows/framework/src/main/java/org/robolectric/shadows/AndroidManifestPullParser.java
+++ shadows/framework/src/main/java/org/robolectric/shadows/AndroidManifestPullParser.java
@@ -1,12 +1,20 @@
 package org.robolectric.shadows;
 
+import static android.content.pm.PackageManager.*;
+import static android.content.pm.PackageManager.MATCH_DIRECT_BOOT_AWARE;
+import static android.content.pm.PackageManager.MATCH_DIRECT_BOOT_UNAWARE;
 import static org.robolectric.util.ReflectionHelpers.ClassParameter.from;
 
 import android.app.Application;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageUserState;
 import android.content.res.AssetManager;
+import java.util.HashSet;
 import org.robolectric.res.Fs;
 import org.robolectric.res.builder.XmlBlock;
-import org.robolectric.shadows.PackageParser.Package;
+import android.content.pm.PackageParser;
+import android.content.pm.PackageParser.Package;
 import android.content.res.Resources;
 import android.content.res.XmlResourceParser;
 import libcore.io.IoUtils;
@@ -29,21 +37,29 @@ public class AndroidManifestPullParser {
       parser = ShadowAssetManager
           .getXmlResourceParser(RuntimeEnvironment.getAppResourceTable(), xmlBlock, "org.robolectric");
 
-      int flags = 0;
       final String[] outError = new String[1];
 
       // PackageParser packageParser = new PackageParser();
       // final Package pkg = parseBaseApk(apkPath, res, parser, flags, outError);
 
       PackageParser packageParser = new PackageParser();
-      final Package pkg = packageParser.parseBaseApk("dunno", RuntimeEnvironment.application.getResources(), parser, 0, new String[1]);
+      Resources resources = RuntimeEnvironment.application.getResources();
+      // final Package pkg = packageParser.parseBaseApk("dunno",
+      //     resources, parser, 0, new String[1]);
 
+      final Package pkg =
+              ReflectionHelpers.callInstanceMethod(PackageParser.class, packageParser, "parseBaseApk",
+                      from(Resources.class, resources),
+                      from(XmlResourceParser.class, parser),
+                      from(int.class, 0),
+                      from(String[].class, outError)
+              );
       // final Package pkg =
       //         ReflectionHelpers.callInstanceMethod(PackageParser.class, packageParser, "parseBaseApk",
       //                 from(String.class, "dunno"),
-      //                 from(Resources.class, application.getResources()),
+      //                 from(Resources.class, resources),
       //                 from(XmlResourceParser.class, parser),
-      //                 from(int.class, flags),
+      //                 from(int.class, 0),
       //                 from(String[].class, outError)
       //         );
       if (pkg == null) {
@@ -52,12 +68,37 @@ public class AndroidManifestPullParser {
         //     apkPath + " (at " + parser.getPositionDescription() + "): " + outError[0]);
       }
 
+      int flags =
+          GET_ACTIVITIES |
+              GET_RECEIVERS |
+              GET_SERVICES |
+              GET_PROVIDERS |
+              GET_INSTRUMENTATION |
+              GET_INTENT_FILTERS |
+              GET_SIGNATURES |
+              GET_RESOLVED_FILTER |
+              GET_META_DATA |
+              GET_GIDS |
+              MATCH_DISABLED_COMPONENTS |
+              GET_SHARED_LIBRARY_FILES |
+              GET_URI_PERMISSION_PATTERNS |
+              GET_PERMISSIONS |
+              MATCH_UNINSTALLED_PACKAGES |
+              GET_CONFIGURATIONS |
+              MATCH_DISABLED_UNTIL_USED_COMPONENTS |
+              MATCH_DIRECT_BOOT_UNAWARE |
+              MATCH_DIRECT_BOOT_AWARE
+          ;
+
+      PackageInfo packageInfo = PackageParser.generatePackageInfo(pkg, new int[]{0}, flags, 0, 0, new HashSet<String>(), new PackageUserState());
+
       // pkg.setVolumeUuid(volumeUuid);
       // pkg.setApplicationVolumeUuid(volumeUuid);
       // pkg.setBaseCodePath(apkPath);
       pkg.setSignatures(null);
 
       System.out.println("pkg = " + pkg);
+      System.out.println("packageInfo = " + packageInfo);
 
     } catch (Exception e) {
       throw new RuntimeException(e);
