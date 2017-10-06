663e9854b27a71cfed5b3ad2b79a37e5cd716e3b
Jonathan Gerrish <jonathan@indiekid.org>
2017-10-06 14:34:20 -0700

    Manifest parsing proof of concept

--- robolectric/src/test/java/org/robolectric/shadows/ShadowPackageManagerTest.java
+++ robolectric/src/test/java/org/robolectric/shadows/ShadowPackageManagerTest.java
@@ -105,7 +105,7 @@ public class ShadowPackageManagerTest {
     shadowPackageManager = shadowOf(packageManager);
   }
 
-  @Test
+  @Test @Config(sdk=25)
   public void testManifestParser() {
     AndroidManifestPullParser parser = new AndroidManifestPullParser();
     parser.parse();
