e56b988494739218b33ce92659f5601fb39757e5
Jonathan Gerrish <jongerrish@jongerrish3.roam.corp.google.com>
2017-11-17 09:13:10 -0800

    Added times, catch exceptions and continute.

--- sandbox/src/main/java/org/robolectric/InstrumentJar.java
+++ sandbox/src/main/java/org/robolectric/InstrumentJar.java
@@ -27,25 +27,36 @@ public class InstrumentJar {
       System.out.println("Instrumenting from " + inFile + " to " + outFile + "...");
       JarOutputStream jarOutputStream = new JarOutputStream(new FileOutputStream(outFile));
       Enumeration<JarEntry> entries = jarFile.entries();
+      long totalMs = 0;
+      long totalClassesInstrumented = 0;
       while (entries.hasMoreElements()) {
         JarEntry jarEntry = entries.nextElement();
 
         byte[] bytes;
         if (jarEntry.getName().endsWith(".class")) {
-          System.out.println("Instrumenting " + jarEntry.getName());
-          bytes = sandboxClassLoader.instrument(jarEntry.getName().replaceAll("\\.class$", "").replaceAll("/", "."));
+          long startTime = System.currentTimeMillis();
+          try {
+            bytes = sandboxClassLoader.instrument(jarEntry.getName().replaceAll("\\.class$", "").replaceAll("/", "."));
+          } catch (Exception e) {
+            System.out.println("Could not instrument: " + jarEntry.getName() + " Exception: " + e.toString());
+            bytes = ByteStreams.toByteArray(jarFile.getInputStream(jarEntry));
+          }
+          long endTime = System.currentTimeMillis();
+          long timeTaken = endTime - startTime;
+//          System.out.println("Instrumented " + jarEntry.getName() + " in " + timeTaken);
+          totalMs += timeTaken;
+          totalClassesInstrumented++;
         } else {
-          System.out.println("Copying " + jarEntry.getName());
+//          System.out.println("Copying " + jarEntry.getName());
           bytes = ByteStreams.toByteArray(jarFile.getInputStream(jarEntry));
         }
 
         jarOutputStream.putNextEntry(new JarEntry(jarEntry.getName()));
         jarOutputStream.write(bytes);
       }
+      System.out.println("Instrumented " + totalClassesInstrumented + " in " + totalMs + "ms");
       jarOutputStream.close();
     } catch (IOException e) {
-      e.printStackTrace();
-    } catch (ClassNotFoundException e) {
       e.printStackTrace();
     }
   }
