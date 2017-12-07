a6e9092fd97aae2852b4221d8bafce746bd6721e
Brett Chabot <brettchabot@google.com>
2017-12-07 14:03:01 -0800

    Add support for ActivityTestRule on Robolectric.
    PiperRevId: 176416662

--- integration_tests/android_support_test/src/test/java/org/robolectric/integration_tests/atsl/InstrumentationRegistryTest.java
+++ integration_tests/android_support_test/src/test/java/org/robolectric/integration_tests/atsl/InstrumentationRegistryTest.java
@@ -29,7 +29,9 @@ public class InstrumentationRegistryTest {
   @Test
   public void getTargetContext() {
     assertThat(InstrumentationRegistry.getTargetContext()).isNotNull();
-    assertThat(InstrumentationRegistry.getTargetContext()).isEqualTo(InstrumentationRegistry.getContext());
+    assertThat(InstrumentationRegistry.getContext()).isNotNull();
+    assertThat(InstrumentationRegistry.getTargetContext().getPackageName()).isEqualTo(
+        InstrumentationRegistry.getContext().getPackageName());
   }
 
   @Test
@@ -55,5 +57,4 @@ public class InstrumentationRegistryTest {
       assertThat(priorContext).isNotEqualTo(InstrumentationRegistry.getTargetContext());
     }
   }
-
 }
