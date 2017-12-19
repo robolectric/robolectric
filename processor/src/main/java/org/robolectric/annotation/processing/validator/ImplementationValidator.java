a17bb184e3023fe8aa71bf4db58984bec7d30f88
Christian Williams <christianw@google.com>
2017-12-19 14:13:30 -0800

    Allow @Implementation methods to be protected.
    Test authors should use Android API methods where available rather than
    shadow implementations. In the future, @Implementation methods will be
    switched to protected.
    
    * __constructor__ and  __staticInitializer__ methods must be marked
      @Implementation.
    * @Implementation methods should be protected (preferreed) or public
      (transitional, deprecated).

--- processor/src/main/java/org/robolectric/annotation/processing/validator/ImplementationValidator.java
+++ processor/src/main/java/org/robolectric/annotation/processing/validator/ImplementationValidator.java
@@ -1,20 +1,39 @@
 package org.robolectric.annotation.processing.validator;
 
+import com.google.common.collect.ImmutableSet;
+import javax.lang.model.element.Name;
+import org.robolectric.annotation.processing.RobolectricModel;
+
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
-import org.robolectric.annotation.processing.RobolectricModel;
+import javax.tools.Diagnostic.Kind;
+import java.util.Set;
 
 /**
  * Validator that checks usages of {@link org.robolectric.annotation.Implementation}.
  */
 public class ImplementationValidator extends FoundOnImplementsValidator {
+  public static final Set<String> METHODS_ALLOWED_TO_BE_PUBLIC = ImmutableSet.of(
+      "toString",
+      "hashCode",
+      "equals"
+  );
+
   public ImplementationValidator(RobolectricModel model, ProcessingEnvironment env) {
     super(model, env, "org.robolectric.annotation.Implementation");
   }
 
   @Override
   public Void visitExecutable(ExecutableElement elem, TypeElement parent) {
+    Set<Modifier> modifiers = elem.getModifiers();
+    if (!METHODS_ALLOWED_TO_BE_PUBLIC.contains(elem.getSimpleName().toString())) {
+      if (!modifiers.contains(Modifier.PUBLIC) && !modifiers.contains(Modifier.PROTECTED)) {
+        message(Kind.ERROR, "@Implementation methods should be protected (preferred) or public (deprecated)");
+      }
+    }
+
     // TODO: Check that it has the right signature
     return null;
   }
--- robolectric/src/test/java/org/robolectric/RobolectricTest.java
+++ robolectric/src/test/java/org/robolectric/RobolectricTest.java
@@ -159,7 +159,7 @@ public class RobolectricTest {
   @Implements(View.class)
   public static class TestShadowView {
     @Implementation
-    public Context getContext() {
+    protected Context getContext() {
       return null;
     }
   }
