5100c8eace79055ea02864251918adb58a5c8164
Christian Williams <christianw@google.com>
2017-12-21 16:59:12 -0800

    Fix more warnings.

--- resources/src/test/java/org/robolectric/res/android/ResTable_configTest.java
+++ resources/src/test/java/org/robolectric/res/android/ResTable_configTest.java
@@ -3,7 +3,10 @@ package org.robolectric.res.android;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
+@RunWith(JUnit4.class)
 public class ResTable_configTest {
 
   @Test
