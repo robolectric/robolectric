e5d9f6765791798c56bea4da4ac80c3bb83c9c11
brettchabot <brettchabot@google.com>
2018-03-26 14:10:53 -0700

    Add more ActivityTestRule integration tests.
    PiperOrigin-RevId: 190522838

--- robolectric/src/main/java/org/robolectric/android/fakes/RoboMonitoringInstrumentation.java
+++ robolectric/src/main/java/org/robolectric/android/fakes/RoboMonitoringInstrumentation.java
@@ -3,7 +3,6 @@ package org.robolectric.android.fakes;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
-import android.os.Bundle;
 import android.support.test.runner.MonitoringInstrumentation;
 import org.robolectric.Robolectric;
 import org.robolectric.android.controller.ActivityController;
@@ -37,8 +36,8 @@ public class RoboMonitoringInstrumentation extends MonitoringInstrumentation {
       Class<? extends Activity> activityClass = Class.forName(ai.name).asSubclass(Activity.class);
       ActivityController<? extends Activity> controller = Robolectric.buildActivity(activityClass);
       Activity activity = controller.get();
-      callActivityOnCreate(activity, new Bundle());
-      controller.postCreate(new Bundle());
+      callActivityOnCreate(activity, null);
+      controller.postCreate(null);
       callActivityOnStart(activity);
       callActivityOnResume(activity);
       controller.visible().windowFocusChanged(true);
