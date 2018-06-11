8ec48efb46b21202eb3a2f02dcacfd64bd623644
Googler <noreply@google.com>
2018-06-11 12:29:51 -0700

    Add BluetoothDevice#getUuids/setUuids
    PiperOrigin-RevId: 200093923

--- robolectric/src/test/java/org/robolectric/shadows/ShadowBluetoothDeviceTest.java
+++ robolectric/src/test/java/org/robolectric/shadows/ShadowBluetoothDeviceTest.java
@@ -1,8 +1,11 @@
 package org.robolectric.shadows;
 
 import static com.google.common.truth.Truth.assertThat;
+import static org.robolectric.Shadows.shadowOf;
 
+import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.os.ParcelUuid;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
@@ -22,5 +25,24 @@ public class ShadowBluetoothDeviceTest {
             BluetoothDevice.class,
             ReflectionHelpers.ClassParameter.from(String.class, MOCK_MAC_ADDRESS));
     assertThat(bluetoothDevice).isNotNull();
+  }
+
+  @Test
+  public void canSetAndGetUuids() throws Exception {
+    BluetoothDevice device = BluetoothAdapter.getDefaultAdapter().getRemoteDevice(MOCK_MAC_ADDRESS);
+    ParcelUuid[] uuids =
+        new ParcelUuid[] {
+          ParcelUuid.fromString("00000000-1111-2222-3333-000000000011"),
+          ParcelUuid.fromString("00000000-1111-2222-3333-0000000000aa")
+        };
+
+    shadowOf(device).setUuids(uuids);
+    assertThat(device.getUuids()).isEqualTo(uuids);
+  }
+
+  @Test
+  public void getUuids_setUuidsNotCalled_shouldReturnNull() throws Exception {
+    BluetoothDevice device = BluetoothAdapter.getDefaultAdapter().getRemoteDevice(MOCK_MAC_ADDRESS);
+    assertThat(device.getUuids()).isNull();
   }
 }
--- shadows/framework/src/main/java/org/robolectric/shadows/ShadowBluetoothDevice.java
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowBluetoothDevice.java
@@ -4,6 +4,7 @@ import static org.robolectric.shadow.api.Shadow.directlyOn;
 
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.IBluetooth;
+import android.os.ParcelUuid;
 import org.robolectric.annotation.Implementation;
 import org.robolectric.annotation.Implements;
 
@@ -11,6 +12,7 @@ import org.robolectric.annotation.Implements;
 public class ShadowBluetoothDevice {
 
   private String name;
+  private ParcelUuid[] uuids;
 
   /**
    * Implements getService() in the same way the original method does, but ignores any Exceptions
@@ -36,5 +38,21 @@ public class ShadowBluetoothDevice {
   @Implementation
   public String getName() {
     return name;
+  }
+
+  /** Sets the return value for {@link BluetoothDevice#getUuids}. */
+  public void setUuids(ParcelUuid[] uuids) {
+    this.uuids = uuids;
+  }
+
+  /**
+   * Overrides behavior of {@link BluetoothDevice#getUuids} to return pre-set result.
+   *
+   * @returns Value set by calling {@link ShadowBluetoothDevice#setUuids}. If setUuids has not
+   *     previously been called, will return null.
+   */
+  @Implementation
+  protected ParcelUuid[] getUuids() {
+    return uuids;
   }
 }
