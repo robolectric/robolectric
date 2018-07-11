b161252b4baa1554ab33df859cecae98afd00e66
Brett Chabot <brettchabot@google.com>
2018-07-11 16:24:12 -0700

    Set FileUtils enable copy optimizations to false for current platform instead of calling copyInternalUserspace directly.
    Change-Id: I38e3d6fe7e84189bc95740cdbb439e948365825b
    Test: make -j56 Run_robolectric_robolectric_tests

--- shadows/framework/src/main/java/org/robolectric/shadows/ShadowFileUtils.java
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowFileUtils.java
@@ -1,5 +1,7 @@
-// BEGIN-INTERNAL
 package org.robolectric.shadows;
+
+import static android.os.Build.VERSION_CODES.P;
+import static org.robolectric.util.ReflectionHelpers.ClassParameter.from;
 
 import android.os.CancellationSignal;
 import android.os.FileUtils;
@@ -8,16 +10,31 @@ import java.io.FileDescriptor;
 import java.io.IOException;
 import org.robolectric.annotation.Implementation;
 import org.robolectric.annotation.Implements;
+import org.robolectric.util.ReflectionHelpers;
 
-@Implements(value = FileUtils.class, isInAndroidSdk = false)
+@Implements(value = FileUtils.class, isInAndroidSdk = false, minSdk=P)
 public class ShadowFileUtils {
 
-  @Implementation
+  // BEGIN-INTERNAL
+  @Implementation(minSdk=android.os.Build.VERSION_CODES.Q)
+  protected static void __staticInitializer__() {
+    // don't do native copy optimizations on Robolectric
+    ReflectionHelpers.setStaticField(FileUtils.class, "sEnableCopyOptimizations", false);
+  }
+  // END-INTERNAL
+
+  @Implementation(minSdk=P, maxSdk=P)
   protected static long copy( FileDescriptor in,  FileDescriptor out,
       ProgressListener listener,  CancellationSignal signal, long count)
       throws IOException {
-    // never do the native copy optimization block
-    return FileUtils.copyInternalUserspace(in, out, listener, signal, count);
+    // ENABLE_COPY_OPTIMIZATIONS is final on P so call directly to userspace method
+    // to simulate behavior when ENABLE_COPY_OPTIMIZATIONS is false
+    return ReflectionHelpers.callStaticMethod(FileUtils.class,
+        "copyInternalUserspace",
+        from(FileDescriptor.class, in),
+        from(FileDescriptor.class, out),
+        from(ProgressListener.class, listener),
+        from(CancellationSignal.class, signal),
+        from(long.class, count));
   }
 }
-// END-INTERNAL
