b889a3a4045685c974aef41e83a1a4dbff222051
jongerrish <jongerrish@google.com>
2018-07-18 12:26:01 -0700

    Strip away much of the fake implementation in ShadowAccessibility{Event,Record}
    This uses and @Implementation for ShadowAccessibiltiyEvent.equals() which changes real Android behavior and will be banned shortly.
    
    PiperOrigin-RevId: 205117895

--- shadows/framework/src/main/java/org/robolectric/shadows/ShadowAccessibilityRecord.java
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowAccessibilityRecord.java
@@ -1,6 +1,5 @@
 package org.robolectric.shadows;
 
-import android.os.Parcelable;
 import android.view.View;
 import android.view.accessibility.AccessibilityRecord;
 import org.robolectric.annotation.Implementation;
@@ -14,16 +13,13 @@ import org.robolectric.util.ReflectionHelpers.ClassParameter;
  */
 @Implements(AccessibilityRecord.class)
 public class ShadowAccessibilityRecord {
-  private Parcelable parcelableData;
-  @RealObject public AccessibilityRecord realRecord;
+
+  @RealObject private AccessibilityRecord realRecord;
 
   public static final int NO_VIRTUAL_ID = -1;
 
   private View sourceRoot;
   private int virtualDescendantId;
-  protected CharSequence contentDescription;
-  protected CharSequence className;
-  protected boolean enabled;
 
   @Implementation
   public void setSource(View root, int virtualDescendantId) {
@@ -41,47 +37,6 @@ public class ShadowAccessibilityRecord {
     Shadow.directlyOn(realRecord, AccessibilityRecord.class, "setSource",
         ClassParameter.from(View.class, root));
   }
-
-  @Implementation
-  public void setParcelableData(Parcelable data) {
-    parcelableData = data;
-  }
-
-  @Implementation
-  public Parcelable getParcelableData() {
-    return parcelableData;
-  }
-
-  @Implementation
-  public void setContentDescription(CharSequence description) {
-    contentDescription = description;
-  }
-
-  @Implementation
-  public CharSequence getContentDescription() {
-    return contentDescription;
-  }
-
-  @Implementation
-  public void setClassName(CharSequence name) {
-    className = name;
-  }
-
-  @Implementation
-  public CharSequence getClassName() {
-    return className;
-  }
-
-  @Implementation
-  public void setEnabled(boolean value) {
-    enabled = value;
-  }
-
-  @Implementation
-  public boolean isEnabled() {
-    return enabled;
-  }
-
 
   public View getSourceRoot() {
     return sourceRoot;
