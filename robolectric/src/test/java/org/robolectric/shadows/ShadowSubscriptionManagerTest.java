59cf9ebb5b4bbbc6c9cf04bf9c834c6eeb6aac3c
Googler <noreply@google.com>
2018-07-24 19:08:27 -0700

    Add a couple of methods to ShadowSubscriptionManager
    - Added getDefaultSubscriptionId, getPhoneId, addOnSubscriptionsChangedListener,
      removeOnSubscriptionsChangedListener, getActiveSubscriptionIdList and
      getActiveSubscriptionInfo
    
    PiperOrigin-RevId: 205929743

--- robolectric/src/test/java/org/robolectric/shadows/ShadowSubscriptionManagerTest.java
+++ robolectric/src/test/java/org/robolectric/shadows/ShadowSubscriptionManagerTest.java
@@ -6,14 +6,12 @@ import static com.google.common.truth.Truth.assertThat;
 import static org.robolectric.RuntimeEnvironment.application;
 import static org.robolectric.Shadows.shadowOf;
 
-import android.telephony.SubscriptionInfo;
 import android.telephony.SubscriptionManager;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
-import org.robolectric.shadows.ShadowSubscriptionManager.SubscriptionInfoBuilder;
 
 /** Test for {@link ShadowSubscriptionManager}. */
 @RunWith(RobolectricTestRunner.class)
@@ -33,88 +31,21 @@ public class ShadowSubscriptionManagerTest {
   @Test
   public void shouldGiveDefaultDataSubscriptionId() {
     int testId = 42;
-    ShadowSubscriptionManager.setDefaultDataSubscriptionId(testId);
-
-    assertThat(SubscriptionManager.getDefaultDataSubscriptionId()).isEqualTo(testId);
+    shadowSubscriptionManager.setDefaultDataSubscriptionId(testId);
+    assertThat(subscriptionManager.getDefaultDataSubscriptionId()).isEqualTo(testId);
   }
 
   @Test
   public void shouldGiveDefaultSmsSubscriptionId() {
     int testId = 42;
-    ShadowSubscriptionManager.setDefaultSmsSubscriptionId(testId);
-
-    assertThat(SubscriptionManager.getDefaultSmsSubscriptionId()).isEqualTo(testId);
+    shadowSubscriptionManager.setDefaultSmsSubscriptionId(testId);
+    assertThat(subscriptionManager.getDefaultSmsSubscriptionId()).isEqualTo(testId);
   }
 
   @Test
   public void shouldGiveDefaultVoiceSubscriptionId() {
     int testId = 42;
-    ShadowSubscriptionManager.setDefaultVoiceSubscriptionId(testId);
-
-    assertThat(SubscriptionManager.getDefaultVoiceSubscriptionId()).isEqualTo(testId);
-  }
-
-  @Test
-  public void shouldGiveDefaultSubscriptionId() {
-    int expectedDefaultSubscriptionId = 1234;
-    ShadowSubscriptionManager.setDefaultSubscriptionId(expectedDefaultSubscriptionId);
-
-    assertThat(SubscriptionManager.getDefaultSubscriptionId())
-        .isEqualTo(expectedDefaultSubscriptionId);
-  }
-
-  @Test
-  public void shouldGivePhoneId() {
-    int subId = 1;
-    int expectedPhoneId = 111;
-    ShadowSubscriptionManager.setPhoneId(subId, expectedPhoneId);
-
-    assertThat(SubscriptionManager.getPhoneId(subId)).isEqualTo(expectedPhoneId);
-  }
-
-  @Test
-  public void addOnSubscriptionsChangedListener_shouldAddListener() {
-    DummySubscriptionsChangedListener listener = new DummySubscriptionsChangedListener();
-    shadowSubscriptionManager.addOnSubscriptionsChangedListener(listener);
-
-    shadowSubscriptionManager.setActiveSubscriptionInfos(
-        SubscriptionInfoBuilder.newBuilder().setId(123).buildSubscriptionInfo());
-
-    assertThat(listener.subscriptionChanged).isTrue();
-  }
-
-  @Test
-  public void removeOnSubscriptionsChangedListener_shouldRemoveListener() {
-    DummySubscriptionsChangedListener listener = new DummySubscriptionsChangedListener();
-    DummySubscriptionsChangedListener listener2 = new DummySubscriptionsChangedListener();
-    shadowSubscriptionManager.addOnSubscriptionsChangedListener(listener);
-    shadowSubscriptionManager.addOnSubscriptionsChangedListener(listener2);
-
-    shadowSubscriptionManager.removeOnSubscriptionsChangedListener(listener);
-    shadowSubscriptionManager.setActiveSubscriptionInfos(
-        SubscriptionInfoBuilder.newBuilder().setId(123).buildSubscriptionInfo());
-
-    assertThat(listener.subscriptionChanged).isFalse();
-    assertThat(listener2.subscriptionChanged).isTrue();
-  }
-
-  @Test
-  public void getActiveSubscriptionInfo_shouldReturnInfoWithSubId() {
-    SubscriptionInfo expectedSubscriptionInfo =
-        SubscriptionInfoBuilder.newBuilder().setId(123).buildSubscriptionInfo();
-    shadowSubscriptionManager.setActiveSubscriptionInfos(expectedSubscriptionInfo);
-
-    assertThat(shadowSubscriptionManager.getActiveSubscriptionInfo(123))
-        .isSameAs(expectedSubscriptionInfo);
-  }
-
-  private static class DummySubscriptionsChangedListener
-      extends SubscriptionManager.OnSubscriptionsChangedListener {
-    private boolean subscriptionChanged = false;
-
-    @Override
-    public void onSubscriptionsChanged() {
-      subscriptionChanged = true;
-    }
+    shadowSubscriptionManager.setDefaultVoiceSubscriptionId(testId);
+    assertThat(subscriptionManager.getDefaultVoiceSubscriptionId()).isEqualTo(testId);
   }
 }
--- shadows/framework/src/main/java/org/robolectric/shadows/ShadowSubscriptionManager.java
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowSubscriptionManager.java
@@ -1,215 +1,51 @@
 package org.robolectric.shadows;
 
-import static android.os.Build.VERSION_CODES.LOLLIPOP_MR1;
 import static android.os.Build.VERSION_CODES.N;
 
-import android.telephony.SubscriptionInfo;
 import android.telephony.SubscriptionManager;
-import android.telephony.SubscriptionManager.OnSubscriptionsChangedListener;
-import android.util.SparseIntArray;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import org.robolectric.annotation.HiddenApi;
 import org.robolectric.annotation.Implementation;
 import org.robolectric.annotation.Implements;
-import org.robolectric.util.ReflectionHelpers;
 
-/** Shadow for {@link SubscriptionManager}. */
-@Implements(value = SubscriptionManager.class, minSdk = LOLLIPOP_MR1)
+/**
+ * Shadow for {@link SubscriptionManager}.
+ *
+ * <p>Although {@link SubscriptionManager} itself has been added in LMR1, this shadow is only
+ * available since N because all shadowed methods have been added in N.
+ */
+@Implements(value = SubscriptionManager.class, minSdk = N)
 public class ShadowSubscriptionManager {
 
   private static int defaultDataSubscriptionId = SubscriptionManager.INVALID_SUBSCRIPTION_ID;
   private static int defaultSmsSubscriptionId = SubscriptionManager.INVALID_SUBSCRIPTION_ID;
   private static int defaultVoiceSubscriptionId = SubscriptionManager.INVALID_SUBSCRIPTION_ID;
-  private static int defaultSubscriptionId = SubscriptionManager.DEFAULT_SUBSCRIPTION_ID;
-  private static SparseIntArray phoneIds = new SparseIntArray();
 
   /** Returns value set with {@link #setDefaultDataSubscriptionId(int)}. */
-  @Implementation(minSdk = N)
+  @Implementation
   protected static int getDefaultDataSubscriptionId() {
     return defaultDataSubscriptionId;
   }
 
   /** Returns value set with {@link #setDefaultSmsSubscriptionId(int)}. */
-  @Implementation(minSdk = N)
+  @Implementation
   protected static int getDefaultSmsSubscriptionId() {
     return defaultSmsSubscriptionId;
   }
 
   /** Returns value set with {@link #setDefaultVoiceSubscriptionId(int)}. */
-  @Implementation(minSdk = N)
+  @Implementation
   protected static int getDefaultVoiceSubscriptionId() {
     return defaultVoiceSubscriptionId;
   }
 
-  /** Returns value set with {@link #setDefaultSubscriptionId(int)}. */
-  @Implementation(minSdk = N)
-  protected static int getDefaultSubscriptionId() {
-    return defaultSubscriptionId;
-  }
-
-  /** Returns value set with {@link #setPhoneId(int, int)}. */
-  @Implementation
-  @HiddenApi
-  protected static int getPhoneId(int subId) {
-    return phoneIds.get(subId);
-  }
-
-  /** Set the value to be returned by {@link #getDefaultDataSubscriptionId()} */
   public static void setDefaultDataSubscriptionId(int defaultDataSubscriptionId) {
     ShadowSubscriptionManager.defaultDataSubscriptionId = defaultDataSubscriptionId;
   }
 
-  /** Set the value to be returned by {@link #getDefaultSmsSubscriptionId()} */
   public static void setDefaultSmsSubscriptionId(int defaultSmsSubscriptionId) {
     ShadowSubscriptionManager.defaultSmsSubscriptionId = defaultSmsSubscriptionId;
   }
 
-  /** Set the value to be returned by {@link #getDefaultVoiceSubscriptionId()} */
   public static void setDefaultVoiceSubscriptionId(int defaultVoiceSubscriptionId) {
     ShadowSubscriptionManager.defaultVoiceSubscriptionId = defaultVoiceSubscriptionId;
-  }
-
-  /** Set the value to be returned by {@link #getDefaultSubscriptionId()} */
-  public static void setDefaultSubscriptionId(int defaultSubscriptionId) {
-    ShadowSubscriptionManager.defaultSubscriptionId = defaultSubscriptionId;
-  }
-
-  /** Set the phone id to be returned for the given {@code subId}. */
-  public static void setPhoneId(int subId, int phoneId) {
-    phoneIds.put(subId, phoneId);
-  }
-
-  /** Clears the subId to phoneId mapping. */
-  public static void clearPhoneIds() {
-    phoneIds.clear();
-  }
-
-  private List<SubscriptionInfo> subscriptionList = new ArrayList<>();
-  private List<OnSubscriptionsChangedListener> listeners = new ArrayList<>();
-
-  @Implementation(minSdk = LOLLIPOP_MR1)
-  protected List<SubscriptionInfo> getActiveSubscriptionInfoList() {
-    return subscriptionList;
-  }
-
-  @Implementation(minSdk = LOLLIPOP_MR1)
-  protected SubscriptionInfo getActiveSubscriptionInfo(int subId) {
-    for (SubscriptionInfo info : subscriptionList) {
-      if (info.getSubscriptionId() == subId) {
-        return info;
-      }
-    }
-    return null;
-  }
-
-  /**
-   * Sets the active list of {@link SubscriptionInfo}. This call internally triggers {@link
-   * OnSubscriptionsChangedListener#onSubscriptionsChanged()} to all the listeners.
-   */
-  public void setActiveSubscriptionInfoList(List<SubscriptionInfo> list) {
-    subscriptionList = list;
-    dispatchOnSubscriptionsChanged();
-  }
-
-  /**
-   * Sets the active list of {@link SubscriptionInfo}. This call internally triggers {@link
-   * OnSubscriptionsChangedListener#onSubscriptionsChanged()} to all the listeners.
-   */
-  public void setActiveSubscriptionInfos(SubscriptionInfo... infos) {
-    setActiveSubscriptionInfoList(Arrays.asList(infos));
-  }
-
-  @Implementation(minSdk = LOLLIPOP_MR1)
-  protected void addOnSubscriptionsChangedListener(OnSubscriptionsChangedListener listener) {
-    listeners.add(listener);
-  }
-
-  @Implementation(minSdk = LOLLIPOP_MR1)
-  protected void removeOnSubscriptionsChangedListener(OnSubscriptionsChangedListener listener) {
-    listeners.remove(listener);
-  }
-
-  @Implementation(minSdk = LOLLIPOP_MR1)
-  @HiddenApi
-  protected int[] getActiveSubscriptionIdList() {
-    final List<SubscriptionInfo> infos = getActiveSubscriptionInfoList();
-    if (infos == null) {
-      return new int[0];
-    }
-    int[] ids = new int[infos.size()];
-    for (int i = 0; i < infos.size(); i++) {
-      ids[i] = infos.get(i).getSubscriptionId();
-    }
-    return ids;
-  }
-
-  private void dispatchOnSubscriptionsChanged() {
-    for (OnSubscriptionsChangedListener listener : listeners) {
-      listener.onSubscriptionsChanged();
-    }
-  }
-
-  /** Builder class to create instance of {@link SubscriptionInfo}. */
-  public static class SubscriptionInfoBuilder {
-    private final SubscriptionInfo subscriptionInfo =
-        ReflectionHelpers.callConstructor(SubscriptionInfo.class);
-
-    public static SubscriptionInfoBuilder newBuilder() {
-      return new SubscriptionInfoBuilder();
-    }
-
-    public SubscriptionInfo buildSubscriptionInfo() {
-      return subscriptionInfo;
-    }
-
-    public SubscriptionInfoBuilder setId(int id) {
-      ReflectionHelpers.setField(subscriptionInfo, "mId", id);
-      return this;
-    }
-
-    public SubscriptionInfoBuilder setIccId(String iccId) {
-      ReflectionHelpers.setField(subscriptionInfo, "mIccId", iccId);
-      return this;
-    }
-
-    public SubscriptionInfoBuilder setSimSlotIndex(int index) {
-      ReflectionHelpers.setField(subscriptionInfo, "mSimSlotIndex", index);
-      return this;
-    }
-
-    public SubscriptionInfoBuilder setDisplayName(String name) {
-      ReflectionHelpers.setField(subscriptionInfo, "mDisplayName", name);
-      return this;
-    }
-
-    public SubscriptionInfoBuilder setCarrierName(String carrierName) {
-      ReflectionHelpers.setField(subscriptionInfo, "mCarrierName", carrierName);
-      return this;
-    }
-
-    public SubscriptionInfoBuilder setIconTint(int iconTint) {
-      ReflectionHelpers.setField(subscriptionInfo, "mIconTint", iconTint);
-      return this;
-    }
-
-    public SubscriptionInfoBuilder setNumber(String number) {
-      ReflectionHelpers.setField(subscriptionInfo, "mNumber", number);
-      return this;
-    }
-
-    public SubscriptionInfoBuilder setDataRoaming(int dataRoaming) {
-      ReflectionHelpers.setField(subscriptionInfo, "mDataRoaming", dataRoaming);
-      return this;
-    }
-
-    public SubscriptionInfoBuilder setCountryIso(String countryIso) {
-      ReflectionHelpers.setField(subscriptionInfo, "mCountryIso", countryIso);
-      return this;
-    }
-
-    // Use {@link #newBuilder} to construct builders.
-    private SubscriptionInfoBuilder() {}
   }
 }
