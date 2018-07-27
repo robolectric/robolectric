8f1efece2cdd47da84722b77f57f36ee9ddb0d83
Googler <noreply@google.com>
2018-07-26 19:42:42 -0700

    Add ShadowTimeZoneFinder
    - Add shadow for TimeZoneFinder which parses the time zone data from android-all jar
    
    PiperOrigin-RevId: 206261302

---
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowTimeZoneFinder.java
@@ -0,0 +1,50 @@
+package org.robolectric.shadows;
+
+import static android.os.Build.VERSION_CODES.O;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import libcore.util.TimeZoneFinder;
+import org.robolectric.annotation.Implementation;
+import org.robolectric.annotation.Implements;
+
+/** Shadow for {@code TimeZoneFinder}. */
+@Implements(value = TimeZoneFinder.class, minSdk = O, isInAndroidSdk = false)
+public class ShadowTimeZoneFinder {
+
+  private static final String TZLOOKUP_PATH = "/usr/share/zoneinfo/tzlookup.xml";
+
+  @Implementation
+  protected static Object getInstance() {
+    return TimeZoneFinder.createInstanceForTests(readTzlookup());
+  }
+
+  /**
+   * Reads tzlookup.xml from the files bundled inside android-all JARs. We need to read the file
+   * instead of passing in the path because the real implementation uses {@link java.nio.file.Paths}
+   * which doesn't support reading from JARs.
+   */
+  private static String readTzlookup() {
+    StringBuilder stringBuilder = new StringBuilder();
+    InputStream is = null;
+    try {
+      try {
+        is = ShadowTimeZoneFinder.class.getResourceAsStream(TZLOOKUP_PATH);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        for (String line; (line = reader.readLine()) != null; ) {
+          stringBuilder.append(line);
+        }
+      } finally {
+        if (is != null) {
+          is.close();
+        }
+      }
+    } catch (IOException e) {
+      // ignore
+    }
+
+    return stringBuilder.toString();
+  }
+}
