774aa376573a31a4923ee5aa6178bc16a2e3e1f7
Googler <noreply@google.com>
2018-08-16 02:29:48 -0700

    Implement myTid, setThreadPriority and getThreadPriority in ShadowProcess.
    PiperOrigin-RevId: 208955193

--- robolectric/src/test/java/org/robolectric/shadows/ShadowProcessTest.java
+++ robolectric/src/test/java/org/robolectric/shadows/ShadowProcessTest.java
@@ -2,6 +2,8 @@ package org.robolectric.shadows;
 
 import static com.google.common.truth.Truth.assertThat;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
@@ -24,6 +26,78 @@ public class ShadowProcessTest {
   public void shouldGetMyUidAsSet() {
     ShadowProcess.setUid(123);
     assertThat(android.os.Process.myUid()).isEqualTo(123);
+  }
+
+  @Test
+  public void myTid_mainThread_returnsCurrentThreadId() {
+    assertThat(android.os.Process.myTid()).isEqualTo(Thread.currentThread().getId());
+  }
+
+  @Test
+  public void myTid_backgroundThread_returnsCurrentThreadId() throws Exception {
+    AtomicBoolean ok = new AtomicBoolean(false);
+
+    Thread thread =
+        new Thread(
+            () -> {
+              ok.set(android.os.Process.myTid() == Thread.currentThread().getId());
+            });
+    thread.start();
+    thread.join();
+
+    assertThat(ok.get()).isTrue();
+  }
+
+  @Test
+  public void myTid_returnsDifferentValuesForDifferentThreads() throws Exception {
+    AtomicInteger tid1 = new AtomicInteger(0);
+    AtomicInteger tid2 = new AtomicInteger(0);
+
+    Thread thread1 =
+        new Thread(
+            () -> {
+              tid1.set(android.os.Process.myTid());
+            });
+    Thread thread2 =
+        new Thread(
+            () -> {
+              tid2.set(android.os.Process.myTid());
+            });
+    thread1.start();
+    thread2.start();
+    thread1.join();
+    thread2.join();
+
+    assertThat(tid1).isNotEqualTo(tid2);
+  }
+
+  @Test
+  public void getThreadPriority_notSet_returnsZero() {
+    assertThat(android.os.Process.getThreadPriority(123)).isEqualTo(0);
+  }
+
+  @Test
+  public void getThreadPriority_returnsThreadPriority() {
+    android.os.Process.setThreadPriority(123, android.os.Process.THREAD_PRIORITY_VIDEO);
+
+    assertThat(android.os.Process.getThreadPriority(123))
+        .isEqualTo(android.os.Process.THREAD_PRIORITY_VIDEO);
+  }
+
+  @Test
+  public void getThreadPriority_currentThread_returnsCurrentThreadPriority() {
+    android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_AUDIO);
+
+    assertThat(android.os.Process.getThreadPriority(/*tid=*/ 0))
+        .isEqualTo(android.os.Process.THREAD_PRIORITY_AUDIO);
+  }
+
+  @Test
+  public void setThreadPriorityOneArgument_setsCurrentThreadPriority() {
+    android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_URGENT_AUDIO);
+
+    assertThat(android.os.Process.getThreadPriority(android.os.Process.myTid()))
+        .isEqualTo(android.os.Process.THREAD_PRIORITY_URGENT_AUDIO);
   }
 }
 
