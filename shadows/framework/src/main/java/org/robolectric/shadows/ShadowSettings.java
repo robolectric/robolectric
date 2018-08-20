c6a8908c85010e4f9b8043ce7efc29d024a788a0
jongerrish <jongerrish@google.com>
2018-08-20 10:50:43 -0700

    Remove inherritImplementationMethods from ShadowSettings.Global + Secure Fix Robolectric bug where System, Secure + Global settings were shared.
    PiperOrigin-RevId: 209446085

--- shadows/framework/src/main/java/org/robolectric/shadows/ShadowSettings.java
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowSettings.java
@@ -126,12 +126,230 @@ public class ShadowSettings {
     }
   }
 
-  @Implements(value = Settings.Secure.class, inheritImplementationMethods = true)
-  public static class ShadowSecure extends ShadowSystem {
+  /**
+   * Shadow for {@link Settings.Secure}
+   */
+  @Implements(value = Settings.Secure.class)
+  public static class ShadowSecure {
+    private static final WeakHashMap<ContentResolver, Map<String, Object>> dataMap =
+        new WeakHashMap<ContentResolver, Map<String, Object>>();
+
+    @Implementation
+    protected static boolean putInt(ContentResolver cr, String name, int value) {
+      get(cr).put(name, value);
+      return true;
+    }
+
+    @Implementation
+    protected static int getInt(ContentResolver cr, String name, int def) {
+      if (get(cr).get(name) instanceof Integer) {
+        return (Integer) get(cr).get(name);
+      } else {
+        return def;
+      }
+    }
+
+    @Implementation
+    protected static int getInt(ContentResolver cr, String name)
+        throws Settings.SettingNotFoundException {
+      if (get(cr).get(name) instanceof Integer) {
+        return (Integer) get(cr).get(name);
+      } else {
+        throw new Settings.SettingNotFoundException(name);
+      }
+    }
+
+    @Implementation
+    protected static boolean putString(ContentResolver cr, String name, String value) {
+      get(cr).put(name, value);
+      return true;
+    }
+
+    @Implementation
+    protected static String getString(ContentResolver cr, String name) {
+      if (get(cr).get(name) instanceof String) {
+        return (String) get(cr).get(name);
+      } else {
+        return null;
+      }
+    }
+
+    @Implementation(minSdk = JELLY_BEAN_MR1)
+    protected static String getStringForUser(ContentResolver cr, String name, int userHandle) {
+      return getString(cr, name);
+    }
+
+    @Implementation
+    protected static boolean putLong(ContentResolver cr, String name, long value) {
+      get(cr).put(name, value);
+      return true;
+    }
+
+    @Implementation
+    protected static long getLong(ContentResolver cr, String name, long def) {
+      if (get(cr).get(name) instanceof Long) {
+        return (Long) get(cr).get(name);
+      } else {
+        return def;
+      }
+    }
+
+    @Implementation
+    protected static long getLong(ContentResolver cr, String name)
+        throws Settings.SettingNotFoundException {
+      if (get(cr).get(name) instanceof Long) {
+        return (Long) get(cr).get(name);
+      } else {
+        throw new Settings.SettingNotFoundException(name);
+      }
+    }
+
+    @Implementation
+    protected static boolean putFloat(ContentResolver cr, String name, float value) {
+      get(cr).put(name, value);
+      return true;
+    }
+
+    @Implementation
+    protected static float getFloat(ContentResolver cr, String name, float def) {
+      if (get(cr).get(name) instanceof Float) {
+        return (Float) get(cr).get(name);
+      } else {
+        return def;
+      }
+    }
+
+    @Implementation
+    protected static float getFloat(ContentResolver cr, String name)
+        throws Settings.SettingNotFoundException {
+      if (get(cr).get(name) instanceof Float) {
+        return (Float) get(cr).get(name);
+      } else {
+        throw new Settings.SettingNotFoundException(name);
+      }
+    }
+
+    private static Map<String, Object> get(ContentResolver cr) {
+      Map<String, Object> map = dataMap.get(cr);
+      if (map == null) {
+        map = new HashMap<>();
+        dataMap.put(cr, map);
+      }
+      return map;
+    }
   }
 
-  @Implements(value = Settings.Global.class, inheritImplementationMethods = true, minSdk = JELLY_BEAN_MR1)
-  public static class ShadowGlobal extends ShadowSystem {
+  /**
+   * Shadow for {@link Settings.Global}
+   */
+  @Implements(value = Settings.Global.class, minSdk = JELLY_BEAN_MR1)
+  public static class ShadowGlobal {
+    private static final WeakHashMap<ContentResolver, Map<String, Object>> dataMap =
+        new WeakHashMap<ContentResolver, Map<String, Object>>();
+
+    @Implementation
+    protected static boolean putInt(ContentResolver cr, String name, int value) {
+      get(cr).put(name, value);
+      return true;
+    }
+
+    @Implementation
+    protected static int getInt(ContentResolver cr, String name, int def) {
+      if (get(cr).get(name) instanceof Integer) {
+        return (Integer) get(cr).get(name);
+      } else {
+        return def;
+      }
+    }
+
+    @Implementation
+    protected static int getInt(ContentResolver cr, String name)
+        throws Settings.SettingNotFoundException {
+      if (get(cr).get(name) instanceof Integer) {
+        return (Integer) get(cr).get(name);
+      } else {
+        throw new Settings.SettingNotFoundException(name);
+      }
+    }
+
+    @Implementation
+    protected static boolean putString(ContentResolver cr, String name, String value) {
+      get(cr).put(name, value);
+      return true;
+    }
+
+    @Implementation
+    protected static String getString(ContentResolver cr, String name) {
+      if (get(cr).get(name) instanceof String) {
+        return (String) get(cr).get(name);
+      } else {
+        return null;
+      }
+    }
+
+    @Implementation(minSdk = JELLY_BEAN_MR1)
+    protected static String getStringForUser(ContentResolver cr, String name, int userHandle) {
+      return getString(cr, name);
+    }
+
+    @Implementation
+    protected static boolean putLong(ContentResolver cr, String name, long value) {
+      get(cr).put(name, value);
+      return true;
+    }
+
+    @Implementation
+    protected static long getLong(ContentResolver cr, String name, long def) {
+      if (get(cr).get(name) instanceof Long) {
+        return (Long) get(cr).get(name);
+      } else {
+        return def;
+      }
+    }
+
+    @Implementation
+    protected static long getLong(ContentResolver cr, String name)
+        throws Settings.SettingNotFoundException {
+      if (get(cr).get(name) instanceof Long) {
+        return (Long) get(cr).get(name);
+      } else {
+        throw new Settings.SettingNotFoundException(name);
+      }
+    }
+
+    @Implementation
+    protected static boolean putFloat(ContentResolver cr, String name, float value) {
+      get(cr).put(name, value);
+      return true;
+    }
+
+    @Implementation
+    protected static float getFloat(ContentResolver cr, String name, float def) {
+      if (get(cr).get(name) instanceof Float) {
+        return (Float) get(cr).get(name);
+      } else {
+        return def;
+      }
+    }
+
+    @Implementation
+    protected static float getFloat(ContentResolver cr, String name)
+        throws Settings.SettingNotFoundException {
+      if (get(cr).get(name) instanceof Float) {
+        return (Float) get(cr).get(name);
+      } else {
+        throw new Settings.SettingNotFoundException(name);
+      }
+    }
+
+    private static Map<String, Object> get(ContentResolver cr) {
+      Map<String, Object> map = dataMap.get(cr);
+      if (map == null) {
+        map = new HashMap<>();
+        dataMap.put(cr, map);
+      }
+      return map;
+    }
   }
 
   /**
@@ -144,6 +362,10 @@ public class ShadowSettings {
         RuntimeEnvironment.application.getContentResolver(),
         Settings.Global.AIRPLANE_MODE_ON,
         isAirplaneMode ? 1 : 0);
+    Settings.System.putInt(
+        RuntimeEnvironment.application.getContentResolver(),
+        Settings.System.AIRPLANE_MODE_ON,
+        isAirplaneMode ? 1 : 0);
   }
 
   /**
@@ -154,6 +376,8 @@ public class ShadowSettings {
   public static void setWifiOn(boolean isOn) {
     Settings.Global.putInt(
         RuntimeEnvironment.application.getContentResolver(), Settings.Global.WIFI_ON, isOn ? 1 : 0);
+    Settings.System.putInt(
+        RuntimeEnvironment.application.getContentResolver(), Settings.System.WIFI_ON, isOn ? 1 : 0);
   }
 
   /**
