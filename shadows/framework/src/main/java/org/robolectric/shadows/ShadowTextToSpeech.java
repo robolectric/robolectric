697169304617ffce070dc036b2b272ad9f16018b
pastychang <pastychang@google.com>
2018-09-06 16:19:13 +0800

    Update shadows for ShadowTextToSpeech
    add asynchronous speak function and setOnUtteranceProgressListener
    function.
    
    Bug: 113300281
    Test: rerun robolectric tests
    Change-Id: Ia3ea92152a236eec0763bab882ab85e13641ca7f

--- shadows/framework/src/main/java/org/robolectric/shadows/ShadowTextToSpeech.java
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowTextToSpeech.java
@@ -1,7 +1,14 @@
 package org.robolectric.shadows;
 
+import static android.os.Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1;
+import static android.os.Build.VERSION_CODES.LOLLIPOP;
+
 import android.content.Context;
+import android.os.Build.VERSION;
+import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
+import android.speech.tts.TextToSpeech.Engine;
+import android.speech.tts.UtteranceProgressListener;
 import java.util.HashMap;
 import org.robolectric.annotation.Implementation;
 import org.robolectric.annotation.Implements;
@@ -13,6 +20,7 @@ public class ShadowTextToSpeech {
   private String lastSpokenText;
   private boolean shutdown = false;
   private int queueMode = -1;
+  private UtteranceProgressListener utteranceProgressListener;
 
   @Implementation
   public void __constructor__(Context context, TextToSpeech.OnInitListener listener) {
@@ -20,10 +28,41 @@ public class ShadowTextToSpeech {
     this.listener = listener;
   }
 
+  /**
+   * Speaks the string using the specified queuing strategy and speech parameters.
+   *
+   * @param params The real implementation converts the hashmap into a bundle, but the bundle
+   *     argument is not used in the shadow implementation.
+   */
   @Implementation
   public int speak(final String text, final int queueMode, final HashMap<String, String> params) {
-    lastSpokenText = text;
+    return speak(
+        text,
+        queueMode,
+        null,
+        params == null ? null : params.get(Engine.KEY_PARAM_UTTERANCE_ID));
+  }
+
+  @Implementation(minSdk = LOLLIPOP)
+  protected int speak(final CharSequence text,
+      final int queueMode,
+      final Bundle params,
+      final String utteranceId) {
+    lastSpokenText = text.toString();
     this.queueMode = queueMode;
+
+    if (VERSION.SDK_INT >= ICE_CREAM_SANDWICH_MR1) {
+      if (utteranceId != null && utteranceProgressListener != null) {
+        utteranceProgressListener.onStart(utteranceId);
+        utteranceProgressListener.onDone(utteranceId);
+      }
+    }
+    return TextToSpeech.SUCCESS;
+  }
+
+  @Implementation(minSdk = ICE_CREAM_SANDWICH_MR1)
+  protected int setOnUtteranceProgressListener(UtteranceProgressListener listener) {
+    utteranceProgressListener = listener;
     return TextToSpeech.SUCCESS;
   }
 
