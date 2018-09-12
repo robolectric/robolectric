a9fe3ac12870629600301644c0ce0c2e46ec90cf
Googler <noreply@google.com>
2018-09-11 18:36:17 -0700

    Added activeSubscriptionInfo methods to ShadowSubscriptionManager - addOnSubscriptionsChangedListener - removeOnSubscriptionsChangedListener - getActiveSubscriptionIdList - getActiveSubscriptionInfo - isNetworkRoaming
    PiperOrigin-RevId: 212563086

--- robolectric/src/test/java/org/robolectric/shadows/ShadowSubscriptionManagerTest.java
+++ robolectric/src/test/java/org/robolectric/shadows/ShadowSubscriptionManagerTest.java
@@ -6,12 +6,14 @@ import static com.google.common.truth.Truth.assertThat;
 import static org.robolectric.RuntimeEnvironment.application;
 import static org.robolectric.Shadows.shadowOf;
 
+import android.telephony.SubscriptionInfo;
 import android.telephony.SubscriptionManager;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowSubscriptionManager.SubscriptionInfoBuilder;
 
 /** Test for {@link ShadowSubscriptionManager}. */
 @RunWith(RobolectricTestRunner.class)
@@ -47,5 +49,95 @@ public class ShadowSubscriptionManagerTest {
     int testId = 42;
     shadowSubscriptionManager.setDefaultVoiceSubscriptionId(testId);
     assertThat(subscriptionManager.getDefaultVoiceSubscriptionId()).isEqualTo(testId);
+  }
+
+  @Test
+  public void addOnSubscriptionsChangedListener_shouldAddListener() {
+    DummySubscriptionsChangedListener listener = new DummySubscriptionsChangedListener();
+    shadowSubscriptionManager.addOnSubscriptionsChangedListener(listener);
+
+    shadowSubscriptionManager.setActiveSubscriptionInfos(
+        SubscriptionInfoBuilder.newBuilder().setId(123).buildSubscriptionInfo());
+
+    assertThat(listener.subscriptionChanged).isTrue();
+  }
+
+  @Test
+  public void removeOnSubscriptionsChangedListener_shouldRemoveListener() {
+    DummySubscriptionsChangedListener listener = new DummySubscriptionsChangedListener();
+    DummySubscriptionsChangedListener listener2 = new DummySubscriptionsChangedListener();
+    shadowSubscriptionManager.addOnSubscriptionsChangedListener(listener);
+    shadowSubscriptionManager.addOnSubscriptionsChangedListener(listener2);
+
+    shadowSubscriptionManager.removeOnSubscriptionsChangedListener(listener);
+    shadowSubscriptionManager.setActiveSubscriptionInfos(
+        SubscriptionInfoBuilder.newBuilder().setId(123).buildSubscriptionInfo());
+
+    assertThat(listener.subscriptionChanged).isFalse();
+    assertThat(listener2.subscriptionChanged).isTrue();
+  }
+
+  @Test
+  public void getActiveSubscriptionInfo_shouldReturnInfoWithSubId() {
+    SubscriptionInfo expectedSubscriptionInfo =
+        SubscriptionInfoBuilder.newBuilder().setId(123).buildSubscriptionInfo();
+    shadowSubscriptionManager.setActiveSubscriptionInfos(expectedSubscriptionInfo);
+
+    assertThat(shadowSubscriptionManager.getActiveSubscriptionInfo(123))
+        .isSameAs(expectedSubscriptionInfo);
+  }
+
+  @Test
+  public void getActiveSubscriptionInfo_shouldReturnNullForNullList() {
+    shadowSubscriptionManager.setActiveSubscriptionInfoList(null);
+    assertThat(shadowSubscriptionManager.getActiveSubscriptionInfo(123)).isNull();
+  }
+
+  @Test
+  public void getActiveSubscriptionInfo_shouldReturnNullForNullVaargsList() {
+    shadowSubscriptionManager.setActiveSubscriptionInfos(null);
+    assertThat(shadowSubscriptionManager.getActiveSubscriptionInfo(123)).isNull();
+  }
+
+  @Test
+  public void getActiveSubscriptionInfo_shouldReturnNullForEmptyList() {
+    shadowSubscriptionManager.setActiveSubscriptionInfos();
+    assertThat(shadowSubscriptionManager.getActiveSubscriptionInfo(123)).isNull();
+  }
+
+  @Test
+  public void isNetworkRoaming_shouldReturnTrueIfSet() {
+    shadowSubscriptionManager.setNetworkRoamingStatus(123, /*isNetworkRoaming=*/true);
+    assertThat(shadowSubscriptionManager.isNetworkRoaming(123)).isTrue();
+  }
+
+  /** Multi act-asserts are discouraged but here we are testing the set+unset. */
+  @Test
+  public void isNetworkRoaming_shouldReturnFalseIfUnset() {
+    shadowSubscriptionManager.setNetworkRoamingStatus(123, /*isNetworkRoaming=*/true);
+    assertThat(shadowSubscriptionManager.isNetworkRoaming(123)).isTrue();
+    
+    shadowSubscriptionManager.setNetworkRoamingStatus(123, /*isNetworkRoaming=*/false);
+    assertThat(shadowSubscriptionManager.isNetworkRoaming(123)).isFalse();
+  }
+
+  /** Multi act-asserts are discouraged but here we are testing the set+clear. */
+  @Test
+  public void isNetworkRoaming_shouldReturnFalseOnClear() {
+    shadowSubscriptionManager.setNetworkRoamingStatus(123, /*isNetworkRoaming=*/true);
+    assertThat(shadowSubscriptionManager.isNetworkRoaming(123)).isTrue();
+
+    shadowSubscriptionManager.clearNetworkRoamingStatus();
+    assertThat(shadowSubscriptionManager.isNetworkRoaming(123)).isFalse();
+  }
+
+  private static class DummySubscriptionsChangedListener
+      extends SubscriptionManager.OnSubscriptionsChangedListener {
+    private boolean subscriptionChanged = false;
+
+    @Override
+    public void onSubscriptionsChanged() {
+      subscriptionChanged = true;
+    }
   }
 }
