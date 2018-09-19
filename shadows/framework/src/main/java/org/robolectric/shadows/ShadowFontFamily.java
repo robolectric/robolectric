fe8230eca079d2f596dd5ab8a71a76b2be4bb52f
jongerrish <jongerrish@google.com>
2018-09-19 10:09:10 -0700

    Add dummy behaviour to ShadowFontFamily to prevent NPE on binary resource mode.
    PiperOrigin-RevId: 213646809

---
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowFontFamily.java
@@ -0,0 +1,21 @@
+package org.robolectric.shadows;
+
+import static android.os.Build.VERSION_CODES.LOLLIPOP;
+import static android.os.Build.VERSION_CODES.O;
+
+import android.graphics.FontFamily;
+import org.robolectric.annotation.Implementation;
+import org.robolectric.annotation.Implements;
+
+/** Shadow for {@link FontFamily} */
+@Implements(value = FontFamily.class, minSdk = LOLLIPOP, isInAndroidSdk = false)
+public class ShadowFontFamily {
+
+  @Implementation(minSdk = O)
+  protected static long nInitBuilder(String lang, int variant) {
+    return 1;
+  }
+
+  @Implementation(minSdk = O)
+  protected void abortCreation() {}
+}
