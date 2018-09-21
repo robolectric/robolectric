8b534bb57262395e7fcf589c8370fae240debbba
Googler <noreply@google.com>
2018-09-20 17:02:07 -0700

    1. Add startActivity(Intent) method to ActivityInvoker interface and use it from ActivityScenario instead of directly calling Instrumentation#startActivitySync.
    Instrumentation#startActivitySync has an assertion that the resolved Activity's process name equals to the test target package name. This is to ensure that the Activity is launched in the same process as instrumentation but it is possible to run Activities with different process name in a same process if they share the same android:sharedUserId in the both AndroidManifests.
    
    InstrumentationActivityInvoker#startActivity uses Context#startActivity to avoid that assertion. (If the Activity ended up with running in the different process, the test will be timed out.)
    
    LocalActivityInvoker#startActivity continues to use Instrumentation#startActivitySync since its Shadow never check it (and Context#startActivity never start Activity by design).
    
    2. A small bug fix in LocalActivityInvoker#recreate where it doesn't call onRetainNonConfigurationInstance thus losing data over recreation.
    
    PiperOrigin-RevId: 213909555

--- robolectric/src/main/java/org/robolectric/android/internal/LocalActivityInvoker.java
+++ robolectric/src/main/java/org/robolectric/android/internal/LocalActivityInvoker.java
@@ -3,12 +3,16 @@ package org.robolectric.android.internal;
 import static androidx.test.InstrumentationRegistry.getInstrumentation;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.test.internal.platform.app.ActivityInvoker;
 import androidx.test.runner.lifecycle.ActivityLifecycleMonitorRegistry;
 import androidx.test.runner.lifecycle.Stage;
 import org.robolectric.Robolectric;
 import org.robolectric.android.controller.ActivityController;
+import org.robolectric.shadow.api.Shadow;
+import org.robolectric.shadows.ShadowActivity;
+import org.robolectric.util.ReflectionHelpers.ClassParameter;
 
 /**
  * An {@link ActivityInvoker} that drives {@link Activity} lifecycles manually.
@@ -16,6 +20,13 @@ import org.robolectric.android.controller.ActivityController;
  * <p>All the methods in this class are blocking API.
  */
 public class LocalActivityInvoker implements ActivityInvoker {
+  @Override
+  public void startActivity(Intent intent) {
+    getInstrumentation()
+        .startActivitySync(
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP));
+  }
+
   @Override
   public void resumeActivity(Activity activity) {
     Stage stage = ActivityLifecycleMonitorRegistry.getInstance().getLifecycleStageOf(activity);
@@ -76,7 +87,7 @@ public class LocalActivityInvoker implements ActivityInvoker {
   }
 
   @Override
-  public void recreateActivity(final Activity activity) {
+  public void recreateActivity(Activity activity) {
     Stage originalStage =
         ActivityLifecycleMonitorRegistry.getInstance().getLifecycleStageOf(activity);
 
@@ -85,14 +96,22 @@ public class LocalActivityInvoker implements ActivityInvoker {
 
     Bundle outState = new Bundle();
     getInstrumentation().callActivityOnSaveInstanceState(activity, outState);
+    Object nonConfigInstance = activity.onRetainNonConfigurationInstance();
     getInstrumentation().callActivityOnDestroy(activity);
 
     ActivityController<? extends Activity> controller =
         Robolectric.buildActivity(activity.getClass(), activity.getIntent());
     Activity recreatedActivity = controller.get();
+    Shadow.<ShadowActivity>extract(recreatedActivity)
+        .setLastNonConfigurationInstance(nonConfigInstance);
     getInstrumentation().callActivityOnCreate(recreatedActivity, outState);
     getInstrumentation().callActivityOnPostCreate(recreatedActivity, outState);
     getInstrumentation().callActivityOnStart(recreatedActivity);
+    Shadow.directlyOn(
+        recreatedActivity,
+        Activity.class,
+        "onRestoreInstanceState",
+        ClassParameter.from(Bundle.class, outState));
     getInstrumentation().callActivityOnResume(recreatedActivity);
     controller.visible().windowFocusChanged(true);
 
