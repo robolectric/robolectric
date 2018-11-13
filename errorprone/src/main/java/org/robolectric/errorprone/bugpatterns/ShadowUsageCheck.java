2109cf3d2f72fa5f07bb35259c183e24c3efbaf5
christianw <christianw@google.com>
2018-11-13 12:06:59 -0800

    Fix bug checker links.
    PiperOrigin-RevId: 221309469

--- errorprone/src/main/java/org/robolectric/errorprone/bugpatterns/ShadowUsageCheck.java
+++ errorprone/src/main/java/org/robolectric/errorprone/bugpatterns/ShadowUsageCheck.java
@@ -89,7 +89,7 @@ import org.robolectric.errorprone.bugpatterns.Helpers.AnnotatedMethodMatcher;
     severity = SUGGESTION,
     documentSuppression = false,
     tags = StandardTags.REFACTORING,
-    link = "http://robolectric.org/errorprone-refactorings/",
+    link = "http://robolectric.org/migrating/#improper-use-of-shadows",
     linkType = LinkType.CUSTOM,
     providesFix = ProvidesFix.REQUIRES_HUMAN_ATTENTION)
 public final class ShadowUsageCheck extends BugChecker implements ClassTreeMatcher {
