6d36bf56955a26a5ae5cdf480c2b65d7f24905f2
Googler <noreply@google.com>
2018-11-30 16:12:22 -0800

    Copybara import of the project:
    --
    223591556 by brettchabot <brettchabot@google.com>:
    
    Change API baseline to 4.0.2
    
    PiperOrigin-RevId: 223591556

---
+++ robolectric/src/main/java/org/robolectric/android/internal/RoboUiController.java
@@ -0,0 +1,171 @@
+package org.robolectric.android.internal;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.base.Preconditions.checkState;
+import static com.google.common.collect.Iterables.getOnlyElement;
+
+import android.annotation.SuppressLint;
+import android.os.Build;
+import android.os.Build.VERSION_CODES;
+import android.os.Looper;
+import android.os.SystemClock;
+import android.util.Log;
+import android.view.KeyCharacterMap;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.ViewRootImpl;
+import android.view.WindowManagerGlobal;
+import android.view.WindowManagerImpl;
+import androidx.test.platform.ui.InjectEventSecurityException;
+import androidx.test.platform.ui.UiController;
+import com.google.common.annotations.VisibleForTesting;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import org.robolectric.RuntimeEnvironment;
+import org.robolectric.shadows.ShadowLooper;
+import org.robolectric.util.ReflectionHelpers;
+
+/** Custom implementation of {@link UiController} for Robolectric. */
+public class RoboUiController implements UiController {
+
+  private static final String TAG = "RoboUiController";
+
+  @Override
+  public boolean injectMotionEvent(MotionEvent event) throws InjectEventSecurityException {
+    checkNotNull(event);
+    checkState(Looper.myLooper() == Looper.getMainLooper(), "Expecting to be on main thread!");
+    loopMainThreadUntilIdle();
+
+    // TODO: temporarily restrict to one view root for now
+    getOnlyElement(getViewRoots()).getView().dispatchTouchEvent(event);
+
+    loopMainThreadUntilIdle();
+
+    return true;
+  }
+
+  @Override
+  public boolean injectKeyEvent(KeyEvent event) throws InjectEventSecurityException {
+    checkNotNull(event);
+    checkState(Looper.myLooper() == Looper.getMainLooper(), "Expecting to be on main thread!");
+
+    loopMainThreadUntilIdle();
+    // TODO: temporarily restrict to one view root for now
+    getOnlyElement(getViewRoots()).getView().dispatchKeyEvent(event);
+
+    loopMainThreadUntilIdle();
+    return true;
+  }
+
+  // TODO(b/80130000): implementation copied from espresso's UIControllerImpl. Refactor code into common location
+  @Override
+  public boolean injectString(String str) throws InjectEventSecurityException {
+    checkNotNull(str);
+    checkState(Looper.myLooper() == Looper.getMainLooper(), "Expecting to be on main thread!");
+
+    // No-op if string is empty.
+    if (str.isEmpty()) {
+      Log.w(TAG, "Supplied string is empty resulting in no-op (nothing is typed).");
+      return true;
+    }
+
+    boolean eventInjected = false;
+    KeyCharacterMap keyCharacterMap = getKeyCharacterMap();
+
+    // TODO(b/80130875): Investigate why not use (as suggested in javadoc of
+    // keyCharacterMap.getEvents):
+    // http://developer.android.com/reference/android/view/KeyEvent.html#KeyEvent(long,
+    // java.lang.String, int, int)
+    KeyEvent[] events = keyCharacterMap.getEvents(str.toCharArray());
+    if (events == null) {
+      throw new RuntimeException(
+          String.format(
+              "Failed to get key events for string %s (i.e. current IME does not understand how to"
+                  + " translate the string into key events). As a workaround, you can use"
+                  + " replaceText action to set the text directly in the EditText field.",
+              str));
+    }
+
+    Log.d(TAG, String.format("Injecting string: \"%s\"", str));
+
+    for (KeyEvent event : events) {
+      checkNotNull(
+          event,
+          String.format(
+              "Failed to get event for character (%c) with key code (%s)",
+              event.getKeyCode(), event.getUnicodeChar()));
+
+      eventInjected = false;
+      for (int attempts = 0; !eventInjected && attempts < 4; attempts++) {
+        // We have to change the time of an event before injecting it because
+        // all KeyEvents returned by KeyCharacterMap.getEvents() have the same
+        // time stamp and the system rejects too old events. Hence, it is
+        // possible for an event to become stale before it is injected if it
+        // takes too long to inject the preceding ones.
+        event = KeyEvent.changeTimeRepeat(event, SystemClock.uptimeMillis(), 0);
+        eventInjected = injectKeyEvent(event);
+      }
+
+      if (!eventInjected) {
+        Log.e(
+            TAG,
+            String.format(
+                "Failed to inject event for character (%c) with key code (%s)",
+                event.getUnicodeChar(), event.getKeyCode()));
+        break;
+      }
+    }
+
+    return eventInjected;
+  }
+
+  @SuppressLint("InlinedApi")
+  @VisibleForTesting
+  @SuppressWarnings("deprecation")
+  static KeyCharacterMap getKeyCharacterMap() {
+    KeyCharacterMap keyCharacterMap = null;
+
+    // KeyCharacterMap.VIRTUAL_KEYBOARD is present from API11.
+    // For earlier APIs we use KeyCharacterMap.BUILT_IN_KEYBOARD
+    if (Build.VERSION.SDK_INT < 11) {
+      keyCharacterMap = KeyCharacterMap.load(KeyCharacterMap.BUILT_IN_KEYBOARD);
+    } else {
+      keyCharacterMap = KeyCharacterMap.load(KeyCharacterMap.VIRTUAL_KEYBOARD);
+    }
+    return keyCharacterMap;
+  }
+
+  @Override
+  public void loopMainThreadUntilIdle() {
+    ShadowLooper.getShadowMainLooper().idle();
+  }
+
+  @Override
+  public void loopMainThreadForAtLeast(long millisDelay) {
+    ShadowLooper.getShadowMainLooper().idle(millisDelay, TimeUnit.MILLISECONDS);
+  }
+
+  private static List<ViewRootImpl> getViewRoots() {
+    Object windowManager = getViewRootsContainer();
+    Object viewRootsObj = ReflectionHelpers.getField(windowManager, "mRoots");
+    Class<?> viewRootsClass = viewRootsObj.getClass();
+    if (ViewRootImpl[].class.isAssignableFrom(viewRootsClass)) {
+      return Arrays.asList((ViewRootImpl[]) viewRootsObj);
+    } else if (List.class.isAssignableFrom(viewRootsClass)) {
+      return (List<ViewRootImpl>) viewRootsObj;
+    } else {
+      throw new IllegalStateException(
+          "WindowManager.mRoots is an unknown type " + viewRootsClass.getName());
+    }
+  }
+
+  private static Object getViewRootsContainer() {
+    if (RuntimeEnvironment.getApiLevel() <= VERSION_CODES.JELLY_BEAN) {
+      return ReflectionHelpers.callStaticMethod(WindowManagerImpl.class, "getDefault");
+    } else {
+      return WindowManagerGlobal.getInstance();
+    }
+  }
+}
