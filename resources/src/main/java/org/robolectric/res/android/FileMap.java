7bf683167987a543b91a5f1cbe674272473576b8
Christian Williams <christianw@google.com>
2018-12-30 14:53:39 -0800

    Don't try to simulate Android's memory mapping for uncompressed zip entries.

--- resources/src/main/java/org/robolectric/res/android/FileMap.java
+++ resources/src/main/java/org/robolectric/res/android/FileMap.java
@@ -130,15 +130,15 @@ public class FileMap {
     this.zipFile = zipFile;
     this.zipEntry = entry;
 
-    int     prot, flags, adjust;
-    long adjOffset;
-    int  adjLength;
+    // int     prot, flags, adjust;
+    // long adjOffset;
+    // int  adjLength;
 
-    int ptr;
-    long offset = guessOffsetFor(zipFile, entry);
+    // int ptr;
+    long offset = -1; // guessOffsetFor(zipFile, entry);
 
     assert(fd >= 0);
-    assert(offset >= 0);
+    // assert(offset >= 0);
     // assert(length > 0);
 
     // init on first use
