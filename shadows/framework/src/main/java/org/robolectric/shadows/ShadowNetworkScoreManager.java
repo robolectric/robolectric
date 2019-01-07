6e48bf19b51e7c3042a3eedb755438c9ebcf8687
Googler <noreply@google.com>
2019-01-07 10:10:38 -0800

    Internal change
    PiperOrigin-RevId: 228187131

--- shadows/framework/src/main/java/org/robolectric/shadows/ShadowNetworkScoreManager.java
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowNetworkScoreManager.java
@@ -4,15 +4,18 @@ import android.net.NetworkScoreManager;
 import android.os.Build;
 import org.robolectric.annotation.Implementation;
 import org.robolectric.annotation.Implements;
+import org.robolectric.annotation.RealObject;
+import org.robolectric.shadow.api.Shadow;
 
 /** Provides testing APIs for {@link NetworkScoreManager}. */
 @Implements(
-  value = NetworkScoreManager.class,
-  isInAndroidSdk = false,
-  minSdk = Build.VERSION_CODES.LOLLIPOP
-)
+    value = NetworkScoreManager.class,
+    isInAndroidSdk = false,
+    minSdk = Build.VERSION_CODES.LOLLIPOP)
 public class ShadowNetworkScoreManager {
+  @RealObject private NetworkScoreManager realObject;
   private String activeScorerPackage;
+  private boolean isScoringEnabled = true;
 
   @Implementation
   public String getActiveScorerPackage() {
@@ -23,5 +26,17 @@ public class ShadowNetworkScoreManager {
   public boolean setActiveScorer(String packageName) {
     activeScorerPackage = packageName;
     return true;
+  }
+
+  /** @see #isScoringEnabled() */
+  @Implementation
+  protected void disableScoring() {
+    isScoringEnabled = false;
+    Shadow.directlyOn(realObject, NetworkScoreManager.class, "disableScoring");
+  }
+
+  /** Whether scoring is enabled. */
+  public boolean isScoringEnabled() {
+    return isScoringEnabled;
   }
 }
