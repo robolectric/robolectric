4b2de1dc87c23d42fdfac0559a3b0f19cb6784ff
Googler <noreply@google.com>
2019-01-25 12:52:27 -0800

    Adds basic MediaCodec and MediaMuxer shadows to robolectric. The MediaCodec shadow implements basic passthrough asynchronous encoding, and the MediaMuxer shadow muxes input bytes directly to the file without modification.
    PiperOrigin-RevId: 230952094

---
+++ robolectric/src/test/java/org/robolectric/shadows/ShadowMediaMuxerTest.java
@@ -0,0 +1,58 @@
+package org.robolectric.shadows;
+
+import static android.os.Build.VERSION_CODES.LOLLIPOP;
+import static com.google.common.truth.Truth.assertThat;
+
+import android.media.MediaCodec;
+import android.media.MediaFormat;
+import android.media.MediaMuxer;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.Random;
+import java.util.UUID;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.annotation.Config;
+
+/** Tests for {@link ShadowMediaMuxer}. */
+@RunWith(AndroidJUnit4.class)
+public final class ShadowMediaMuxerTest {
+
+  @Test
+  @Config(minSdk = LOLLIPOP)
+  public void basicMuxingFlow() throws IOException {
+    int inputSize = 512;
+    String tempFilePath = "/tmp/" + UUID.randomUUID().toString();
+    MediaMuxer muxer = new MediaMuxer(tempFilePath, MediaMuxer.OutputFormat.MUXER_OUTPUT_MPEG_4);
+
+    MediaFormat format = new MediaFormat();
+    format.setString(MediaFormat.KEY_MIME, MediaFormat.MIMETYPE_AUDIO_AAC);
+
+    int trackIndex = muxer.addTrack(format);
+    muxer.start();
+
+    byte[] inputBytes = new byte[inputSize];
+    new Random().nextBytes(inputBytes);
+    ByteBuffer inputBuffer = ByteBuffer.wrap(inputBytes);
+    MediaCodec.BufferInfo bufferInfo = new MediaCodec.BufferInfo();
+    bufferInfo.set(0, inputSize, 0, 0);
+
+    muxer.writeSampleData(trackIndex, inputBuffer, bufferInfo);
+    muxer.stop();
+
+    // Read in what was muxed.
+    byte[] outputBytes = new byte[inputSize];
+    FileInputStream tempFile = new FileInputStream(tempFilePath);
+
+    int offset = 0;
+    int bytesRead = 0;
+    while (inputSize - offset > 0
+        && (bytesRead = tempFile.read(outputBytes, offset, inputSize - offset)) != -1) {
+      offset += bytesRead;
+    }
+
+    assertThat(outputBytes).isEqualTo(inputBytes);
+  }
+}
