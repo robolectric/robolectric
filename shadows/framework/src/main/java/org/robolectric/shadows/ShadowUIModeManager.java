c3c888eddf7d3d1b7a41181e15277c0fbfb2c3cc
Googler <noreply@google.com>
2019-02-07 17:39:35 -0800

    UiModeManager
    Added a night mode getter and setter for ShadowUIModeManager
    
    PiperOrigin-RevId: 232979405

--- shadows/framework/src/main/java/org/robolectric/shadows/ShadowUIModeManager.java
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowUIModeManager.java
@@ -2,6 +2,8 @@ package org.robolectric.shadows;
 
 import android.app.UiModeManager;
 import android.content.res.Configuration;
+import java.util.HashSet;
+import java.util.Set;
 import org.robolectric.annotation.Implementation;
 import org.robolectric.annotation.Implements;
 
@@ -9,6 +11,15 @@ import org.robolectric.annotation.Implements;
 @Implements(UiModeManager.class)
 public class ShadowUIModeManager {
   public int currentModeType = Configuration.UI_MODE_TYPE_UNDEFINED;
+  public int currentNightMode = UiModeManager.MODE_NIGHT_AUTO;
+
+  private static Set<Integer> validNightModes = new HashSet<>();
+
+  static {
+    validNightModes.add(UiModeManager.MODE_NIGHT_AUTO);
+    validNightModes.add(UiModeManager.MODE_NIGHT_NO);
+    validNightModes.add(UiModeManager.MODE_NIGHT_YES);
+  }
 
   @Implementation
   public int getCurrentModeType() {
@@ -23,5 +34,19 @@ public class ShadowUIModeManager {
   @Implementation
   public void disableCarMode(int flags) {
     currentModeType = Configuration.UI_MODE_TYPE_NORMAL;
+  }
+
+  @Implementation
+  public int getNightMode() {
+    return currentNightMode;
+  }
+
+  @Implementation
+  public void setNightMode(int mode) {
+    if (!validNightModes.contains(mode)) {
+      currentNightMode = UiModeManager.MODE_NIGHT_AUTO;
+    } else {
+      currentNightMode = mode;
+    }
   }
 }
