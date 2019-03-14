30c467a1717ef821ddd758d847acd54392e81896
Christian Williams <christianw@google.com>
2019-03-14 13:29:19 -0700

    Faster circleci sharded test run.
    Run integration tests only for the set of robolectric.enabledSdks.
    Parallelize snapshot upload.

---
+++ buildSrc/src/main/groovy/org/robolectric/gradle/AndroidProjectConfigPlugin.groovy
@@ -0,0 +1,39 @@
+package org.robolectric.gradle
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+public class AndroidProjectConfigPlugin implements Plugin<Project> {
+    @Override
+    public void apply(Project project) {
+        project.android.testOptions.unitTests.all {
+            // TODO: DRY up code with RoboJavaModulePlugin...
+            testLogging {
+                exceptionFormat "full"
+                showCauses true
+                showExceptions true
+                showStackTraces true
+                showStandardStreams true
+                events = ["failed", "skipped"]
+            }
+
+            minHeapSize = "1024m"
+            maxHeapSize = "4096m"
+
+            if (System.env['GRADLE_MAX_PARALLEL_FORKS'] != null) {
+                maxParallelForks = Integer.parseInt(System.env['GRADLE_MAX_PARALLEL_FORKS'])
+            }
+
+            def forwardedSystemProperties = System.properties
+                    .findAll { k,v -> k.startsWith("robolectric.") }
+                    .collect { k,v -> "-D$k=$v" }
+            jvmArgs = ["-XX:MaxPermSize=1024m"] + forwardedSystemProperties
+
+            doFirst {
+                if (!forwardedSystemProperties.isEmpty()) {
+                    println "Running tests with ${forwardedSystemProperties}"
+                }
+            }
+        }
+    }
+}
