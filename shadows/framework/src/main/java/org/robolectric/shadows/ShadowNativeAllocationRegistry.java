1f5f0fe0b8d5007029dd84c4e90f73046f2db6f7
brettchabot <brettchabot@google.com>
2019-03-21 14:49:45 -0700

    Enable callThroughByDefault for NativeAllocationRegistry, and explictly shadow methods instead.
    PiperOrigin-RevId: 239678323

--- shadows/framework/src/main/java/org/robolectric/shadows/ShadowNativeAllocationRegistry.java
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowNativeAllocationRegistry.java
@@ -3,8 +3,20 @@ package org.robolectric.shadows;
 import static android.os.Build.VERSION_CODES.N;
 
 import libcore.util.NativeAllocationRegistry;
+import libcore.util.NativeAllocationRegistry.Allocator;
+import org.robolectric.annotation.Implementation;
 import org.robolectric.annotation.Implements;
 
-@Implements(value = NativeAllocationRegistry.class, callThroughByDefault = false, minSdk = N, isInAndroidSdk = false)
+@Implements(value = NativeAllocationRegistry.class, minSdk = N, isInAndroidSdk = false)
 public class ShadowNativeAllocationRegistry {
+
+  @Implementation
+  protected Runnable registerNativeAllocation(Object referent, Allocator allocator) {
+    return null;
+  }
+
+  @Implementation
+  protected Runnable registerNativeAllocation(Object referent, long nativePtr) {
+    return null;
+  }
 }
