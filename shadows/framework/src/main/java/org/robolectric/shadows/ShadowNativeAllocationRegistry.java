5409c0d9f4f18c6ed6c41a24d142033e56db773a
brettchabot <brettchabot@google.com>
2019-03-28 12:15:06 -0700

    Change ShadowNativeAllocationRegistry to use looseSignatures
    PiperOrigin-RevId: 240825231

--- shadows/framework/src/main/java/org/robolectric/shadows/ShadowNativeAllocationRegistry.java
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowNativeAllocationRegistry.java
@@ -3,15 +3,14 @@ package org.robolectric.shadows;
 import static android.os.Build.VERSION_CODES.N;
 
 import libcore.util.NativeAllocationRegistry;
-import libcore.util.NativeAllocationRegistry.Allocator;
 import org.robolectric.annotation.Implementation;
 import org.robolectric.annotation.Implements;
 
-@Implements(value = NativeAllocationRegistry.class, minSdk = N, isInAndroidSdk = false)
+@Implements(value = NativeAllocationRegistry.class, minSdk = N, isInAndroidSdk = false, looseSignatures = true)
 public class ShadowNativeAllocationRegistry {
 
   @Implementation
-  protected Runnable registerNativeAllocation(Object referent, Allocator allocator) {
+  protected Runnable registerNativeAllocation(Object referent, Object allocator) {
     return null;
   }
 
