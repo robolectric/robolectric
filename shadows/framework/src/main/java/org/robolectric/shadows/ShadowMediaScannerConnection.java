a2a6b78a020599dd7e59080ece5b089cbf724f30
Googler <noreply@google.com>
2019-04-01 20:57:15 -0700

    PhotosGo: remove ShadowMediaScannerConnection to make it possible to use the class in Robolectric tests.
    PiperOrigin-RevId: 241456629

--- shadows/framework/src/main/java/org/robolectric/shadows/ShadowMediaScannerConnection.java
+++
@@ -1,8 +0,0 @@
-package org.robolectric.shadows;
-
-import android.media.MediaScannerConnection;
-import org.robolectric.annotation.Implements;
-
-@Implements(MediaScannerConnection.class)
-public class ShadowMediaScannerConnection {
-}
