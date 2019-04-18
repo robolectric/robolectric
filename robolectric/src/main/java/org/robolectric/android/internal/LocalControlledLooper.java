8e458cd76e06675959217635e02525d01b1cf7ac
brettchabot <brettchabot@google.com>
2019-04-18 10:02:46 -0700

    Add hook in androidx.test to simulate a window focus changed for local test environments, and use it when Espresso is waiting for a root view to gain focus.
    For https://github.com/robolectric/robolectric/issues/4354, https://github.com/robolectric/robolectric/issues/4862
    
    PiperOrigin-RevId: 244205804

--- robolectric/src/main/java/org/robolectric/android/internal/LocalControlledLooper.java
+++ robolectric/src/main/java/org/robolectric/android/internal/LocalControlledLooper.java
@@ -1,13 +1,30 @@
 package org.robolectric.android.internal;
 
 import static org.robolectric.shadows.ShadowLooper.shadowMainLooper;
+import static org.robolectric.util.ReflectionHelpers.ClassParameter.from;
 
+import android.view.View;
+import android.view.ViewRootImpl;
 import androidx.test.internal.platform.os.ControlledLooper;
+import org.robolectric.util.ReflectionHelpers;
 
 public class LocalControlledLooper implements ControlledLooper {
 
   @Override
   public void drainMainThreadUntilIdle() {
     shadowMainLooper().idle();
+  }
+
+  // TODO: add back Override when robolectric github depends on androidx.test-alpha04
+  // @Override
+  public void simulateWindowFocus(View decorView) {
+    ViewRootImpl viewRoot = ReflectionHelpers.callInstanceMethod(decorView, "getViewRootImpl");
+    if (viewRoot != null) {
+      ReflectionHelpers.callInstanceMethod(
+          viewRoot,
+          "windowFocusChanged",
+          from(boolean.class, true), /* hasFocus */
+          from(boolean.class, false) /* inTouchMode */);
+    }
   }
 }
