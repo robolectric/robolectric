2af2e4ea9ebc354f2e7c4bab599b999d73c11c72
brettchabot <brettchabot@google.com>
2019-05-13 20:50:05 -0700

    Revert back to android.support for DrawerLayout
    PiperOrigin-RevId: 248066517

--- shadows/supportv4/src/main/java/org/robolectric/shadows/support/v4/ShadowDrawerLayout.java
+++ shadows/supportv4/src/main/java/org/robolectric/shadows/support/v4/ShadowDrawerLayout.java
@@ -3,7 +3,7 @@ package org.robolectric.shadows.support.v4;
 import static org.robolectric.shadow.api.Shadow.directlyOn;
 
 import android.view.View;
-import androidx.drawerlayout.widget.DrawerLayout;
+import android.support.v4.widget.DrawerLayout;
 import org.robolectric.annotation.Implementation;
 import org.robolectric.annotation.Implements;
 import org.robolectric.annotation.RealObject;
