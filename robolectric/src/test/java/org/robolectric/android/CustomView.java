0f4d74551bfbe13a98a855714a29837ae11888a2
Googler <noreply@google.com>
2019-05-25 16:26:56 -0700

    Remove stray nested layout from CustomView.
    This was added in commit 6f39d63484 to test a shadow which no longer exists.
    ShadowLayoutInflaterTest.shouldInflateMergeLayoutIntoParent covers this
    scenario in case LayoutInflater needs to be shadowed again.
    
    PiperOrigin-RevId: 250000697

--- robolectric/src/test/java/org/robolectric/android/CustomView.java
+++ robolectric/src/test/java/org/robolectric/android/CustomView.java
@@ -3,7 +3,6 @@ package org.robolectric.android;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.widget.LinearLayout;
-import org.robolectric.R;
 
 public class CustomView extends LinearLayout {
   public static final String ROBOLECTRIC_RES_URI = "http://schemas.android.com/apk/res/org.robolectric";
@@ -14,7 +13,6 @@ public class CustomView extends LinearLayout {
 
   public CustomView(Context context, AttributeSet attrs) {
     super(context, attrs);
-    inflate(context, R.layout.inner_merge, this);
     attributeResourceValue = attrs.getAttributeResourceValue(ROBOLECTRIC_RES_URI, "message", -1);
     namespacedResourceValue = attrs.getAttributeResourceValue(FAKE_URI, "message", -1);
   }
