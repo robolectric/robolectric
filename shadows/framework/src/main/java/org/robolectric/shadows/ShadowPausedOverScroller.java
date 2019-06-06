23bf0045f3e21e6a225ade9019296a23197a7156
jongerrish <jongerrish@google.com>
2019-06-06 08:36:02 -0700

    Remove ShadowOverscroller - this obscures real Android behavior.
    PiperOrigin-RevId: 251857361

--- shadows/framework/src/main/java/org/robolectric/shadows/ShadowPausedOverScroller.java
+++
@@ -1,8 +0,0 @@
-package org.robolectric.shadows;
-
-import android.widget.OverScroller;
-import org.robolectric.annotation.Implements;
-
-/** The OverScroller shadow for {@link org.robolectric.annotation.LooperMode.Mode#PAUSED}. */
-@Implements(value = OverScroller.class, isInAndroidSdk = false)
-public class ShadowPausedOverScroller extends ShadowOverScroller {}
