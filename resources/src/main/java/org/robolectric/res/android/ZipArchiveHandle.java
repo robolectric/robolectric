8339f801f6ab3cebb948f87e4815826408d2b45c
Googler <noreply@google.com>
2019-06-17 19:39:13 -0700

    Tolerate gaps between ZIP entries when jumping between Local File Headers.
    AAPT2 stores CRC/size of compressed entries using the "data descriptor"
    instead of in the local file header.  Since we don't parse headers, this
    essentially looks like a 16-byte gap.  As far as I can tell, AAPTv1 stored
    CRC/size of compressed entries in the local file headers.
    
    This change also fixes things so that we use the LFHs for reading
    filenames/sizes.  java.util.zip.ZipEntry traverses the Central Directory, but:
    * resources/assets/exampleapp.apk has a Central Directory whose contents aren't
      sorted, and
    * Gradle test infrastructure generates ZIPs where the files have inconsistent
      "extra" data in the LFH and CD.
    Parsing the CD is greatly preferred to the LFH, but out of scope of this
    change.  https://github.com/robolectric/robolectric/issues/5123 tracks using
    the CD.
    
    Finally, since Robolectric tests might load entries from android-all-9-robolectric.jar
    (which contains ~45k entries) we need to ensure that the directory is only
    loaded once:
    
    * ZipArchiveHandle contains precomputed offsets of ZIP file entries; this
      roughly mirrors Android's system/core/libziparchive/zip_archive.cc
    * CppApkAssets now only opens ZipFileRO once.  Previously, this was (merely) a
      resource leak, but now we really don't want to rescan ZIP files.  FWIW,
      frameworks/base/libs/androidfw/ApkAssets.cpp has never used ZipFileRO.
    
    PiperOrigin-RevId: 253710781

--- resources/src/main/java/org/robolectric/res/android/ZipArchiveHandle.java
+++ resources/src/main/java/org/robolectric/res/android/ZipArchiveHandle.java
@@ -1,11 +1,14 @@
 package org.robolectric.res.android;
 
+import com.google.common.collect.ImmutableMap;
 import java.util.zip.ZipFile;
 
 public class ZipArchiveHandle {
   final ZipFile zipFile;
+  final ImmutableMap<String, Long> dataOffsets;
 
-  public ZipArchiveHandle(ZipFile zipFile) {
+  public ZipArchiveHandle(ZipFile zipFile, ImmutableMap<String, Long> dataOffsets) {
     this.zipFile = zipFile;
+    this.dataOffsets = dataOffsets;
   }
 }
