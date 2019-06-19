ddce6c4933b2c567d595d01e6f20b780e69bd787
Googler <noreply@google.com>
2019-06-19 08:55:05 -0700

    Allows adding custom pattern to ShadowDateFormat, adds implememtation for getMediumDateFormat() and also changes the way to get current locale to follow the best practice.
    PiperOrigin-RevId: 254004957

--- shadows/framework/src/main/java/org/robolectric/shadows/ShadowDateFormat.java
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowDateFormat.java
@@ -2,26 +2,53 @@ package org.robolectric.shadows;
 
 import android.content.Context;
 import android.text.format.DateFormat;
-import java.util.Locale;
 import org.robolectric.annotation.Implementation;
 import org.robolectric.annotation.Implements;
 
 @Implements(DateFormat.class)
 public class ShadowDateFormat {
+  private static String timeFormatPattern = "HH:mm:ss";
+  private static String dateFormatPattern = "MMM-dd-yyyy";
+  private static String mediumDateFormatPattern = "MMM dd, yyyy";
+  private static String longDateFormatPattern = "MMMMM dd, yyyy";
+
+  public static void setTimeFormatPattern(String pattern) {
+    timeFormatPattern = pattern;
+  }
+
+  public static void setDateFormatPattern(String pattern) {
+    dateFormatPattern = pattern;
+  }
+
+  public static void setMediumDateFormatPattern(String pattern) {
+    mediumDateFormatPattern = pattern;
+  }
+
+  public static void setLongDateFormatPattern(String pattern) {
+    longDateFormatPattern = pattern;
+  }
 
   @Implementation
   protected static java.text.DateFormat getDateFormat(Context context) {
-    return new java.text.SimpleDateFormat("MMM-dd-yyyy", Locale.ROOT);
+    return new java.text.SimpleDateFormat(
+        dateFormatPattern, context.getResources().getConfiguration().locale);
+  }
+
+  @Implementation
+  protected static java.text.DateFormat getMediumDateFormat(Context context) {
+    return new java.text.SimpleDateFormat(
+        mediumDateFormatPattern, context.getResources().getConfiguration().locale);
   }
 
   @Implementation
   protected static java.text.DateFormat getLongDateFormat(Context context) {
     return new java.text.SimpleDateFormat(
-        "MMMMM dd, yyyy", context.getResources().getConfiguration().locale);
+        longDateFormatPattern, context.getResources().getConfiguration().locale);
   }
 
   @Implementation
   protected static java.text.DateFormat getTimeFormat(Context context) {
-    return new java.text.SimpleDateFormat("HH:mm:ss", Locale.ROOT);
+    return new java.text.SimpleDateFormat(
+        timeFormatPattern, context.getResources().getConfiguration().locale);
   }
 }
