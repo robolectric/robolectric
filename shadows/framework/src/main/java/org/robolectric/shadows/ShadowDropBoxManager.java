eb4c087e18846bdd2610a79ac2a6e874d9ea07a6
Googler <noreply@google.com>
2019-07-10 13:31:31 -0700

    Make ShadowDropBoxManager usable.
    Previously, the only way to add entries was package-private. This:
    
    *   Exposes the ability to easily add entries.
    *   Implements the platform API of addText()
    
    PiperOrigin-RevId: 257470461

--- shadows/framework/src/main/java/org/robolectric/shadows/ShadowDropBoxManager.java
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowDropBoxManager.java
@@ -1,5 +1,7 @@
 package org.robolectric.shadows;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+
 import android.os.DropBoxManager;
 import android.os.DropBoxManager.Entry;
 import java.util.SortedMap;
@@ -19,20 +21,38 @@ public class ShadowDropBoxManager {
   /**
    * Adds entry to the DropboxManager with the flag indicating data is text.
    *
-   * <p>The existing {@link DropBoxManager#addData}, {@link DropBoxManager#addFile}, and {@link
-   * DropBoxManager#addText} methods in DropBoxManager are not shadowed. This method is a
-   * convenience for quickly adding multiple historical entries. The entries can be added in any
-   * order since this shadow will sort the entries by the specified timestamp.
+   * <p>The existing {@link DropBoxManager#addData} and {@link DropBoxManager#addFile} methods in
+   * DropBoxManager are not shadowed (and do not work), but {@link DropBoxManager#addText} is. This
+   * method is a convenience for quickly adding multiple historical entries. The entries can be
+   * added in any order since this shadow will sort the entries by the specified timestamp.
    *
    * <p>The flag will be set to {@link DropBoxManager#IS_TEXT} so that {@link
    * DropBoxManager.Entry#getText} can be used.
    *
    * @param tag can be any arbitrary string
-   * @param timestamp is an arbitrary timestamp that must be unique from all other entries
+   * @param timestamp a unique timestamp for the entry, relative to {@link
+   *     System#currentTimeMillis()}
    * @param data must not be null
    */
-  void addData(String tag, long timestamp, byte[] data) {
-    entries.put(timestamp, new DropBoxManager.Entry(tag, timestamp, data, DropBoxManager.IS_TEXT));
+  public void addData(String tag, long wallTimestamp, byte[] data) {
+    if (entries.containsKey(wallTimestamp)) {
+      throw new AssertionError("Cannot add multiple entries with the exact same timestamp.");
+    }
+    entries.put(
+        wallTimestamp, new DropBoxManager.Entry(tag, wallTimestamp, data, DropBoxManager.IS_TEXT));
+  }
+
+  /**
+   * Adds a text entry to dropbox with the current timestamp using UTF-8 encoding.
+   *
+   * <p>If adding multiple entries, it is required to ensure they have unique timestamps by bumping
+   * the wall-clock time, using {@link android.os.SystemClock} or similar.
+   */
+  @Implementation
+  protected void addText(String tag, String data) {
+    // NOTE: Need to use ShadowSystemClock for current time, because this doesn't run in the
+    // ClassLoader that customizes System.currentTimeMillis.
+    addData(tag, ShadowSystem.currentTimeMillis(), data.getBytes(UTF_8));
   }
 
   /**
