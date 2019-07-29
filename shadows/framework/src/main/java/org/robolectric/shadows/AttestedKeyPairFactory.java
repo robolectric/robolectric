12ced72bfd1a581ee616dfb485e12b98e5d094b5
Googler <noreply@google.com>
2019-07-29 10:47:40 -0700

    Move to using KeyGenParameterSpec and the two DPM APIs generateKeyPair & setKeyPairCertificate.
    PiperOrigin-RevId: 260533121

---
+++ shadows/framework/src/main/java/org/robolectric/shadows/AttestedKeyPairFactory.java
@@ -0,0 +1,13 @@
+package org.robolectric.shadows;
+
+import android.security.AttestedKeyPair;
+import java.security.KeyPair;
+import java.security.cert.Certificate;
+
+/** Factory to create AttestedKeyPair. */
+public class AttestedKeyPairFactory {
+
+  public static AttestedKeyPair create(KeyPair keyPair, Certificate[] attestationRecord) {
+    return new AttestedKeyPair(keyPair, attestationRecord);
+  }
+}
