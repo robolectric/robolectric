f7f16b5975db0efe606a7bf281b441066d845130
Googler <noreply@google.com>
2019-09-17 03:29:50 -0700

    GetStorageVolume checks also files contained within a volume path.
    PiperOrigin-RevId: 269532964

--- robolectric/src/test/java/org/robolectric/shadows/ShadowStorageManagerTest.java
+++ robolectric/src/test/java/org/robolectric/shadows/ShadowStorageManagerTest.java
@@ -22,6 +22,9 @@ import org.robolectric.annotation.Config;
 @RunWith(AndroidJUnit4.class)
 public class ShadowStorageManagerTest {
 
+  private final String internalStorage = "/storage/internal";
+  private final String sdcardStorage = "/storage/sdcard";
+
   private StorageManager storageManager;
 
   @Before
@@ -37,7 +40,7 @@ public class ShadowStorageManagerTest {
   @Test
   @Config(minSdk = N)
   public void getStorageVolumes() {
-    File file1 = new File("/storage/sdcard");
+    File file1 = new File(sdcardStorage);
     shadowOf(storageManager).addStorageVolume(buildAndGetStorageVolume(file1, "sd card"));
     assertThat(shadowOf(storageManager).getStorageVolumes()).isNotNull();
   }
@@ -45,11 +48,15 @@ public class ShadowStorageManagerTest {
   @Test
   @Config(minSdk = N)
   public void getStorageVolume() {
-    File file1 = new File("/storage/internal");
-    File file2 = new File("/storage/sdcard");
+    File file1 = new File(internalStorage);
+    File file2 = new File(sdcardStorage);
+    File file3 = new File(internalStorage, "test_folder");
+    File file4 = new File(sdcardStorage, "test_folder");
     shadowOf(storageManager).addStorageVolume(buildAndGetStorageVolume(file1, "internal"));
     assertThat(shadowOf(storageManager).getStorageVolume(file1)).isNotNull();
     assertThat(shadowOf(storageManager).getStorageVolume(file2)).isNull();
+    assertThat(shadowOf(storageManager).getStorageVolume(file3)).isNotNull();
+    assertThat(shadowOf(storageManager).getStorageVolume(file4)).isNull();
   }
 
   @Test
