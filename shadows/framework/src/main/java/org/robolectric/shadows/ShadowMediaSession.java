c101d346f035dea19636c24e0fd4e08b533544a5
Googler <noreply@google.com>
2019-10-08 11:15:46 -0700

    remove the ShadowSessionInfo from ShadawMediaSession for Q
    PiperOrigin-RevId: 273564203

--- shadows/framework/src/main/java/org/robolectric/shadows/ShadowMediaSession.java
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowMediaSession.java
@@ -4,8 +4,6 @@ import static android.os.Build.VERSION_CODES.LOLLIPOP;
 
 import android.content.Context;
 import android.media.session.MediaSession;
-import android.os.Build.VERSION_CODES;
-import android.os.Bundle;
 import org.robolectric.annotation.Implementation;
 import org.robolectric.annotation.Implements;
 
@@ -14,7 +12,4 @@ public class ShadowMediaSession {
 
   @Implementation
   protected void __constructor__(Context context, String tag) {}
-
-  @Implementation(minSdk = VERSION_CODES.Q)
-  protected void __constructor__(Context context, String tag, Bundle bundle) {}
 }
