cb601be18813d5e5db49423ca9bbeb71fb758e21
Googler <noreply@google.com>
2019-10-23 13:47:21 -0700

    Add best-effort permission granting
    PiperOrigin-RevId: 276343205

--- robolectric/src/main/java/org/robolectric/android/internal/LocalPermissionGranter.java
+++ robolectric/src/main/java/org/robolectric/android/internal/LocalPermissionGranter.java
@@ -1,29 +1,37 @@
 package org.robolectric.android.internal;
 
-import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.base.Preconditions.checkState;
 
 import android.app.Application;
 import androidx.test.internal.platform.content.PermissionGranter;
 import androidx.test.platform.app.InstrumentationRegistry;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import org.robolectric.shadow.api.Shadow;
 import org.robolectric.shadows.ShadowApplication;
 
 /** A {@link PermissionGranter} that runs on a local JVM with Robolectric. */
 public class LocalPermissionGranter implements PermissionGranter {
 
-  private String[] permissions;
+  private final List<String> permissions = new ArrayList<>();
 
   @Override
   public void addPermissions(String... permissions) {
-    this.permissions = permissions;
+    this.permissions.addAll(Arrays.asList(permissions));
+  }
+
+  @Override
+  public void addOptionalPermissions(String... permissions) {
+    this.permissions.addAll(Arrays.asList(permissions));
   }
 
   @Override
   public void requestPermissions() {
-    checkNotNull(permissions);
+    checkState(!permissions.isEmpty());
     Application application =
         (Application) InstrumentationRegistry.getInstrumentation().getTargetContext();
     ShadowApplication shadowApplication = Shadow.extract(application);
-    shadowApplication.grantPermissions(permissions);
+    shadowApplication.grantPermissions(permissions.toArray(new String[permissions.size()]));
   }
 }
