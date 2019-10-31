b1eada15977f0bc594d7118bfef10b751bfda684
Googler <noreply@google.com>
2019-10-31 12:02:05 -0700

    Ensure that the ShadowTouchDelegate class invokes the real object's constructor correctly when initializing itself. Without this, tests that invoke methods on TouchDelegate don't work correctly.
    PiperOrigin-RevId: 277772139

--- shadows/framework/src/main/java/org/robolectric/shadows/ShadowTouchDelegate.java
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowTouchDelegate.java
@@ -1,14 +1,11 @@
 package org.robolectric.shadows;
 
-import static org.robolectric.shadow.api.Shadow.invokeConstructor;
-
 import android.graphics.Rect;
 import android.view.TouchDelegate;
 import android.view.View;
 import org.robolectric.annotation.Implementation;
 import org.robolectric.annotation.Implements;
 import org.robolectric.annotation.RealObject;
-import org.robolectric.util.ReflectionHelpers.ClassParameter;
 
 @Implements(TouchDelegate.class)
 public class ShadowTouchDelegate {
@@ -20,9 +17,6 @@ public class ShadowTouchDelegate {
   protected void __constructor__(Rect bounds, View delegateView) {
     this.bounds = bounds;
     this.delegateView = delegateView;
-    invokeConstructor(TouchDelegate.class, realObject,
-        ClassParameter.from(Rect.class, bounds),
-        ClassParameter.from(View.class, delegateView));
   }
 
   public Rect getBounds() {
