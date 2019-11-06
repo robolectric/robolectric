12184386de931864794693a8356419311ae32e7c
Googler <noreply@google.com>
2019-11-06 03:24:05 -0800

    Added a plugin mechanism to provide a custom ShadowWrangler specialization
    PiperOrigin-RevId: 278823004

--- robolectric/src/main/java/org/robolectric/RobolectricTestRunner.java
+++ robolectric/src/main/java/org/robolectric/RobolectricTestRunner.java
@@ -45,6 +45,7 @@ import org.robolectric.internal.bytecode.Sandbox;
 import org.robolectric.internal.bytecode.SandboxClassLoader;
 import org.robolectric.internal.bytecode.ShadowMap;
 import org.robolectric.internal.bytecode.ShadowWrangler;
+import org.robolectric.internal.bytecode.ShadowWranglerBuilder;
 import org.robolectric.manifest.AndroidManifest;
 import org.robolectric.pluginapi.Sdk;
 import org.robolectric.pluginapi.SdkPicker;
@@ -125,7 +126,8 @@ public class RobolectricTestRunner extends SandboxTestRunner {
   @Override
   @Nonnull
   protected ClassHandler createClassHandler(ShadowMap shadowMap, Sandbox sandbox) {
-    return new ShadowWrangler(shadowMap, ((AndroidSandbox) sandbox).getSdk().getApiLevel(), getInterceptors());
+    return ShadowWranglerBuilder
+        .build(shadowMap, ((AndroidSandbox) sandbox).getSdk().getApiLevel(), getInterceptors());
   }
 
   @Override
