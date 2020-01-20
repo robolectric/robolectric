af338d9fac2ca4791d80497d69d53146345bcfac
Googler <noreply@google.com>
2020-01-19 20:33:17 -0800

    Add capability to override autofill supported state and enabled state to ShadowAutofillManager.
    PiperOrigin-RevId: 290551351

--- shadows/framework/src/main/java/org/robolectric/shadows/ShadowAutofillManager.java
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowAutofillManager.java
@@ -16,6 +16,8 @@ import org.robolectric.annotation.Implements;
 @Implements(value = AutofillManager.class, minSdk = O)
 public class ShadowAutofillManager {
   @Nullable private ComponentName autofillServiceComponentName = null;
+  private boolean autofillSupported = false;
+  private boolean enabled = false;
 
   @Implementation
   protected FillEventHistory getFillEventHistory() {
@@ -31,11 +33,39 @@ public class ShadowAutofillManager {
     return autofillServiceComponentName;
   }
 
+  /** Returns the overridden value set by {@link #setAutofillSupported(boolean)}. */
+  @Implementation
+  protected boolean isAutofillSupported() {
+    return autofillSupported;
+  }
+
+  /** Returns the overridden value set by {@link #setEnabled(boolean)}. */
+  @Implementation
+  protected boolean isEnabled() {
+    return enabled;
+  }
+
   /**
    * Overrides the component name of the autofill service enabled for the current user. See {@link
    * AutofillManager#getAutofillServiceComponentName()}.
    */
   public void setAutofillServiceComponentName(@Nullable ComponentName componentName) {
     this.autofillServiceComponentName = componentName;
+  }
+
+  /**
+   * Overrides the autofill supported state for the current device and current user. See {@link
+   * AutofillManager#isAutofillSupported()}.
+   */
+  public void setAutofillSupported(boolean supported) {
+    this.autofillSupported = supported;
+  }
+
+  /**
+   * Overrides the autofill enabled state for the current user. See {@link
+   * AutofillManager#isEnabled()}.
+   */
+  public void setEnabled(boolean enabled) {
+    this.enabled = enabled;
   }
 }
