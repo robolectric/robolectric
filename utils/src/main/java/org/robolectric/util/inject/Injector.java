d1588068f0b0c91fae82215a7a863c1b434a503c
Zac Sweers <zsweers@slack-corp.com>
2020-02-11 18:56:28 -0500

    Switch to guava

--- utils/src/main/java/org/robolectric/util/inject/Injector.java
+++ utils/src/main/java/org/robolectric/util/inject/Injector.java
@@ -1,6 +1,6 @@
 package org.robolectric.util.inject;
 
-import android.support.annotation.VisibleForTesting;
+import com.google.common.annotations.VisibleForTesting;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.AnnotatedType;
 import java.lang.reflect.Array;
