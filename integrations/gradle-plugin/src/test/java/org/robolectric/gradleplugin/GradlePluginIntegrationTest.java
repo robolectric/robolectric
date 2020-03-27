a956ce483e8d4ba14ba9db7c810565bd7d8989cd
Christian Williams <christianw@bushelpowered.com>
2020-03-26 18:23:28 -0700

    Oops, duh, pass along env with ANDROID_HOME.

--- integrations/gradle-plugin/src/test/java/org/robolectric/gradleplugin/GradlePluginIntegrationTest.java
+++ integrations/gradle-plugin/src/test/java/org/robolectric/gradleplugin/GradlePluginIntegrationTest.java
@@ -1,33 +1,32 @@
 package org.robolectric.gradleplugin;
 
-import static com.google.common.truth.Truth.assertThat;
-import static org.robolectric.gradleplugin.GradlePluginTest.mapOf;
-
-import java.io.File;
-import java.util.Map;
 import org.gradle.testkit.runner.BuildResult;
 import org.gradle.testkit.runner.GradleRunner;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
+import java.util.HashMap;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.robolectric.gradleplugin.GradlePluginTest.mapOf;
+
 public class GradlePluginIntegrationTest {
 
   private File gradleProjectDir;
-  private String androidSdkRoot;
 
   @Before
   public void setUp() throws Exception {
     gradleProjectDir = new File(getClass().getResource("/testProject").getFile());
-    androidSdkRoot = Helpers.findAndroidSdkRoot();
   }
 
   @Test
   public void loadingProject() {
-    Map<String, String> env = mapOf(
-        "gradle-robolectric-plugin.classpath", System.getenv("gradle-robolectric-plugin.classpath"),
-        "gradle-robolectric-plugin.sdkPath", System.getenv("gradle-robolectric-plugin.sdkPath"),
-        "ANDROID_SDK_ROOT", androidSdkRoot
-    );
+    HashMap<String, String> env = new HashMap<>(System.getenv());
+    env.putAll(mapOf(
+            "gradle-robolectric-plugin.classpath", System.getenv("gradle-robolectric-plugin.classpath"),
+            "gradle-robolectric-plugin.sdkPath", System.getenv("gradle-robolectric-plugin.sdkPath")
+    ));
     System.out.println("env = " + env);
     GradleRunner gradleRunner = GradleRunner.create()
         .withProjectDir(gradleProjectDir)
