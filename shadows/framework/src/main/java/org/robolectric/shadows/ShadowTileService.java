6717479947e6efb1479e352d9cfba0f53449f350
brettchabot <brettchabot@google.com>
2020-05-26 10:30:38 -0700

    Update to androidx.test 1.3.0-beta02
    PiperOrigin-RevId: 313220090

--- shadows/framework/src/main/java/org/robolectric/shadows/ShadowTileService.java
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowTileService.java
@@ -1,16 +1,19 @@
 package org.robolectric.shadows;
 
+import android.content.Intent;
 import android.os.Build;
 import android.service.quicksettings.Tile;
 import android.service.quicksettings.TileService;
 import org.robolectric.annotation.Implementation;
 import org.robolectric.annotation.Implements;
+import org.robolectric.annotation.RealObject;
 import org.robolectric.shadow.api.Shadow;
 
 @Implements(value = TileService.class, minSdk = Build.VERSION_CODES.N)
 public final class ShadowTileService {
 
   private Tile tile;
+  @RealObject private TileService realObject;
 
   @Implementation
   protected final Tile getQsTile() {
@@ -18,6 +21,12 @@ public final class ShadowTileService {
       tile = createTile();
     }
     return tile;
+  }
+
+  /** Starts an activity without collapsing the quick settings panel. */
+  @Implementation
+  protected void startActivityAndCollapse(Intent intent) {
+    realObject.startActivity(intent);
   }
 
   private static Tile createTile() {
