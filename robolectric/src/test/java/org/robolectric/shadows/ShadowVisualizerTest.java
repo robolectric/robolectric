43cbe4041314380bf5b7d4653b75dc056e4f807f
Googler <noreply@google.com>
2020-08-04 08:24:50 -0700

    Extend ShadowVisualizer to support a data capture listener.
    This CL adds support for
    * Visualizer.setDataCaptureListener()
    * Visualizer.setCaptureSize() (native_setCaptureSize)
    * Visualizer.getCaptureSize() (native_getCaptureSize)
    and adds a Shadow-only method
    * ShadowVisualizer.triggerDataCapture()
    to trigger the listener added in setDataCaptureListener().
    
    PiperOrigin-RevId: 324815665

--- robolectric/src/test/java/org/robolectric/shadows/ShadowVisualizerTest.java
+++ robolectric/src/test/java/org/robolectric/shadows/ShadowVisualizerTest.java
@@ -5,8 +5,10 @@ import static com.google.common.truth.Truth.assertThat;
 import static org.robolectric.Shadows.shadowOf;
 
 import android.media.audiofx.Visualizer;
+import android.media.audiofx.Visualizer.OnDataCaptureListener;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicBoolean;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -43,21 +45,27 @@ public class ShadowVisualizerTest {
   }
 
   @Test
+  public void getWaveform_returnsWaveformFromSource() {
+    byte[] waveformInput = new byte[10];
+    Arrays.fill(waveformInput, (byte) 5);
+
+    // Default behaviour
+    assertThat(visualizer.getWaveForm(waveformInput)).isEqualTo(Visualizer.SUCCESS);
+
+    shadowOf(visualizer).setSource(createVisualizerSourceReturningValue(42));
+
+    assertThat(visualizer.getWaveForm(waveformInput)).isEqualTo(42);
+  }
+
+  @Test
   public void getFft_returnsFftFromSource() {
     byte[] fftInput = new byte[10];
     Arrays.fill(fftInput, (byte) 5);
 
-    // Default behaviour, length of the input array
-    assertThat(visualizer.getFft(fftInput)).isEqualTo(10);
+    // Default behaviour
+    assertThat(visualizer.getFft(fftInput)).isEqualTo(Visualizer.SUCCESS);
 
-    shadowOf(visualizer)
-        .setSource(
-            new VisualizerSource() {
-              @Override
-              public int getFft(byte[] fft) {
-                return 42;
-              }
-            });
+    shadowOf(visualizer).setSource(createVisualizerSourceReturningValue(42));
 
     assertThat(visualizer.getFft(fftInput)).isEqualTo(42);
   }
@@ -73,5 +81,91 @@ public class ShadowVisualizerTest {
 
     assertThat(status).isEqualTo(Visualizer.SUCCESS);
     assertThat(visualizer.getEnabled()).isTrue();
+  }
+
+  @Test
+  public void setCaptureSize_changedCaptureSize() {
+    // The capture size can only be set while the Visualizer is disabled.
+    visualizer.setEnabled(false);
+
+    int status = visualizer.setCaptureSize(2000);
+
+    assertThat(status).isEqualTo(Visualizer.SUCCESS);
+    assertThat(visualizer.getCaptureSize()).isEqualTo(2000);
+  }
+
+  @Test
+  public void triggerDataCapture_waveformAndFftTriggered() {
+    AtomicBoolean waveformCalled = new AtomicBoolean(false);
+    AtomicBoolean fftCalled = new AtomicBoolean(false);
+    visualizer.setDataCaptureListener(
+        createSimpleDataListener(waveformCalled, fftCalled),
+        /* rate= */ 1,
+        /* waveform= */ true,
+        /* fft= */ true);
+
+    shadowOf(visualizer).triggerDataCapture();
+
+    assertThat(waveformCalled.get()).isTrue();
+    assertThat(fftCalled.get()).isTrue();
+  }
+
+  @Test
+  public void triggerDataCapture_waveformDisabled_waveFormNotTriggered() {
+    AtomicBoolean waveformCalled = new AtomicBoolean(false);
+    AtomicBoolean fftCalled = new AtomicBoolean(false);
+    visualizer.setDataCaptureListener(
+        createSimpleDataListener(waveformCalled, fftCalled),
+        /* rate= */ 1,
+        /* waveform= */ false,
+        /* fft= */ true);
+
+    shadowOf(visualizer).triggerDataCapture();
+
+    assertThat(waveformCalled.get()).isFalse();
+  }
+
+  @Test
+  public void triggerDataCapture_fftDisabled_fftNotTriggered() {
+    AtomicBoolean waveformCalled = new AtomicBoolean(false);
+    AtomicBoolean fftCalled = new AtomicBoolean(false);
+    visualizer.setDataCaptureListener(
+        createSimpleDataListener(waveformCalled, fftCalled),
+        /* rate= */ 1,
+        /* waveform= */ true,
+        /* fft= */ false);
+
+    shadowOf(visualizer).triggerDataCapture();
+
+    assertThat(fftCalled.get()).isFalse();
+  }
+
+  private static VisualizerSource createVisualizerSourceReturningValue(int returnValue) {
+    return new VisualizerSource() {
+      @Override
+      public int getWaveForm(byte[] waveform) {
+        return returnValue;
+      }
+
+      @Override
+      public int getFft(byte[] fft) {
+        return returnValue;
+      }
+    };
+  }
+
+  private static OnDataCaptureListener createSimpleDataListener(
+      AtomicBoolean waveformCalled, AtomicBoolean fftCalled) {
+    return new OnDataCaptureListener() {
+      @Override
+      public void onWaveFormDataCapture(Visualizer visualizer, byte[] waveform, int samplingRate) {
+        waveformCalled.set(true);
+      }
+
+      @Override
+      public void onFftDataCapture(Visualizer visualizer, byte[] fft, int samplingRate) {
+        fftCalled.set(true);
+      }
+    };
   }
 }
