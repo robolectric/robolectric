109402fe4098b037f9f94e182dedd2ab37c2ace0
Michael Hoisie <hoisie@google.com>
2020-08-12 12:59:43 -0700

    Add Conscrypt integration test
    Also, Conscrypt can only be instantiated once in a Robolectric
    environment, otherwise UnsatisfiedLinkErrors may result (similar to
    SQLite).

--- integration_tests/security-providers/src/test/java/org/robolectric/integrationtests/securityproviders/SecurityProvidersTest.java
+++ integration_tests/security-providers/src/test/java/org/robolectric/integrationtests/securityproviders/SecurityProvidersTest.java
@@ -1,6 +1,11 @@
 package org.robolectric.integrationtests.securityproviders;
 
 import java.net.URL;
+import java.security.Provider;
+import java.security.Security;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import org.conscrypt.OpenSSLProvider;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
@@ -10,9 +15,24 @@ import org.robolectric.annotation.Config;
 @RunWith(RobolectricTestRunner.class)
 @Config(sdk = Config.ALL_SDKS)
 public class SecurityProvidersTest {
+  // Conscrypt (which calls into native libraries) can only be loaded once as per JNI spec:
+  // https://docs.oracle.com/javase/1.5.0/docs/guide/jni/spec/invocation.html#library_version
+  // If we try to load it into multiple class loaders, UnsatisfiedLinkErrors will be thrown.
+  private static final Provider CONSCRYPT_PROVIDER = new OpenSSLProvider();
+
   @Test
   public void JSSE_provider_isFunctioning() throws Exception {
     URL url = new URL("https://www.google.com");
     url.openConnection().getInputStream();
+  }
+
+  @Test
+  public void conscryptProvider_isSupported() throws Exception {
+    if (!"Conscrypt".equals(Security.getProviders()[0].getName())) {
+      Security.insertProviderAt(CONSCRYPT_PROVIDER, 1);
+    }
+    OkHttpClient client = new OkHttpClient.Builder().build();
+    Request request = new Request.Builder().url("https://www.google.com").build();
+    client.newCall(request).execute();
   }
 }
