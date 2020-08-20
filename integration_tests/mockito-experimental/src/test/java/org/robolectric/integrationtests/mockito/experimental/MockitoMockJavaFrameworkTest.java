c674b9d9c93225c8acd2fa1e8b8a333d0047832a
Michael Hoisie <hoisie@google.com>
2020-08-20 10:32:28 -0700

    Switch to using mockito-inline in mockito-experimental project
    Also, add an additional Java framework mock test that mocks the final method
    Cipher.getIV.

--- integration_tests/mockito-experimental/src/test/java/org/robolectric/integrationtests/mockito/experimental/MockitoMockJavaFrameworkTest.java
+++ integration_tests/mockito-experimental/src/test/java/org/robolectric/integrationtests/mockito/experimental/MockitoMockJavaFrameworkTest.java
@@ -5,7 +5,7 @@ import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 
 import java.io.File;
-import org.junit.Ignore;
+import javax.crypto.Cipher;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
@@ -13,16 +13,17 @@ import org.robolectric.RobolectricTestRunner;
 /** Tests the ability to mock Java framework methods with mockito-inline. */
 @RunWith(RobolectricTestRunner.class)
 public class MockitoMockJavaFrameworkTest {
-  /**
-   * Mocking Java classes with mockito-inline is currently broken.
-   *
-   * @see <a href="https://github.com/robolectric/robolectric/issues/5522">Issue 5522</a>
-   */
   @Test
-  @Ignore("Enable when Mockito 3.5.4 is released")
   public void file_getAbsolutePath_isMockable() throws Exception {
     File file = mock(File.class);
     doReturn("absolute/path").when(file).getAbsolutePath();
     assertThat(file.getAbsolutePath()).isEqualTo("absolute/path");
+  }
+
+  @Test
+  public void cipher_getIV_isMockable() {
+    Cipher cipher = mock(Cipher.class);
+    doReturn("fake".getBytes()).when(cipher).getIV();
+    assertThat(cipher.getIV()).isEqualTo("fake".getBytes());
   }
 }
