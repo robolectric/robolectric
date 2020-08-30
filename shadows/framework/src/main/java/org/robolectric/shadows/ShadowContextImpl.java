eb26f9a7d40571ddeea8f950a836d04b16d52533
hoisie <hoisie@google.com>
2020-08-29 22:43:26 -0700

    Support colons in shared preference names on Windows
    Update Context.getSharedPreferences to handle colons in the preferences names
    on Windows. Otherwise, an IOException is thrown when trying to get the shared
    preferences files. Some popular libraries such as Firebase use colons in their
    shared preference names.
    
    Fixes https://github.com/robolectric/robolectric/issues/5186
    
    PiperOrigin-RevId: 329151423

--- shadows/framework/src/main/java/org/robolectric/shadows/ShadowContextImpl.java
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowContextImpl.java
@@ -22,6 +22,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.IntentSender;
 import android.content.ServiceConnection;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Build.VERSION_CODES;
 import android.os.Bundle;
@@ -29,6 +30,7 @@ import android.os.Environment;
 import android.os.FileUtils;
 import android.os.Handler;
 import android.os.UserHandle;
+import com.google.common.base.Strings;
 import java.io.File;
 import java.nio.file.Paths;
 import java.util.HashMap;
@@ -436,6 +438,21 @@ public class ShadowContextImpl {
       return directlyOn(realContextImpl, ShadowContextImpl.CLASS_NAME, "getDatabasePath",
           ClassParameter.from(String.class, name));
     }
+  }
+
+  @Implementation
+  protected SharedPreferences getSharedPreferences(String name, int mode) {
+    // Windows does not allow colons in file names, which may be used in shared preference
+    // names. URL-encode any colons in Windows.
+    if (!Strings.isNullOrEmpty(name) && File.separatorChar == '\\') {
+      name = name.replace(":", "%3A");
+    }
+    return directlyOn(
+        realContextImpl,
+        ShadowContextImpl.CLASS_NAME,
+        "getSharedPreferences",
+        ClassParameter.from(String.class, name),
+        ClassParameter.from(int.class, mode));
   }
 
   /** Accessor interface for {@link android.app.ContextImpl}'s internals. */
