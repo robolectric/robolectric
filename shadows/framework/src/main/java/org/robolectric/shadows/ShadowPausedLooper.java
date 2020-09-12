b59c62b62c1bea63f9f251f200b3d2038266b6c9
brettchabot <brettchabot@google.com>
2020-09-12 15:11:33 -0700

    Cache result of ShadowLooper.looperMode().
    This method can be called frequently for example during animations.
    
    PiperOrigin-RevId: 331351123

--- shadows/framework/src/main/java/org/robolectric/shadows/ShadowPausedLooper.java
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowPausedLooper.java
@@ -26,6 +26,7 @@ import org.robolectric.annotation.Implements;
 import org.robolectric.annotation.LooperMode;
 import org.robolectric.annotation.RealObject;
 import org.robolectric.annotation.Resetter;
+import org.robolectric.config.ConfigurationRegistry;
 import org.robolectric.shadow.api.Shadow;
 import org.robolectric.util.Scheduler;
 
@@ -210,7 +211,8 @@ public final class ShadowPausedLooper extends ShadowLooper {
 
   @Resetter
   public static synchronized void resetLoopers() {
-    if (looperMode() != LooperMode.Mode.PAUSED) {
+    // do not use looperMode() here, because its cached value might already have been reset
+    if (ConfigurationRegistry.get(LooperMode.Mode.class) != LooperMode.Mode.PAUSED) {
       // ignore if not realistic looper
       return;
     }
