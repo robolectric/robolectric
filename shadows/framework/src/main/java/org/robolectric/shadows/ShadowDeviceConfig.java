5d089398ee621b0adf26942155c46f1c26b837bd
Thomas Keller <thomas.keller-extern@deutschebahn.com>
2020-10-29 18:19:41 +0100

    Fix a memory leak in DeviceConfig (#5530)

--- shadows/framework/src/main/java/org/robolectric/shadows/ShadowDeviceConfig.java
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowDeviceConfig.java
@@ -4,6 +4,9 @@ import android.os.Build;
 import android.provider.DeviceConfig;
 import org.robolectric.annotation.Implementation;
 import org.robolectric.annotation.Implements;
+import org.robolectric.annotation.Resetter;
+import org.robolectric.util.ReflectionHelpers;
+import java.util.Map;
 
 @Implements(value = DeviceConfig.class, isInAndroidSdk = false, minSdk = Build.VERSION_CODES.Q)
 public class ShadowDeviceConfig {
@@ -12,5 +15,21 @@ public class ShadowDeviceConfig {
   protected static String getProperty(String namespace, String name) {
     // avoid call to Settings.Config
     return null;
+  }
+
+  @Resetter
+  public static void reset() {
+    Object lock = ReflectionHelpers.getStaticField(DeviceConfig.class, "sLock");
+    //noinspection SynchronizationOnLocalVariableOrMethodParameter
+    synchronized (lock) {
+      Map singleListeners = ReflectionHelpers.getStaticField(DeviceConfig.class, "sSingleListeners");
+      singleListeners.clear();
+
+      Map listeners = ReflectionHelpers.getStaticField(DeviceConfig.class, "sListeners");
+      listeners.clear();
+
+      Map namespaces = ReflectionHelpers.getStaticField(DeviceConfig.class, "sNamespaces");
+      namespaces.clear();
+    }
   }
 }
