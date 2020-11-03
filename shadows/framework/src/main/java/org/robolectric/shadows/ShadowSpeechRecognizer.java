c91933827c0776a7c9d56efc9e7cb6707cbf7e6b
Googler <noreply@google.com>
2020-11-03 10:58:58 -0800

    Make handleChangeListener() public
    PiperOrigin-RevId: 340480614

---
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowSpeechRecognizer.java
@@ -0,0 +1,34 @@
+package org.robolectric.shadows;
+
+import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.SpeechRecognizer;
+import org.robolectric.annotation.Implements;
+
+/** Robolectric shadow for SpeechRecognizer. */
+@Implements(SpeechRecognizer.class)
+public class ShadowSpeechRecognizer {
+
+  private RecognitionListener recognitionListener;
+
+  /** Handles setting the listener directly. */
+  public void setRecognitionListener(RecognitionListener listener) {
+    recognitionListener = listener;
+  }
+
+  public void triggerOnError(int error) {
+    recognitionListener.onError(error);
+  }
+
+  public void triggerOnPartialResults(Bundle bundle) {
+    recognitionListener.onPartialResults(bundle);
+  }
+
+  public void triggerOnResults(Bundle bundle) {
+    recognitionListener.onResults(bundle);
+  }
+
+  public void triggerOnRmsChanged(float rmsdB) {
+    recognitionListener.onRmsChanged(rmsdB);
+  }
+}
