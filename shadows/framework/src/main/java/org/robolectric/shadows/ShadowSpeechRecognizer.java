716650ef1d464164e8781dea38608d22f4b5e05e
brettchabot <brettchabot@google.com>
2020-11-06 17:12:56 -0800

    Second attempt of fix ShadowSpeechRecognizer#checkOpenConnection
    Previously checkOpenConnection would always log an error since
    mService was null.
    
    This CL modifies startListening to correctly set up the response to bindService, so a non-null binder object is passed through to
    SpeechRecognizer#onConnection callback
    
    PiperOrigin-RevId: 341145409

--- shadows/framework/src/main/java/org/robolectric/shadows/ShadowSpeechRecognizer.java
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowSpeechRecognizer.java
@@ -4,17 +4,19 @@ import static org.robolectric.shadow.api.Shadow.directlyOn;
 import static org.robolectric.util.reflector.Reflector.reflector;
 
 import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.Bundle;
-import android.os.IBinder;
+import android.provider.Settings;
 import android.speech.IRecognitionService;
 import android.speech.RecognitionListener;
+import android.speech.RecognitionService;
 import android.speech.SpeechRecognizer;
 import org.robolectric.annotation.Implementation;
 import org.robolectric.annotation.Implements;
 import org.robolectric.annotation.RealObject;
 import org.robolectric.util.ReflectionHelpers;
-import org.robolectric.util.ReflectionHelpers.ClassParameter;
 import org.robolectric.util.reflector.Accessor;
 import org.robolectric.util.reflector.ForType;
 
@@ -26,21 +28,29 @@ public class ShadowSpeechRecognizer {
   private RecognitionListener recognitionListener;
 
   @Implementation
-  protected void startListening(Intent intent) {
+  protected void startListening(Intent recognizerIntent) {
     // simulate the response to the real startListening's bindService call
     SpeechRecognizerReflector reflector = reflector(SpeechRecognizerReflector.class, realObject);
-    IBinder recognitionServiceBinder =
-        ReflectionHelpers.createNullProxy(IRecognitionService.class).asBinder();
+    Binder recognitionServiceBinder = new Binder();
+    recognitionServiceBinder.attachInterface(
+        ReflectionHelpers.createNullProxy(IRecognitionService.class),
+        IRecognitionService.class.getName());
+
+    Intent serviceIntent = new Intent(RecognitionService.SERVICE_INTERFACE);
+    ComponentName componentName = reflector.getServiceComponent();
+    if (componentName == null) {
+      componentName = new ComponentName("org.robolectric", "FakeSpeechRecognizerService");
+      ShadowSettings.ShadowSecure.putString(
+          reflector.getContext().getContentResolver(),
+          Settings.Secure.VOICE_RECOGNITION_SERVICE,
+          componentName.flattenToString());
+    }
+    serviceIntent.setComponent(componentName);
+
     ShadowContextWrapper.getShadowInstrumentation()
         .setComponentNameAndServiceForBindServiceForIntent(
-            intent, reflector.getServiceComponent(), recognitionServiceBinder);
-    directlyOn(realObject, SpeechRecognizer.class).startListening(intent);
-
-    ReflectionHelpers.callInstanceMethod(
-        reflector.getConnection(),
-        "onServiceConnected",
-        ClassParameter.from(ComponentName.class, null),
-        ClassParameter.from(IBinder.class, recognitionServiceBinder));
+            serviceIntent, reflector.getServiceComponent(), recognitionServiceBinder);
+    directlyOn(realObject, SpeechRecognizer.class).startListening(recognizerIntent);
   }
 
   /** Handles changing the listener and allows access to the internal listener to trigger events. */
@@ -73,5 +83,8 @@ public class ShadowSpeechRecognizer {
 
     @Accessor("mServiceComponent")
     ComponentName getServiceComponent();
+
+    @Accessor("mContext")
+    Context getContext();
   }
 }
