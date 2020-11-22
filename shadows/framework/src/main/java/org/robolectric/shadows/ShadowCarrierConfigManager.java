5bfec5bde066573ed53b8f203cbeb45441ed330a
hoisie <hoisie@google.com>
2020-11-21 23:06:24 -0800

    Tweak API for returning null from CarrierConfigManager.getConfigForSubId
    Add support for ShadowCarrierConfigManager.setConfigForSubId(subId, null)
    instead of a separate config provider interface.
    
    PiperOrigin-RevId: 343703069

--- shadows/framework/src/main/java/org/robolectric/shadows/ShadowCarrierConfigManager.java
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowCarrierConfigManager.java
@@ -4,15 +4,14 @@ import static android.os.Build.VERSION_CODES.M;
 
 import android.os.PersistableBundle;
 import android.telephony.CarrierConfigManager;
-import android.util.SparseArray;
-import androidx.annotation.Nullable;
+import java.util.HashMap;
 import org.robolectric.annotation.Implementation;
 import org.robolectric.annotation.Implements;
 
 @Implements(value = CarrierConfigManager.class, minSdk = M)
 public class ShadowCarrierConfigManager {
 
-  private final SparseArray<CarrierConfigProvider> bundles = new SparseArray<>();
+  private final HashMap<Integer, PersistableBundle> bundles = new HashMap<>();
 
   /**
    * Returns {@link android.os.PersistableBundle} previously set by {@link #setConfigForSubId(int)},
@@ -20,21 +19,10 @@ public class ShadowCarrierConfigManager {
    */
   @Implementation
   protected PersistableBundle getConfigForSubId(int subId) {
-    CarrierConfigProvider configProvider = bundles.get(subId);
-    if (configProvider == null) {
-      return new PersistableBundle();
+    if (bundles.containsKey(subId)) {
+      return bundles.get(subId);
     }
-    return configProvider.get();
-  }
-
-  /**
-   * Sets that the {@code config} CarrierConfigProvider for a particular {@code subId}; controls the
-   * return value of {@link CarrierConfigManager#getConfigForSubId()}.
-   *
-   * <p>CarrierConfigProvider provides an optional CarrierConfig.
-   */
-  public void setConfigProviderForSubId(int subId, CarrierConfigProvider configProvider) {
-    bundles.put(subId, configProvider);
+    return new PersistableBundle();
   }
 
   /**
@@ -42,20 +30,6 @@ public class ShadowCarrierConfigManager {
    * return value of {@link CarrierConfigManager#getConfigForSubId()}.
    */
   public void setConfigForSubId(int subId, PersistableBundle config) {
-    bundles.put(
-        subId,
-        new CarrierConfigProvider() {
-          @Override
-          @Nullable
-          public PersistableBundle get() {
-            return config;
-          }
-        });
-  }
-
-  /** Provides a {@code @Nullable PersistableBundle} which is a CarrierConfig. */
-  public interface CarrierConfigProvider {
-    @Nullable
-    PersistableBundle get();
+    bundles.put(subId, config);
   }
 }
