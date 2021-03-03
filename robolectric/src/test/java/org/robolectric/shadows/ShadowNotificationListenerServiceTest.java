d5109ebb66b67f853ec9db643f3cfefcd2818984
Googler <noreply@google.com>
2021-03-03 00:02:17 -0800

    Create a shadow implementation of NotificationListenerService.
    PiperOrigin-RevId: 360604596

---
+++ robolectric/src/test/java/org/robolectric/shadows/ShadowNotificationListenerServiceTest.java
@@ -0,0 +1,189 @@
+package org.robolectric.shadows;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.robolectric.Shadows.shadowOf;
+
+import android.app.Notification;
+import android.content.ComponentName;
+import android.content.Context;
+import android.os.Build;
+import android.os.Build.VERSION_CODES;
+import android.service.notification.NotificationListenerService;
+import android.service.notification.StatusBarNotification;
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import com.google.common.collect.ImmutableList;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.annotation.Config;
+
+/** Test for ShadowNotificationListenerService. */
+@Config(minSdk = Build.VERSION_CODES.LOLLIPOP)
+@RunWith(AndroidJUnit4.class)
+public final class ShadowNotificationListenerServiceTest {
+
+  /** NotificationListenerService needs to be extended to function. */
+  private static final class TestNotificationListenerService extends NotificationListenerService {
+    TestNotificationListenerService() {}
+  }
+
+  private static final String DEFAULT_PACKAGE = "com.google.android.apps.example";
+  private static final int DEFAULT_ID = 0;
+
+  private final Context context = ApplicationProvider.getApplicationContext();
+
+  private NotificationListenerService service;
+  private ShadowNotificationListenerService shadowService;
+
+  @Before
+  public void setUp() {
+    service = Robolectric.buildService(TestNotificationListenerService.class).get();
+    shadowService = shadowOf(service);
+    ShadowNotificationListenerService.reset();
+  }
+
+  @Test
+  public void getActiveNotification_noActiveNotification_returnsEmptyResult() {
+    StatusBarNotification[] activeNotifications = service.getActiveNotifications();
+
+    assertThat(activeNotifications).isEmpty();
+  }
+
+  @Test
+  public void getActiveNotification_oneActiveNotification_returnsOneNotification() {
+    Notification dummyNotification = createDummyNotification();
+    shadowService.addActiveNotification(DEFAULT_PACKAGE, DEFAULT_ID, dummyNotification);
+
+    StatusBarNotification[] activeNotifications = service.getActiveNotifications();
+
+    assertThat(activeNotifications).hasLength(1);
+    assertThat(activeNotifications[0].getPackageName()).isEqualTo(DEFAULT_PACKAGE);
+    assertThat(activeNotifications[0].getNotification()).isEqualTo(dummyNotification);
+  }
+
+  @Test
+  public void getActiveNotification_multipleActiveNotifications_returnsAllNotifications() {
+    ImmutableList<Notification> dummyNotifications =
+        ImmutableList.of(
+            createDummyNotification(), createDummyNotification(), createDummyNotification());
+    dummyNotifications.stream()
+        .forEach(
+            notification ->
+                shadowService.addActiveNotification(DEFAULT_PACKAGE, DEFAULT_ID, notification));
+
+    StatusBarNotification[] activeNotifications = service.getActiveNotifications();
+
+    assertThat(activeNotifications).hasLength(dummyNotifications.size());
+    for (int i = 0; i < dummyNotifications.size(); i++) {
+      assertThat(activeNotifications[i].getPackageName()).isEqualTo(DEFAULT_PACKAGE);
+      assertThat(activeNotifications[i].getNotification()).isEqualTo(dummyNotifications.get(i));
+    }
+  }
+
+  @Test
+  public void
+      getActiveNotifications_activeNotificationsFromMultiplePackages_returnsAllNotifications() {
+    Notification dummyNotification1 = createDummyNotification();
+    Notification dummyNotification2 = createDummyNotification();
+    String package1 = "com.google.android.app.first";
+    String package2 = "com.google.android.app.second";
+    shadowService.addActiveNotification(package1, DEFAULT_ID, dummyNotification1);
+    shadowService.addActiveNotification(package2, DEFAULT_ID, dummyNotification2);
+
+    StatusBarNotification[] activeNotifications = service.getActiveNotifications();
+
+    assertThat(activeNotifications).hasLength(2);
+    assertThat(activeNotifications[0].getPackageName()).isEqualTo(package1);
+    assertThat(activeNotifications[0].getNotification()).isEqualTo(dummyNotification1);
+    assertThat(activeNotifications[1].getPackageName()).isEqualTo(package2);
+    assertThat(activeNotifications[1].getNotification()).isEqualTo(dummyNotification2);
+  }
+
+  @Test
+  public void getActiveNotification_filterByKeys_returnsAllMatchedNotifications() {
+    ImmutableList<Notification> dummyNotifications =
+        ImmutableList.of(
+            createDummyNotification(),
+            createDummyNotification(),
+            createDummyNotification(),
+            createDummyNotification());
+    String[] keys = new String[dummyNotifications.size()];
+    for (int i = 0; i < dummyNotifications.size(); i++) {
+      keys[i] = shadowService.addActiveNotification(DEFAULT_PACKAGE, i, dummyNotifications.get(i));
+    }
+
+    StatusBarNotification[] activeNotifications =
+        service.getActiveNotifications(new String[] {keys[0], keys[2]});
+
+    assertThat(activeNotifications).hasLength(2);
+    assertThat(activeNotifications[0].getNotification()).isEqualTo(dummyNotifications.get(0));
+    assertThat(activeNotifications[1].getNotification()).isEqualTo(dummyNotifications.get(2));
+  }
+
+  @Test
+  public void cancelNotification_keyFound_removesActiveNotification() {
+    String key =
+        shadowService.addActiveNotification(DEFAULT_PACKAGE, DEFAULT_ID, createDummyNotification());
+
+    service.cancelNotification(key);
+
+    assertThat(service.getActiveNotifications()).isEmpty();
+  }
+
+  @Test
+  public void cancelNotification_keyNotFound_noOp() {
+    shadowService.addActiveNotification(DEFAULT_PACKAGE, DEFAULT_ID, createDummyNotification());
+
+    service.cancelNotification("made_up_key");
+
+    assertThat(service.getActiveNotifications()).hasLength(1);
+  }
+
+  @Test
+  public void cancelAllNotifications_removesAllActiveNotifications() {
+    ImmutableList<Notification> dummyNotifications =
+        ImmutableList.of(
+            createDummyNotification(), createDummyNotification(), createDummyNotification());
+    dummyNotifications.stream()
+        .forEach(
+            notification ->
+                shadowService.addActiveNotification(DEFAULT_PACKAGE, DEFAULT_ID, notification));
+
+    service.cancelAllNotifications();
+
+    assertThat(service.getActiveNotifications()).isEmpty();
+  }
+
+  @Test
+  public void requestInterruptionFilter_updatesInterruptionFilter() {
+    service.requestInterruptionFilter(NotificationListenerService.INTERRUPTION_FILTER_ALARMS);
+
+    assertThat(service.getCurrentInterruptionFilter())
+        .isEqualTo(NotificationListenerService.INTERRUPTION_FILTER_ALARMS);
+  }
+
+  @Test
+  public void requestListenerHints_updatesListenerHints() {
+    service.requestListenerHints(NotificationListenerService.HINT_HOST_DISABLE_CALL_EFFECTS);
+
+    assertThat(service.getCurrentListenerHints())
+        .isEqualTo(NotificationListenerService.HINT_HOST_DISABLE_CALL_EFFECTS);
+  }
+
+  @Test
+  @Config(minSdk = VERSION_CODES.N)
+  public void requestRebind_incrementsCounter() {
+    TestNotificationListenerService.requestRebind(
+        ComponentName.createRelative(DEFAULT_PACKAGE, "Test"));
+    TestNotificationListenerService.requestRebind(
+        ComponentName.createRelative(DEFAULT_PACKAGE, "Test"));
+
+    assertThat(ShadowNotificationListenerService.getRebindRequestCount()).isEqualTo(2);
+  }
+
+  private Notification createDummyNotification() {
+    return new Notification.Builder(context).build();
+  }
+}
---
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowNotificationListenerService.java
@@ -0,0 +1,146 @@
+package org.robolectric.shadows;
+
+import static java.util.stream.Collectors.toCollection;
+
+import android.app.Notification;
+import android.content.ComponentName;
+import android.os.Build.VERSION;
+import android.os.Build.VERSION_CODES;
+import android.os.UserHandle;
+import android.service.notification.NotificationListenerService;
+import android.service.notification.NotificationListenerService.Ranking;
+import android.service.notification.NotificationListenerService.RankingMap;
+import android.service.notification.StatusBarNotification;
+import com.google.common.collect.ImmutableSet;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import org.robolectric.annotation.Implementation;
+import org.robolectric.annotation.Implements;
+import org.robolectric.annotation.Resetter;
+import org.robolectric.util.ReflectionHelpers;
+import org.robolectric.util.ReflectionHelpers.ClassParameter;
+
+/** Shadow implementation of {@link NotificationListenerService}. */
+@Implements(value = NotificationListenerService.class, minSdk = VERSION_CODES.LOLLIPOP)
+public class ShadowNotificationListenerService extends ShadowService {
+  private static final AtomicInteger rebindRequestCount = new AtomicInteger(0);
+
+  private final List<StatusBarNotification> activeNotifications =
+      Collections.synchronizedList(new ArrayList<>());
+  private final AtomicInteger interruptionFilter =
+      new AtomicInteger(NotificationListenerService.INTERRUPTION_FILTER_UNKNOWN);
+  private final AtomicInteger hint = new AtomicInteger(0);
+  private final RankingMap emptyRankingMap = createEmptyRankingMap();
+
+  /**
+   * Adds the given Notification to the list of active Notification.
+   *
+   * @return the key of the generated {@link StatusBarNotification}
+   */
+  public String addActiveNotification(String packageName, int id, Notification notification) {
+    StatusBarNotification statusBarNotification =
+        new StatusBarNotification(
+            /* pkg= */ packageName,
+            /* opPkg= */ packageName,
+            id,
+            /* tag= */ null,
+            /* uid= */ 0,
+            /* initialPid= */ 0,
+            /* score= */ 0,
+            notification,
+            UserHandle.CURRENT,
+            notification.when);
+    activeNotifications.add(statusBarNotification);
+    return statusBarNotification.getKey();
+  }
+
+  @Implementation(minSdk = VERSION_CODES.N)
+  protected static void requestRebind(ComponentName componentName) {
+    rebindRequestCount.incrementAndGet();
+  }
+
+  @Implementation
+  protected final void cancelAllNotifications() {
+    activeNotifications.clear();
+  }
+
+  @Implementation
+  protected final void cancelNotification(String key) {
+    synchronized (activeNotifications) {
+      Iterator<StatusBarNotification> iterator = activeNotifications.iterator();
+      while (iterator.hasNext()) {
+        StatusBarNotification statusBarNotification = iterator.next();
+        if (statusBarNotification.getKey().equals(key)) {
+          iterator.remove();
+          break;
+        }
+      }
+    }
+  }
+
+  /**
+   * Returns zero or more notifications, added by {@link #addActiveNotification}, that match one of
+   * the provided keys.
+   *
+   * @param keys the keys to match
+   * @param trim ignored, trimming is not supported
+   */
+  @Implementation
+  protected StatusBarNotification[] getActiveNotifications(String[] keys, int trim) {
+    if (keys == null) {
+      return activeNotifications.toArray(new StatusBarNotification[0]);
+    }
+
+    ImmutableSet<String> keySet = ImmutableSet.copyOf(keys);
+    return activeNotifications.stream()
+        .filter(notification -> keySet.contains(notification.getKey()))
+        .collect(toCollection(ArrayList::new))
+        .toArray(new StatusBarNotification[0]);
+  }
+
+  @Implementation
+  protected void requestInterruptionFilter(int interruptionFilter) {
+    this.interruptionFilter.set(interruptionFilter);
+  }
+
+  @Implementation
+  protected int getCurrentInterruptionFilter() {
+    return interruptionFilter.get();
+  }
+
+  @Implementation
+  protected void requestListenerHints(int hint) {
+    this.hint.set(hint);
+  }
+
+  @Implementation
+  protected int getCurrentListenerHints() {
+    return hint.get();
+  }
+
+  @Implementation
+  protected RankingMap getCurrentRanking() {
+    return emptyRankingMap;
+  }
+
+  /** Returns the number of time rebind was requested. */
+  public static int getRebindRequestCount() {
+    return rebindRequestCount.get();
+  }
+
+  /** Resets this shadow instance. */
+  @Resetter
+  public static void reset() {
+    rebindRequestCount.set(0);
+  }
+
+  private static RankingMap createEmptyRankingMap() {
+    return VERSION.SDK_INT < VERSION_CODES.Q
+        ? ReflectionHelpers.callConstructor(RankingMap.class)
+        : ReflectionHelpers.callConstructor(
+            RankingMap.class, ClassParameter.from(Ranking[].class, new Ranking[] {}));
+  }
+}
