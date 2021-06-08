c583fe86d5aeda7107f97a6e1fc486536c13f029
Googler <noreply@google.com>
2021-06-08 02:34:21 -0700

    Extend ShadowBluetoothLeScanner capabilities.
    - Support the PendingIntent APIs
    - Allow querying of scanning parameters.
    
    PiperOrigin-RevId: 378108607

--- shadows/framework/src/main/java/org/robolectric/shadows/ShadowBluetoothLeScanner.java
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowBluetoothLeScanner.java
@@ -1,14 +1,21 @@
 package org.robolectric.shadows;
 
 import static android.os.Build.VERSION_CODES.LOLLIPOP;
+import static android.os.Build.VERSION_CODES.O;
+import static java.util.Collections.unmodifiableList;
+import static java.util.Objects.requireNonNull;
+import static java.util.Optional.ofNullable;
 
+import android.app.PendingIntent;
 import android.bluetooth.le.BluetoothLeScanner;
 import android.bluetooth.le.ScanCallback;
 import android.bluetooth.le.ScanFilter;
 import android.bluetooth.le.ScanSettings;
 import java.util.Collections;
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 import org.robolectric.annotation.Implementation;
 import org.robolectric.annotation.Implements;
@@ -17,7 +24,20 @@ import org.robolectric.annotation.Implements;
 @Implements(value = BluetoothLeScanner.class, minSdk = LOLLIPOP)
 public class ShadowBluetoothLeScanner {
 
-  private final Set<ScanCallback> scanCallbacks = new HashSet<ScanCallback>();
+  private static class ScanParams {
+    private final List<ScanFilter> filters;
+    private final ScanSettings settings;
+
+    private ScanParams(List<ScanFilter> filters, ScanSettings settings) {
+      this.filters = filters;
+      this.settings = settings;
+    }
+  }
+
+  private static final ScanParams EMPTY = new ScanParams(null, null);
+
+  private final Map<ScanCallback, ScanParams> scanCallbacks = new HashMap<>();
+  private final Map<PendingIntent, ScanParams> pendingIntents = new HashMap<>();
 
   /**
    * Tracks ongoing scans. Use {@link #getScanCallbacks} to get a list of any currently registered
@@ -25,7 +45,18 @@ public class ShadowBluetoothLeScanner {
    */
   @Implementation
   protected void startScan(List<ScanFilter> filters, ScanSettings settings, ScanCallback callback) {
-    scanCallbacks.add(callback);
+    scanCallbacks.put(callback, new ScanParams(unmodifiableList(filters), settings));
+  }
+
+  /**
+   * Tracks ongoing scans. Use {@link #getScanCallbacks} to get a list of any currently registered
+   * {@link ScanCallback}s.
+   */
+  @Implementation(minSdk = O)
+  protected int startScan(
+      List<ScanFilter> filters, ScanSettings settings, PendingIntent pendingIntent) {
+    pendingIntents.put(pendingIntent, new ScanParams(unmodifiableList(filters), settings));
+    return 0;
   }
 
   @Implementation
@@ -33,8 +64,38 @@ public class ShadowBluetoothLeScanner {
     scanCallbacks.remove(callback);
   }
 
+  @Implementation(minSdk = O)
+  protected void stopScan(PendingIntent pendingIntent) {
+    pendingIntents.remove(pendingIntent);
+  }
+
   /** Returns all currently active {@link ScanCallback}s. */
   public Set<ScanCallback> getScanCallbacks() {
-    return Collections.unmodifiableSet(scanCallbacks);
+    return Collections.unmodifiableSet(scanCallbacks.keySet());
+  }
+
+  /** Returns all currently active {@link PendingIntent}s. */
+  public Set<PendingIntent> getPendingIntents() {
+    return Collections.unmodifiableSet(pendingIntents.keySet());
+  }
+
+  /** Returns filters associated with an active {@link ScanCallback} */
+  public Optional<List<ScanFilter>> getScanFilters(ScanCallback callback) {
+    return ofNullable(requireNonNull(scanCallbacks.getOrDefault(callback, EMPTY)).filters);
+  }
+
+  /** Returns filters associated with an active {@link PendingIntent} */
+  public Optional<List<ScanFilter>> getScanFilters(PendingIntent pendingIntent) {
+    return ofNullable(requireNonNull(pendingIntents.getOrDefault(pendingIntent, EMPTY)).filters);
+  }
+
+  /** Returns filters associated with an active {@link ScanCallback} */
+  public Optional<ScanSettings> getScanSettings(ScanCallback callback) {
+    return ofNullable(requireNonNull(scanCallbacks.getOrDefault(callback, EMPTY)).settings);
+  }
+
+  /** Returns filters associated with an active {@link PendingIntent} */
+  public Optional<ScanSettings> getScanSettings(PendingIntent pendingIntent) {
+    return ofNullable(requireNonNull(pendingIntents.getOrDefault(pendingIntent, EMPTY)).settings);
   }
 }
