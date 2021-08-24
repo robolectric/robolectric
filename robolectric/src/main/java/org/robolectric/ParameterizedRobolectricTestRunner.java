b630549cf5886d93e92d0da7eb65cccf6c8939cc
Googler <noreply@google.com>
2021-08-24 09:13:33 -0700

    Add parameterized test name method to the test runner
    Previously this was just using the default implementation of the test name method which returns the method name, this means each version of the parameterized test has the same test name. This eventually gets populated into the description passed into e.g. the rules.
    
    This change makes this runner consistent with junit Parameterized runner.
    
    PiperOrigin-RevId: 392678724

--- robolectric/src/main/java/org/robolectric/ParameterizedRobolectricTestRunner.java
+++ robolectric/src/main/java/org/robolectric/ParameterizedRobolectricTestRunner.java
@@ -253,6 +253,11 @@ public final class ParameterizedRobolectricTestRunner extends Suite {
           }
 
           @Override
+          protected String testName(FrameworkMethod method) {
+            return TestClassRunnerForParameters.this.testName(method);
+          }
+
+          @Override
           public String toString() {
             return "HelperTestRunner for " + TestClassRunnerForParameters.this.toString();
           }
