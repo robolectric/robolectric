80aacbb9d00f76408a09c96ac28ac491598b3a1f
hoisie <hoisie@google.com>
2021-10-13 14:41:10 -0700

    Use more conventional name for the native runtime library
    Rename the Linux x86_64 library from robolectric-nativeruntime.so to
    librobolectric-nativeruntime.so, which is consistent with
    System.mapLibrary.
    
    Other platforms will also use a library name that is consistent
    with System.mapLibrary, such as Mac using librobolectric-nativeruntime.dylib.
    
    PiperOrigin-RevId: 402925730

--- nativeruntime/src/main/java/org/robolectric/nativeruntime/NativeRuntimeLoader.java
+++ nativeruntime/src/main/java/org/robolectric/nativeruntime/NativeRuntimeLoader.java
@@ -1,18 +1,30 @@
 package org.robolectric.nativeruntime;
 
+import static com.google.common.base.StandardSystemProperty.OS_ARCH;
+import static com.google.common.base.StandardSystemProperty.OS_NAME;
+
 import com.google.common.io.Files;
 import com.google.common.io.Resources;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Locale;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-/** Loads the Roboelctric native runtime. */
+/** Loads the Robolectric native runtime. */
 public final class NativeRuntimeLoader {
   private static final AtomicBoolean loaded = new AtomicBoolean(false);
 
   static {
-    ensureLoaded();
+    if (isSupported()) {
+      ensureLoaded();
+    } else {
+      String errorMessage =
+          String.format(
+              "The Robolectric native runtime is not supported on OS/Architecture %s/%s",
+              OS_NAME.value(), OS_ARCH.value());
+      throw new AssertionError(errorMessage);
+    }
   }
 
   private NativeRuntimeLoader() {}
@@ -20,15 +32,45 @@ public final class NativeRuntimeLoader {
   static void ensureLoaded() {
     if (loaded.compareAndSet(false, true)) {
       try {
-        File tmpLibraryFile =
-            java.nio.file.Files.createTempFile("", "robolectric-nativeruntime.so").toFile();
+        String libraryName = System.mapLibraryName("robolectric-nativeruntime");
+        File tmpLibraryFile = java.nio.file.Files.createTempFile("", libraryName).toFile();
         tmpLibraryFile.deleteOnExit();
-        URL resource = Resources.getResource("native/linux/64/robolectric-nativeruntime.so");
+        URL resource = Resources.getResource(nativeLibraryPath());
         Resources.asByteSource(resource).copyTo(Files.asByteSink(tmpLibraryFile));
         System.load(tmpLibraryFile.getAbsolutePath());
       } catch (IOException e) {
-        throw new AssertionError("Unable to load Robolectric nativeruntime library", e);
+        throw new AssertionError("Unable to load Robolectric native runtime library", e);
       }
     }
+  }
+
+  private static boolean isSupported() {
+    return ("mac".equals(osName()) && "aarch64".equals(arch()))
+        || ("linux".equals(osName()) && "x86_64".equals(arch()));
+  }
+
+  private static String nativeLibraryPath() {
+    return String.format(
+        "native/%s/%s/%s", osName(), arch(), System.mapLibraryName("robolectric-nativeruntime"));
+  }
+
+  private static String osName() {
+    String osName = OS_NAME.value().toLowerCase(Locale.US);
+    if (osName.contains("linux")) {
+      return "linux";
+    } else if (osName.contains("mac")) {
+      return "mac";
+    } else if (osName.contains("win")) {
+      return "windows";
+    }
+    return "unknown";
+  }
+
+  private static String arch() {
+    String arch = OS_ARCH.value().toLowerCase(Locale.US);
+    if (arch.equals("x86_64") || arch.equals("amd64")) {
+      return "x86_64";
+    }
+    return arch;
   }
 }
