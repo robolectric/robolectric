e3992d0ed03c8fc40f18d36a561304117514e5c4
brettchabot <brettchabot@google.com>
2021-10-13 17:25:40 -0700

    Make IntentsTest work on device without browser.
    PiperOrigin-RevId: 402960278

---
+++ integration_tests/androidx_test/src/main/java/org/robolectric/integrationtests/axt/StubBrowserActivity.java
@@ -0,0 +1,9 @@
+package org.robolectric.integrationtests.axt;
+
+import android.app.Activity;
+
+/**
+ * A Activity that receives browser intents. Used for
+ * IntentsTest#testIntendedSuccess_truthChainedCorrespondence
+ */
+public class StubBrowserActivity extends Activity {}
