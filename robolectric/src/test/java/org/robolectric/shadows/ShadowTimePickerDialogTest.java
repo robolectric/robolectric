53b05c3f0484fc7f8b1162c474075282e1debda4
Googler <noreply@google.com>
2021-10-28 23:52:55 -0700

    Add ShadowTimePickerDialog.getIs24HourView
    This returns the boolean 'is24HourView' passed into the constructor.
    
    Also, migrate the shadow methods to use a reflector to get the
    underlying TimePickerDialog data.
    
    PiperOrigin-RevId: 406297901

--- robolectric/src/test/java/org/robolectric/shadows/ShadowTimePickerDialogTest.java
+++ robolectric/src/test/java/org/robolectric/shadows/ShadowTimePickerDialogTest.java
@@ -4,6 +4,7 @@ import static com.google.common.truth.Truth.assertThat;
 import static org.robolectric.Shadows.shadowOf;
 
 import android.app.TimePickerDialog;
+import android.os.Bundle;
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import org.junit.Test;
@@ -13,11 +14,23 @@ import org.junit.runner.RunWith;
 public class ShadowTimePickerDialogTest {
 
   @Test
-  public void returnsTheIntialHourAndMinutePassedIntoTheTimePickerDialog() {
+  public void testGettersReturnInitialConstructorValues() {
     TimePickerDialog timePickerDialog =
-        new TimePickerDialog(ApplicationProvider.getApplicationContext(), 0, null, 6, 55, false);
+        new TimePickerDialog(ApplicationProvider.getApplicationContext(), 0, null, 6, 55, true);
     ShadowTimePickerDialog shadow = shadowOf(timePickerDialog);
     assertThat(shadow.getHourOfDay()).isEqualTo(6);
     assertThat(shadow.getMinute()).isEqualTo(55);
+    assertThat(shadow.getIs24HourView()).isEqualTo(true);
+  }
+
+  @Test
+  public void updateTime_shouldUpdateHourAndMinute() {
+    TimePickerDialog timePickerDialog =
+        new TimePickerDialog(ApplicationProvider.getApplicationContext(), 0, null, 6, 55, true);
+    timePickerDialog.updateTime(1, 2);
+
+    Bundle bundle = timePickerDialog.onSaveInstanceState();
+    assertThat(bundle.getInt("hour")).isEqualTo(1);
+    assertThat(bundle.getInt("minute")).isEqualTo(2);
   }
 }
