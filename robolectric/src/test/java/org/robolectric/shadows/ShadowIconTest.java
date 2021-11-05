6beade5567e86265394364e7635ba4717b8ed21a
Googler <noreply@google.com>
2021-11-05 07:51:23 -0700

    Support overriding Icon loading executor from Icon#loadDrawableAsync.
    Icon#loadDrawableAsync is loading icon on AsyncTask#THREAD_POOL_EXECUTOR, that test has no way to control the execution. This patch adds a dedicated executor overriding as ShadowIcon#overrideExecutor to let test control where the async icon loading will happen.
    
    PiperOrigin-RevId: 407821420

--- robolectric/src/test/java/org/robolectric/shadows/ShadowIconTest.java
+++ robolectric/src/test/java/org/robolectric/shadows/ShadowIconTest.java
@@ -2,11 +2,18 @@ package org.robolectric.shadows;
 
 import static android.os.Build.VERSION_CODES.M;
 import static com.google.common.truth.Truth.assertThat;
+import static com.google.common.util.concurrent.MoreExecutors.directExecutor;
 import static org.robolectric.Shadows.shadowOf;
 
+import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.graphics.drawable.Icon;
 import android.net.Uri;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import androidx.annotation.Nullable;
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import org.junit.Test;
@@ -16,16 +23,30 @@ import org.robolectric.annotation.Config;
 @RunWith(AndroidJUnit4.class)
 @Config(minSdk = M)
 public class ShadowIconTest {
+
+  private static final int MSG_ICON_LOADED = 312;
+
   public static final int TYPE_BITMAP = 1;
   public static final int TYPE_RESOURCE = 2;
   public static final int TYPE_DATA = 3;
   public static final int TYPE_URI = 4;
 
+  @Nullable private Drawable loadedDrawable;
+
+  private final Context appContext = ApplicationProvider.getApplicationContext();
+  private final Handler mainHandler =
+      new Handler(Looper.getMainLooper()) {
+        @Override
+        public void handleMessage(Message msg) {
+          if (msg.what == MSG_ICON_LOADED) {
+            loadedDrawable = (Drawable) msg.obj;
+          }
+        }
+      };
+
   @Test
   public void testGetRes() {
-    Icon icon =
-        Icon.createWithResource(
-            ApplicationProvider.getApplicationContext(), android.R.drawable.ic_delete);
+    Icon icon = Icon.createWithResource(appContext, android.R.drawable.ic_delete);
     assertThat(shadowOf(icon).getType()).isEqualTo(TYPE_RESOURCE);
     assertThat(shadowOf(icon).getResId()).isEqualTo(android.R.drawable.ic_delete);
   }
@@ -54,5 +75,33 @@ public class ShadowIconTest {
     Icon icon = Icon.createWithContentUri(uri);
     assertThat(shadowOf(icon).getType()).isEqualTo(TYPE_URI);
     assertThat(shadowOf(icon).getUri()).isEqualTo(uri);
+  }
+
+  @Test
+  public void testLoadDrawableAsyncWithMessage() {
+    ShadowIcon.overrideExecutor(directExecutor());
+
+    Icon icon = Icon.createWithResource(appContext, android.R.drawable.ic_delete);
+
+    Message andThen = Message.obtain(mainHandler, MSG_ICON_LOADED);
+
+    icon.loadDrawableAsync(appContext, andThen);
+    ShadowLooper.idleMainLooper();
+
+    assertThat(shadowOf(loadedDrawable).getCreatedFromResId())
+        .isEqualTo(android.R.drawable.ic_delete);
+  }
+
+  @Test
+  public void testLoadDrawableAsyncWithListener() {
+    ShadowIcon.overrideExecutor(directExecutor());
+
+    Icon icon = Icon.createWithResource(appContext, android.R.drawable.ic_delete);
+
+    icon.loadDrawableAsync(appContext, drawable -> this.loadedDrawable = drawable, mainHandler);
+    ShadowLooper.idleMainLooper();
+
+    assertThat(shadowOf(loadedDrawable).getCreatedFromResId())
+        .isEqualTo(android.R.drawable.ic_delete);
   }
 }
