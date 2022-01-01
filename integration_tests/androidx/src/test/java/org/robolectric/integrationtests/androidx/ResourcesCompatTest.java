5f9075035a68f9b62f97fcfc73aef3da7633ab7c
utzcoz <utzcoz@outlook.com>
2022-01-02 01:05:47 +0800

    Intercept native Font states at Java level for Android S
    vt323_bold.ttf is copied from vt323_regular.ttf, and it is fine
    to use real reguar ttf as bold ttf for tests.
    
    Signed-off-by: utzcoz <utzcoz@outlook.com>

--- integration_tests/androidx/src/test/java/org/robolectric/integrationtests/androidx/ResourcesCompatTest.java
+++ integration_tests/androidx/src/test/java/org/robolectric/integrationtests/androidx/ResourcesCompatTest.java
@@ -4,24 +4,25 @@ import static androidx.test.core.app.ApplicationProvider.getApplicationContext;
 import static com.google.common.truth.Truth.assertThat;
 
 import android.graphics.Typeface;
-import android.os.Build;
 import androidx.core.content.res.ResourcesCompat;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.robolectric.annotation.Config;
-import org.robolectric.annotation.internal.DoNotInstrument;
 import org.robolectric.testapp.R;
 
 /** Compatibility test for {@link ResourcesCompat} */
-@DoNotInstrument
 @RunWith(AndroidJUnit4.class)
 public class ResourcesCompatTest {
 
   @Test
-  @Config(maxSdk = Build.VERSION_CODES.Q)
   public void getFont() {
     Typeface typeface = ResourcesCompat.getFont(getApplicationContext(), R.font.vt323_regular);
+    assertThat(typeface).isNotNull();
+  }
+
+  @Test
+  public void getFontFamily() {
+    Typeface typeface = ResourcesCompat.getFont(getApplicationContext(), R.font.vt323);
     assertThat(typeface).isNotNull();
   }
 }
