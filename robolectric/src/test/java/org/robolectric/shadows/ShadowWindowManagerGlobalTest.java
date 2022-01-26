2bb63069e7836b70f0ad93522979f8b67c6acfbb
Googler <noreply@google.com>
2022-01-26 11:58:57 -0800

    Add `ShadowWindowManagerGlobal#getLastDragClipData` method
    Implements the perform drag APIs on the window session so that interactions with `View#startDrag` and `View#startDragAndDrop` are successful and captures the clip data passed to the start drag methods.
    
    PiperOrigin-RevId: 424407456

--- robolectric/src/test/java/org/robolectric/shadows/ShadowWindowManagerGlobalTest.java
+++ robolectric/src/test/java/org/robolectric/shadows/ShadowWindowManagerGlobalTest.java
@@ -3,24 +3,69 @@ package org.robolectric.shadows;
 import static android.os.Build.VERSION_CODES.JELLY_BEAN_MR1;
 import static com.google.common.truth.Truth.assertThat;
 
+import android.app.Activity;
+import android.content.ClipData;
+import android.os.Build.VERSION_CODES;
+import android.os.Bundle;
 import android.os.Looper;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.DragShadowBuilder;
+import androidx.annotation.Nullable;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.Config;
 
 @RunWith(AndroidJUnit4.class)
-@Config(minSdk = JELLY_BEAN_MR1)
 public class ShadowWindowManagerGlobalTest {
 
   @Test
+  @Config(minSdk = JELLY_BEAN_MR1)
   public void getWindowSession_shouldReturnSession() {
     assertThat(ShadowWindowManagerGlobal.getWindowSession()).isNotNull();
   }
 
   @Test
+  @Config(minSdk = JELLY_BEAN_MR1)
   public void getWindowSession_withLooper_shouldReturnSession() {
     // method not available in JELLY BEAN, sorry :(
     assertThat(ShadowWindowManagerGlobal.getWindowSession(Looper.getMainLooper())).isNotNull();
+  }
+
+  @Test
+  public void getLastDragClipData() {
+    MotionEvent downEvent = MotionEvent.obtain(0L, 0L, MotionEvent.ACTION_DOWN, 12f, 34f, 0);
+    Robolectric.buildActivity(DragActivity.class)
+        .setup()
+        .get()
+        .findViewById(android.R.id.content)
+        .dispatchTouchEvent(downEvent);
+
+    assertThat(ShadowWindowManagerGlobal.getLastDragClipData()).isNotNull();
+  }
+
+  static final class DragActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle bundle) {
+      super.onCreate(bundle);
+      View contentView = new View(this);
+      contentView.setOnTouchListener(
+          (view, motionEvent) -> {
+            if (motionEvent.getActionMasked() == MotionEvent.ACTION_DOWN) {
+              ClipData clipData = ClipData.newPlainText("label", "text");
+              DragShadowBuilder dragShadowBuilder = new DragShadowBuilder(view);
+              if (RuntimeEnvironment.getApiLevel() >= VERSION_CODES.N) {
+                view.startDragAndDrop(clipData, dragShadowBuilder, null, 0);
+              } else {
+                view.startDrag(clipData, dragShadowBuilder, null, 0);
+              }
+            }
+            return true;
+          });
+      setContentView(contentView);
+    }
   }
 }
