8e41547ea1e2bbebd1f8998b8599702830efba19
utzcoz <utzcoz@outlook.com>
2022-02-05 00:32:10 +0800

    Remove deprecated supportv4 shadows
    Related discussion is here:
    https://github.com/robolectric/robolectric/discussions/7005.
    
    Signed-off-by: utzcoz <utzcoz@outlook.com>

--- shadows/supportv4/src/test/java/org/robolectric/R.java
+++ shadows/supportv4/src/test/java/org/robolectric/R.java
@@ -11,12 +11,9 @@ public final class R {
   public static final class attr {}
 
   public static final class id {
-    public static final int burritos = 0x7f030002;
     public static final int tacos = 0x7f030001;
-    public static final int title = 0x7f030000;
   }
   public static final class layout {
-    public static final int dialog_fragment = 0x7f020000;
     public static final int fragment_contents = 0x7f020001;
   }
 }
