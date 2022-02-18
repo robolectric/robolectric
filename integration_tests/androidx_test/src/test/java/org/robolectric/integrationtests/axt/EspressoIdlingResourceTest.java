614744d676c37c06d5ecab73819d0f1d5304bb68
Googler <noreply@google.com>
2022-02-17 23:19:50 -0800

    Block `UiController#loopMainThreadUntilIdle` on registered idling resources being idle.
    When using the paused looper collect the registered idling resources and wait for them to become idle before returning from `loopMainThreadUntilIdle`. Because Robolectric runs on the same thread as the main looper we need to continually loop the main looper until all idling resources transition to idle state.
    
    To do this we'll first drain the looper of currently scheduled tasks and then collect all of the idling resources that are not reporting idle. While this list is not empty (we need to loop as one idling resource becoming idle may cause another idling resource to become not idle, we need to observe them all idle at once) wait on the message queue to receive new messages up to the error timeout.
    
    Issue: #4807
    PiperOrigin-RevId: 436334339

---
+++ integration_tests/androidx_test/src/test/java/org/robolectric/integrationtests/axt/EspressoIdlingResourceTest.java
@@ -0,0 +1,181 @@
+package org.robolectric.integrationtests.axt;
+
+import static androidx.test.espresso.Espresso.onIdle;
+import static com.google.common.truth.Truth.assertThat;
+
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Looper;
+import androidx.test.espresso.IdlingRegistry;
+import androidx.test.espresso.IdlingResource;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicBoolean;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/** Test Espresso IdlingResource support. */
+@RunWith(AndroidJUnit4.class)
+public final class EspressoIdlingResourceTest {
+  private final IdlingRegistry idlingRegistry = IdlingRegistry.getInstance();
+
+  private ExecutorService executor;
+
+  @Before
+  public void setup() {
+    executor = Executors.newSingleThreadExecutor();
+  }
+
+  @After
+  public void teardown() {
+    for (IdlingResource resource : idlingRegistry.getResources()) {
+      idlingRegistry.unregister(resource);
+    }
+    for (Looper looper : idlingRegistry.getLoopers()) {
+      idlingRegistry.unregisterLooperAsIdlingResource(looper);
+    }
+    executor.shutdown();
+  }
+
+  @Test
+  public void onIdle_idlingResourceIsIdle_doesntBlock() {
+    AtomicBoolean didCheckIdle = new AtomicBoolean();
+    idlingRegistry.register(
+        new NamedIdleResource("Test", /* isIdle= */ true) {
+          @Override
+          public boolean isIdleNow() {
+            didCheckIdle.set(true);
+            return super.isIdleNow();
+          }
+        });
+
+    onIdle();
+
+    assertThat(didCheckIdle.get()).isTrue();
+  }
+
+  @SuppressWarnings("FutureReturnValueIgnored")
+  @Test
+  public void onIdle_postToMainThread() {
+    idlingRegistry.register(
+        new NamedIdleResource("Test", /* isIdle= */ false) {
+          boolean submitted;
+
+          @Override
+          public boolean isIdleNow() {
+            if (!submitted) {
+              submitted = true;
+              executor.submit(this::postToMainLooper);
+            }
+            return super.isIdleNow();
+          }
+
+          void postToMainLooper() {
+            new Handler(Looper.getMainLooper()).post(() -> setIdle(true));
+          }
+        });
+
+    onIdle();
+  }
+
+  @SuppressWarnings("FutureReturnValueIgnored")
+  @Test
+  public void onIdle_cooperativeResources() {
+    NamedIdleResource a = new NamedIdleResource("A", /* isIdle= */ true);
+    NamedIdleResource b = new NamedIdleResource("B", /* isIdle= */ false);
+    NamedIdleResource c = new NamedIdleResource("C", /* isIdle= */ false);
+    idlingRegistry.register(a, b);
+    executor.submit(
+        () -> {
+          a.setIdle(false);
+          b.setIdle(true);
+          c.setIdle(false);
+          executor.submit(
+              () -> {
+                a.setIdle(true);
+                b.setIdle(false);
+                c.setIdle(false);
+                executor.submit(
+                    () -> {
+                      a.setIdle(true);
+                      b.setIdle(true);
+                      c.setIdle(true);
+                    });
+              });
+        });
+
+    onIdle();
+
+    assertThat(a.isIdleNow()).isTrue();
+    assertThat(b.isIdleNow()).isTrue();
+    assertThat(c.isIdleNow()).isTrue();
+  }
+
+  @SuppressWarnings("FutureReturnValueIgnored")
+  @Test
+  public void onIdle_looperIsIdle() throws Exception {
+    HandlerThread handlerThread = new HandlerThread("Test");
+    try {
+      handlerThread.start();
+      Handler handler = new Handler(handlerThread.getLooper());
+      CountDownLatch handlerStarted = new CountDownLatch(1);
+      CountDownLatch releaseHandler = new CountDownLatch(1);
+      handler.post(
+          () -> {
+            handlerStarted.countDown();
+            try {
+              releaseHandler.await();
+            } catch (InterruptedException e) {
+              // ignore
+            }
+          });
+      handlerStarted.await();
+      idlingRegistry.registerLooperAsIdlingResource(handlerThread.getLooper());
+
+      executor.submit(releaseHandler::countDown);
+      onIdle();
+
+      // onIdle should have blocked on the looper waiting on the release latch
+      assertThat(releaseHandler.getCount()).isEqualTo(0);
+    } finally {
+      handlerThread.quit();
+    }
+  }
+
+  private static class NamedIdleResource implements IdlingResource {
+    final String name;
+    final AtomicBoolean isIdle;
+    ResourceCallback callback;
+
+    NamedIdleResource(String name, boolean isIdle) {
+      this.name = name;
+      this.isIdle = new AtomicBoolean(isIdle);
+    }
+
+    @Override
+    public String getName() {
+      return name;
+    }
+
+    @Override
+    public boolean isIdleNow() {
+      return isIdle.get();
+    }
+
+    void setIdle(boolean isIdle) {
+      this.isIdle.set(isIdle);
+      if (isIdle && callback != null) {
+        callback.onTransitionToIdle();
+      }
+    }
+
+    @Override
+    public void registerIdleTransitionCallback(ResourceCallback callback) {
+      this.callback = callback;
+    }
+  }
+}
--- robolectric/src/main/java/org/robolectric/android/internal/LocalUiController.java
+++ robolectric/src/main/java/org/robolectric/android/internal/LocalUiController.java
@@ -7,12 +7,16 @@ import static android.view.WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH;
 import static com.google.common.base.Preconditions.checkNotNull;
 import static com.google.common.base.Preconditions.checkState;
 import static java.util.Comparator.comparingInt;
+import static java.util.concurrent.TimeUnit.NANOSECONDS;
+import static java.util.concurrent.TimeUnit.SECONDS;
 import static java.util.stream.Collectors.toList;
+import static org.robolectric.Shadows.shadowOf;
 import static org.robolectric.shadows.ShadowLooper.shadowMainLooper;
 
 import android.annotation.SuppressLint;
 import android.os.Build;
 import android.os.Build.VERSION_CODES;
+import android.os.Handler;
 import android.os.Looper;
 import android.os.SystemClock;
 import android.util.Log;
@@ -23,15 +27,31 @@ import android.view.ViewRootImpl;
 import android.view.WindowManager.LayoutParams;
 import android.view.WindowManagerGlobal;
 import android.view.WindowManagerImpl;
+import androidx.test.espresso.IdlingRegistry;
+import androidx.test.espresso.IdlingResource;
 import androidx.test.platform.ui.InjectEventSecurityException;
 import androidx.test.platform.ui.UiController;
+import com.google.common.annotations.Beta;
 import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.ImmutableSet;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Predicate;
 import org.robolectric.RuntimeEnvironment;
+import org.robolectric.annotation.LooperMode;
+import org.robolectric.shadow.api.Shadow;
+import org.robolectric.shadows.ShadowLooper;
+import org.robolectric.shadows.ShadowPausedLooper;
 import org.robolectric.util.ReflectionHelpers;
 
 /** A {@link UiController} that runs on a local JVM with Robolectric. */
@@ -45,6 +65,22 @@ public class LocalUiController implements UiController {
       IS_FOCUSABLE.and(hasLayoutFlag(FLAG_NOT_TOUCH_MODAL).negate());
   private static final Predicate<Root> WATCH_TOUCH_OUTSIDE =
       IS_TOUCH_MODAL.negate().and(hasLayoutFlag(FLAG_WATCH_OUTSIDE_TOUCH));
+
+  private static long idlingResourceErrorTimeoutMs = SECONDS.toMillis(26);
+  private final HashSet<IdlingResourceProxyImpl> syncedIdlingResources = new HashSet<>();
+  private final ExecutorService looperIdlingExecutor = Executors.newCachedThreadPool();
+
+  /**
+   * Sets the error timeout for idling resources.
+   *
+   * <p>See {@link androidx.test.espresso.IdlingPolicies#setIdlingResourceTimeout(long, TimeUnit)}.
+   *
+   * <p>Note: This API may be removed in the future in favor of using IdlingPolicies directly.
+   */
+  @Beta
+  public static void setIdlingResourceTimeout(long timeout, TimeUnit unit) {
+    idlingResourceErrorTimeoutMs = unit.toMillis(timeout);
+  }
 
   @Override
   public boolean injectMotionEvent(MotionEvent event) throws InjectEventSecurityException {
@@ -174,7 +210,110 @@ public class LocalUiController implements UiController {
 
   @Override
   public void loopMainThreadUntilIdle() {
-    shadowMainLooper().idle();
+    if (!ShadowLooper.looperMode().equals(LooperMode.Mode.PAUSED)) {
+      shadowMainLooper().idle();
+    } else {
+      ImmutableSet<IdlingResourceProxy> idlingResources = syncIdlingResources();
+      if (idlingResources.isEmpty()) {
+        shadowMainLooper().idle();
+      } else {
+        loopMainThreadUntilIdlingResourcesIdle(idlingResources);
+      }
+    }
+  }
+
+  private void loopMainThreadUntilIdlingResourcesIdle(
+      ImmutableSet<IdlingResourceProxy> idlingResources) {
+    Looper mainLooper = Looper.myLooper();
+    ShadowPausedLooper shadowMainLooper = Shadow.extract(mainLooper);
+    Handler handler = new Handler(mainLooper);
+    Set<IdlingResourceProxy> activeResources = new HashSet<>();
+    long startTimeNanos = System.nanoTime();
+
+    shadowMainLooper.idle();
+    while (true) {
+      // Gather the list of resources that are not idling.
+      for (IdlingResourceProxy resource : idlingResources) {
+        // Add the resource as active and check if it's idle, if it is already is will be removed
+        // synchronously. The idle callback is synchronized in the resource which avoids a race
+        // between registering the idle callback and checking the idle state.
+        activeResources.add(resource);
+        resource.notifyOnIdle(
+            () -> {
+              if (Looper.myLooper() == mainLooper) {
+                activeResources.remove(resource);
+              } else {
+                // Post to restart the main thread.
+                handler.post(() -> activeResources.remove(resource));
+              }
+            });
+      }
+      // If all are idle then just return, we're done.
+      if (activeResources.isEmpty()) {
+        break;
+      }
+      // While the resources that weren't idle haven't transitioned to idle continue to loop the
+      // main looper waiting for any new messages. Once all resources have transitioned to idle loop
+      // around again to make sure all resources are idle at the same time.
+      while (!activeResources.isEmpty()) {
+        long elapsedTimeMs = NANOSECONDS.toMillis(System.nanoTime() - startTimeNanos);
+        if (elapsedTimeMs >= idlingResourceErrorTimeoutMs) {
+          throw new IdlingResourceTimeoutException(idlingResourceNames(activeResources));
+        }
+        // Poll the queue and suspend the thread until we get new messages or the idle transition.
+        shadowMainLooper.poll(idlingResourceErrorTimeoutMs - elapsedTimeMs);
+        shadowMainLooper.idle();
+      }
+    }
+  }
+
+  private ImmutableSet<IdlingResourceProxy> syncIdlingResources() {
+    // Collect unique registered idling resources.
+    HashMap<String, IdlingResource> registeredResourceByName = new HashMap<>();
+    for (IdlingResource resource : IdlingRegistry.getInstance().getResources()) {
+      String name = resource.getName();
+      if (registeredResourceByName.containsKey(name)) {
+        logDuplicate(name, registeredResourceByName.get(name), resource);
+      } else {
+        registeredResourceByName.put(name, resource);
+      }
+    }
+    Iterator<IdlingResourceProxyImpl> iterator = syncedIdlingResources.iterator();
+    while (iterator.hasNext()) {
+      IdlingResourceProxyImpl proxy = iterator.next();
+      if (registeredResourceByName.get(proxy.name) == proxy.resource) {
+        // Already registered, don't need to add.
+        registeredResourceByName.remove(proxy.name);
+      } else {
+        // Previously registered, but no longer registered, remove.
+        iterator.remove();
+      }
+    }
+    // Add new idling resources that weren't previously registered.
+    for (Map.Entry<String, IdlingResource> entry : registeredResourceByName.entrySet()) {
+      syncedIdlingResources.add(new IdlingResourceProxyImpl(entry.getKey(), entry.getValue()));
+    }
+
+    return ImmutableSet.<IdlingResourceProxy>builder()
+        .addAll(syncedIdlingResources)
+        .addAll(
+            IdlingRegistry.getInstance().getLoopers().stream()
+                .map(LooperIdlingResource::new)
+                .iterator())
+        .build();
+  }
+
+  private static void logDuplicate(String name, IdlingResource a, IdlingResource b) {
+    Log.e(
+        TAG,
+        String.format(
+            "Attempted to register resource with same names:"
+                + " %s. R1: %s R2: %s.\nDuplicate resource registration will be ignored.",
+            name, a, b));
+  }
+
+  private static List<String> idlingResourceNames(Set<IdlingResourceProxy> idlingResources) {
+    return idlingResources.stream().map(IdlingResourceProxy::getName).collect(toList());
   }
 
   @Override
@@ -273,6 +412,90 @@ public class LocalUiController implements UiController {
 
     boolean watchTouchOutside() {
       return WATCH_TOUCH_OUTSIDE.test(this);
+    }
+  }
+
+  private interface IdlingResourceProxy {
+    String getName();
+
+    void notifyOnIdle(Runnable idleCallback);
+  }
+
+  private static final class IdlingResourceProxyImpl implements IdlingResourceProxy {
+    private final String name;
+    private final IdlingResource resource;
+
+    private Runnable idleCallback;
+
+    IdlingResourceProxyImpl(String name, IdlingResource resource) {
+      this.name = name;
+      this.resource = resource;
+      resource.registerIdleTransitionCallback(this::onIdle);
+    }
+
+    @Override
+    public String getName() {
+      return this.name;
+    }
+
+    @Override
+    public synchronized void notifyOnIdle(Runnable idleCallback) {
+      if (resource.isIdleNow()) {
+        this.idleCallback = null;
+        idleCallback.run();
+      } else {
+        this.idleCallback = idleCallback;
+      }
+    }
+
+    private synchronized void onIdle() {
+      if (idleCallback != null) {
+        idleCallback.run();
+        idleCallback = null;
+      }
+    }
+  }
+
+  private final class LooperIdlingResource implements IdlingResourceProxy {
+    private final Looper looper;
+    private final ShadowLooper shadowLooper;
+    private Runnable idleCallback;
+
+    LooperIdlingResource(Looper looper) {
+      this.looper = looper;
+      this.shadowLooper = shadowOf(looper);
+    }
+
+    @Override
+    public String getName() {
+      return looper.toString();
+    }
+
+    @Override
+    public synchronized void notifyOnIdle(Runnable idleCallback) {
+      if (shadowLooper.isIdle()) {
+        this.idleCallback = null;
+        idleCallback.run();
+      } else {
+        this.idleCallback = idleCallback;
+        // Note idle() doesn't throw an exception if called from another thread, the looper would
+        // die with an unhandled exception.
+        // TODO(paulsowden): It's not technically necessary to idle the looper from another thread,
+        //  it can be idled from its own thread, however we'll need API access to do this and
+        //  observe the idle state--the idle() api blocks the calling thread by default. Perhaps a
+        //  ListenableFuture idleAsync() variant?
+        looperIdlingExecutor.execute(this::idleLooper);
+      }
+    }
+
+    private void idleLooper() {
+      shadowLooper.idle();
+      synchronized (this) {
+        if (idleCallback != null) {
+          idleCallback.run();
+          idleCallback = null;
+        }
+      }
     }
   }
 }
