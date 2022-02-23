78e202cda518237be11ab0aa7b22770ae700d2d9
Googler <noreply@google.com>
2022-02-22 18:31:18 -0800

    Add some concurrency tests for ShadowPausedSystemClockTest
    PiperOrigin-RevId: 430340855

--- robolectric/src/test/java/org/robolectric/shadows/ShadowPausedSystemClockTest.java
+++ robolectric/src/test/java/org/robolectric/shadows/ShadowPausedSystemClockTest.java
@@ -12,7 +12,11 @@ import static org.robolectric.annotation.LooperMode.Mode.PAUSED;
 import android.os.SystemClock;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import java.time.DateTimeException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.annotation.Config;
@@ -31,12 +35,80 @@ public class ShadowPausedSystemClockTest {
   }
 
   @Test
+  public void sleep_notifiesListener() {
+    AtomicBoolean listenerCalled = new AtomicBoolean();
+    ShadowPausedSystemClock.addListener(() -> listenerCalled.set(true));
+
+    SystemClock.sleep(100);
+
+    assertThat(listenerCalled.get()).isTrue();
+  }
+
+  @SuppressWarnings("FutureReturnValueIgnored")
+  @Test
+  public void sleep_concurrentAccess() throws Exception {
+    ExecutorService executor = Executors.newFixedThreadPool(2);
+    try {
+      CountDownLatch latch = new CountDownLatch(2);
+      executor.submit(
+          () -> {
+            SystemClock.sleep(100);
+            latch.countDown();
+          });
+      executor.submit(
+          () -> {
+            SystemClock.sleep(100);
+            latch.countDown();
+          });
+      latch.await();
+
+      assertThat(SystemClock.uptimeMillis()).isEqualTo(300);
+    } finally {
+      executor.shutdown();
+    }
+  }
+
+  @Test
   public void testSetCurrentTime() {
     assertTrue(SystemClock.setCurrentTimeMillis(1034));
     assertThat(SystemClock.uptimeMillis()).isEqualTo(1034);
     assertThat(SystemClock.currentThreadTimeMillis()).isEqualTo(1034);
     assertFalse(SystemClock.setCurrentTimeMillis(1000));
     assertThat(SystemClock.uptimeMillis()).isEqualTo(1034);
+  }
+
+  @Test
+  public void setCurrentTimeMillis_notifiesListener() {
+    AtomicBoolean listenerCalled = new AtomicBoolean();
+    ShadowPausedSystemClock.addListener(() -> listenerCalled.set(true));
+
+    SystemClock.setCurrentTimeMillis(200);
+
+    assertThat(listenerCalled.get()).isTrue();
+  }
+
+  @SuppressWarnings("FutureReturnValueIgnored")
+  @Test
+  public void setCurrentTimeMillis_concurrentAccess() throws Exception {
+    ExecutorService executor = Executors.newFixedThreadPool(2);
+    try {
+      CountDownLatch latch = new CountDownLatch(2);
+      executor.submit(
+          () -> {
+            SystemClock.setCurrentTimeMillis(300);
+            latch.countDown();
+          });
+      executor.submit(
+          () -> {
+            SystemClock.setCurrentTimeMillis(200);
+            latch.countDown();
+          });
+      latch.await();
+
+      assertThat(SystemClock.uptimeMillis()).isEqualTo(300);
+    } finally {
+      executor.shutdown();
+    }
   }
 
   @Test
