0f3372b79535ccc5a1302941042480d884fbb7ca
ZSmallX <diosmallx@gmail.com>
2022-02-24 22:22:47 +0800

    Added integration tests to check if issues still exist on the latest robolectric.
    Those issues are about mocking Kotlin Function with Mockito and Robolectric in runtime.
    
    Thus problem could be one of those modules.
    
    We can close those issues if it does none bussiness with Robolectric.
    
    Current modules version:
    - Kotlin 1.4.31
    - Mockito 4.1.0
    - Robolectric latest dev
    
    mockito-kotlin module is for Kotlin & Mockito related tests in some scenerios under Robolectric like:
    - Using mockito to mock Kotlin class in Java or Kotlin.
    - Using mockito-kotlin to mock in Kotlin.
    - etc.
    
    Optimized test class naming and import format.
    
    Avoid using static import all star.
    Removed braces for Gradle dependencies.
    
    Signed-off-by: ZSmallX <diosmallx@gmail.com>

---
+++ integration_tests/mockito-kotlin/src/test/java/org/robolectric/integrationtests/mockito/kotlin/MockitoKotlinFunctionTest.java
@@ -0,0 +1,38 @@
+package org.robolectric.integrationtests.mockito.kotlin;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.doReturn;
+
+import kotlin.Function;
+import kotlin.jvm.functions.Function0;
+import kotlin.jvm.functions.Function1;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+public class MockitoKotlinFunctionTest {
+  @Test
+  public void testFunctionMock() {
+    Function function = Mockito.mock(Function.class);
+    assertThat(function).isNotNull();
+  }
+
+  @Test
+  public void testFunction0Mock() {
+    Function0 function = Mockito.mock(Function0.class);
+    doReturn(null).when(function).invoke();
+    Object retVal = function.invoke();
+    assertThat(retVal).isNull();
+  }
+
+  @Test
+  public void testFunction1Mock() {
+    Function1 function = Mockito.mock(Function1.class);
+    doReturn(null).when(function).invoke(any());
+    Object retVal = function.invoke(null);
+    assertThat(retVal).isNull();
+  }
+}
