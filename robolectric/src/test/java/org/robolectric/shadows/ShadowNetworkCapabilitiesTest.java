f36e8389282075cebb2ebe3f4e986649f66185da
Denis Buzmakov <me@bacecek.dev>
2022-05-11 02:43:22 +0300

    Add support for changing NetworkSpecifier in NetworkCapabilities

--- robolectric/src/test/java/org/robolectric/shadows/ShadowNetworkCapabilitiesTest.java
+++ robolectric/src/test/java/org/robolectric/shadows/ShadowNetworkCapabilitiesTest.java
@@ -7,14 +7,20 @@ import static android.net.NetworkCapabilities.NET_CAPABILITY_TRUSTED;
 import static android.net.NetworkCapabilities.TRANSPORT_CELLULAR;
 import static android.net.NetworkCapabilities.TRANSPORT_WIFI;
 import static android.os.Build.VERSION_CODES.LOLLIPOP;
+import static android.os.Build.VERSION_CODES.N;
+import static android.os.Build.VERSION_CODES.N_MR1;
+import static android.os.Build.VERSION_CODES.O;
 import static com.google.common.truth.Truth.assertThat;
+import static org.mockito.Mockito.mock;
 import static org.robolectric.Shadows.shadowOf;
 
 import android.net.NetworkCapabilities;
+import android.net.NetworkSpecifier;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.annotation.Config;
+import org.robolectric.util.ReflectionHelpers;
 
 @RunWith(AndroidJUnit4.class)
 @Config(minSdk = LOLLIPOP)
@@ -56,5 +62,31 @@ public class ShadowNetworkCapabilitiesTest {
     assertThat(networkCapabilities.hasCapability(NET_CAPABILITY_NOT_RESTRICTED)).isTrue();
     assertThat(networkCapabilities.hasCapability(NET_CAPABILITY_TRUSTED)).isTrue();
     assertThat(networkCapabilities.hasCapability(NET_CAPABILITY_NOT_VPN)).isTrue();
+  }
+
+  @Test
+  @Config(minSdk = O)
+  public void getNetworkSpecifier_shouldReturnTheSpecifiedValue_fromO() {
+    NetworkCapabilities networkCapabilities = ShadowNetworkCapabilities.newInstance();
+    // Required to set NetworkSpecifier
+    shadowOf(networkCapabilities).addTransportType(TRANSPORT_WIFI);
+
+    NetworkSpecifier testNetworkSpecifier = mock(NetworkSpecifier.class);
+    shadowOf(networkCapabilities).setNetworkSpecifier(testNetworkSpecifier);
+    assertThat(networkCapabilities.getNetworkSpecifier()).isEqualTo(testNetworkSpecifier);
+  }
+
+  @Test
+  @Config(minSdk = N, maxSdk = N_MR1)
+  public void getNetworkSpecifier_shouldReturnTheSpecifiedValue_beforeO() {
+    NetworkCapabilities networkCapabilities = ShadowNetworkCapabilities.newInstance();
+    // Required to set NetworkSpecifier
+    shadowOf(networkCapabilities).addTransportType(TRANSPORT_WIFI);
+
+    String testNetworkSpecifier = "testNetworkSpecifier";
+    shadowOf(networkCapabilities).setNetworkSpecifier(testNetworkSpecifier);
+    String checkedNetworkSpecifier =
+        ReflectionHelpers.callInstanceMethod(networkCapabilities, "getNetworkSpecifier");
+    assertThat(checkedNetworkSpecifier).isEqualTo(testNetworkSpecifier);
   }
 }
