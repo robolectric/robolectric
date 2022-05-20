fa2f1d83dda64667089de6efde9b27497aba04e5
Michael Hoisie <hoisie@google.com>
2022-05-19 20:34:52 -0700

    Remove ShadowDrawable.createFromResourceStream
    The real Android implementation can be used instead.
    
    PiperOrigin-RevId: 449893908

--- shadows/framework/src/main/java/org/robolectric/shadows/ShadowDrawable.java
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowDrawable.java
@@ -2,16 +2,9 @@ package org.robolectric.shadows;
 
 import static org.robolectric.util.reflector.Reflector.reflector;
 
-import android.content.res.Resources;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.NinePatch;
-import android.graphics.Rect;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
-import android.graphics.drawable.NinePatchDrawable;
-import android.util.DisplayMetrics;
-import android.util.TypedValue;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
@@ -21,7 +14,6 @@ import org.robolectric.annotation.RealObject;
 import org.robolectric.annotation.Resetter;
 import org.robolectric.shadow.api.Shadow;
 import org.robolectric.util.ReflectionHelpers;
-import org.robolectric.util.ReflectionHelpers.ClassParameter;
 import org.robolectric.util.reflector.Direct;
 import org.robolectric.util.reflector.ForType;
 
@@ -52,39 +44,6 @@ public class ShadowDrawable {
     shadowBitmapDrawable.drawableCreateFromStreamSource = srcName;
     shadowBitmapDrawable.validate(); // start off not invalidated
     return drawable;
-  }
-
-  @Implementation // todo: this sucks, it's all just so we can detect 9-patches
-  protected static Drawable createFromResourceStream(
-      Resources res, TypedValue value, InputStream is, String srcName, BitmapFactory.Options opts) {
-    if (is == null) {
-      return null;
-    }
-    Rect pad = new Rect();
-    if (opts == null) opts = new BitmapFactory.Options();
-    opts.inScreenDensity = DisplayMetrics.DENSITY_DEFAULT;
-
-    Bitmap  bm = BitmapFactory.decodeResourceStream(res, value, is, pad, opts);
-    if (bm != null) {
-      boolean isNinePatch = srcName != null && srcName.contains(".9.");
-      if (isNinePatch) {
-        ReflectionHelpers.callInstanceMethod(
-            bm, "setNinePatchChunk", ClassParameter.from(byte[].class, new byte[0]));
-      }
-      byte[] np = bm.getNinePatchChunk();
-      if (np == null || !NinePatch.isNinePatchChunk(np)) {
-        np = null;
-        pad = null;
-      }
-
-      if (np != null) {
-        // todo: wrong
-        return new NinePatchDrawable(res, bm, np, pad, srcName);
-      }
-
-      return new BitmapDrawable(res, bm);
-    }
-    return null;
   }
 
   @Implementation
