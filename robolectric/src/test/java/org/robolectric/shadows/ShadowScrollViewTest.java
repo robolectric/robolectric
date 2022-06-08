bb0fa4fb7fa0b07f08ac603ebb66cd0354c73c57
Michael Hoisie <hoisie@google.com>
2022-06-08 14:41:28 -0700

    Add an option to use real scrolling logic in ShadowView
    This is part of an ongoing effort to
    greatly improve the fidelity of graphics-related classes in Robolectric.
    
    Add a system property to disable the fake View scroll methods and call the real code instead.
    
    Longer-term instead of a System property, a new graphics mode will be
    used for the check.
    
    Unfortunately too many tests likely depend on the broken scrolling logic for
    it to be enabled by default.
    
    PiperOrigin-RevId: 453770196

--- robolectric/src/test/java/org/robolectric/shadows/ShadowScrollViewTest.java
+++ robolectric/src/test/java/org/robolectric/shadows/ShadowScrollViewTest.java
@@ -2,11 +2,15 @@ package org.robolectric.shadows;
 
 import static org.junit.Assert.assertEquals;
 
+import android.app.Activity;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ScrollView;
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
 
 @RunWith(AndroidJUnit4.class)
 public class ShadowScrollViewTest {
@@ -27,5 +31,50 @@ public class ShadowScrollViewTest {
 
     assertEquals(17, scrollView.getScrollX());
     assertEquals(26, scrollView.getScrollY());
+  }
+
+  @Test
+  public void realCode_shouldSmoothScrollTo() {
+    try {
+      System.setProperty("robolectric.useRealGraphics", "true");
+      Activity activity = Robolectric.setupActivity(Activity.class);
+      ScrollView scrollView = new ScrollView(activity);
+      View view = new View(activity);
+      view.setLayoutParams(new ViewGroup.LayoutParams(1000, 1000));
+      view.layout(
+          0,
+          0,
+          activity.findViewById(android.R.id.content).getWidth(),
+          activity.findViewById(android.R.id.content).getHeight());
+      scrollView.addView(view);
+      scrollView.smoothScrollTo(7, 6);
+      assertEquals(7, scrollView.getScrollX());
+      assertEquals(6, scrollView.getScrollY());
+    } finally {
+      System.clearProperty("robolectric.useRealGraphics");
+    }
+  }
+
+  @Test
+  public void realCode_shouldSmoothScrollBy() {
+    try {
+      System.setProperty("robolectric.useRealGraphics", "true");
+      Activity activity = Robolectric.setupActivity(Activity.class);
+      ScrollView scrollView = new ScrollView(activity);
+      View view = new View(activity);
+      view.setLayoutParams(new ViewGroup.LayoutParams(1000, 1000));
+      view.layout(
+          0,
+          0,
+          activity.findViewById(android.R.id.content).getWidth(),
+          activity.findViewById(android.R.id.content).getHeight());
+      scrollView.addView(view);
+      scrollView.smoothScrollTo(7, 6);
+      scrollView.smoothScrollBy(10, 20);
+      assertEquals(17, scrollView.getScrollX());
+      assertEquals(26, scrollView.getScrollY());
+    } finally {
+      System.clearProperty("robolectric.useRealGraphics");
+    }
   }
 }
--- shadows/framework/src/main/java/org/robolectric/shadows/ShadowScrollView.java
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowScrollView.java
@@ -1,19 +1,43 @@
 package org.robolectric.shadows;
 
+import static org.robolectric.util.reflector.Reflector.reflector;
+
 import android.widget.ScrollView;
 import org.robolectric.annotation.Implementation;
 import org.robolectric.annotation.Implements;
+import org.robolectric.annotation.RealObject;
+import org.robolectric.util.reflector.Direct;
+import org.robolectric.util.reflector.ForType;
 
 @Implements(ScrollView.class)
 public class ShadowScrollView extends ShadowViewGroup {
 
+  @RealObject ScrollView realScrollView;
+
   @Implementation
   protected void smoothScrollTo(int x, int y) {
-    scrollTo(x, y);
+    if (useRealGraphics()) {
+      reflector(ScrollViewReflector.class, realScrollView).smoothScrollTo(x, y);
+    } else {
+      scrollTo(x, y);
+    }
   }
 
   @Implementation
   protected void smoothScrollBy(int x, int y) {
-    scrollBy(x, y);
+    if (useRealGraphics()) {
+      reflector(ScrollViewReflector.class, realScrollView).smoothScrollBy(x, y);
+    } else {
+      scrollBy(x, y);
+    }
+  }
+
+  @ForType(ScrollView.class)
+  interface ScrollViewReflector {
+    @Direct
+    void smoothScrollBy(int x, int y);
+
+    @Direct
+    void smoothScrollTo(int x, int y);
   }
 }
