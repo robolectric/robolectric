f5e53044f325ed68d24d03c90d8c4f6b9fe9cea9
umesh <ums945891@gmail.com>
2022-07-13 19:56:40 +0000

    modifications for BootstrapDeferringRobolectricTestRunner

--- robolectric/src/test/java/org/robolectric/android/internal/ConscryptSecurityModeTest.java
+++ robolectric/src/test/java/org/robolectric/android/internal/ConscryptSecurityModeTest.java
@@ -1,19 +1,29 @@
 package org.robolectric.android.internal;
 
 import static com.google.common.truth.Truth.assertThat;
+import static org.robolectric.annotation.LooperMode.Mode.LEGACY;
 import static org.robolectric.annotation.SecurityMode.Mode.CONSCRYPT;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.BootstrapDeferringRobolectricTestRunner;
+import org.robolectric.annotation.LooperMode;
 import org.robolectric.annotation.SecurityMode;
 
 import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 
+@RunWith(BootstrapDeferringRobolectricTestRunner.class)
+@LooperMode(LEGACY)
 public class ConscryptSecurityModeTest {
+
+    @BootstrapDeferringRobolectricTestRunner.RoboInject
+    BootstrapDeferringRobolectricTestRunner.BootstrapWrapperI bootstrapWrapper;
 
     @Test
     @SecurityMode(CONSCRYPT)
     public void ensureConscryptInstalled() throws CertificateException {
+
 //    bootstrapWrapper.callSetUpApplicationState();
 //    MessageDigest digest = MessageDigest.getInstance("SHA256");
 //    assertThat(digest.getProvider().getName()).isEqualTo(CONSCRYPT_PROVIDER);
