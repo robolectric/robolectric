ee59ef5be3a1459e189aa20b3cbf6b10e5d6acbc
umesh <ums945891@gmail.com>
2022-07-13 20:04:17 +0000

    removed unused code from ConscryptSecurityModeTest

--- robolectric/src/test/java/org/robolectric/android/internal/ConscryptSecurityModeTest.java
+++ robolectric/src/test/java/org/robolectric/android/internal/ConscryptSecurityModeTest.java
@@ -13,23 +13,12 @@ import org.robolectric.annotation.SecurityMode;
 import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 
-@RunWith(BootstrapDeferringRobolectricTestRunner.class)
-@LooperMode(LEGACY)
 public class ConscryptSecurityModeTest {
-
-    @BootstrapDeferringRobolectricTestRunner.RoboInject
-    BootstrapDeferringRobolectricTestRunner.BootstrapWrapperI bootstrapWrapper;
 
     @Test
     @SecurityMode(CONSCRYPT)
     public void ensureConscryptInstalled() throws CertificateException {
 
-//    bootstrapWrapper.callSetUpApplicationState();
-//    MessageDigest digest = MessageDigest.getInstance("SHA256");
-//    assertThat(digest.getProvider().getName()).isEqualTo(CONSCRYPT_PROVIDER);
-//
-//    Cipher aesCipher = Cipher.getInstance("AES/CBC/PKCS7Padding");
-//    assertThat(aesCipher.getProvider().getName()).isEqualTo(CONSCRYPT_PROVIDER);
         CertificateFactory factory = CertificateFactory.getInstance("X.509");
         assertThat(factory.getProvider().getName()).isEqualTo("Conscrypt");
 
