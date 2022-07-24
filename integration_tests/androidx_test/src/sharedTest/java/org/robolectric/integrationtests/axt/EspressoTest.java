5a32e05371ed3c9f5b298330b736e4143117080e
umesh <ums945891@gmail.com>
2022-07-24 17:39:31 +0000

    Formatted code using Google Java format

--- integration_tests/androidx_test/src/sharedTest/java/org/robolectric/integrationtests/axt/EspressoTest.java
+++ integration_tests/androidx_test/src/sharedTest/java/org/robolectric/integrationtests/axt/EspressoTest.java
@@ -23,7 +23,6 @@ import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.SdkSuppress;
 import java.util.concurrent.atomic.AtomicReference;
-
 import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
