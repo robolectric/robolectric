70ecd74e973f6927d15389979b37d12ec7431cf8
hellosagar <sagarkhurana00786@gmail.com>
2022-08-14 00:59:04 +0530

    refac: convert the tests to Kotlin for MavenRoboSettingsTest.kt

---
+++ plugins/maven-dependency-resolver/src/test/java/org/robolectric/MavenRoboSettingsTest.kt
@@ -0,0 +1,68 @@
+package org.robolectric
+
+import org.junit.After
+import org.junit.Assert
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class MavenRoboSettingsTest {
+
+    private lateinit var originalMavenRepositoryId: String
+    private lateinit var originalMavenRepositoryUrl: String
+    private lateinit var originalMavenRepositoryUserName: String
+    private lateinit var originalMavenRepositoryPassword: String
+
+    @Before
+    fun setUp() {
+        originalMavenRepositoryId = MavenRoboSettings.getMavenRepositoryId()
+        originalMavenRepositoryUrl = MavenRoboSettings.getMavenRepositoryUrl()
+        originalMavenRepositoryUserName = MavenRoboSettings.getMavenRepositoryUserName()
+        originalMavenRepositoryPassword = MavenRoboSettings.getMavenRepositoryPassword()
+    }
+
+    @After
+    fun tearDown() {
+        MavenRoboSettings.setMavenRepositoryId(originalMavenRepositoryId)
+        MavenRoboSettings.setMavenRepositoryUrl(originalMavenRepositoryUrl)
+        MavenRoboSettings.setMavenRepositoryUserName(originalMavenRepositoryUserName)
+        MavenRoboSettings.setMavenRepositoryPassword(originalMavenRepositoryPassword)
+    }
+
+    @Test
+    fun getMavenRepositoryId_defaultSonatype() {
+            assertEquals("mavenCentral", MavenRoboSettings.getMavenRepositoryId())
+    }
+
+    @Test
+    fun setMavenRepositoryId() {
+        MavenRoboSettings.setMavenRepositoryId("testRepo")
+        assertEquals("testRepo", MavenRoboSettings.getMavenRepositoryId())
+    }
+
+    @Test
+    fun getMavenRepositoryUrl_defaultSonatype() {
+            assertEquals("https://repo1.maven.org/maven2", MavenRoboSettings.getMavenRepositoryUrl())
+    }
+
+    @Test
+    fun setMavenRepositoryUrl() {
+        MavenRoboSettings.setMavenRepositoryUrl("http://local")
+        assertEquals("http://local", MavenRoboSettings.getMavenRepositoryUrl())
+    }
+
+    @Test
+    fun setMavenRepositoryUserName() {
+        MavenRoboSettings.setMavenRepositoryUserName("username")
+        assertEquals("username", MavenRoboSettings.getMavenRepositoryUserName())
+    }
+
+    @Test
+    fun setMavenRepositoryPassword() {
+        MavenRoboSettings.setMavenRepositoryPassword("password")
+        assertEquals("password", MavenRoboSettings.getMavenRepositoryPassword())
+    }
+}
