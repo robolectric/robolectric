7094fc93f4826eeb60057466109aaa3f45c9d040
Michael Hoisie <hoisie@google.com>
2022-08-30 19:54:48 -0700

    Support self-equality check in Region
    The real Region.equals calls into native code, so the result
    is always false, no matter what regions are compared. Add a special
    case for comparing regions against themselves.
    
    PiperOrigin-RevId: 471151934

--- shadows/framework/src/main/java/org/robolectric/shadows/ShadowRegion.java
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowRegion.java
@@ -1,17 +1,48 @@
 package org.robolectric.shadows;
 
+import static org.robolectric.util.reflector.Reflector.reflector;
+
 import android.graphics.Region;
 import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.HiddenApi;
 import org.robolectric.annotation.Implementation;
 import org.robolectric.annotation.Implements;
+import org.robolectric.annotation.RealObject;
+import org.robolectric.util.reflector.Direct;
+import org.robolectric.util.reflector.ForType;
 
 @Implements(Region.class)
 public class ShadowRegion {
+  @RealObject Region realRegion;
+
   public static long nextId = 1;
 
-  @HiddenApi @Implementation
-  public static Number nativeConstructor() {
+  /**
+   * The real {@link Region#equals(Object)} calls into native code, which is a no-op in Robolectric,
+   * and will always return false no matter what is compared. We can special-case some simple
+   * scenarios here.
+   */
+  @Implementation
+  @SuppressWarnings("EqualsHashCode")
+  public boolean equals(Object obj) {
+    if (!(obj instanceof Region)) {
+      return false;
+    }
+    if (obj == realRegion) {
+      return true;
+    }
+    return reflector(RegionReflector.class, realRegion).equals(obj);
+  }
+
+  @HiddenApi
+  @Implementation
+  protected static Number nativeConstructor() {
     return RuntimeEnvironment.castNativePtr(nextId++);
+  }
+
+  @ForType(Region.class)
+  interface RegionReflector {
+    @Direct
+    boolean equals(Object obj);
   }
 }
