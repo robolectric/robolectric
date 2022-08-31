robolectric/robolectric

commit fb9a39def92ac604678fd5eddd0d257b255dec6c

refac: emptyCatch warning fix

resources/src/main/java/org/robolectric/res/ResType.java
@@ -2,6 +2,7 @@ package org.robolectric.res;
 
 import java.util.regex.Pattern;
 import javax.annotation.Nullable;
+import org.robolectric.util.Logger;
 
 public enum ResType {
   DRAWABLE,
@@ -54,13 +55,16 @@ public enum ResType {
       try {
         Integer.parseInt(value);
         return INTEGER;
-      } catch (NumberFormatException nfe) {}
+      } catch (NumberFormatException nfe) {
+        Logger.error("Failed to infer int from value", nfe);
+      }
 
       try {
         Float.parseFloat(value);
         return FRACTION;
-      } catch (NumberFormatException nfe) {}
-
+      } catch (NumberFormatException nfe) {
+        Logger.error("Failed to infer float from value", nfe);
+      }
 
       return CHAR_SEQUENCE;
     }

utils/src/main/java/org/robolectric/util/TempDirectory.java
@@ -107,6 +107,7 @@ public class TempDirectory {
       clearDirectory(basePath);
       Files.delete(basePath);
     } catch (IOException ignored) {
+      Logger.error("Failed to destroy temp directory", ignored);
     }
   }
 

commit 5b729ecdbd71c9f22c4622932e371d98035c7e10

Fix logspam in RobolectricTestRunnerMultiApiTest
This was inadvertently caused by #7379.

With this change the size of the log of
RobolectricTestRunnerMultiApiTest goes from 17MB to 3.7KB.

Fixes #7562

resources/src/main/java/org/robolectric/res/ResType.java
@@ -2,7 +2,6 @@ package org.robolectric.res;
 
 import java.util.regex.Pattern;
 import javax.annotation.Nullable;
-import org.robolectric.util.Logger;
 
 public enum ResType {
   DRAWABLE,
@@ -51,22 +50,30 @@ public enum ResType {
       return BOOLEAN;
     } else if (DIMEN_RE.matcher(value).find()) {
       return DIMEN;
+    } else if (isInteger(value)) {
+      return INTEGER;
+    } else if (isFloat(value)) {
+      return FRACTION;
     } else {
-      try {
-        Integer.parseInt(value);
-        return INTEGER;
-      } catch (NumberFormatException nfe) {
-        Logger.error("Failed to infer int from value", nfe);
-      }
-
-      try {
-        Float.parseFloat(value);
-        return FRACTION;
-      } catch (NumberFormatException nfe) {
-        Logger.error("Failed to infer float from value", nfe);
-      }
-
       return CHAR_SEQUENCE;
+    }
+  }
+
+  private static boolean isInteger(String value) {
+    try {
+      Integer.parseInt(value);
+      return true;
+    } catch (NumberFormatException nfe) {
+      return false;
+    }
+  }
+
+  private static boolean isFloat(String value) {
+    try {
+      Float.parseFloat(value);
+      return true;
+    } catch (NumberFormatException nfe) {
+      return false;
     }
   }
 }