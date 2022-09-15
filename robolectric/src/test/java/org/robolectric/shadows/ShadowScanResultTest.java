60f97af897cc46792753f60c4242182381873c00
Brett Chabot <brettchabot@google.com>
2022-09-15 09:54:07 -0700

    Deprecate and un-shadow ShadowScanResult class.
    ShadowScanResult no longer seems useful since ScanResult now
    has a public constructor, and has a valid toString implementation.
    
    This commit removes the shadow toString method, and deprecates the
    newInstance methods.
    
    PiperOrigin-RevId: 474587404

--- robolectric/src/test/java/org/robolectric/shadows/ShadowScanResultTest.java
+++ robolectric/src/test/java/org/robolectric/shadows/ShadowScanResultTest.java
@@ -1,8 +1,6 @@
 package org.robolectric.shadows;
 
 import static com.google.common.truth.Truth.assertThat;
-import static org.junit.Assert.assertNotNull;
-import static org.robolectric.Shadows.shadowOf;
 
 import android.net.wifi.ScanResult;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -20,6 +18,5 @@ public class ShadowScanResultTest {
     assertThat(scanResult.capabilities).isEqualTo("Caps");
     assertThat(scanResult.level).isEqualTo(11);
     assertThat(scanResult.frequency).isEqualTo(42);
-    assertNotNull(shadowOf(scanResult).realObject);
   }
 }
