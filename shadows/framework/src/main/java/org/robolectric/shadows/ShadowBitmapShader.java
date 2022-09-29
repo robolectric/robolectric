34cffc9a9c57d280c7adf6dbb6269ec44161ae9f
Michael Hoisie <hoisie@google.com>
2022-09-29 10:34:51 -0700

    Remove empty ShadowBitmapShader
    PiperOrigin-RevId: 477761951

--- shadows/framework/src/main/java/org/robolectric/shadows/ShadowBitmapShader.java
+++
@@ -1,8 +0,0 @@
-package org.robolectric.shadows;
-
-import android.graphics.BitmapShader;
-import org.robolectric.annotation.Implements;
-
-@Implements(BitmapShader.class)
-public class ShadowBitmapShader {
-}
