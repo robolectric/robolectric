ed2d7d3d600972090de29bcf9ad37d65a4d7ef47
Samuel Liu <slliu@google.com>
2022-10-17 11:24:10 -0700

    Update ShadowLineBreaker and ShadowStaticLayout to work with Compose TextViews.
    Note: This only provides a fix for SDK >= 28.
    
    For #7461
    
    PiperOrigin-RevId: 481693147

--- shadows/framework/src/main/java/org/robolectric/shadows/ShadowLineBreaker.java
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowLineBreaker.java
@@ -4,29 +4,45 @@ import android.graphics.text.LineBreaker;
 import android.os.Build;
 import org.robolectric.annotation.Implementation;
 import org.robolectric.annotation.Implements;
+import org.robolectric.res.android.NativeObjRegistry;
 
+/** Shadow for android.graphics.text.LineBreaker */
 @Implements(value = LineBreaker.class, isInAndroidSdk = false, minSdk = Build.VERSION_CODES.Q)
 public class ShadowLineBreaker {
-  //
-  // @Implementation
-  // @HiddenApi
-  // protected static long nComputeLineBreaks(
-  //     /* non zero */ long nativePtr,
-  //     // Inputs
-  //     char[] text,
-  //     long measuredTextPtr,
-  //     int length,
-  //     float firstWidth,
-  //     int firstWidthLineCount,
-  //     float restWidth,
-  //     int[] variableTabStops,
-  //     int defaultTabStop,
-  //     int indentsOffset) {
-  //   return 1;
-  // }
+
+  static class NativeLineBreakerResult {
+    char[] chars;
+  }
+
+  static final NativeObjRegistry<NativeLineBreakerResult> nativeObjectRegistry =
+      new NativeObjRegistry<>(NativeLineBreakerResult.class);
+
+  @Implementation
+  protected static long nComputeLineBreaks(
+      /* non zero */ long nativePtr,
+      // Inputs
+      char[] text,
+      long measuredTextPtr,
+      int length,
+      float firstWidth,
+      int firstWidthLineCount,
+      float restWidth,
+      float[] variableTabStops,
+      float defaultTabStop,
+      int indentsOffset) {
+    NativeLineBreakerResult nativeLineBreakerResult = new NativeLineBreakerResult();
+    nativeLineBreakerResult.chars = text;
+    return nativeObjectRegistry.register(nativeLineBreakerResult);
+  }
 
   @Implementation
   protected static int nGetLineCount(long ptr) {
     return 1;
+  }
+
+  @Implementation
+  protected static int nGetLineBreakOffset(long ptr, int idx) {
+    NativeLineBreakerResult nativeLineBreakerResult = nativeObjectRegistry.getNativeObject(ptr);
+    return (nativeLineBreakerResult != null) ? nativeLineBreakerResult.chars.length : 1;
   }
 }
--- shadows/framework/src/main/java/org/robolectric/shadows/ShadowStaticLayout.java
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowStaticLayout.java
@@ -5,30 +5,58 @@ import static android.os.Build.VERSION_CODES.LOLLIPOP_MR1;
 import static android.os.Build.VERSION_CODES.M;
 import static android.os.Build.VERSION_CODES.O_MR1;
 import static android.os.Build.VERSION_CODES.P;
+import static org.robolectric.util.reflector.Reflector.reflector;
 
+import android.text.DynamicLayout;
 import android.text.StaticLayout;
+import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.HiddenApi;
 import org.robolectric.annotation.Implementation;
 import org.robolectric.annotation.Implements;
+import org.robolectric.annotation.Resetter;
+import org.robolectric.util.ReflectionHelpers;
+import org.robolectric.util.reflector.Accessor;
+import org.robolectric.util.reflector.ForType;
 
+/** Shadow for android.text.StaticLayout */
 @Implements(value = StaticLayout.class, looseSignatures = true)
 public class ShadowStaticLayout {
 
-  @Implementation(minSdk = LOLLIPOP, maxSdk = LOLLIPOP_MR1)
+  @ForType(className = "android.text.StaticLayout$LineBreaks")
+  interface LineBreaksReflector {
+    @Accessor("breaks")
+    void setBreaks(int[] breaks);
+  }
+
+  @Resetter
+  public static void reset() {
+    if (RuntimeEnvironment.getApiLevel() >= M) {
+      ReflectionHelpers.setStaticField(DynamicLayout.class, "sStaticLayout", null);
+      ReflectionHelpers.setStaticField(DynamicLayout.class, "sBuilder", null);
+    }
+  }
+
   @HiddenApi
-  public static int[] nLineBreakOpportunities(String locale, char[] text, int length, int[] recycle) {
+  @Implementation(minSdk = LOLLIPOP, maxSdk = LOLLIPOP_MR1)
+  public static int[] nLineBreakOpportunities(
+      String locale, char[] text, int length, int[] recycle) {
     return new int[] {-1};
   }
 
   @HiddenApi
   @Implementation(minSdk = M, maxSdk = O_MR1)
-  public static int nComputeLineBreaks(Object nativePtr, Object recycle,
-            Object recycleBreaks, Object recycleWidths, Object recycleFlags, Object recycleLength) {
+  public static int nComputeLineBreaks(
+      Object nativePtr,
+      Object recycle,
+      Object recycleBreaks,
+      Object recycleWidths,
+      Object recycleFlags,
+      Object recycleLength) {
     return 1;
   }
 
-  @Implementation(minSdk = P, maxSdk = P)
   @HiddenApi
+  @Implementation(minSdk = P, maxSdk = P)
   protected static int nComputeLineBreaks(
       Object nativePtr,
       Object text,
@@ -48,6 +76,7 @@ public class ShadowStaticLayout {
       Object recycleDescents,
       Object recycleFlags,
       Object charWidths) {
+    reflector(LineBreaksReflector.class, recycle).setBreaks(new int[] {((char[]) text).length});
     return 1;
   }
 }
