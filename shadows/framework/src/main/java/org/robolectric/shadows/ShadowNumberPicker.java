e41c11169f8e0cf6973fbc6f73709563dac0ad91
Julia Sullivan <juliansull@google.com>
2022-11-07 17:21:39 -0800

    Deletes some ShadowNumberPicker functions that don't do anything, which allows DatePickerSpinner to use real values and the real class.
    This fixes a bug where the spinner would generate correct values but not display those values properly.
    
    PiperOrigin-RevId: 486805743

--- shadows/framework/src/main/java/org/robolectric/shadows/ShadowNumberPicker.java
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowNumberPicker.java
@@ -12,62 +12,7 @@ import org.robolectric.util.reflector.ForType;
 @Implements(value = NumberPicker.class)
 public class ShadowNumberPicker extends ShadowLinearLayout {
   @RealObject private NumberPicker realNumberPicker;
-  private int value;
-  private int minValue;
-  private int maxValue;
-  private boolean wrapSelectorWheel;
-  private String[] displayedValues;
   private NumberPicker.OnValueChangeListener onValueChangeListener;
-
-  @Implementation
-  protected void setValue(int value) {
-    this.value = value;
-  }
-
-  @Implementation
-  protected int getValue() {
-    return value;
-  }
-
-  @Implementation
-  protected void setDisplayedValues(String[] displayedValues) {
-    this.displayedValues = displayedValues;
-  }
-
-  @Implementation
-  protected String[] getDisplayedValues() {
-    return displayedValues;
-  }
-
-  @Implementation
-  protected void setMinValue(int minValue) {
-    this.minValue = minValue;
-  }
-
-  @Implementation
-  protected void setMaxValue(int maxValue) {
-    this.maxValue = maxValue;
-  }
-
-  @Implementation
-  protected int getMinValue() {
-    return this.minValue;
-  }
-
-  @Implementation
-  protected int getMaxValue() {
-    return this.maxValue;
-  }
-
-  @Implementation
-  protected void setWrapSelectorWheel(boolean wrapSelectorWheel) {
-    this.wrapSelectorWheel = wrapSelectorWheel;
-  }
-
-  @Implementation
-  protected boolean getWrapSelectorWheel() {
-    return wrapSelectorWheel;
-  }
 
   @Implementation
   protected void setOnValueChangedListener(NumberPicker.OnValueChangeListener listener) {
@@ -81,7 +26,6 @@ public class ShadowNumberPicker extends ShadowLinearLayout {
 
   @ForType(NumberPicker.class)
   interface NumberPickerReflector {
-
     @Direct
     void setOnValueChangedListener(NumberPicker.OnValueChangeListener listener);
   }
