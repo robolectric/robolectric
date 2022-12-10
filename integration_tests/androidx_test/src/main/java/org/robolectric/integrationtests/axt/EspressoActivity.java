f7e335c4e4699b1052f72daec75a8f764cbc4926
utzcoz <utzcoz@outlook.com>
2022-12-10 12:54:23 +0800

    Disable auto-correct for EspressoTest's EditText
    Signed-off-by: utzcoz <utzcoz@outlook.com>

--- integration_tests/androidx_test/src/main/java/org/robolectric/integrationtests/axt/EspressoActivity.java
+++ integration_tests/androidx_test/src/main/java/org/robolectric/integrationtests/axt/EspressoActivity.java
@@ -2,6 +2,7 @@ package org.robolectric.integrationtests.axt;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.widget.Button;
 import android.widget.EditText;
 import org.robolectric.integration.axt.R;
@@ -20,6 +21,10 @@ public class EspressoActivity extends Activity {
     setContentView(R.layout.espresso_activity);
 
     editText = findViewById(R.id.edit_text);
+    // Disable auto-correct for EditText to avoid typed text is changed
+    // by these features when running tests.
+    editText.setInputType(editText.getInputType() & (~InputType.TYPE_TEXT_FLAG_AUTO_CORRECT));
+
     button = findViewById(R.id.button);
     button.setOnClickListener(view -> buttonClicked = true);
   }
