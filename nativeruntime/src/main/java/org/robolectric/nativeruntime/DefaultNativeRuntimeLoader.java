82454a29bee6995cbb76e791193dbe5f083bb766
Michael Hoisie <hoisie@google.com>
2022-12-21 14:51:44 -0800

    Update DefaultNativeRuntimeLoader to work in RNG
    With Robolectric Native Graphics (RNG), the structure of the JAR file(s)
    containing data will now include a `fonts` directory that contains system
    fonts, as well as a separate ICU dat file. Previously the dat file was built
    into the shared objects, but a separate dat files is equivalent.
    
      fonts
        fonts.xml
        *.ttf
      icu
        icudt68l.dat
      native
        linux
          x86_64
            librobolectric-nativeruntime.so
    
    Update DefaultNativeRuntimeLoader to support this new format. There will
    be forthcoming commits to actually bring these resources to the Robolectric
    build process.
    
    PiperOrigin-RevId: 497012102

--- nativeruntime/src/main/java/org/robolectric/nativeruntime/DefaultNativeRuntimeLoader.java
+++ nativeruntime/src/main/java/org/robolectric/nativeruntime/DefaultNativeRuntimeLoader.java
@@ -1,22 +1,33 @@
 package org.robolectric.nativeruntime;
 
+import static android.os.Build.VERSION_CODES.O;
 import static com.google.common.base.StandardSystemProperty.OS_ARCH;
 import static com.google.common.base.StandardSystemProperty.OS_NAME;
 
 import android.database.CursorWindow;
+import android.os.Build;
 import com.google.auto.service.AutoService;
 import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.ImmutableMap;
 import com.google.common.io.Files;
 import com.google.common.io.Resources;
-import java.io.File;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Iterator;
 import java.util.Locale;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Stream;
 import javax.annotation.Priority;
 import org.robolectric.pluginapi.NativeRuntimeLoader;
 import org.robolectric.util.PerfStatsCollector;
+import org.robolectric.util.TempDirectory;
 import org.robolectric.util.inject.Injector;
 
 /** Loads the Robolectric native runtime. */
@@ -27,6 +38,8 @@ public class DefaultNativeRuntimeLoader implements NativeRuntimeLoader {
 
   private static final AtomicReference<NativeRuntimeLoader> nativeRuntimeLoader =
       new AtomicReference<>();
+
+  private TempDirectory extractDirectory;
 
   public static void injectAndLoad() {
     // Ensure a single instance.
@@ -60,18 +73,84 @@ public class DefaultNativeRuntimeLoader implements NativeRuntimeLoader {
           .measure(
               "loadNativeRuntime",
               () -> {
-                String libraryName = System.mapLibraryName("robolectric-nativeruntime");
+                extractDirectory = new TempDirectory("nativeruntime");
                 System.setProperty(
                     "robolectric.nativeruntime.languageTag", Locale.getDefault().toLanguageTag());
-                File tmpLibraryFile = java.nio.file.Files.createTempFile("", libraryName).toFile();
-                tmpLibraryFile.deleteOnExit();
-                URL resource = Resources.getResource(nativeLibraryPath());
-                Resources.asByteSource(resource).copyTo(Files.asByteSink(tmpLibraryFile));
-                System.load(tmpLibraryFile.getAbsolutePath());
+                if (Build.VERSION.SDK_INT >= O) {
+                  maybeCopyFonts(extractDirectory);
+                }
+                maybeCopyIcuData(extractDirectory);
+                loadLibrary(extractDirectory);
               });
     } catch (IOException e) {
       throw new AssertionError("Unable to load Robolectric native runtime library", e);
     }
+  }
+
+  /** Attempts to load the ICU dat file. This is only relevant for native graphics. */
+  private void maybeCopyIcuData(TempDirectory tempDirectory) throws IOException {
+    URL icuDatUrl;
+    try {
+      icuDatUrl = Resources.getResource("icu/icudt68l.dat");
+    } catch (IllegalArgumentException e) {
+      return;
+    }
+    Path icuPath = tempDirectory.create("icu");
+    Path icuDatPath = tempDirectory.getBasePath().resolve("icu/icudt68l.dat");
+    Resources.asByteSource(icuDatUrl).copyTo(Files.asByteSink(icuDatPath.toFile()));
+    System.setProperty("icu.dir", icuPath.toAbsolutePath().toString());
+  }
+
+  /**
+   * Attempts to copy the system fonts to a temporary directory. This is only relevant for native
+   * graphics.
+   */
+  private void maybeCopyFonts(TempDirectory tempDirectory) throws IOException {
+    URI fontsUri = null;
+    try {
+      fontsUri = Resources.getResource("fonts/").toURI();
+    } catch (IllegalArgumentException | URISyntaxException e) {
+      return;
+    }
+
+    FileSystem zipfs = null;
+
+    if ("jar".equals(fontsUri.getScheme())) {
+      zipfs = FileSystems.newFileSystem(fontsUri, ImmutableMap.of("create", "true"));
+    }
+
+    Path fontsInputPath = Paths.get(fontsUri);
+    Path fontsOutputPath = tempDirectory.create("fonts");
+
+    try (Stream<Path> pathStream = java.nio.file.Files.walk(fontsInputPath)) {
+      Iterator<Path> fileIterator = pathStream.iterator();
+      while (fileIterator.hasNext()) {
+        Path path = fileIterator.next();
+        // Avoid copying parent directory.
+        if ("fonts".equals(path.getFileName().toString())) {
+          continue;
+        }
+        String fontPath = "fonts/" + path.getFileName();
+        URL resource = Resources.getResource(fontPath);
+        Path outputPath = tempDirectory.getBasePath().resolve(fontPath);
+        Resources.asByteSource(resource).copyTo(Files.asByteSink(outputPath.toFile()));
+      }
+    }
+    System.setProperty(
+        "robolectric.nativeruntime.fontdir", fontsOutputPath.toAbsolutePath().toString());
+    if (zipfs != null) {
+      zipfs.close();
+    }
+  }
+
+  private void loadLibrary(TempDirectory tempDirectory) throws IOException {
+    String libraryName = System.mapLibraryName("robolectric-nativeruntime");
+    System.setProperty(
+        "robolectric.nativeruntime.languageTag", Locale.getDefault().toLanguageTag());
+    Path libraryPath = tempDirectory.getBasePath().resolve(libraryName);
+    URL libraryResource = Resources.getResource(nativeLibraryPath());
+    Resources.asByteSource(libraryResource).copyTo(Files.asByteSink(libraryPath.toFile()));
+    System.load(libraryPath.toAbsolutePath().toString());
   }
 
   private static boolean isSupported() {
@@ -110,5 +189,15 @@ public class DefaultNativeRuntimeLoader implements NativeRuntimeLoader {
   @VisibleForTesting
   static boolean isLoaded() {
     return loaded.get();
+  }
+
+  @VisibleForTesting
+  Path getDirectory() {
+    return extractDirectory == null ? null : extractDirectory.getBasePath();
+  }
+
+  @VisibleForTesting
+  static void resetLoaded() {
+    loaded.set(false);
   }
 }
--- nativeruntime/src/test/java/org/robolectric/nativeruntime/DefaultNativeRuntimeLoaderTest.java
+++ nativeruntime/src/test/java/org/robolectric/nativeruntime/DefaultNativeRuntimeLoaderTest.java
@@ -1,25 +1,59 @@
 package org.robolectric.nativeruntime;
 
-import static org.robolectric.annotation.Config.ALL_SDKS;
+import static android.os.Build.VERSION_CODES.O;
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assume.assumeTrue;
 
 import android.database.CursorWindow;
 import android.database.sqlite.SQLiteDatabase;
+import java.nio.file.Path;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
-import org.robolectric.annotation.Config;
+import org.robolectric.RuntimeEnvironment;
 
-@Config(sdk = ALL_SDKS)
 @RunWith(RobolectricTestRunner.class)
 public final class DefaultNativeRuntimeLoaderTest {
   ExecutorService executor = Executors.newSingleThreadExecutor();
+
+  @Before
+  public void setUp() {
+    DefaultNativeRuntimeLoader.resetLoaded();
+  }
 
   @Test
   public void concurrentLoad() throws Exception {
     executor.execute(() -> SQLiteDatabase.create(null));
     CursorWindow cursorWindow = new CursorWindow("sdfsdf");
     cursorWindow.close();
+  }
+
+  @Test
+  public void extracts_fontsAndIcuData() {
+    assumeTrue(hasResource("fonts"));
+    assumeTrue(hasResource("icu/icudt68l.dat"));
+    DefaultNativeRuntimeLoader defaultNativeRuntimeLoader = new DefaultNativeRuntimeLoader();
+    defaultNativeRuntimeLoader.ensureLoaded();
+    // Check that extraction of some key files worked.
+    Path root = defaultNativeRuntimeLoader.getDirectory();
+    assertThat(root.resolve("icu/icudt68l.dat").toFile().exists()).isTrue();
+    if (RuntimeEnvironment.getApiLevel() >= O) {
+      assertThat(root.resolve("fonts/fonts.xml").toFile().exists()).isTrue();
+    }
+  }
+
+  @Test
+  public void tempDirectory() {
+    DefaultNativeRuntimeLoader defaultNativeRuntimeLoader = new DefaultNativeRuntimeLoader();
+    assertThat((Object) defaultNativeRuntimeLoader.getDirectory()).isNull();
+    defaultNativeRuntimeLoader.ensureLoaded();
+    assertThat((Object) defaultNativeRuntimeLoader.getDirectory()).isNotNull();
+  }
+
+  private static boolean hasResource(String name) {
+    return Thread.currentThread().getContextClassLoader().getResource(name) != null;
   }
 }
