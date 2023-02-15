5d68ebc3f3332f1ffccfdd4e621dc3692407d633
Michael Hoisie <hoisie@google.com>
2023-02-15 11:13:08 -0800

    Add a system property to only use real spinner selection code
    The AbsSpinner.setSelection(int) shadow method is broken in multiple
    ways. It causes the item selection callback to be invoked in many
    situations where it shouldn't be invoked, leading to low-fidelity behavior
    and infinite loops. However, there are a lot of tests that depend
    on this broken behavior.
    
    Add a system property to control whether only real spinner selection code
    is used. It is false for now and at some point will be switched to true.
    
    PiperOrigin-RevId: 509884692

--- robolectric/src/test/java/org/robolectric/shadows/ShadowAbsSpinnerTest.java
+++ robolectric/src/test/java/org/robolectric/shadows/ShadowAbsSpinnerTest.java
@@ -3,16 +3,23 @@ package org.robolectric.shadows;
 import static com.google.common.truth.Truth.assertThat;
 import static org.robolectric.Shadows.shadowOf;
 
+import android.app.Activity;
 import android.content.Context;
+import android.os.Looper;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
 
 @RunWith(AndroidJUnit4.class)
 public class ShadowAbsSpinnerTest {
@@ -22,8 +29,10 @@ public class ShadowAbsSpinnerTest {
 
   @Before
   public void setUp() throws Exception {
+    Activity activity = Robolectric.setupActivity(Activity.class);
     Context context = ApplicationProvider.getApplicationContext();
-    spinner = new Spinner(context);
+    spinner = new Spinner(activity);
+    ((ViewGroup) activity.findViewById(android.R.id.content)).addView(spinner);
     shadowSpinner = shadowOf(spinner);
     String [] testItems = {"foo", "bar"};
     arrayAdapter = new MyArrayAdapter(context, testItems);
@@ -60,6 +69,57 @@ public class ShadowAbsSpinnerTest {
     assertThat((String) spinner.getSelectedItem()).isNotEqualTo("bar");
 
     assertThat(shadowSpinner.isAnimatedTransition()).isTrue();
+  }
+
+  @Test
+  public void useRealSelection_doesNotCauseInfiniteLoop() {
+    try {
+      System.setProperty("robolectric.useRealSpinnerSelection", "true");
+      final AtomicBoolean itemSelected = new AtomicBoolean(false);
+      spinner.setOnItemSelectedListener(
+          new OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+              itemSelected.set(true);
+              // This will result in an stack overflow if the fake selection ShadowAbsSpinner
+              // selection logic is used.
+              spinner.setSelection(0);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {}
+          });
+      spinner.setAdapter(arrayAdapter);
+      spinner.setSelection(1);
+      shadowOf(Looper.getMainLooper()).idle();
+      assertThat(itemSelected.get()).isTrue();
+    } finally {
+      System.clearProperty("robolectric.useRealSpinnerSelection");
+    }
+  }
+
+  @Test
+  public void useRealSelection_callbackCalledOnce() {
+    try {
+      System.setProperty("robolectric.useRealSpinnerSelection", "true");
+      final AtomicInteger invocations = new AtomicInteger(0);
+      spinner.setOnItemSelectedListener(
+          new OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+              invocations.incrementAndGet();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {}
+          });
+      spinner.setAdapter(arrayAdapter);
+      spinner.setSelection(1);
+      shadowOf(Looper.getMainLooper()).idle();
+      assertThat(invocations.get()).isEqualTo(1);
+    } finally {
+      System.clearProperty("robolectric.useRealSpinnerSelection");
+    }
   }
 
   private static class MyArrayAdapter extends ArrayAdapter<String> {
--- shadows/framework/src/main/java/org/robolectric/shadows/ShadowAbsSpinner.java
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowAbsSpinner.java
@@ -25,10 +25,36 @@ public class ShadowAbsSpinner extends ShadowAdapterView {
   @Implementation
   protected void setSelection(int position) {
     reflector(AbsSpinnerReflector.class, realAbsSpinner).setSelection(position);
-    SpinnerAdapter adapter = realAbsSpinner.getAdapter();
-    if (getItemSelectedListener() != null && adapter != null) {
-      getItemSelectedListener().onItemSelected(realAbsSpinner, null, position, adapter.getItemId(position));
+    if (!useRealSpinnerSelection()) {
+      // Use the broken legacy spinner selection callback invocation logic.
+      SpinnerAdapter adapter = realAbsSpinner.getAdapter();
+      if (getItemSelectedListener() != null && adapter != null) {
+        getItemSelectedListener()
+            .onItemSelected(realAbsSpinner, null, position, adapter.getItemId(position));
+      }
     }
+  }
+
+  /**
+   * Currently the shadow method {@link #setSelection(int)} broken and low-fidelity. In real
+   * Android, the item selected callback is invoked during layout passes. However, in Robolectric,
+   * the callback is always invoked explicitly, which leads to many issues:
+   *
+   * <ol>
+   *   <li>Item selection callbacks are invoked even if the selection hasn't changed, which can lead
+   *       to infinite loops.
+   *   <li>Item selection callbacks are still invoked if spinners are not attached to an Activity.
+   *   <li>Item selection callbacks are invoked even if spinners are non-visible.
+   *   <li>Item selection callbacks are invoked twice if spinners are attached to an Activity and
+   *       visible.
+   * </ol>
+   *
+   * <p>Long-term we want to eliminate this broken shadow method, but in the mean time the real
+   * scrolling behavior is only enabled if a system property is set, due to tests depending on the
+   * broken behavior.
+   */
+  private static boolean useRealSpinnerSelection() {
+    return Boolean.getBoolean("robolectric.useRealSpinnerSelection");
   }
 
   // Non-implementation helper method
