e33561c3d53b322ceaee09c3a7c3e17f9df1369e
Brett Chabot <brettchabot@google.com>
2023-03-16 12:04:35 -0700

    Prep for NativeFontsFontFamily#nBuild signature change in SDKs > T.
    PiperOrigin-RevId: 517188883

--- shadows/framework/src/main/java/org/robolectric/shadows/ShadowNativeFontsFontFamily.java
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowNativeFontsFontFamily.java
@@ -1,7 +1,9 @@
 package org.robolectric.shadows;
 
+import static android.os.Build.VERSION_CODES.CUR_DEVELOPMENT;
 import static android.os.Build.VERSION_CODES.Q;
 import static android.os.Build.VERSION_CODES.S;
+import static android.os.Build.VERSION_CODES.TIRAMISU;
 
 import android.graphics.fonts.FontFamily;
 import org.robolectric.annotation.Implementation;
@@ -56,9 +58,19 @@ public class ShadowNativeFontsFontFamily {
       FontFamilyBuilderNatives.nAddFont(builderPtr, fontPtr);
     }
 
-    @Implementation
+    @Implementation(maxSdk = TIRAMISU)
     protected static long nBuild(
         long builderPtr, String langTags, int variant, boolean isCustomFallback) {
+      return FontFamilyBuilderNatives.nBuild(builderPtr, langTags, variant, isCustomFallback);
+    }
+
+    @Implementation(minSdk = CUR_DEVELOPMENT)
+    protected static long nBuild(
+        long builderPtr,
+        String langTags,
+        int variant,
+        boolean isCustomFallback,
+        boolean isDefaultFallback) {
       return FontFamilyBuilderNatives.nBuild(builderPtr, langTags, variant, isCustomFallback);
     }
 
