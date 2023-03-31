16e72adba6a394373ee08cc9cbe380911db17b89
Googler <noreply@google.com>
2023-03-31 11:00:12 -0700

    Fix DateFormatTest on android U.
    PiperOrigin-RevId: 520964527

--- integration_tests/ctesque/src/sharedTest/java/android/text/format/DateFormatTest.java
+++ integration_tests/ctesque/src/sharedTest/java/android/text/format/DateFormatTest.java
@@ -7,6 +7,9 @@ import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.SdkSuppress;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,18 +19,38 @@ import org.robolectric.annotation.internal.DoNotInstrument;
 /** Tests that Robolectric's android.text.format.DateFormat support is consistent with device. */
 @RunWith(AndroidJUnit4.class)
 @DoNotInstrument
+@Config(qualifiers = "+en-rUS")
 public class DateFormatTest {
 
+  private Locale originalLocale;
+  private TimeZone originalTimeZone;
   private Date dateAM;
   private Date datePM;
 
   @Before
   public void setDate() {
+    // Always set default Locale+Timezone in any time-related unit test to
+    // avoid flakiness when testing in non-US environments.
+    originalLocale = Locale.getDefault();
+    Locale.setDefault(Locale.US);
+    originalTimeZone = TimeZone.getDefault();
+    TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+
     Calendar c = Calendar.getInstance();
     c.set(2000, 10, 25, 8, 24, 30);
     dateAM = c.getTime();
     c.set(2000, 10, 25, 16, 24, 30);
     datePM = c.getTime();
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    if (originalTimeZone != null) {
+      TimeZone.setDefault(originalTimeZone);
+    }
+    if (originalLocale != null) {
+      Locale.setDefault(originalLocale);
+    }
   }
 
   @Test
@@ -60,15 +83,17 @@ public class DateFormatTest {
 
   @Test
   public void getTimeFormat_am() {
-    // allow both regular and thin whitespace separators
+    // Allow both ASCII and Unicode space-class separators.
+    // Output may also contain U+202F (UTF-8 E2 80 AF), a.k.a. NARROW NO-BREAK SPACE
+    // which is in the \p{Zs} Unicode category.
     assertThat(DateFormat.getTimeFormat(getApplicationContext()).format(dateAM))
-        .matches("8:24\\sAM");
+        .matches("8:24\\p{Z}AM");
   }
 
   @Test
   public void getTimeFormat_pm() {
-    // allow both regular and thin whitespace separators
+    // Allow both ASCII and Unicode space-class separators.
     assertThat(DateFormat.getTimeFormat(getApplicationContext()).format(datePM))
-        .matches("4:24\\sPM");
+        .matches("4:24\\p{Z}PM");
   }
 }
