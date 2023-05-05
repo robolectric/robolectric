ae03e4a1ac8a3395de2d156a806d003fcaa43172
Googler <noreply@google.com>
2023-05-05 11:54:46 -0700

    Add new method setNotification() in ShadowJobService. This new method is newly introduced in Android U.
    PiperOrigin-RevId: 529776636

--- shadows/framework/src/main/java/org/robolectric/shadows/ShadowJobService.java
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowJobService.java
@@ -2,6 +2,7 @@ package org.robolectric.shadows;
 
 import static android.os.Build.VERSION_CODES.LOLLIPOP;
 
+import android.app.Notification;
 import android.app.job.JobParameters;
 import android.app.job.JobService;
 import org.robolectric.annotation.Implementation;
@@ -18,6 +19,14 @@ public class ShadowJobService extends ShadowService {
     this.isJobFinished = true;
     this.isRescheduleNeeded = needsReschedule;
   }
+
+  /** Stubbed out for now, as the real implementation throws an NPE when executed in Robolectric. */
+  @Implementation(minSdk = ShadowBuild.UPSIDE_DOWN_CAKE)
+  protected void setNotification(
+      JobParameters params,
+      int notificationId,
+      Notification notification,
+      int jobEndNotificationPolicy) {}
 
   /**
    * Returns whether the job has finished running. When using this shadow this returns true after
