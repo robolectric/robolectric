20b268e19442d1bd1aa894ba364f5a2847480448
Googler <noreply@google.com>
2023-06-16 15:16:48 -0700

    Added shadows and builders for BackupDataInput and BackupDataOutput to support testing BackupAgents.
    PiperOrigin-RevId: 541017784

---
+++ robolectric/src/test/java/org/robolectric/shadows/ShadowBackupDataInputTest.java
@@ -0,0 +1,188 @@
+package org.robolectric.shadows;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.assertThrows;
+import static org.robolectric.util.reflector.Reflector.reflector;
+
+import android.app.backup.BackupDataInput;
+import android.app.backup.BackupDataInputStream;
+import android.os.Build.VERSION_CODES;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.annotation.Config;
+import org.robolectric.util.reflector.Constructor;
+import org.robolectric.util.reflector.ForType;
+
+@RunWith(AndroidJUnit4.class)
+@Config(minSdk = VERSION_CODES.LOLLIPOP)
+public final class ShadowBackupDataInputTest {
+
+  private static final String TEST_KEY_1 = "key_1";
+  private static final byte[] TEST_DATA_1 = {1, 2, 3, 4};
+  private static final String TEST_KEY_2 = "key_2";
+  private static final byte[] TEST_DATA_2 = {5, 6, 7, 8};
+
+  private final BackupDataInput backupDataInput =
+      BackupDataInputBuilder.newBuilder()
+          .addEntity(BackupDataEntity.create(TEST_KEY_1, TEST_DATA_1))
+          .addEntity(BackupDataEntity.create(TEST_KEY_2, TEST_DATA_2))
+          .build();
+
+  @Test
+  public void readNextHeader_onFirstItem_returnsTrue() throws IOException {
+    boolean result = backupDataInput.readNextHeader();
+
+    assertThat(result).isTrue();
+  }
+
+  @Test
+  public void readNextHeader_afterReadData_returnsTrue() throws IOException {
+    backupDataInput.readNextHeader();
+    backupDataInput.readEntityData(new byte[TEST_DATA_1.length], 0, TEST_DATA_1.length);
+
+    boolean result = backupDataInput.readNextHeader();
+
+    assertThat(result).isTrue();
+  }
+
+  @Test
+  public void readNextHeader_afterSkipData_returnsTrue() throws IOException {
+    backupDataInput.readNextHeader();
+    backupDataInput.skipEntityData();
+
+    boolean result = backupDataInput.readNextHeader();
+
+    assertThat(result).isTrue();
+  }
+
+  @Test
+  public void readNextHeader_afterLastItem_returnsFalse() throws IOException {
+    backupDataInput.readNextHeader();
+    backupDataInput.skipEntityData();
+    backupDataInput.readNextHeader();
+    backupDataInput.skipEntityData();
+
+    boolean result = backupDataInput.readNextHeader();
+
+    assertThat(result).isFalse();
+  }
+
+  @Test
+  public void readNextHeader_withoutReadOrSkipData_throwsException() throws IOException {
+    backupDataInput.readNextHeader();
+
+    assertThrows(IOException.class, backupDataInput::readNextHeader);
+  }
+
+  @Test
+  public void getKey_beforeReadNextHeader_throwsException() {
+    assertThrows(IllegalStateException.class, backupDataInput::getKey);
+  }
+
+  @Test
+  public void getKey_afterReadNextHeader_returnsKey() throws IOException {
+    backupDataInput.readNextHeader();
+
+    String key = backupDataInput.getKey();
+
+    assertThat(key).isEqualTo(TEST_KEY_1);
+  }
+
+  @Test
+  public void getDataSize_beforeReadNextHeader_throwsException() {
+    assertThrows(IllegalStateException.class, backupDataInput::getDataSize);
+  }
+
+  @Test
+  public void getDataSize_afterReadNextHeader_returnsArrayLength() throws IOException {
+    backupDataInput.readNextHeader();
+
+    int dataSize = backupDataInput.getDataSize();
+
+    assertThat(dataSize).isEqualTo(TEST_DATA_1.length);
+  }
+
+  @Test
+  public void readEntityData_afterLastItem_throwsException() throws IOException {
+    backupDataInput.readNextHeader();
+    backupDataInput.skipEntityData();
+    backupDataInput.readNextHeader();
+    backupDataInput.skipEntityData();
+    backupDataInput.readNextHeader();
+
+    assertThrows(
+        IllegalStateException.class,
+        () -> backupDataInput.readEntityData(TEST_DATA_1, 0, TEST_DATA_1.length));
+  }
+
+  @Test
+  public void readEntityData_afterAllBytesRead_returns0() throws IOException {
+    backupDataInput.readNextHeader();
+    backupDataInput.readEntityData(new byte[TEST_DATA_1.length], 0, TEST_DATA_1.length);
+
+    int result =
+        backupDataInput.readEntityData(new byte[TEST_DATA_1.length], 0, TEST_DATA_1.length);
+
+    assertThat(result).isEqualTo(0);
+  }
+
+  @Test
+  public void readEntityData_withSizeGreaterThanDestination_throwsException() throws IOException {
+    backupDataInput.readNextHeader();
+
+    assertThrows(
+        IOException.class,
+        () -> backupDataInput.readEntityData(new byte[2], 0, TEST_DATA_1.length));
+  }
+
+  @Test
+  public void readEntityData_withFullDataRead_copiesSourceData() throws IOException {
+    backupDataInput.readNextHeader();
+    byte[] data = new byte[TEST_DATA_1.length];
+
+    int result = backupDataInput.readEntityData(data, 0, data.length);
+
+    assertThat(result).isEqualTo(TEST_DATA_1.length);
+    assertThat(data).isEqualTo(TEST_DATA_1);
+  }
+
+  @Test
+  public void readEntityData_withPartialDataRead_copiesSourceData() throws IOException {
+    backupDataInput.readNextHeader();
+    byte[] data1 = new byte[2];
+    byte[] data2 = new byte[2];
+
+    int result1 = backupDataInput.readEntityData(data1, 0, 2);
+    int result2 = backupDataInput.readEntityData(data2, 0, 2);
+
+    assertThat(result1).isEqualTo(2);
+    assertThat(result2).isEqualTo(2);
+    assertThat(data1).isEqualTo(new byte[] {1, 2});
+    assertThat(data2).isEqualTo(new byte[] {3, 4});
+  }
+
+  @Test
+  public void readEntityData_usingBackupDataInputStream_matchesSourceData() throws IOException {
+    backupDataInput.readNextHeader();
+    InputStream inputStream =
+        new BufferedInputStream(
+            reflector(BackupDataInputStreamReflector.class).newInstance(backupDataInput), 1);
+    byte[] data = new byte[TEST_DATA_1.length];
+
+    int result = inputStream.read(data);
+
+    assertThat(result).isEqualTo(TEST_DATA_1.length);
+    assertThat(data).isEqualTo(TEST_DATA_1);
+  }
+
+  @ForType(BackupDataInputStream.class)
+  private interface BackupDataInputStreamReflector {
+
+    @Constructor
+    BackupDataInputStream newInstance(BackupDataInput backupDataInput);
+  }
+}
---
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowBackupDataInput.java
@@ -0,0 +1,107 @@
+package org.robolectric.shadows;
+
+import static java.lang.Math.min;
+import static org.robolectric.util.reflector.Reflector.reflector;
+
+import android.app.backup.BackupDataInput;
+import android.os.Build.VERSION_CODES;
+import com.google.common.collect.ImmutableList;
+import java.io.FileDescriptor;
+import java.util.ArrayList;
+import java.util.List;
+import org.robolectric.annotation.Implementation;
+import org.robolectric.annotation.Implements;
+import org.robolectric.util.reflector.Accessor;
+import org.robolectric.util.reflector.ForType;
+
+/** Shadow for BackupDataInput. */
+@Implements(value = BackupDataInput.class, minSdk = VERSION_CODES.LOLLIPOP, looseSignatures = true)
+public class ShadowBackupDataInput {
+
+  private List<BackupDataEntity> entities = new ArrayList<>();
+  private int currentEntityIndex = -1;
+  private int currentBytesRead = 0;
+  private int currentBytesToRead = 0;
+
+  /**
+   * Sets the entities to return when reading from this {@link BackupDataInput}. Use {@link
+   * org.robolectric.shadows.BackupDataInputBuilder} to get a populated instance.
+   */
+  void setEntities(ImmutableList<BackupDataEntity> entities) {
+    this.entities = entities;
+  }
+
+  @Implementation
+  protected static long ctor(FileDescriptor fd) {
+    // Return value greater than 0 to indicate successful allocation of backup reader. The real
+    // implementation would return an allocated pointer address, but since the methods are not
+    // static, we do not need a native object in this shadow implementation, and can return a fixed
+    // value instead.
+    return 1;
+  }
+
+  // Using loose signature because EntityHeader is a private nested class.
+  @Implementation
+  protected int readNextHeader_native(Object backupReader, Object entity) {
+    if (currentBytesRead < currentBytesToRead) {
+      // Return failure to read header due to unread data bytes.
+      return -1;
+    }
+
+    currentEntityIndex++;
+
+    if (currentEntityIndex >= entities.size()) {
+      // Return end of backup input data.
+      return 1;
+    }
+
+    BackupDataEntity shadowEntity = entities.get(currentEntityIndex);
+
+    currentBytesRead = 0;
+    currentBytesToRead = shadowEntity.dataSize();
+
+    // Accessing using reflection because EntityHeader is a private nested class.
+    reflector(EntityHeaderReflector.class, entity).setKey(shadowEntity.key());
+    reflector(EntityHeaderReflector.class, entity).setDataSize(shadowEntity.dataSize());
+    return 0;
+  }
+
+  @Implementation
+  protected int readEntityData_native(long backupReader, byte[] data, int offset, int size) {
+    if (currentEntityIndex >= entities.size() || currentBytesRead >= currentBytesToRead) {
+      // Return end of data.
+      return 0;
+    }
+
+    if (offset + size > data.length) {
+      // Return error reading data.
+      return -1;
+    }
+
+    byte[] shadowData = entities.get(currentEntityIndex).data();
+    int remainingBytes = currentBytesToRead - currentBytesRead;
+    int bytesToRead = min(size, remainingBytes);
+
+    System.arraycopy(shadowData, currentBytesRead, data, offset, bytesToRead);
+    currentBytesRead += bytesToRead;
+    return bytesToRead;
+  }
+
+  @Implementation
+  protected int skipEntityData_native(long backupReader) {
+    if (currentEntityIndex < entities.size()) {
+      currentBytesRead = entities.get(currentEntityIndex).dataSize();
+    }
+    return 0;
+  }
+
+  @ForType(className = "android.app.backup.BackupDataInput$EntityHeader")
+  private interface EntityHeaderReflector {
+
+    @Accessor("key")
+    void setKey(String key);
+
+    @Accessor("dataSize")
+    void setDataSize(int dataSize);
+  }
+}
