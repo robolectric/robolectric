a3a276226a42bc54a452227e612deb02bc627c51
utzcoz <utzcoz@outlook.com>
2023-07-09 21:57:29 +0800

    Add javadoc for ShadowCryptoObject#getOpId
    Signed-off-by: utzcoz <utzcoz@outlook.com>

--- shadows/framework/src/main/java/org/robolectric/shadows/ShadowCryptoObject.java
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowCryptoObject.java
@@ -11,6 +11,18 @@ import org.robolectric.annotation.Implements;
 @Implements(value = CryptoObject.class, isInAndroidSdk = false, minSdk = P)
 public class ShadowCryptoObject {
 
+  /**
+   * The shadow method of CryptoObject#getOpId.
+   *
+   * <p>The CryptoObject#getOpId implementation in AOSP calls javax.crypto.CipherSpi#getCurrentSpi
+   * to retrieve javax.crypto.Cipher, but this API is added by Android JDK implementation, and not
+   * supported by OpenJDK. To avoid this issue, we shadow CryptoObject#getOpId to intercept
+   * call-chain early. Related issue: <a
+   * href="https://github.com/robolectric/robolectric/issues/8242">java.lang.NoSuchMethodError:
+   * 'javax.crypto.CipherSpi javax.crypto.Cipher.getCurrentSpi()</a>.
+   *
+   * @return 0L as default value.
+   */
   @Implementation
   @HiddenApi
   protected final long getOpId() {
