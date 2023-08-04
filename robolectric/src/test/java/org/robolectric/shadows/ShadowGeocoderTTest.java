de395d9dad7e3f8a9e0474aa6006edca4040c04c
utzcoz <utzcoz@outlook.com>
2023-08-04 23:10:32 +0800

    Reland "Add support to set error message for ShadowGeocoder."
    Android 12 and 12L android-all jars contain Geocoder.GeocodeListener too
    but Geocoder.GeocodeListener is internal private class not public
    interface as Android 13. So ClassLoader finds Geocoder.GeocodeListener
    from Android 12 and 12L android-all jars first and try to let
    Geocoder.GeocodeListener asynchronous instance in ShadowGeocoderTest to
    implement interface from a class. It's the main reason that code that
    looks fine causes un-expected failures on SDK 31 and 32.
    
    This CL just splits ShadowGeocoderTest to two separate tests for
    ShadowGeocoder before Android 13 and from Android 13 to avoid this
    error. Hope there is a better solution to avoid this scenario.

---
+++ robolectric/src/test/java/org/robolectric/shadows/ShadowGeocoderTTest.java
@@ -0,0 +1,82 @@
+package org.robolectric.shadows;
+
+import static android.os.Build.VERSION_CODES.TIRAMISU;
+import static com.google.common.truth.Truth.assertThat;
+import static org.robolectric.Shadows.shadowOf;
+
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+import javax.annotation.Nullable;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.annotation.Config;
+
+@RunWith(AndroidJUnit4.class)
+public class ShadowGeocoderTTest {
+  private static final String GEOCODER_ERROR_MESSAGE = "Failed to get location";
+  private Geocoder geocoder;
+  private List<Address> decodedAddresses;
+  private String errorMessage = "";
+
+  @Before
+  public void setUp() throws Exception {
+    Context context = ApplicationProvider.getApplicationContext();
+    geocoder = new Geocoder(context);
+  }
+
+  @Test
+  @Config(minSdk = TIRAMISU)
+  public void getFromLocationSetsListenerWithTheOverwrittenListLimitingByMaxResults() {
+    ShadowGeocoder shadowGeocoder = shadowOf(geocoder);
+
+    List<Address> list =
+        Arrays.asList(new Address(Locale.getDefault()), new Address(Locale.CANADA));
+    shadowGeocoder.setFromLocation(list);
+
+    Geocoder.GeocodeListener geocodeListener = addresses -> decodedAddresses = addresses;
+
+    geocoder.getFromLocation(90.0, 90.0, 1, geocodeListener);
+    assertThat(decodedAddresses).containsExactly(list.get(0));
+
+    geocoder.getFromLocation(90.0, 90.0, 2, geocodeListener);
+    assertThat(decodedAddresses).containsExactly(list.get(0), list.get(1)).inOrder();
+
+    geocoder.getFromLocation(90.0, 90.0, 3, geocodeListener);
+    assertThat(decodedAddresses).containsExactly(list.get(0), list.get(1)).inOrder();
+  }
+
+  @Test
+  @Config(minSdk = TIRAMISU)
+  public void getFromLocation_onError() {
+    ShadowGeocoder shadowGeocoder = shadowOf(geocoder);
+
+    List<Address> list =
+        Arrays.asList(new Address(Locale.getDefault()), new Address(Locale.CANADA));
+    shadowGeocoder.setFromLocation(list);
+    shadowGeocoder.setErrorMessage(GEOCODER_ERROR_MESSAGE);
+
+    Geocoder.GeocodeListener geocodeListener =
+        new Geocoder.GeocodeListener() {
+          @Override
+          public void onGeocode(List<Address> list) {
+            decodedAddresses = list;
+          }
+
+          @Override
+          public void onError(@Nullable String message) {
+            errorMessage = message;
+          }
+        };
+
+    geocoder.getFromLocation(90.0, 90.0, 1, geocodeListener);
+    assertThat(decodedAddresses).isNull();
+    assertThat(errorMessage).isEqualTo(GEOCODER_ERROR_MESSAGE);
+  }
+}
