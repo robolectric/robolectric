00348d47ef700cd38d95497718a6c084f06bc7e1
Googler <noreply@google.com>
2023-11-09 13:22:28 -0800

    Add `isClosed()` to `ShadowContextHubClient`.
    Currently, there is no method of determining if `ContextHubClient.close()` has been called. This change adds an `isClosed()` to the shadow and also throws a remote exception from server if trying to send a message after closed.
    
    PiperOrigin-RevId: 581006165

--- shadows/framework/src/main/java/org/robolectric/shadows/ShadowContextHubClient.java
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowContextHubClient.java
@@ -1,33 +1,68 @@
 package org.robolectric.shadows;
 
+import static org.robolectric.util.reflector.Reflector.reflector;
+
+import android.annotation.TargetApi;
 import android.hardware.location.ContextHubClient;
+import android.hardware.location.ContextHubInfo;
 import android.hardware.location.ContextHubTransaction;
 import android.hardware.location.NanoAppMessage;
 import android.os.Build.VERSION_CODES;
+import androidx.annotation.RequiresApi;
 import com.google.common.collect.ImmutableList;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 import org.robolectric.annotation.HiddenApi;
 import org.robolectric.annotation.Implementation;
 import org.robolectric.annotation.Implements;
+import org.robolectric.annotation.RealObject;
+import org.robolectric.util.reflector.Accessor;
+import org.robolectric.util.reflector.Constructor;
+import org.robolectric.util.reflector.ForType;
 
 /** Shadow for {@link ContextHubClient}. */
 @Implements(value = ContextHubClient.class, minSdk = VERSION_CODES.P, isInAndroidSdk = false)
 public class ShadowContextHubClient {
+  @RealObject private ContextHubClient realContextHubClient;
   private final List<NanoAppMessage> messages = new ArrayList<>();
 
   @Implementation(minSdk = VERSION_CODES.P)
   @HiddenApi
   protected int sendMessageToNanoApp(NanoAppMessage message) {
+    if (isClosed()) {
+      return ContextHubTransaction.RESULT_FAILED_UNKNOWN;
+    }
     messages.add(message);
     return ContextHubTransaction.RESULT_SUCCESS;
   }
 
   @Implementation(minSdk = VERSION_CODES.P)
   @HiddenApi
-  protected void close() {}
+  protected void close() {
+    reflector(ContextHubClientReflector.class, realContextHubClient).getIsClosed().set(true);
+  }
 
   public List<NanoAppMessage> getMessages() {
     return ImmutableList.copyOf(messages);
+  }
+
+  @RequiresApi(VERSION_CODES.Q)
+  public boolean isClosed() {
+    return reflector(ContextHubClientReflector.class, realContextHubClient).getIsClosed().get();
+  }
+
+  @ForType(ContextHubClient.class)
+  interface ContextHubClientReflector {
+    @Constructor
+    @TargetApi(VERSION_CODES.P)
+    ContextHubClient newContextHubClient();
+
+    @Constructor
+    @RequiresApi(VERSION_CODES.Q)
+    ContextHubClient newContextHubClient(ContextHubInfo hubInfo, boolean persistent);
+
+    @Accessor("mIsClosed")
+    AtomicBoolean getIsClosed();
   }
 }
