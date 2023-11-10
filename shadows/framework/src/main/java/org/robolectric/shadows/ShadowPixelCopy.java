961385c80cd9deff0fa321625fc7ab962e38c622
Googler <noreply@google.com>
2023-11-10 13:51:32 -0800

    Use HardwareRendering screenshot in UiAutomation.takeScreenshot().
    This refactors the new hardware-based rendering used in ShadowPixelCopy to also use it in ShadowUiAutomation.
    
    PiperOrigin-RevId: 581359599

--- shadows/framework/src/main/java/org/robolectric/shadows/ShadowPixelCopy.java
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowPixelCopy.java
@@ -4,24 +4,12 @@ import static android.os.Build.VERSION_CODES.O;
 import static com.google.common.base.Preconditions.checkNotNull;
 import static org.robolectric.util.reflector.Reflector.reflector;
 
-import android.content.Context;
-import android.content.res.Resources;
-import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
-import android.graphics.HardwareRenderer;
 import android.graphics.Paint;
-import android.graphics.PixelFormat;
 import android.graphics.Rect;
-import android.graphics.RenderNode;
-import android.media.Image;
-import android.media.Image.Plane;
-import android.media.ImageReader;
-import android.os.Build.VERSION;
-import android.os.Build.VERSION_CODES;
 import android.os.Handler;
 import android.os.Looper;
-import android.util.DisplayMetrics;
 import android.view.PixelCopy;
 import android.view.PixelCopy.OnPixelCopyFinishedListener;
 import android.view.Surface;
@@ -32,14 +20,11 @@ import android.view.Window;
 import android.view.WindowManagerGlobal;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import com.android.internal.R;
-import java.nio.IntBuffer;
 import java.util.function.Consumer;
 import org.robolectric.annotation.Implementation;
 import org.robolectric.annotation.Implements;
 import org.robolectric.shadow.api.Shadow;
 import org.robolectric.shadows.ShadowWindowManagerGlobal.WindowManagerGlobalReflector;
-import org.robolectric.util.ReflectionHelpers;
 import org.robolectric.util.reflector.Accessor;
 import org.robolectric.util.reflector.Constructor;
 import org.robolectric.util.reflector.ForType;
@@ -55,9 +40,6 @@ import org.robolectric.versioning.AndroidVersions.U;
  */
 @Implements(value = PixelCopy.class, minSdk = O, looseSignatures = true)
 public class ShadowPixelCopy {
-
-  private static final String USE_HARDWARE_RENDERER_NATIVE_ENV =
-      "robolectric.screenshot.hwrdr.native";
 
   @Implementation
   protected static void request(
@@ -184,10 +166,9 @@ public class ShadowPixelCopy {
     validateBitmap(screenshot);
 
     Bitmap bitmap = Bitmap.createBitmap(view.getWidth(), view.getHeight(), Bitmap.Config.ARGB_8888);
-    if (VERSION.SDK_INT >= VERSION_CODES.S
-        && Boolean.getBoolean(USE_HARDWARE_RENDERER_NATIVE_ENV)
-        && ShadowView.useRealGraphics()) {
-      generateBitmapUsingHardwareRenderNative(view, bitmap);
+
+    if (HardwareRenderingScreenshot.canTakeScreenshot()) {
+      HardwareRenderingScreenshot.takeScreenshot(view, bitmap);
     } else {
       Canvas screenshotCanvas = new Canvas(bitmap);
       view.draw(screenshotCanvas);
@@ -274,80 +255,4 @@ public class ShadowPixelCopy {
     PixelCopy.Result newResult(int copyResult, Bitmap bitmap);
   }
 
-  /**
-   * Generates a bitmap given the current view using HardwareRenderer with native graphics calls.
-   * Requires API 31+ (S).
-   *
-   * <p>This code mirrors the behavior of LayoutLib's RenderSessionImpl.renderAndBuildResult(); see
-   * https://googleplex-android.googlesource.com/platform/frameworks/layoutlib/+/refs/heads/master-layoutlib-native/bridge/src/com/android/layoutlib/bridge/impl/RenderSessionImpl.java#573
-   */
-  private static void generateBitmapUsingHardwareRenderNative(View view, Bitmap destBitmap) {
-    int width = view.getWidth();
-    int height = view.getHeight();
-
-    try (ImageReader imageReader =
-        ImageReader.newInstance(width, height, PixelFormat.RGBA_8888, 1)) {
-      // Note on pixel format:
-      // - Android Bitmap requires ARGB_8888.
-      // - ImageReader is configured as RGBA_8888.
-      // - However the native libs/hwui/pipeline/skia/SkiaHostPipeline.cpp always treats
-      //   the buffer as BGRA_8888, thus matching what the Android Bitmap object requires.
-
-      HardwareRenderer renderer = new HardwareRenderer();
-      Surface surface = imageReader.getSurface();
-      renderer.setSurface(surface);
-      Image nativeImage = imageReader.acquireNextImage();
-
-      setupRendererShadowProperties(renderer, view);
-
-      RenderNode node = getRenderNode(view);
-      renderer.setContentRoot(node);
-
-      renderer.createRenderRequest().syncAndDraw();
-
-      int[] renderPixels = new int[width * height];
-
-      Plane[] planes = nativeImage.getPlanes();
-      IntBuffer srcBuff = planes[0].getBuffer().asIntBuffer();
-      srcBuff.get(renderPixels);
-
-      destBitmap.setPixels(
-          renderPixels,
-          /* offset= */ 0,
-          /* stride= */ width,
-          /* x= */ 0,
-          /* y= */ 0,
-          width,
-          height);
-      surface.release();
-    }
-  }
-
-  private static RenderNode getRenderNode(View view) {
-    return ReflectionHelpers.callInstanceMethod(view, "updateDisplayListIfDirty");
-  }
-
-  private static void setupRendererShadowProperties(HardwareRenderer renderer, View view) {
-    Context context = view.getContext();
-    Resources resources = context.getResources();
-    DisplayMetrics displayMetrics = resources.getDisplayMetrics();
-
-    // Get the LightSourceGeometry and LightSourceAlpha from resources.
-    // The default values are the ones recommended by the getLightSourceGeometry() and
-    // getLightSourceAlpha() documentation.
-    // This matches LayoutLib's RenderSessionImpl#renderAndBuildResult() implementation.
-
-    TypedArray a = context.obtainStyledAttributes(null, R.styleable.Lighting, 0, 0);
-    float lightX = displayMetrics.widthPixels / 2f;
-    float lightY = a.getDimension(R.styleable.Lighting_lightY, 0f);
-    float lightZ = a.getDimension(R.styleable.Lighting_lightZ, 600f * displayMetrics.density);
-    float lightRadius =
-        a.getDimension(R.styleable.Lighting_lightRadius, 800f * displayMetrics.density);
-    float ambientShadowAlpha = a.getFloat(R.styleable.Lighting_ambientShadowAlpha, 0.039f);
-    float spotShadowAlpha = a.getFloat(R.styleable.Lighting_spotShadowAlpha, 0.19f);
-    a.recycle();
-
-    renderer.setLightSourceGeometry(lightX, lightY, lightZ, lightRadius);
-    renderer.setLightSourceAlpha(ambientShadowAlpha, spotShadowAlpha);
-  }
 }
