0f9b489bf4e10912cafa106027d969e4d22bdd34
Michael Hoisie <hoisie@google.com>
2023-12-13 13:51:35 -0800

    Add additional testing for NativeAllocationRegistry.applyFreeFunction
    Add a test to ensure that applyFreeFunction is functional
    across all supported SDK levels for RNG.
    
    PiperOrigin-RevId: 590702493

--- integration_tests/nativegraphics/src/test/java/org/robolectric/integrationtests/nativegraphics/ShadowNativeAllocationRegistryTest.java
+++ integration_tests/nativegraphics/src/test/java/org/robolectric/integrationtests/nativegraphics/ShadowNativeAllocationRegistryTest.java
@@ -1,16 +1,37 @@
 package org.robolectric.integrationtests.nativegraphics;
 
+import static android.os.Build.VERSION_CODES.O;
+import static android.os.Build.VERSION_CODES.S;
+import static com.google.common.truth.Truth.assertThat;
+import static org.robolectric.util.reflector.Reflector.reflector;
+
 import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.graphics.Matrix;
+import libcore.util.NativeAllocationRegistry;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
+import org.robolectric.util.reflector.Accessor;
+import org.robolectric.util.reflector.ForType;
+import org.robolectric.util.reflector.Static;
 
 @RunWith(RobolectricTestRunner.class)
-@Config(sdk = 30)
+@Config(minSdk = O)
 public final class ShadowNativeAllocationRegistryTest {
+  @Test
+  public void applyFreeFunction_matrix() {
+    Matrix matrix = new Matrix();
+    long pointer = reflector(MatrixReflector.class, matrix).getNativeInstance();
+    long freeFunction = reflector(MatrixReflector.class).nGetNativeFinalizer();
+    assertThat(pointer).isNotEqualTo(0);
+    assertThat(freeFunction).isNotEqualTo(0);
+    NativeAllocationRegistry.applyFreeFunction(freeFunction, pointer);
+    reflector(MatrixReflector.class, matrix).setNativeInstance(0); // Zero the pointer
+  }
 
+  @Config(sdk = S) // No need to re-run on multiple SDK levels
   @Test
   public void nativeAllocationRegistryStressTest() {
     for (int i = 0; i < 10_000; i++) {
@@ -20,5 +41,17 @@ public final class ShadowNativeAllocationRegistryTest {
         System.gc();
       }
     }
+  }
+
+  @ForType(Matrix.class)
+  interface MatrixReflector {
+    @Accessor("native_instance")
+    long getNativeInstance();
+
+    @Accessor("native_instance")
+    void setNativeInstance(long value);
+
+    @Static
+    long nGetNativeFinalizer();
   }
 }
