6c0f0f2d03499b4dba1a17f824f19e892c1d32e9
utzcoz <utzcoz@outlook.com>
2024-02-14 17:44:59 +0800

    Fix UnrecognisedJavadocTag for Implements
    Signed-off-by: utzcoz <utzcoz@outlook.com>

--- annotations/src/main/java/org/robolectric/annotation/Implements.java
+++ annotations/src/main/java/org/robolectric/annotation/Implements.java
@@ -71,7 +71,7 @@ public @interface Implements {
    * If set to true, Robolectric will invoke the native method variant instead of the no-op variant.
    * This requires the native method to be bound, or an {@link UnsatisfiedLinkError} will occur.
    *
-   * <p>{@link Implements#callNativeMethodsByDefault()} has precedence over {@link *
+   * <p>{@link Implements#callNativeMethodsByDefault()} has precedence over {@link
    * Implements#callThroughByDefault()} For instance, if both {@link
    * Implements#callNativeMethodsByDefault()} and {@link Implements#callThroughByDefault()} are
    * true, the native method variant will be preferred over the no-op native variant.
