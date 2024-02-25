0f1b3085f86466ad845bd84ee86f7a7335fcfde3
Devashishbasu <devashishbasu587@gmail.com>
2024-02-26 00:02:53 +0530

    Added code to stop gl thread
     terminate the thread in callOnDetachFromWindow
    
    Added code to stop gl thread
    
    I added fields to hold the renderer and the GL thread.
    
    In onPause(), I interrupt the GL thread if it's not null.
    
    In onResume(), I start a new GL thread only if the renderer is not null.
    
    I added a setRenderer() method to set the renderer externally.
    
    I introduced a reset() method, annotated with @Resetter, which you can use to clean up resources and terminate the GL thread if needed after each test.
    
     terminate the thread in callOnDetachFromWindow
    
    Update ShadowGLSurfaceView.java

--- shadows/framework/src/main/java/org/robolectric/shadows/ShadowGLSurfaceView.java
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowGLSurfaceView.java
@@ -7,9 +7,21 @@ import org.robolectric.annotation.Implements;
 /** Fake implementation of GLSurfaceView */
 @Implements(GLSurfaceView.class)
 public class ShadowGLSurfaceView extends ShadowSurfaceView {
+  private Thread glThread;
+
   @Implementation
   protected void onPause() {}
 
   @Implementation
   protected void onResume() {}
+
+  @Override
+  public void callOnDetachedFromWindow() {
+    // Terminate the GL thread if it's still running
+    if (glThread != null && glThread.isAlive()) {
+      glThread.interrupt();
+      glThread = null;
+    }
+    super.callOnDetachedFromWindow();
+  }
 }
