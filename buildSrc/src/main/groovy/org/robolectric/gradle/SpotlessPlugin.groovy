1305339878a84751399bbb2dbffd7feb17deacbc
Devashishbasu <devashishbasu587@gmail.com>
2024-03-10 23:42:25 +0530

    Extract Spotless configuration into a single plugin
    This commit extracts the Spotless configuration to a single plugin, utilizing this plugin for the project's Spotless configuration. Instead of dispersing Spotless configuration across different submodules, this change applies the plugin only to the top-level build.gradle. The plugin's target configuration now includes all Kotlin files, addressing an issue where ctesque was not guarded by Spotless previously.
    
    Previously, ctesque lacked Spotless formatting, leading to failures in the latest CI run due to Kotlin code within. By consolidating Spotless configuration into the top-level build file and applying it universally, Spotless now checks Kotlin code in the ctesque module as well. This change ensures consistent formatting throughout the project.

---
+++ buildSrc/src/main/groovy/org/robolectric/gradle/SpotlessPlugin.groovy
@@ -0,0 +1,27 @@
+package org.robolectric.gradle
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class SpotlessPlugin implements Plugin<Project> {
+    void apply(Project project) {
+        project.getPlugins().apply('com.diffplug.spotless')
+
+        project.spotless {
+            kotlin {
+                // Add configurations for Kotlin files
+                target '**/*.kt'
+                ktfmt('0.42').googleStyle()
+            }
+            groovy {
+                // Add configurations for Groovy files
+                target("**/*.groovy")
+            }
+            groovyGradle {
+                // Add configurations for Groovy Gradle files
+                target('*.gradle', "**/*.gradle")
+            }
+
+        }
+    }
+}
