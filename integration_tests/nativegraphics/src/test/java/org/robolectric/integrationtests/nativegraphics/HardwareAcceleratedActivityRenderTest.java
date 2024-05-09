1f3054206b3bf71e4dbb80b0b84df83df50be995
Julia Sullivan <juliansull@google.com>
2024-05-09 13:40:01 -0700

    Set FLAG_HARDWARE_ACCELERATED  in ShadowPackageManger.addActivityIfNotPresent
    Previously, only Activities that were present in an AndroidManifest.xml would
    contain the FLAG_HARDWARE_ACCELERATED window flag, which gets
    populated by PackageParser. This means that Activities not
    present in a manifest could not be rendered with a HardwareRenderer, as it
    would get ignored due to the missing flag. This meant that the HW rendering
    version of PixelCopy would not operate on that Activity.
    
    The FLAG_HARDWARE_ACCELERATED was true by default since
    Android ICS (SDK 14), so it should be true by default in Robolectric
    as well.
    
    Fixes #8982
    
    PiperOrigin-RevId: 632255888

--- integration_tests/nativegraphics/src/test/java/org/robolectric/integrationtests/nativegraphics/HardwareAcceleratedActivityRenderTest.java
+++ integration_tests/nativegraphics/src/test/java/org/robolectric/integrationtests/nativegraphics/HardwareAcceleratedActivityRenderTest.java
@@ -13,7 +13,6 @@ import android.view.PixelCopy;
 import android.view.View;
 import android.view.ViewGroup.LayoutParams;
 import android.view.Window;
-import android.view.WindowManager;
 import android.widget.FrameLayout;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
@@ -57,12 +56,6 @@ public class HardwareAcceleratedActivityRenderTest {
     @Override
     public void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
-      // TODO(hoisie): manually setting these flags should not be required. Robolectric should
-      // set them automatically by default (they have been default since ICS).
-      getWindow()
-          .setFlags(
-              WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED,
-              WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED);
       FrameLayout frameLayout = new FrameLayout(this);
       frameLayout.setLayoutParams(
           new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
