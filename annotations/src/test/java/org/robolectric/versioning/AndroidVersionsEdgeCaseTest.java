f5ad890603f59fe74a5c7d6cdcaea7a81324bc5f
Googler <noreply@google.com>
2024-05-25 13:14:02 -0700

    Don't throw exceptions in AndroidVersions when running in annotation processors.
    Android platform development has build configurations report being different android (pre-)releases.
    To prevent breaking the build, report all AndroidVersions errors as warnings in annotation-processors.
    
    PiperOrigin-RevId: 637260391

--- annotations/src/test/java/org/robolectric/versioning/AndroidVersionsEdgeCaseTest.java
+++ annotations/src/test/java/org/robolectric/versioning/AndroidVersionsEdgeCaseTest.java
@@ -2,6 +2,7 @@ package org.robolectric.versioning;
 
 import static com.google.common.truth.Truth.assertThat;
 
+import java.lang.reflect.Field;
 import java.util.Arrays;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,6 +14,16 @@ import org.robolectric.versioning.AndroidVersions.SdkInformation;
 @RunWith(JUnit4.class)
 public final class AndroidVersionsEdgeCaseTest {
 
+  private void forceWarningMode(boolean warnMode) {
+    try {
+      Field field = AndroidVersions.class.getDeclaredField("warnOnly");
+      field.setAccessible(true);
+      field.set(null, warnMode);
+    } catch (NoSuchFieldException | IllegalAccessException ex) {
+      throw new RuntimeException("Could not update warnOnly field", ex);
+    }
+  }
+
   /**
    * sdkInt higher than any known release, claims it's released. Expects an error message to update
    * to update the AndroidVersions.class
@@ -21,6 +32,7 @@ public final class AndroidVersionsEdgeCaseTest {
   public void sdkIntHigherThanKnownReleasesClaimsIsReleased_throwsException() {
     AndroidRelease earliestUnrelease = null;
     try {
+      forceWarningMode(false);
       SdkInformation information = AndroidVersions.gatherStaticSdkInformationFromThisClass();
       earliestUnrelease = information.earliestUnreleased;
       information.computeCurrentSdk(
@@ -49,6 +61,7 @@ public final class AndroidVersionsEdgeCaseTest {
   public void sdkIntReleasedButStillReportsCodeName_throwsException() {
     AndroidRelease latestRelease = null;
     try {
+      forceWarningMode(false);
       SdkInformation information = AndroidVersions.gatherStaticSdkInformationFromThisClass();
       latestRelease = information.latestRelease;
       information.computeCurrentSdk(
@@ -66,6 +79,37 @@ public final class AndroidVersionsEdgeCaseTest {
                   + " has been been marked as released. Please update the contents of current sdk"
                   + " jar to the released version.");
       assertThat(e).isInstanceOf(RuntimeException.class);
+    }
+  }
+
+  @Test
+  public void sdkIntReleasedButStillReportsCodeName_warningMode() {
+    AndroidRelease latestRelease = null;
+    try {
+      forceWarningMode(true);
+      SdkInformation information = AndroidVersions.gatherStaticSdkInformationFromThisClass();
+      latestRelease = information.latestRelease;
+      information.computeCurrentSdk(
+          latestRelease.getSdkInt(),
+          null,
+          information.latestRelease.getShortCode(),
+          Arrays.asList(latestRelease.getShortCode()));
+    } catch (Throwable t) {
+      assertThat(t).isNull();
+    }
+  }
+
+  @Test
+  public void unknownSdkInt_warningMode() {
+    try {
+      forceWarningMode(true);
+      SdkInformation information = AndroidVersions.gatherStaticSdkInformationFromThisClass();
+      AndroidRelease found =
+          information.computeCurrentSdk(
+              35, "zzzz", "Z", Arrays.asList("wwww", "xxxx", "yyyy", "zzzz"));
+      assertThat(found.getSdkInt()).isEqualTo(10000);
+    } catch (Throwable t) {
+      assertThat(t).isNull();
     }
   }
 }
