f85e9f1e1dc4b0918005a7c4c17faea3ff5a6b78
Brett Chabot <brettchabot@google.com>
2024-05-28 16:35:44 -0700

    Support static references to Choreographer.getInstance.
    Previously Robolectric just cleared the thread local Choreographer.getInstance
    between each test. This would cause issues with any code paths that
    kept static references to Choreographer.getInstance (like Compose's
    AndroidUiDispatcher), because those code paths would use stale Choreographer
    instances with inconsistent views of the current system time.
    
    This commit changes the Choreographer resetter to just reset the local
    state of each active Choreographer.
    
    Fixes #9043
    
    PiperOrigin-RevId: 638065889

--- shadows/framework/src/main/java/org/robolectric/shadows/ShadowPausedChoreographer.java
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowPausedChoreographer.java
@@ -2,20 +2,28 @@ package org.robolectric.shadows;
 
 import static org.robolectric.util.reflector.Reflector.reflector;
 
+import android.graphics.FrameInfo;
+import android.os.Build.VERSION_CODES;
 import android.os.Looper;
 import android.view.Choreographer;
 import android.view.DisplayEventReceiver;
 import com.android.internal.annotations.VisibleForTesting;
+import java.util.Arrays;
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
+import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.Implementation;
 import org.robolectric.annotation.Implements;
 import org.robolectric.annotation.LooperMode;
 import org.robolectric.annotation.RealObject;
 import org.robolectric.shadow.api.Shadow;
 import org.robolectric.shadows.ShadowDisplayEventReceiver.DisplayEventReceiverReflector;
+import org.robolectric.util.ReflectionHelpers;
+import org.robolectric.util.ReflectionHelpers.ClassParameter;
 import org.robolectric.versioning.AndroidVersions.NMR1;
 import org.robolectric.versioning.AndroidVersions.O;
+import org.robolectric.versioning.AndroidVersions.Q;
+import org.robolectric.versioning.AndroidVersions.S;
 import org.robolectric.versioning.AndroidVersions.T;
 import org.robolectric.versioning.AndroidVersions.U;
 
@@ -33,51 +41,82 @@ import org.robolectric.versioning.AndroidVersions.U;
     isInAndroidSdk = false)
 public class ShadowPausedChoreographer extends ShadowChoreographer {
 
-  // keep track of all Loopers with active Choreographer so they can be selectively reset
-  private static final Set<Looper> choreographedLoopers = new CopyOnWriteArraySet<>();
+  // keep track of all active Choreographers so they can be selectively reset
+  private static final Set<Choreographer> activeChoreographers = new CopyOnWriteArraySet<>();
 
   @RealObject private Choreographer realChoreographer;
 
   @Implementation(maxSdk = NMR1.SDK_INT)
   protected void __constructor__(Looper looper) {
     reflector(ChoreographerReflector.class, realChoreographer).__constructor__(looper);
-    choreographedLoopers.add(looper);
+    activeChoreographers.add(realChoreographer);
   }
 
   @Implementation(minSdk = O.SDK_INT, maxSdk = T.SDK_INT)
   protected void __constructor__(Looper looper, int vsyncSource) {
     reflector(ChoreographerReflector.class, realChoreographer).__constructor__(looper, vsyncSource);
-    choreographedLoopers.add(looper);
+    activeChoreographers.add(realChoreographer);
   }
 
   @Implementation(minSdk = U.SDK_INT)
   protected void __constructor__(Looper looper, int vsyncSource, long layerHandle) {
     reflector(ChoreographerReflector.class, realChoreographer)
         .__constructor__(looper, vsyncSource, layerHandle);
-    choreographedLoopers.add(looper);
+    activeChoreographers.add(realChoreographer);
+  }
+
+  @Implementation(minSdk = VERSION_CODES.N)
+  protected void dispose() {
+    activeChoreographers.remove(realChoreographer);
   }
 
   /**
-   * Resets the choreographer ThreadLocal instance for the given Looper
+   * Resets the Choreographer state
    *
-   * @param looper an active looper whose queue has already been reset
+   * <p>Called from ShadowPausedLooper reset to ensure this occurs before Loopers are reset.
    */
-  static void reset(Looper looper) {
-    if (choreographedLoopers.remove(looper)) {
+  static void resetChoreographers() {
+    for (Choreographer choreographer : activeChoreographers) {
+      Looper looper = reflector(ChoreographerReflector.class, choreographer).getLooper();
+      ShadowPausedChoreographer shadowChoreographer = Shadow.extract(choreographer);
       if (looper.getThread() == Thread.currentThread()) {
-        reflector(ChoreographerReflector.class).getThreadInstance().remove();
+        shadowChoreographer.resetState();
       } else if (looper.getThread().isAlive()) {
         ShadowPausedLooper shadowLooper = Shadow.extract(looper);
-        shadowLooper.postSyncQuiet(
-            () -> reflector(ChoreographerReflector.class).getThreadInstance().remove());
+        shadowLooper.postSyncQuiet(shadowChoreographer::resetState);
       }
     }
   }
 
-  // safeguard that clears the list of choreographed Loopers. Intended to clean up references
-  // to Loopers that are no longer running
-  static void clearLoopers() {
-    choreographedLoopers.clear();
+  private void resetState() {
+    ChoreographerReflector choreographerReflector =
+        reflector(ChoreographerReflector.class, realChoreographer);
+    choreographerReflector.setLastFrameTimeNanos(Long.MIN_VALUE);
+    if (RuntimeEnvironment.getApiLevel() >= S.SDK_INT) {
+      choreographerReflector.setLastFrameIntervalNanos(0);
+    }
+    choreographerReflector.setFrameScheduled(false);
+    Object[] /* CallbackQueue */ callbackQueues = choreographerReflector.getCallbackQueues();
+    for (Object callbackQueue : callbackQueues) {
+      reflector(CallbackQueueReflector.class, callbackQueue).setHead(null);
+    }
+    choreographerReflector.setCallbackPool(null);
+    choreographerReflector.setCallbacksRunning(false);
+    if (RuntimeEnvironment.getApiLevel() >= U.SDK_INT) {
+      ReflectionHelpers.callInstanceMethod(
+          choreographerReflector.getFrameData(),
+          "update",
+          ClassParameter.from(long.class, 0),
+          ClassParameter.from(int.class, 0));
+    }
+
+    if (RuntimeEnvironment.getApiLevel() >= Q.SDK_INT) {
+      Arrays.fill(((FrameInfo) choreographerReflector.getFrameInfo()).frameInfo, 0);
+    }
+    DisplayEventReceiver receiver =
+        reflector(ChoreographerReflector.class, realObject).getReceiver();
+    ShadowDisplayEventReceiver shadowReceiver = Shadow.extract(receiver);
+    shadowReceiver.resetState();
   }
 
   /**
