a689481f2f2c1a2f15a0e57dada7228a6fafe947
Googler <noreply@google.com>
2024-06-24 09:43:48 -0700

    Add methods to ShadowWearableSensingManager for fetching all provided data
    Instead of just exposing methods for the last provided data, store all
    data and expose it via getters.
    
    PiperOrigin-RevId: 646126645

--- robolectric/src/test/java/org/robolectric/shadows/ShadowWearableSensingManagerTest.java
+++ robolectric/src/test/java/org/robolectric/shadows/ShadowWearableSensingManagerTest.java
@@ -3,6 +3,7 @@ package org.robolectric.shadows;
 import static android.os.Build.VERSION_CODES.UPSIDE_DOWN_CAKE;
 import static androidx.test.core.app.ApplicationProvider.getApplicationContext;
 import static com.google.common.truth.Truth.assertThat;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 import android.app.wearable.WearableSensingManager;
@@ -59,16 +60,46 @@ public class ShadowWearableSensingManagerTest {
     ShadowWearableSensingManager shadowWearableSensingManager =
         Shadow.extract(wearableSensingManager);
 
-    PersistableBundle persistableBundle = new PersistableBundle();
-    SharedMemory sharedMemory = SharedMemory.create("name", 100);
+    PersistableBundle persistableBundle1 = new PersistableBundle();
+    PersistableBundle persistableBundle2 = new PersistableBundle();
+    SharedMemory sharedMemory1 = SharedMemory.create("name1", 100);
+    SharedMemory sharedMemory2 = SharedMemory.create("name2", 200);
     shadowWearableSensingManager.setProvideDataResult(WearableSensingManager.STATUS_ACCESS_DENIED);
 
     wearableSensingManager.provideData(
-        persistableBundle, sharedMemory, MoreExecutors.directExecutor(), callback);
+        persistableBundle1, sharedMemory1, MoreExecutors.directExecutor(), callback);
+    wearableSensingManager.provideData(
+        persistableBundle2, sharedMemory2, MoreExecutors.directExecutor(), callback);
 
-    verify(callback).accept(WearableSensingManager.STATUS_ACCESS_DENIED);
+    verify(callback, times(2)).accept(WearableSensingManager.STATUS_ACCESS_DENIED);
     assertThat(shadowWearableSensingManager.getLastDataBundle())
-        .isSameInstanceAs(persistableBundle);
-    assertThat(shadowWearableSensingManager.getLastSharedMemory()).isSameInstanceAs(sharedMemory);
+        .isSameInstanceAs(persistableBundle2);
+    assertThat(shadowWearableSensingManager.getLastSharedMemory()).isSameInstanceAs(sharedMemory2);
+    assertThat(shadowWearableSensingManager.getAllDataBundles())
+        .containsExactly(persistableBundle1, persistableBundle2);
+    assertThat(shadowWearableSensingManager.getAllSharedMemories())
+        .containsExactly(sharedMemory1, sharedMemory2);
+  }
+
+  @Test
+  public void getLastDataBundle_noDataProvided_doesNotThrow() throws Exception {
+    WearableSensingManager wearableSensingManager =
+        (WearableSensingManager)
+            getApplicationContext().getSystemService(Context.WEARABLE_SENSING_SERVICE);
+    ShadowWearableSensingManager shadowWearableSensingManager =
+        Shadow.extract(wearableSensingManager);
+
+    shadowWearableSensingManager.getLastDataBundle();
+  }
+
+  @Test
+  public void getLastSharedMemory_noDataProvided_doesNotThrow() throws Exception {
+    WearableSensingManager wearableSensingManager =
+        (WearableSensingManager)
+            getApplicationContext().getSystemService(Context.WEARABLE_SENSING_SERVICE);
+    ShadowWearableSensingManager shadowWearableSensingManager =
+        Shadow.extract(wearableSensingManager);
+
+    shadowWearableSensingManager.getLastSharedMemory();
   }
 }
