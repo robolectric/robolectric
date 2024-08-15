f754795787d1decc57b4e873e72db2ed54b2b829
Devashishbasu <devashishbasu587@gmail.com>
2024-08-15 23:59:48 +0530

    Make-fields-of-ShadowTimeManager-static-for-context-level-instance
    1.Converted instance fields to static fields to ensure proper state management.
    2.add tests for the same in ShadowTimeManagerTest.

--- robolectric/src/test/java/org/robolectric/shadows/ShadowTimeManagerTest.java
+++ robolectric/src/test/java/org/robolectric/shadows/ShadowTimeManagerTest.java
@@ -2,6 +2,7 @@ package org.robolectric.shadows;
 
 import static com.google.common.truth.Truth.assertThat;
 
+import android.app.Activity;
 import android.app.time.TimeManager;
 import android.app.time.TimeZoneCapabilitiesAndConfig;
 import android.app.time.TimeZoneConfiguration;
@@ -10,6 +11,7 @@ import androidx.test.core.app.ApplicationProvider;
 import org.junit.Assume;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
@@ -55,5 +57,36 @@ public final class ShadowTimeManagerTest {
     capabilitiesAndConfig = timeManager.getTimeZoneCapabilitiesAndConfig();
 
     assertThat(capabilitiesAndConfig.getConfiguration()).isEqualTo(updatedConfiguration);
+  }
+
+  @Test
+  @Config(minSdk = Build.VERSION_CODES.S)
+  public void timeManager_activityContextEnabled_differentInstancesRetrieveTimeZoneCapabilities() {
+    String originalProperty = System.getProperty("robolectric.createActivityContexts", "");
+    System.setProperty("robolectric.createActivityContexts", "true");
+    Activity activity = null;
+    try {
+      TimeManager applicationTimeManager =
+          ApplicationProvider.getApplicationContext().getSystemService(TimeManager.class);
+      activity = Robolectric.setupActivity(Activity.class);
+      TimeManager activityTimeManager = activity.getSystemService(TimeManager.class);
+
+      TimeZoneConfiguration timeZoneConfiguration = new TimeZoneConfiguration.Builder().build();
+      applicationTimeManager.updateTimeZoneConfiguration(timeZoneConfiguration);
+
+      assertThat(applicationTimeManager).isNotSameInstanceAs(activityTimeManager);
+
+      TimeZoneCapabilitiesAndConfig applicationCapabilities =
+          applicationTimeManager.getTimeZoneCapabilitiesAndConfig();
+      TimeZoneCapabilitiesAndConfig activityCapabilities =
+          activityTimeManager.getTimeZoneCapabilitiesAndConfig();
+
+      assertThat(activityCapabilities).isEqualTo(applicationCapabilities);
+    } finally {
+      if (activity != null) {
+        activity.finish();
+      }
+      System.setProperty("robolectric.createActivityContexts", originalProperty);
+    }
   }
 }
