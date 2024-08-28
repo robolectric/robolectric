1e2bd7535de3036b108304e4ab2fad049a37b12f
Gaëtan Muller <m.gaetan89@gmail.com>
2024-08-28 15:43:55 +0200

    Migrate some Gradle files to Kotlin (6)
    This PR migrates the `build.gradle` file to Kotlin in the following packages:
    - `processor`
    - `resources`
    - `robolectric`
    - `sandbox`
    - `shadowapi`
    - `shadows/framework`

---
+++ robolectric/build.gradle.kts
@@ -0,0 +1,74 @@
+plugins {
+  alias(libs.plugins.robolectric.deployed.java.module)
+  alias(libs.plugins.robolectric.java.module)
+}
+
+val axtCoreVersion: String by rootProject.extra
+val axtJunitVersion: String by rootProject.extra
+val axtMonitorVersion: String by rootProject.extra
+val axtRunnerVersion: String by rootProject.extra
+val axtTruthVersion: String by rootProject.extra
+val espressoVersion: String by rootProject.extra
+
+dependencies {
+  annotationProcessor(libs.auto.service)
+  annotationProcessor(libs.error.prone.core)
+
+  api(project(":annotations"))
+  api(project(":junit"))
+  api(project(":pluginapi"))
+  api(project(":resources"))
+  api(project(":sandbox"))
+  api(project(":utils"))
+  api(project(":utils:reflector"))
+  api(project(":plugins:maven-dependency-resolver"))
+  api(libs.javax.inject)
+  compileOnly(libs.auto.service.annotations)
+  api(libs.javax.annotation.api)
+
+  // We need to have shadows-framework.jar on the runtime system classpath so ServiceLoader
+  //   can find its META-INF/services/org.robolectric.shadows.ShadowAdapter.
+  api(project(":shadows:framework"))
+
+  implementation(libs.conscrypt.openjdk.uber)
+  api(libs.bcprov.jdk18on)
+  compileOnly(libs.findbugs.jsr305)
+
+  compileOnly(AndroidSdk.MAX_SDK.coordinates)
+  compileOnly(libs.junit4)
+  compileOnly(libs.androidx.annotation)
+
+  api("androidx.test:monitor:$axtMonitorVersion@aar")
+  implementation("androidx.test.espresso:espresso-idling-resource:$espressoVersion@aar")
+
+  testImplementation(libs.androidx.annotation)
+  testImplementation(libs.junit4)
+  testImplementation(libs.truth)
+  testImplementation(libs.mockito)
+  testImplementation(libs.hamcrest.junit)
+  testImplementation("androidx.test:core:$axtCoreVersion@aar")
+  testImplementation("androidx.test.ext:junit:$axtJunitVersion@aar")
+  testImplementation("androidx.test.ext:truth:$axtTruthVersion@aar")
+  testImplementation("androidx.test:runner:$axtRunnerVersion@aar")
+  testImplementation(libs.guava)
+  testCompileOnly(AndroidSdk.MAX_SDK.coordinates) // compile against latest Android SDK
+  testRuntimeOnly(androidStubsJar())
+}
+
+fun androidStubsJar(): ConfigurableFileCollection {
+  val androidStubsVersion = libs.versions.androidstubs.get()
+  if ("ANDROID_HOME" !in System.getenv()) {
+    throw GradleException("Environment variable ANDROID_HOME is not set.")
+  }
+
+  val androidJarPath =
+    "${System.getenv("ANDROID_HOME")}/platforms/android-$androidStubsVersion/android.jar"
+  val androidJar = file(androidJarPath)
+  if (!androidJar.exists()) {
+    throw GradleException(
+      "android.jar $androidStubsVersion not found at $androidJarPath. Download it through Android SDK Manager, or install the `Android SDK Command-line tools` and run `sdkmanager \"platforms;android-$androidStubsVersion\"`."
+    )
+  }
+
+  return files(androidJarPath)
+}
---
+++ shadows/framework/build.gradle.kts
@@ -0,0 +1,67 @@
+import org.gradle.jvm.tasks.Jar
+
+plugins {
+  alias(libs.plugins.robolectric.deployed.java.module)
+  alias(libs.plugins.robolectric.java.module)
+  alias(libs.plugins.robolectric.shadows)
+}
+
+shadows {
+  packageName = "org.robolectric"
+  sdkCheckMode = "ERROR"
+}
+
+val sqlite4java = configurations.create("sqlite4java")
+val sqlite4javaVersion = libs.versions.sqlite4java.get()
+
+val copySqliteNatives by
+  tasks.registering(Copy::class) {
+    from(sqlite4java) {
+      include("**/*.dll")
+      include("**/*.so")
+      include("**/*.dylib")
+
+      rename { filename ->
+        val filenameMatch = "^([^\\-]+)-(.+)-${sqlite4javaVersion}\\.(.+)".toRegex().find(filename)
+        if (filenameMatch != null) {
+          val platformFilename = filenameMatch.groupValues[1]
+          val platformFolder = filenameMatch.groupValues[2]
+          val platformExtension = filenameMatch.groupValues[3]
+
+          "$platformFolder/$platformFilename.$platformExtension"
+        } else {
+          filename
+        }
+      }
+    }
+    into(project.file(layout.buildDirectory.dir("resources/main/sqlite4java")))
+  }
+
+tasks.named<Jar>("jar") { dependsOn(copySqliteNatives) }
+
+tasks.named<Javadoc>("javadoc") { dependsOn(copySqliteNatives) }
+
+val axtMonitorVersion: String by rootProject.extra
+
+dependencies {
+  api(project(":annotations"))
+  api(project(":nativeruntime"))
+  api(project(":resources"))
+  api(project(":pluginapi"))
+  api(project(":sandbox"))
+  api(project(":shadowapi"))
+  api(project(":utils"))
+  api(project(":utils:reflector"))
+
+  api("androidx.test:monitor:$axtMonitorVersion@aar")
+
+  implementation(libs.error.prone.annotations)
+  compileOnly(libs.findbugs.jsr305)
+  api(libs.sqlite4java)
+  compileOnly(AndroidSdk.MAX_SDK.coordinates)
+  api(libs.icu4j)
+  api(libs.auto.value.annotations)
+  annotationProcessor(libs.auto.value)
+
+  sqlite4java(libs.bundles.sqlite4java.native)
+}
