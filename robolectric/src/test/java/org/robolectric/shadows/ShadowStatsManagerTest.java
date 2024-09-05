30856c30829ca335269c22d33596f6299fcd3ed5
Brett Chabot <brettchabot@google.com>
2024-09-05 11:22:26 -0700

    Add support for StatsManager#query and setRestrictedMetricsChangedOperation.
    These methods were added in UPSIDE_DOWN_CAKE, and in real android perform
    IPCs to system server. This change adds test APIs for setting their
    respective results.
    
    PiperOrigin-RevId: 671443489

--- robolectric/src/test/java/org/robolectric/shadows/ShadowStatsManagerTest.java
+++ robolectric/src/test/java/org/robolectric/shadows/ShadowStatsManagerTest.java
@@ -1,20 +1,40 @@
 package org.robolectric.shadows;
 
+import static android.os.Build.VERSION_CODES.UPSIDE_DOWN_CAKE;
+import static androidx.test.core.app.ApplicationProvider.getApplicationContext;
 import static com.google.common.truth.Truth.assertThat;
+import static org.robolectric.Shadows.shadowOf;
 
+import android.app.PendingIntent;
+import android.app.StatsCursor;
 import android.app.StatsManager;
+import android.app.StatsManager.StatsQueryException;
+import android.app.StatsQuery;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Build;
+import android.os.Looper;
+import android.os.OutcomeReceiver;
+import androidx.annotation.NonNull;
 import androidx.test.core.app.ApplicationProvider;
+import com.google.common.collect.ImmutableList;
+import java.util.concurrent.CountDownLatch;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
+import org.robolectric.shadow.api.Shadow;
 
 /** Tests for {@link ShadowStatsManager} */
 @RunWith(RobolectricTestRunner.class)
 @Config(minSdk = Build.VERSION_CODES.P)
 public final class ShadowStatsManagerTest {
+
+  private long[] metricIds;
+  private static CountDownLatch countDownLatch;
 
   @Test
   public void testGetMetadata() throws Exception {
@@ -76,5 +96,79 @@ public final class ShadowStatsManagerTest {
     ShadowStatsManager.reset();
 
     assertThat(statsManager.getMetadata()).isEqualTo(new byte[] {});
+  }
+
+  @Before
+  public void setUp() {
+    IntentFilter filter = new IntentFilter(RestrictedMetricsChangedReceiver.ACTION);
+    getApplicationContext().registerReceiver(new RestrictedMetricsChangedReceiver(), filter);
+  }
+
+  @Test
+  @Config(minSdk = UPSIDE_DOWN_CAKE)
+  public void statsManagerQuery_throwsExceptionWhenResultOrExceptionNotSet() throws Exception {
+    StatsManager statsManager = getApplicationContext().getSystemService(StatsManager.class);
+    StatsQuery query =
+        new StatsQuery.Builder("sqlQuery")
+            .setSqlDialect(StatsQuery.DIALECT_SQLITE)
+            .setMinSqlClientVersion(0)
+            .build();
+    StubOutcomeReceiver stubOutcomeReceiver = new StubOutcomeReceiver();
+    statsManager.query(123L, "package", query, null, stubOutcomeReceiver);
+    assertThat(stubOutcomeReceiver.error)
+        .hasMessageThat()
+        .isEqualTo("Failed to query statsd: Stats result was not configured.");
+  }
+
+  private static class StubOutcomeReceiver
+      implements OutcomeReceiver<StatsCursor, StatsQueryException> {
+    StatsCursor result;
+    StatsQueryException error;
+
+    @Override
+    public void onResult(StatsCursor result) {
+      this.result = result;
+    }
+
+    @Override
+    public void onError(@NonNull StatsQueryException error) {
+      this.error = error;
+    }
+  }
+
+  @Test
+  @Config(minSdk = UPSIDE_DOWN_CAKE)
+  public void statsManagerAddMetric_invokesPassedPendingIntent() throws Exception {
+    StatsManager statsManager = getApplicationContext().getSystemService(StatsManager.class);
+    PendingIntent pi =
+        PendingIntent.getBroadcast(
+            getApplicationContext(),
+            1,
+            new Intent(RestrictedMetricsChangedReceiver.ACTION),
+            PendingIntent.FLAG_MUTABLE | PendingIntent.FLAG_UPDATE_CURRENT);
+    ShadowStatsManager shadowStatsManager = Shadow.extract(statsManager);
+    shadowStatsManager.addMetricIds(ImmutableList.of(123L, 12345L), getApplicationContext());
+
+    countDownLatch = new CountDownLatch(1);
+
+    metricIds = statsManager.setRestrictedMetricsChangedOperation(123, "package", pi);
+    assertThat(metricIds).asList().containsExactly(123L, 12345L);
+
+    shadowStatsManager.addMetricIds(ImmutableList.of(123456L), getApplicationContext());
+    shadowOf(Looper.getMainLooper()).idle();
+    countDownLatch.await();
+
+    assertThat(metricIds).asList().containsExactly(123L, 12345L, 123456L);
+  }
+
+  public final class RestrictedMetricsChangedReceiver extends BroadcastReceiver {
+    public static final String ACTION =
+        "org.robolectric.shadows.RestrictedMetricsChangedReceiver.ACTION";
+
+    @Override
+    public void onReceive(Context mContext, Intent intent) {
+      metricIds = intent.getLongArrayExtra(StatsManager.EXTRA_STATS_RESTRICTED_METRIC_IDS);
+      countDownLatch.countDown();
+    }
   }
 }
--- shadows/framework/src/main/java/org/robolectric/shadows/ShadowStatsManager.java
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowStatsManager.java
@@ -1,25 +1,52 @@
 package org.robolectric.shadows;
 
 import static android.os.Build.VERSION_CODES.P;
+import static android.os.Build.VERSION_CODES.UPSIDE_DOWN_CAKE;
+import static org.robolectric.util.reflector.Reflector.reflector;
 
+import android.app.PendingIntent;
+import android.app.StatsCursor;
 import android.app.StatsManager;
+import android.app.StatsManager.StatsQueryException;
+import android.content.Context;
+import android.content.Intent;
+import android.os.IStatsManagerService;
+import android.os.OutcomeReceiver;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import org.robolectric.annotation.Implementation;
 import org.robolectric.annotation.Implements;
 import org.robolectric.annotation.Resetter;
+import org.robolectric.util.ReflectionHelpers;
+import org.robolectric.util.reflector.Constructor;
+import org.robolectric.util.reflector.ForType;
 
 /** Shadow for {@link ShadowStatsManager} */
-@Implements(value = StatsManager.class, isInAndroidSdk = false, minSdk = P)
+@Implements(value = StatsManager.class, isInAndroidSdk = false, minSdk = P, looseSignatures = true)
 public class ShadowStatsManager {
 
   private static final Map<Long, byte[]> dataMap = new HashMap<>();
   private static byte[] statsMetadata = new byte[] {};
 
+  // StatsQueryException and StatsCursor only exists on U+
+  private static Object /* StatsCursor */ cursor;
+  private static Throwable /* StatsQueryException */ queryException;
+  private static List<Long> metricIds;
+
+  private PendingIntent pendingIntent;
+
   @Resetter
   public static void reset() {
     dataMap.clear();
     statsMetadata = new byte[] {};
+    if (cursor != null) {
+      ((StatsCursor) cursor).close();
+      cursor = null;
+    }
+    queryException = null;
+    metricIds = new ArrayList<>();
   }
 
   public static void addReportData(long configKey, byte[] data) {
@@ -40,5 +67,98 @@ public class ShadowStatsManager {
   @Implementation
   protected byte[] getStatsMetadata() {
     return statsMetadata;
+  }
+
+  /**
+   * Sets the result to be returned from {@link StatsManager#query} .
+   *
+   * @param queryData the data to be returned in the query result.
+   * @param columnNames the column names to be returned in the query result.
+   * @param columnTypes the column types to be returned in the query result.
+   * @param rowCount the number of rows to be returned in the query result.
+   */
+  public void setQueryResult(
+      List<String> queryData, List<String> columnNames, List<Integer> columnTypes, int rowCount) {
+    cursor =
+        new StatsCursor(
+            queryData.toArray(new String[0]),
+            columnNames.toArray(new String[0]),
+            columnTypes.stream().mapToInt(Integer::intValue).toArray(),
+            rowCount);
+    queryException = null;
+  }
+
+  /**
+   * Sets the exception to be thrown when a query is made.
+   *
+   * @param exception the exception to be thrown when a query is made.
+   */
+  public void setQueryException(Throwable /*StatsQueryException*/ exception) {
+    queryException = exception;
+    cursor = null;
+  }
+
+  /**
+   * Adds metrics to be notified via PendingIntent from {@link
+   * StatsManager#setRestrictedMetricsChangedOperation}
+   *
+   * @param metrics the metricId to be added to existing list of metrics.
+   */
+  public void addMetricIds(List<Long> metrics, Context context)
+      throws PendingIntent.CanceledException {
+    metricIds.addAll(metrics);
+    if (pendingIntent != null) {
+      Intent fillIntent =
+          new Intent().putExtra(StatsManager.EXTRA_STATS_RESTRICTED_METRIC_IDS, getMetricIds());
+      pendingIntent.send(context, 0, fillIntent);
+    }
+  }
+
+  @Implementation(minSdk = UPSIDE_DOWN_CAKE)
+  protected void query(
+      Object /* long */ configKey,
+      Object /* String */ configPackage,
+      Object /* StatsQuery */ query,
+      Object /* Executor */ executor,
+      Object /* OutcomeReceiver<StatsCursor, StatsQueryException>*/ outcomeReceiverObj) {
+    // even referencing StatsQueryException inside this method causes ClassNotFoundExceptions
+    // when JVM is running with -Xverify:remote (the default setting)
+    OutcomeReceiver<Object, Throwable> outcomeReceiver =
+        (OutcomeReceiver<Object, Throwable>) outcomeReceiverObj;
+    if (cursor != null) {
+      outcomeReceiver.onResult(cursor);
+    } else if (queryException != null) {
+      outcomeReceiver.onError(queryException);
+    } else {
+      outcomeReceiver.onError(
+          reflector(StatsQueryExceptionReflector.class)
+              .newStatsQueryException("Stats result was not configured."));
+    }
+  }
+
+  @ForType(StatsQueryException.class)
+  interface StatsQueryExceptionReflector {
+    @Constructor
+    Throwable newStatsQueryException(String msg);
+  }
+
+  @Implementation(minSdk = UPSIDE_DOWN_CAKE)
+  @SuppressWarnings("NonApiType")
+  protected long[] setRestrictedMetricsChangedOperation(
+      long configKey, String configPackage, PendingIntent pendingIntent) {
+    this.pendingIntent = pendingIntent;
+    return getMetricIds();
+  }
+
+  @Implementation(minSdk = UPSIDE_DOWN_CAKE)
+  protected IStatsManagerService getIStatsManagerServiceLocked() {
+    return ReflectionHelpers.createNullProxy(IStatsManagerService.class);
+  }
+
+  private long[] getMetricIds() {
+    if (metricIds == null) {
+      metricIds = new ArrayList<>();
+    }
+    return metricIds.stream().mapToLong(Long::longValue).toArray();
   }
 }
