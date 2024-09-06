ed4e4e6daa7d8b8fdd2173cac47bcc6ad530656f
Michael Hoisie <hoisie@google.com>
2024-09-06 15:08:47 -0700

    Improve deprecation message for ShadowRangingResult
    RangingResult is a pure Java class and does not need to be shadowed.
    
    RangingResultBuilder can be used instead.
    
    PiperOrigin-RevId: 671904294

--- shadows/framework/src/main/java/org/robolectric/shadows/ShadowRangingResult.java
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowRangingResult.java
@@ -5,15 +5,21 @@ import android.net.wifi.rtt.RangingResult;
 import android.os.Build.VERSION_CODES;
 import org.robolectric.annotation.Implements;
 
-/** Shadow for {@link android.net.wifi.rtt.RangingResult}. */
-@Implements(value = RangingResult.class, minSdk = VERSION_CODES.P)
+/**
+ * Shadow for {@link android.net.wifi.rtt.RangingResult}.
+ *
+ * @deprecated Use {link RangingResultBuilder} instead.
+ */
 @Deprecated
+@Implements(value = RangingResult.class, minSdk = VERSION_CODES.P)
 public class ShadowRangingResult {
 
   /**
-   * A builder for creating ShadowRangingResults. Status, macaddress, distance [mm] and timestamp
+   * A builder for creating ShadowRangingResults. Status, mac address, distance [mm] and timestamp
    * are all mandatory fields. Additional fields can be specified by setters. Use build() to return
    * the ShadowRangingResult object.
+   *
+   * @deprecated Use {link RangingResultBuilder} instead.
    */
   @Deprecated
   public static class Builder extends RangingResultBuilder {
