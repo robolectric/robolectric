aaab28d796597dbc26355c0f294dad0b7204c57f
utzcoz <utzcoz@outlook.com>
2024-09-07 19:46:22 +0800

    Add GMD group for all targets from API 29
    It can be used by developers to run all APIs'
    integration tests together.
    
    ./gradlew -Pandroid.sdk.channel=3 nexusOneIntegrationTestGroupDebugAndroidTest
    
    Signed-off-by: utzcoz <utzcoz@outlook.com>

--- buildSrc/src/main/java/org/robolectric/gradle/GradleManagedDevicePlugin.kt
+++ buildSrc/src/main/java/org/robolectric/gradle/GradleManagedDevicePlugin.kt
@@ -1,11 +1,10 @@
 package org.robolectric.gradle
 
 import com.android.build.api.dsl.CommonExtension
-import com.android.build.api.dsl.ManagedVirtualDevice
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.kotlin.dsl.create
 import org.gradle.kotlin.dsl.findByType
+import org.gradle.kotlin.dsl.get
 
 class GradleManagedDevicePlugin : Plugin<Project> {
   override fun apply(project: Project) {
@@ -25,19 +24,66 @@ class GradleManagedDevicePlugin : Plugin<Project> {
 
       managedDevices {
         // ./gradlew -Pandroid.sdk.channel=3 nexusOneApi29DebugAndroidTest
-        allDevices.create<ManagedVirtualDevice>("nexusOneApi29") {
+        localDevices.create(NAME_API_29) {
           device = "Nexus One"
           apiLevel = 29
           systemImageSource = "aosp"
         }
 
+        // ./gradlew -Pandroid.sdk.channel=3 nexusOneApi30DebugAndroidTest
+        localDevices.create(NAME_API_30) {
+          device = "Nexus One"
+          apiLevel = 30
+          systemImageSource = "aosp-atd"
+        }
+
+        // ./gradlew -Pandroid.sdk.channel=3 nexusOneApi31DebugAndroidTest
+        localDevices.create(NAME_API_31) {
+          device = "Nexus One"
+          apiLevel = 31
+          systemImageSource = "aosp-atd"
+        }
+
+        // ./gradlew -Pandroid.sdk.channel=3 nexusOneApi32DebugAndroidTest
+        localDevices.create(NAME_API_32) {
+          device = "Nexus One"
+          apiLevel = 32
+          systemImageSource = "aosp-atd"
+        }
+
+        // ./gradlew -Pandroid.sdk.channel=3 nexusOneApi33DebugAndroidTest
+        localDevices.create(NAME_API_33) {
+          device = "Nexus One"
+          apiLevel = 33
+          systemImageSource = "aosp-atd"
+        }
+
         // ./gradlew -Pandroid.sdk.channel=3 nexusOneApi34DebugAndroidTest
-        allDevices.create<ManagedVirtualDevice>("nexusOneApi34") {
+        localDevices.create(NAME_API_34) {
           device = "Nexus One"
           apiLevel = 34
           systemImageSource = "aosp-atd"
         }
-      }
-    }
+
+        // ./gradlew -Pandroid.sdk.channel=3 nexusOneIntegrationTestGroupDebugAndroidTest
+        groups.create("nexusOneIntegrationTestGroup") {
+          targetDevices.add(devices[NAME_API_29])
+          targetDevices.add(devices[NAME_API_30])
+          targetDevices.add(devices[NAME_API_31])
+          targetDevices.add(devices[NAME_API_32])
+          targetDevices.add(devices[NAME_API_33])
+          targetDevices.add(devices[NAME_API_34])
+        }
+      } // managedDevices
+    } // testOptions
+  } // apply
+
+  companion object {
+    const val NAME_API_29 = "nexusOneApi29"
+    const val NAME_API_30 = "nexusOneApi30"
+    const val NAME_API_31 = "nexusOneApi31"
+    const val NAME_API_32 = "nexusOneApi32"
+    const val NAME_API_33 = "nexusOneApi33"
+    const val NAME_API_34 = "nexusOneApi34"
   }
 }
