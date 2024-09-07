1f2dd1c081e153d403750da414aecc717ea90a11
utzcoz <utzcoz@outlook.com>
2024-09-07 16:52:33 +0800

    Make WifiAwareManagerTest safer with necessary manager existing checking
    Signed-off-by: utzcoz <utzcoz@outlook.com>

--- integration_tests/ctesque/src/androidTest/java/android/app/WifiAwareManagerTest.java
+++ integration_tests/ctesque/src/androidTest/java/android/app/WifiAwareManagerTest.java
@@ -1,6 +1,5 @@
 package android.app;
 
-import static androidx.test.core.app.ApplicationProvider.getApplicationContext;
 import static com.google.common.truth.Truth.assertThat;
 import static org.junit.Assume.assumeTrue;
 
@@ -9,8 +8,10 @@ import android.content.Context;
 import android.net.wifi.aware.AwareResources;
 import android.net.wifi.aware.WifiAwareManager;
 import androidx.test.core.app.ActivityScenario;
+import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.GrantPermissionRule;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -18,16 +19,25 @@ import org.robolectric.testapp.TestActivity;
 
 @RunWith(AndroidJUnit4.class)
 public class WifiAwareManagerTest {
+  private final Context application = ApplicationProvider.getApplicationContext();
+  private boolean hasWifiAwareManager = false;
 
   @Rule
   public GrantPermissionRule mRuntimePermissionRule =
       GrantPermissionRule.grant(
           Manifest.permission.ACCESS_WIFI_STATE, Manifest.permission.CHANGE_WIFI_STATE);
 
+  @Before
+  public void setUp() {
+    // WifiAwareManager is not always bound into Android device.
+    hasWifiAwareManager = application.getSystemService(WifiAwareManager.class) != null;
+  }
+
   @Test
   public void wifiAwareManager_applicationInstance_isSameAsActivityInstance() {
+    assumeTrue(hasWifiAwareManager);
     WifiAwareManager applicationWifiAwareManager =
-        (WifiAwareManager) getApplicationContext().getSystemService(Context.WIFI_AWARE_SERVICE);
+        (WifiAwareManager) application.getSystemService(Context.WIFI_AWARE_SERVICE);
     try (ActivityScenario<TestActivity> scenario = ActivityScenario.launch(TestActivity.class)) {
       scenario.onActivity(
           activity -> {
@@ -40,6 +50,7 @@ public class WifiAwareManagerTest {
 
   @Test
   public void wifiAwareManager_activityInstance_isSameAsActivityInstance() {
+    assumeTrue(hasWifiAwareManager);
     try (ActivityScenario<TestActivity> scenario = ActivityScenario.launch(TestActivity.class)) {
       scenario.onActivity(
           activity -> {
@@ -54,10 +65,9 @@ public class WifiAwareManagerTest {
 
   @Test
   public void wifiAwareManager_instance_getAvailableAwareResourcesIsConsistent() {
-    // Skip the test if Wi-Fi Aware is not supported
-    assumeTrue(getApplicationContext().getSystemService(Context.WIFI_AWARE_SERVICE) != null);
+    assumeTrue(hasWifiAwareManager);
     WifiAwareManager applicationWifiAwareManager =
-        (WifiAwareManager) getApplicationContext().getSystemService(Context.WIFI_AWARE_SERVICE);
+        (WifiAwareManager) application.getSystemService(Context.WIFI_AWARE_SERVICE);
 
     try (ActivityScenario<TestActivity> scenario = ActivityScenario.launch(TestActivity.class)) {
       scenario.onActivity(
