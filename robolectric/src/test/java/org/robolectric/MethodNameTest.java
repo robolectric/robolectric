3ff281ff56bd72cc38e9596a2c900dac3c7d8c9b
Michael Hoisie <hoisie@google.com>
2024-09-12 16:11:01 -0700

    Fix edge case when using `@Implementation(methodName=...)`
    Previously, it was possible for a shadow method using `methodName` to be
    invoked for an SDK level that it did not support.  This is because the logic
    that location a methodName shadow method did not check the SDK range. Add this
    check for methodName.

---
+++ robolectric/src/test/java/org/robolectric/MethodNameTest.java
@@ -0,0 +1,35 @@
+package org.robolectric;
+
+import static android.os.Build.VERSION_CODES.LOLLIPOP;
+import static android.os.Build.VERSION_CODES.S;
+
+import android.content.Intent;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.DirectReflectorTest.ShadowClass;
+import org.robolectric.annotation.Config;
+import org.robolectric.annotation.Implementation;
+import org.robolectric.annotation.Implements;
+import org.robolectric.util.reflector.Direct;
+import org.robolectric.util.reflector.Reflector;
+
+/** Tests for @{@link Direct} annotation incorporated inside {@link Reflector}. */
+@RunWith(AndroidJUnit4.class)
+@Config(shadows = ShadowClass.class)
+public class MethodNameTest {
+  @Config(sdk = S, shadows = ShadowThrowingIntent.class)
+  @Test
+  public void methodName_shouldNotInvokeOlderSdks() {
+    Intent intent = new Intent();
+    intent.setAction("test"); // should not crash
+  }
+
+  @Implements(Intent.class)
+  public static class ShadowThrowingIntent {
+    @Implementation(minSdk = LOLLIPOP, maxSdk = LOLLIPOP, methodName = "setAction")
+    protected void setActionImpl(String action) {
+      throw new RuntimeException("Should never get called");
+    }
+  }
+}
