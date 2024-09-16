2b35bdd7a21ef48d9bc05cdc0a6c6b4221398411
utzcoz <utzcoz@outlook.com>
2024-09-16 18:25:15 +0800

    Extract robolectric.createActivityContexts enable logic to a util method
    This CL also uses this util method to refactor existing tests
    of Context instances to reduce duplicatd template code.
    
    Signed-off-by: utzcoz <utzcoz@outlook.com>

--- robolectric/src/test/java/org/robolectric/util/TestUtil.java
+++ robolectric/src/test/java/org/robolectric/util/TestUtil.java
@@ -1,5 +1,6 @@
 package org.robolectric.util;
 
+import android.app.Activity;
 import com.google.common.io.CharStreams;
 import java.io.File;
 import java.io.IOException;
@@ -8,6 +9,8 @@ import java.io.InputStreamReader;
 import java.nio.file.Path;
 import java.util.Properties;
 import org.robolectric.R;
+import org.robolectric.Robolectric;
+import org.robolectric.android.controller.ActivityController;
 import org.robolectric.pluginapi.Sdk;
 import org.robolectric.plugins.SdkCollection;
 import org.robolectric.res.Fs;
@@ -21,6 +24,26 @@ public abstract class TestUtil {
   private static SdkCollection sdkCollection;
   private static final Injector injector =
       new Injector.Builder().bind(Properties.class, System.getProperties()).build();
+  private static final String PROPERTY_NAME_CREATE_ACTIVITY_CONTEXTS =
+      "robolectric.createActivityContexts";
+
+  /**
+   * The utility method to enable robolectric.createActivityContexts and run specific test logic.
+   *
+   * <p>It will reset the robolectric.createActivityContexts to origin value after method finished,
+   * and it prepares an Activity instance with default Activity for test logic.
+   *
+   * @param runnable The runnable that accepts an Activity instance and run some logics.
+   */
+  public static void runTestWithCreateActivityContextsEnabled(Consumer<Activity> runnable) {
+    String originalProperty = System.getProperty(PROPERTY_NAME_CREATE_ACTIVITY_CONTEXTS, "");
+    System.setProperty(PROPERTY_NAME_CREATE_ACTIVITY_CONTEXTS, "true");
+    try (ActivityController<Activity> controller = Robolectric.buildActivity(Activity.class)) {
+      runnable.accept(controller.setup().get());
+    } finally {
+      System.setProperty(PROPERTY_NAME_CREATE_ACTIVITY_CONTEXTS, originalProperty);
+    }
+  }
 
   public static Path resourcesBaseDir() {
     return resourcesBaseDirFile().toPath();
