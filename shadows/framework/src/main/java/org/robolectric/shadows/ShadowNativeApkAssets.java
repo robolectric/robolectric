d6a61226a706c743056bb4e6d952610cde8f6ff3
Brett Chabot <brettchabot@google.com>
2024-10-03 12:12:42 -0700

    Add caching to native apk assets.
    Previously when using native resource mode
    native apk assets would be loaded for each individual test,
    which added approx 100ms to the runtime of each test in
    my local testing.
    
    This change caches the native apk assets per sandbox,
    ensuring the cost to load a native asset only occurs once
    per sandbox.
    
    PiperOrigin-RevId: 681978567

--- shadows/framework/src/main/java/org/robolectric/shadows/ShadowNativeApkAssets.java
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowNativeApkAssets.java
@@ -5,6 +5,8 @@ import static org.robolectric.shadows.ShadowArscApkAssets9.FRAMEWORK_APK_PATH;
 import android.content.res.ApkAssets;
 import android.content.res.loader.AssetsProvider;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.Implementation;
 import org.robolectric.annotation.Implements;
@@ -20,18 +22,37 @@ import org.robolectric.versioning.AndroidVersions.V;
     shadowPicker = ShadowApkAssets.Picker.class,
     isInAndroidSdk = false)
 public class ShadowNativeApkAssets extends ShadowApkAssets {
+
+  // cached native apk assets.
+  // native resources currently cannot use the cross-sandbox ApkAssetsCache cache
+  // because each sandbox currently loads its own copy of the native runtime
+  private static final Map<String, Long> cachedApkAssetsPtrs = new HashMap<>();
+
   @Implementation
   protected static long nativeLoad(int format, String path, int flags, AssetsProvider asset)
       throws IOException {
     if (path.equals(FRAMEWORK_APK_PATH)) {
       path = RuntimeEnvironment.getAndroidFrameworkJarPath().toString();
     }
-    return ReflectionHelpers.callStaticMethod(
-        ApkAssets.class,
-        Shadow.directNativeMethodName(ApkAssets.class.getName(), "nativeLoad"),
-        ClassParameter.from(int.class, format),
-        ClassParameter.from(String.class, path),
-        ClassParameter.from(int.class, flags),
-        ClassParameter.from(AssetsProvider.class, asset));
+    if (cachedApkAssetsPtrs.containsKey(path)) {
+      return cachedApkAssetsPtrs.get(path);
+    }
+    long ptr =
+        ReflectionHelpers.callStaticMethod(
+            ApkAssets.class,
+            Shadow.directNativeMethodName(ApkAssets.class.getName(), "nativeLoad"),
+            ClassParameter.from(int.class, format),
+            ClassParameter.from(String.class, path),
+            ClassParameter.from(int.class, flags),
+            ClassParameter.from(AssetsProvider.class, asset));
+    if (ptr > 0) {
+      cachedApkAssetsPtrs.put(path, ptr);
+    }
+    return ptr;
+  }
+
+  @Implementation
+  protected static void nativeDestroy(long ptr) {
+    // ignoring nativeDestroy in order to cache assets across tests
   }
 }
