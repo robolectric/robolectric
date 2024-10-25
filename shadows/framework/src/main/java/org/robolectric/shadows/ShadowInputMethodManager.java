afc51eb59f4767597ed9ec1d7211cba79e44e9da
Googler <noreply@google.com>
2024-10-25 09:07:22 -0700

    Add @InDevelopment annotation to ShadowInputMethodManager#hideSoftInputFromWindow
    Unblocks aosp work.
    
    PiperOrigin-RevId: 689808629

--- shadows/framework/src/main/java/org/robolectric/shadows/ShadowInputMethodManager.java
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowInputMethodManager.java
@@ -26,6 +26,7 @@ import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.ClassName;
 import org.robolectric.annotation.Implementation;
 import org.robolectric.annotation.Implements;
+import org.robolectric.annotation.InDevelopment;
 import org.robolectric.annotation.Resetter;
 import org.robolectric.util.reflector.Accessor;
 import org.robolectric.util.reflector.Direct;
@@ -100,6 +101,7 @@ public class ShadowInputMethodManager {
   }
 
   @Implementation(minSdk = Baklava.SDK_INT)
+  @InDevelopment
   protected boolean hideSoftInputFromWindow(
       IBinder windowToken,
       int flags,
