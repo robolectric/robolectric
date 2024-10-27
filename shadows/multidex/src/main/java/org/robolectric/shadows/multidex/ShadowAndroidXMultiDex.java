5509f43533505d3e0b18cc410797d1fc92bd6b56
Gaëtan Muller <m.gaetan89@gmail.com>
2024-10-27 10:37:41 +0100

    Deprecate `shadows-multidex`
    Robolectric's min SDK version has been set to 21 since version 4.13.
    Using Multidex is therefore no longer necessary.
    This PR deprecates the `shadows-multidex` module,
    so it can be removed in Robolectric 4.15.

--- shadows/multidex/src/main/java/org/robolectric/shadows/multidex/ShadowAndroidXMultiDex.java
+++ shadows/multidex/src/main/java/org/robolectric/shadows/multidex/ShadowAndroidXMultiDex.java
@@ -4,6 +4,11 @@ import android.content.Context;
 import org.robolectric.annotation.Implementation;
 import org.robolectric.annotation.Implements;
 
+/**
+ * @deprecated With Robolectric's min SDK version being 21, using Multidex libraries is no longer
+ *     necessary. This class will be removed in Robolectric 4.15.
+ */
+@Deprecated
 @Implements(className = "androidx.multidex.MultiDex")
 @SuppressWarnings("robolectric.internal.IgnoreMissingClass")
 public class ShadowAndroidXMultiDex {
--- shadows/multidex/src/main/java/org/robolectric/shadows/multidex/package-info.java
+++ shadows/multidex/src/main/java/org/robolectric/shadows/multidex/package-info.java
@@ -3,5 +3,9 @@
  *
  * <p>To use this in your project, add the artifact {@code org.robolectric:shadows-multidex} to your
  * project.
+ *
+ * @deprecated With Robolectric's min SDK version being 21, using Multidex libraries is no longer
+ *     necessary. This module will be removed in Robolectric 4.15.
  */
+@Deprecated
 package org.robolectric.shadows.multidex;
