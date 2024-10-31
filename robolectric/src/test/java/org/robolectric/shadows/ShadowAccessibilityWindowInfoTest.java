39392f86c7e9564a1dfa7dbc0e8147aa4705d53a
Michael Hoisie <hoisie@google.com>
2024-10-31 15:36:05 -0700

    Remove unnecessary shadow methods in ShadowAccessibilityWindowInfo
    The underlying methods in AccessibilityWindowInfo are pure Java code
    and do not need to be shadowed.
    
    PiperOrigin-RevId: 691956962

--- robolectric/src/test/java/org/robolectric/shadows/ShadowAccessibilityWindowInfoTest.java
+++ robolectric/src/test/java/org/robolectric/shadows/ShadowAccessibilityWindowInfoTest.java
@@ -1,5 +1,7 @@
 package org.robolectric.shadows;
 
+import static android.os.Build.VERSION_CODES.N;
+import static android.os.Build.VERSION_CODES.O;
 import static com.google.common.truth.Truth.assertThat;
 import static org.robolectric.Shadows.shadowOf;
 
@@ -9,16 +11,18 @@ import androidx.test.ext.junit.runners.AndroidJUnit4;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.robolectric.annotation.Config;
 
 @RunWith(AndroidJUnit4.class)
 public class ShadowAccessibilityWindowInfoTest {
+  private AccessibilityWindowInfo window;
   private ShadowAccessibilityWindowInfo shadow;
 
   @Before
   public void setUp() {
     ShadowAccessibilityWindowInfo.resetObtainedInstances();
     assertThat(ShadowAccessibilityWindowInfo.areThereUnrecycledWindows(true)).isEqualTo(false);
-    AccessibilityWindowInfo window = ShadowAccessibilityWindowInfo.obtain();
+    window = ShadowAccessibilityWindowInfo.obtain();
     assertThat(window).isNotNull();
     shadow = shadowOf(window);
   }
@@ -42,12 +46,13 @@ public class ShadowAccessibilityWindowInfoTest {
     assertThat(shadow.getAnchor()).isEqualTo(node);
   }
 
+  @Config(minSdk = N)
   @Test
   public void testSetTitle() {
-    assertThat(shadow.getTitle()).isNull();
+    assertThat(window.getTitle()).isNull();
     CharSequence title = "Title";
-    shadow.setTitle(title);
-    assertThat(shadow.getTitle().toString()).isEqualTo(title.toString());
+    window.setTitle(title);
+    assertThat(window.getTitle().toString()).isEqualTo(title.toString());
   }
 
   @Test
@@ -57,10 +62,11 @@ public class ShadowAccessibilityWindowInfoTest {
     assertThat(shadow.getChild(0)).isEqualTo(window);
   }
 
+  @Config(minSdk = O)
   @Test
   public void testSetPictureInPicture() {
-    assertThat(shadow.isInPictureInPictureMode()).isFalse();
-    shadow.setPictureInPicture(true);
-    assertThat(shadow.isInPictureInPictureMode()).isTrue();
+    assertThat(window.isInPictureInPictureMode()).isFalse();
+    window.setPictureInPicture(true);
+    assertThat(window.isInPictureInPictureMode()).isTrue();
   }
 }
