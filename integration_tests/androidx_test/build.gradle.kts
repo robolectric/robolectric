a9a36f2ed3daa14841292968e1e9c52cb1c302ab
Gaëtan Muller <m.gaetan89@gmail.com>
2024-11-07 11:16:34 +0100

    Bump `compileSdk` to 35
    AndroidX Core 1.15.0 requires compile SDK to be 35 or newer

--- integration_tests/androidx_test/build.gradle.kts
+++ integration_tests/androidx_test/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-  compileSdk = 34
+  compileSdk = 35
   namespace = "org.robolectric.integration.axt"
 
   defaultConfig {
--- integration_tests/room/build.gradle.kts
+++ integration_tests/room/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 android {
-  compileSdk = 34
+  compileSdk = 35
   namespace = "org.robolectric.integrationtests.room"
 
   defaultConfig { minSdk = 21 }
