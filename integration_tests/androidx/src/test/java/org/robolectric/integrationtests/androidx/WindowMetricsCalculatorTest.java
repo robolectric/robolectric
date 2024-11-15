8274bdb3f6cac323684d155f227dded6ea62d4e6
Gaëtan Muller <m.gaetan89@gmail.com>
2024-11-15 14:31:03 +0100

    Cleanup `integration_tests/androidx` module
    This change includes:
    - Remove unused dependencies from `integration_tests/androidx`.
    - Fix warning in `integration_tests/androidx/src/main/AndroidManifest.xml`.
    - Fix warning in `WindowMetricsCalculatorTest`.

--- integration_tests/androidx/src/test/java/org/robolectric/integrationtests/androidx/WindowMetricsCalculatorTest.java
+++ integration_tests/androidx/src/test/java/org/robolectric/integrationtests/androidx/WindowMetricsCalculatorTest.java
@@ -2,12 +2,13 @@ package org.robolectric.integrationtests.androidx;
 
 import static com.google.common.truth.Truth.assertThat;
 
+import android.graphics.Rect;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.window.layout.WindowMetrics;
 import androidx.window.layout.WindowMetricsCalculator;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
+import org.robolectric.android.controller.ActivityController;
 import org.robolectric.annotation.Config;
 import org.robolectric.testapp.TestActivity;
 
@@ -17,11 +18,14 @@ public class WindowMetricsCalculatorTest {
   @Test
   @Config(qualifiers = "w400dp-h600dp")
   public void computeCurrentWindowMetrics() {
-    TestActivity activity = Robolectric.buildActivity(TestActivity.class).setup().get();
-    WindowMetrics windowMetrics =
-        WindowMetricsCalculator.getOrCreate().computeCurrentWindowMetrics(activity);
+    try (ActivityController<TestActivity> activityController =
+        Robolectric.buildActivity(TestActivity.class)) {
+      TestActivity activity = activityController.setup().get();
+      Rect bounds =
+          WindowMetricsCalculator.getOrCreate().computeCurrentWindowMetrics(activity).getBounds();
 
-    assertThat(windowMetrics.getBounds().width()).isEqualTo(400);
-    assertThat(windowMetrics.getBounds().height()).isEqualTo(600);
+      assertThat(bounds.width()).isEqualTo(400);
+      assertThat(bounds.height()).isEqualTo(600);
+    }
   }
 }
