b6bd2f904528b61cd42d82eeca166e79ec1ec169
Michael Hoisie <hoisie@google.com>
2024-11-20 11:44:21 -0800

    internal
    PiperOrigin-RevId: 698466030

--- shadows/multidex/src/main/java/org/robolectric/shadows/multidex/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Shadows for the Android Multidex Library.
- *
- * <p>To use this in your project, add the artifact {@code org.robolectric:shadows-multidex} to your
- * project.
- */
-package org.robolectric.shadows.multidex;
