1af1fa3cc77b92e4a2b48183cc35561f01a8b15d
Michael Hoisie <hoisie@google.com>
2024-11-20 00:41:14 +0000

    Fix regression in loading typefaces in legacy graphics
    In ShadowLegacyTypeface, many of the ClassName annotations were using
    array literals as they would appear in Java source code.
    
    Unfortunately, ShadowWrangler compares these array literals to what is
    returned by the Class.getName(). For array Class objects,
    Class.getName() returns the array type descriptor format, e.g.
    `[Ljava.lang.String;`. Update ShadowLegacyTypeface to use type
    descriptor format for arrays.
    
    Fixes #9732

--- shadows/framework/src/main/java/org/robolectric/shadows/ShadowLegacyTypeface.java
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowLegacyTypeface.java
@@ -171,26 +171,26 @@ public class ShadowLegacyTypeface extends ShadowTypeface {
   @HiddenApi
   @Implementation
   protected static Typeface createFromFamilies(
-      @ClassName("android.graphics.FontFamily[]") Object families) {
+      @ClassName("[Landroid.graphics.FontFamily;") Object families) {
     return null;
   }
 
   @HiddenApi
   @Implementation(maxSdk = N_MR1)
   protected static Typeface createFromFamiliesWithDefault(
-      @ClassName("android.graphics.FontFamily[]") Object families) {
+      @ClassName("[Landroid.graphics.FontFamily;") Object families) {
     return null;
   }
 
   @Implementation(minSdk = O, maxSdk = O_MR1)
   protected static Typeface createFromFamiliesWithDefault(
-      @ClassName("android.graphics.FontFamily[]") Object families, int weight, int italic) {
+      @ClassName("[Landroid.graphics.FontFamily;") Object families, int weight, int italic) {
     return createUnderlyingTypeface("fake-font", Typeface.NORMAL);
   }
 
   @Implementation(minSdk = P)
   protected static Typeface createFromFamiliesWithDefault(
-      @ClassName("android.graphics.FontFamily[]") Object families,
+      @ClassName("[Landroid.graphics.FontFamily;") Object families,
       String fallbackName,
       int weight,
       int italic) {
@@ -202,7 +202,7 @@ public class ShadowLegacyTypeface extends ShadowTypeface {
       String xmlPath,
       String fontDir,
       ArrayMap<String, Typeface> fontMap,
-      ArrayMap<String, @ClassName("android.graphics.FontFamily[]") Object> fallbackMap) {
+      ArrayMap<String, /*android.graphics.FontFamily[]*/ ?> fallbackMap) {
     fontMap.put("sans-serif", createUnderlyingTypeface("sans-serif", 0));
   }
 
@@ -215,7 +215,7 @@ public class ShadowLegacyTypeface extends ShadowTypeface {
   protected static void initSystemDefaultTypefaces(
       Map<String, Typeface> systemFontMap,
       Map<String, /*android.graphics.FontFamily[]*/ ?> fallbacks,
-      @ClassName("android.text.FontConfig$Alias[]") Object aliases) {}
+      @ClassName("[Landroid.text.FontConfig$Alias;") Object aliases) {}
 
   @Resetter
   public static synchronized void reset() {
