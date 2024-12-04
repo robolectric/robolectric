74ad6e004ea935d3ce50b4d6fe40f77506a30537
Michael Hoisie <hoisie@google.com>
2024-12-04 11:34:05 -0800

    Add a way to use the real TextUtils.ellipsize
    With Robolectric native graphics, it is possible to use the real Android
    framework code for TextUtils.ellipsize.
    
    The real code uses TextPaint and MeasuredParagraph, which are supported in
    native graphics.
    
    PiperOrigin-RevId: 702800146

--- shadows/framework/src/main/java/org/robolectric/shadows/ShadowTextUtils.java
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowTextUtils.java
@@ -1,10 +1,15 @@
 package org.robolectric.shadows;
 
+import static org.robolectric.util.reflector.Reflector.reflector;
+
 import android.text.TextPaint;
 import android.text.TextUtils;
 import android.text.TextUtils.TruncateAt;
 import org.robolectric.annotation.Implementation;
 import org.robolectric.annotation.Implements;
+import org.robolectric.util.reflector.Direct;
+import org.robolectric.util.reflector.ForType;
+import org.robolectric.util.reflector.Static;
 
 /** Implement {@link TextUtils#ellipsize} by truncating the text. */
 @SuppressWarnings({"UnusedDeclaration"})
@@ -14,6 +19,9 @@ public class ShadowTextUtils {
   @Implementation
   protected static CharSequence ellipsize(
       CharSequence text, TextPaint p, float avail, TruncateAt where) {
+    if (useRealEllipsize()) {
+      return reflector(TextUtilsReflector.class).ellipsize(text, p, avail, where);
+    }
     // This shadow follows the convention of ShadowPaint#measureText where each
     // characters width is 1.0.
     if (avail <= 0) {
@@ -23,5 +31,17 @@ public class ShadowTextUtils {
     } else {
       return text.subSequence(0, (int) avail);
     }
+  }
+
+  private static boolean useRealEllipsize() {
+    return ShadowView.useRealGraphics()
+        && Boolean.parseBoolean(System.getProperty("robolectric.useRealEllipsize", "false"));
+  }
+
+  @ForType(TextUtils.class)
+  interface TextUtilsReflector {
+    @Direct
+    @Static
+    CharSequence ellipsize(CharSequence text, TextPaint p, float avail, TruncateAt where);
   }
 }
