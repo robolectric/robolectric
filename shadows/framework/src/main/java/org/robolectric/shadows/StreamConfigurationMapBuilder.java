16f8a0c282734c1f4d38ed8d0c1e9f88186e7e8a
Googler <noreply@google.com>
2024-12-10 04:50:15 -0800

    Call real constructors in StreamConfigurationMapBuilder.
    Several arrays in StreamConfigurationMap are not initialized until the constructor is called.
    This change prevents NullPointerException that would occur when hitting code paths that iterate over these arrays.
    
    PiperOrigin-RevId: 704656917

--- shadows/framework/src/main/java/org/robolectric/shadows/StreamConfigurationMapBuilder.java
+++ shadows/framework/src/main/java/org/robolectric/shadows/StreamConfigurationMapBuilder.java
@@ -1,17 +1,22 @@
 package org.robolectric.shadows;
 
+import static org.robolectric.util.reflector.Reflector.reflector;
+
+import android.hardware.camera2.params.HighSpeedVideoConfiguration;
+import android.hardware.camera2.params.ReprocessFormatsMap;
 import android.hardware.camera2.params.StreamConfiguration;
+import android.hardware.camera2.params.StreamConfigurationDuration;
 import android.hardware.camera2.params.StreamConfigurationMap;
 import android.os.Build.VERSION_CODES;
 import android.util.Size;
-import android.util.SparseIntArray;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import org.robolectric.RuntimeEnvironment;
-import org.robolectric.util.ReflectionHelpers;
+import org.robolectric.util.reflector.Constructor;
+import org.robolectric.util.reflector.ForType;
 
 /** Builder for StreamConfigurationMap */
 public final class StreamConfigurationMapBuilder {
@@ -72,13 +77,21 @@ public final class StreamConfigurationMapBuilder {
   /** Builds a StreamConfigurationMap based on data previously added to this builder. */
   public StreamConfigurationMap build() {
     Collection<StreamConfiguration> configsList = new ArrayList<>();
-
+    boolean hasPrivateOutput = false;
     for (Map.Entry<Integer, Collection<Size>> entry : outputFormatWithSupportedSize.entrySet()) {
+      hasPrivateOutput =
+          hasPrivateOutput || (entry.getKey() == HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED);
       for (Size size : entry.getValue()) {
         configsList.add(
             new StreamConfiguration(
                 entry.getKey(), size.getWidth(), size.getHeight(), /* input= */ false));
       }
+    }
+    // The constructor of StreamConfigurationMap asserts that a
+    // HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED config exists.
+    if (!hasPrivateOutput) {
+      configsList.add(
+          new StreamConfiguration(HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED, 1, 1, false));
     }
 
     for (Map.Entry<Integer, Collection<Size>> entry : inputFormatWithSupportedSize.entrySet()) {
@@ -92,34 +105,175 @@ public final class StreamConfigurationMapBuilder {
     StreamConfiguration[] configs = new StreamConfiguration[configsList.size()];
     configsList.toArray(configs);
 
-    StreamConfigurationMap map = ReflectionHelpers.callConstructor(StreamConfigurationMap.class);
-    ReflectionHelpers.setField(StreamConfigurationMap.class, map, "mConfigurations", configs);
-
     if (RuntimeEnvironment.getApiLevel() < VERSION_CODES.M) {
-      HashMap<Integer, Integer> outputFormats = new HashMap<>();
-      for (int format : outputFormatWithSupportedSize.keySet()) {
-        outputFormats.put(format, outputFormatWithSupportedSize.get(format).size());
-      }
-      ReflectionHelpers.setField(
-          StreamConfigurationMap.class, map, "mOutputFormats", outputFormats);
+      return reflector(StreamConfigurationMapReflector.class)
+          .newStreamConfigurationMapL(
+              configs,
+              new StreamConfigurationDuration[0],
+              new StreamConfigurationDuration[0],
+              /* highSpeedVideoConfigurations= */ null);
+    } else if (RuntimeEnvironment.getApiLevel() < VERSION_CODES.Q) {
+      return reflector(StreamConfigurationMapReflector.class)
+          .newStreamConfigurationMapM(
+              configs,
+              new StreamConfigurationDuration[0],
+              new StreamConfigurationDuration[0],
+              /* depthConfigurations= */ null,
+              /* depthMinFrameDurations= */ null,
+              /* depthStallDurations= */ null,
+              /* highSpeedVideoConfigurations= */ null,
+              /* inputOutputFormatsMap= */ null,
+              /* listHighResolution= */ false);
+    } else if (RuntimeEnvironment.getApiLevel() <= VERSION_CODES.TIRAMISU) {
+      return reflector(StreamConfigurationMapReflector.class)
+          .newStreamConfigurationMapQ(
+              configs,
+              new StreamConfigurationDuration[0],
+              new StreamConfigurationDuration[0],
+              /* depthConfigurations= */ null,
+              /* depthMinFrameDurations= */ null,
+              /* depthStallDurations= */ null,
+              /* dynamicDepthConfigurations= */ null,
+              /* dynamicDepthMinFrameDurations= */ null,
+              /* dynamicDepthStallDurations= */ null,
+              /* heicConfigurations= */ null,
+              /* heicMinFrameDurations= */ null,
+              /* heicStallDurations= */ null,
+              /* highSpeedVideoConfigurations= */ null,
+              /* inputOutputFormatsMap= */ null,
+              /* listHighResolution= */ false);
+    } else if (RuntimeEnvironment.getApiLevel() <= VERSION_CODES.VANILLA_ICE_CREAM) {
+      return reflector(StreamConfigurationMapReflector.class)
+          .newStreamConfigurationMapV(
+              configs,
+              new StreamConfigurationDuration[0],
+              new StreamConfigurationDuration[0],
+              /* depthConfigurations= */ null,
+              /* depthMinFrameDurations= */ null,
+              /* depthStallDurations= */ null,
+              /* dynamicDepthConfigurations= */ null,
+              /* dynamicDepthMinFrameDurations= */ null,
+              /* dynamicDepthStallDurations= */ null,
+              /* heicConfigurations= */ null,
+              /* heicMinFrameDurations= */ null,
+              /* heicStallDurations= */ null,
+              /* jpegRConfigurations= */ null,
+              /* jpegRMinFrameDurations= */ null,
+              /* jpegRStallDurations= */ null,
+              /* highSpeedVideoConfigurations= */ null,
+              /* inputOutputFormatsMap= */ null,
+              /* listHighResolution= */ false);
     } else {
-      SparseIntArray outputFormats = new SparseIntArray();
-      for (int format : outputFormatWithSupportedSize.keySet()) {
-        outputFormats.put(format, outputFormatWithSupportedSize.get(format).size());
-      }
-      ReflectionHelpers.setField(
-          StreamConfigurationMap.class, map, "mOutputFormats", outputFormats);
-      ReflectionHelpers.setField(
-          StreamConfigurationMap.class, map, "mAllOutputFormats", outputFormats);
-
-      // Add input formats for reprocessing
-      SparseIntArray inputFormats = new SparseIntArray();
-      for (int format : inputFormatWithSupportedSize.keySet()) {
-        inputFormats.put(format, inputFormatWithSupportedSize.get(format).size());
-      }
-      ReflectionHelpers.setField(StreamConfigurationMap.class, map, "mInputFormats", inputFormats);
+      return reflector(StreamConfigurationMapReflector.class)
+          .newStreamConfigurationMapW(
+              configs,
+              new StreamConfigurationDuration[0],
+              new StreamConfigurationDuration[0],
+              /* depthConfigurations= */ null,
+              /* depthMinFrameDurations= */ null,
+              /* depthStallDurations= */ null,
+              /* dynamicDepthConfigurations= */ null,
+              /* dynamicDepthMinFrameDurations= */ null,
+              /* dynamicDepthStallDurations= */ null,
+              /* heicConfigurations= */ null,
+              /* heicMinFrameDurations= */ null,
+              /* heicStallDurations= */ null,
+              /* jpegRConfigurations= */ null,
+              /* jpegRMinFrameDurations= */ null,
+              /* jpegRStallDurations= */ null,
+              /* heicUltraHDRConfigurations= */ null,
+              /* heicUltraHDRMinFrameDurations= */ null,
+              /* heicUltraHDRStallDurations= */ null,
+              /* highSpeedVideoConfigurations= */ null,
+              /* inputOutputFormatsMap= */ null,
+              /* listHighResolution= */ false);
     }
-    return map;
+  }
+
+  // Constructors for StreamConfigurationMap per SDK version.
+  @ForType(StreamConfigurationMap.class)
+  interface StreamConfigurationMapReflector {
+    @Constructor
+    StreamConfigurationMap newStreamConfigurationMapL(
+        StreamConfiguration[] configurations,
+        StreamConfigurationDuration[] minFrameDurations,
+        StreamConfigurationDuration[] stallDurations,
+        HighSpeedVideoConfiguration[] highSpeedVideoConfigurations);
+
+    @Constructor
+    StreamConfigurationMap newStreamConfigurationMapM(
+        StreamConfiguration[] configurations,
+        StreamConfigurationDuration[] minFrameDurations,
+        StreamConfigurationDuration[] stallDurations,
+        StreamConfiguration[] depthConfigurations,
+        StreamConfigurationDuration[] depthMinFrameDurations,
+        StreamConfigurationDuration[] depthStallDurations,
+        HighSpeedVideoConfiguration[] highSpeedVideoConfigurations,
+        ReprocessFormatsMap inputOutputFormatsMap,
+        boolean listHighResolution);
+
+    @Constructor
+    StreamConfigurationMap newStreamConfigurationMapQ(
+        StreamConfiguration[] configurations,
+        StreamConfigurationDuration[] minFrameDurations,
+        StreamConfigurationDuration[] stallDurations,
+        StreamConfiguration[] depthConfigurations,
+        StreamConfigurationDuration[] depthMinFrameDurations,
+        StreamConfigurationDuration[] depthStallDurations,
+        StreamConfiguration[] dynamicDepthConfigurations,
+        StreamConfigurationDuration[] dynamicDepthMinFrameDurations,
+        StreamConfigurationDuration[] dynamicDepthStallDurations,
+        StreamConfiguration[] heicConfigurations,
+        StreamConfigurationDuration[] heicMinFrameDurations,
+        StreamConfigurationDuration[] heicStallDurations,
+        HighSpeedVideoConfiguration[] highSpeedVideoConfigurations,
+        ReprocessFormatsMap inputOutputFormatsMap,
+        boolean listHighResolution);
+
+    @Constructor
+    StreamConfigurationMap newStreamConfigurationMapV(
+        StreamConfiguration[] configurations,
+        StreamConfigurationDuration[] minFrameDurations,
+        StreamConfigurationDuration[] stallDurations,
+        StreamConfiguration[] depthConfigurations,
+        StreamConfigurationDuration[] depthMinFrameDurations,
+        StreamConfigurationDuration[] depthStallDurations,
+        StreamConfiguration[] dynamicDepthConfigurations,
+        StreamConfigurationDuration[] dynamicDepthMinFrameDurations,
+        StreamConfigurationDuration[] dynamicDepthStallDurations,
+        StreamConfiguration[] heicConfigurations,
+        StreamConfigurationDuration[] heicMinFrameDurations,
+        StreamConfigurationDuration[] heicStallDurations,
+        StreamConfiguration[] jpegRConfigurations,
+        StreamConfigurationDuration[] jpegRMinFrameDurations,
+        StreamConfigurationDuration[] jpegRStallDurations,
+        HighSpeedVideoConfiguration[] highSpeedVideoConfigurations,
+        ReprocessFormatsMap inputOutputFormatsMap,
+        boolean listHighResolution);
+
+    @Constructor
+    StreamConfigurationMap newStreamConfigurationMapW(
+        StreamConfiguration[] configurations,
+        StreamConfigurationDuration[] minFrameDurations,
+        StreamConfigurationDuration[] stallDurations,
+        StreamConfiguration[] depthConfigurations,
+        StreamConfigurationDuration[] depthMinFrameDurations,
+        StreamConfigurationDuration[] depthStallDurations,
+        StreamConfiguration[] dynamicDepthConfigurations,
+        StreamConfigurationDuration[] dynamicDepthMinFrameDurations,
+        StreamConfigurationDuration[] dynamicDepthStallDurations,
+        StreamConfiguration[] heicConfigurations,
+        StreamConfigurationDuration[] heicMinFrameDurations,
+        StreamConfigurationDuration[] heicStallDurations,
+        StreamConfiguration[] jpegRConfigurations,
+        StreamConfigurationDuration[] jpegRMinFrameDurations,
+        StreamConfigurationDuration[] jpegRStallDurations,
+        StreamConfiguration[] heicUltraHDRConfigurations,
+        StreamConfigurationDuration[] heicUltraHDRMinFrameDurations,
+        StreamConfigurationDuration[] heicUltraHDRStallDurations,
+        HighSpeedVideoConfiguration[] highSpeedVideoConfigurations,
+        ReprocessFormatsMap inputOutputFormatsMap,
+        boolean listHighResolution);
   }
 
   private StreamConfigurationMapBuilder() {}
