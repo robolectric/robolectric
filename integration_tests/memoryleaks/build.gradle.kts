6611717457a77c0e435c75bdffb782661d3ca6bf
utzcoz <utzcoz@outlook.com>
2025-05-11 23:13:30 +0800

    integration_tests: Exclude androidTest for modules that not required
    These modules use android library plugin but doesn't
    support androidTest tests, and this CL just removes androidTest
    for them to reduce extra building when running androidTest
    command.
    
    Signed-off-by: utzcoz <utzcoz@outlook.com>

--- integration_tests/memoryleaks/build.gradle.kts
+++ integration_tests/memoryleaks/build.gradle.kts
@@ -18,6 +18,13 @@ android {
     targetSdk = 35
     unitTests.isIncludeAndroidResources = true
   }
+
+  androidComponents {
+    beforeVariants(selector().all()) { variantBuilder ->
+      // memoryleaks does not support AndroidTest.
+      variantBuilder.enableAndroidTest = false
+    }
+  }
 }
 
 dependencies {
