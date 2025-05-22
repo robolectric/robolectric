f31155a481b89c8f35c920659d9d6cc4967fa50d
Googler <noreply@google.com>
2025-05-22 13:04:39 -0700

    Add functionality to block shadow BluetoothSocket connections until other events.
    Real BluetoothSocket connections are blocking but not necessarily immediate operations.
    The methods added here help exercise scenarios where the connection is blocked.
    When the shadow blocks connect(), the connection would unblock either when a shadow-specific unblock method is called,
    OR when the real close() method is called.
    Per the connect() documentation, "close() can be used to abort this call from another thread."
    If the shadow-specific unblock method is called, connect() will succeed.
    If close() is called, connect() will fail (throwing an IOException).
    
    PiperOrigin-RevId: 762092408

--- robolectric/src/test/java/org/robolectric/shadows/ShadowBluetoothSocketTest.java
+++ robolectric/src/test/java/org/robolectric/shadows/ShadowBluetoothSocketTest.java
@@ -1,7 +1,8 @@
 package org.robolectric.shadows;
 
 import static com.google.common.truth.Truth.assertThat;
-import static org.junit.Assert.fail;
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static org.junit.Assert.assertThrows;
 import static org.robolectric.Shadows.shadowOf;
 
 import android.bluetooth.BluetoothSocket;
@@ -10,6 +11,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.PipedOutputStream;
 import java.util.Arrays;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.atomic.AtomicBoolean;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -68,20 +73,117 @@ public class ShadowBluetoothSocketTest {
   public void close() throws Exception {
     bluetoothSocket.close();
 
-    try {
-      bluetoothSocket.connect();
-      fail();
-    } catch (IOException expected) {
-      // Expected.
-    }
+    assertThrows(IOException.class, () -> bluetoothSocket.connect());
   }
 
   @Test
-  public void connect() throws Exception {
+  public void unblockConnect_withoutBlocking_throws() throws Exception {
+    assertThrows(IllegalStateException.class, () -> shadowOf(bluetoothSocket).unblockConnect());
+  }
+
+  @Test
+  public void blockConnect_afterAlreadyBlocking_throws() throws Exception {
+    shadowOf(bluetoothSocket).blockConnect();
+    assertThrows(IllegalStateException.class, () -> shadowOf(bluetoothSocket).blockConnect());
+  }
+
+  @Test
+  public void blockConnect_afterUnblocked_throws() throws Exception {
+    shadowOf(bluetoothSocket).blockConnect();
+    shadowOf(bluetoothSocket).unblockConnect();
+    assertThrows(IllegalStateException.class, () -> shadowOf(bluetoothSocket).blockConnect());
+  }
+
+  @Test
+  public void blockConnect_afterConnectAlreadyDone_completesWithoutBlocking() throws Exception {
+    bluetoothSocket.connect();
+    shadowOf(bluetoothSocket).blockConnect();
+    // Test succeeds if blockConnect() completes.
+  }
+
+  @Test
+  public void connect_withoutBlocking_succeeds() throws Exception {
     assertThat(bluetoothSocket.isConnected()).isFalse();
     bluetoothSocket.connect();
     assertThat(bluetoothSocket.isConnected()).isTrue();
     bluetoothSocket.close();
+    assertThat(bluetoothSocket.isConnected()).isFalse();
+  }
+
+  @Test
+  public void connect_afterUnblocking_succeeds() throws Exception {
+    shadowOf(bluetoothSocket).blockConnect();
+    shadowOf(bluetoothSocket).unblockConnect();
+
+    bluetoothSocket.connect();
+    assertThat(bluetoothSocket.isConnected()).isTrue();
+    bluetoothSocket.close();
+    assertThat(bluetoothSocket.isConnected()).isFalse();
+  }
+
+  @Test
+  public void unblockConnect_isIdempotent() throws Exception {
+    shadowOf(bluetoothSocket).blockConnect();
+    shadowOf(bluetoothSocket).unblockConnect();
+    shadowOf(bluetoothSocket).unblockConnect();
+
+    bluetoothSocket.connect();
+    assertThat(bluetoothSocket.isConnected()).isTrue();
+  }
+
+  @Test
+  public void connect_whileBlocked_blockedUntilShadowUnblocksAndDoesNotThrow() throws Exception {
+    shadowOf(bluetoothSocket).blockConnect();
+
+    AtomicBoolean connectCompletedExceptionally = new AtomicBoolean(false);
+    CountDownLatch aboutToConnectLatch = new CountDownLatch(1);
+    CountDownLatch completedConnectLatch = new CountDownLatch(1);
+    Future<?> unused =
+        Executors.newSingleThreadExecutor()
+            .submit(
+                () -> {
+                  aboutToConnectLatch.countDown();
+                  try {
+                    bluetoothSocket.connect();
+                  } catch (IOException e) {
+                    connectCompletedExceptionally.set(true);
+                  }
+                  completedConnectLatch.countDown();
+                });
+    aboutToConnectLatch.await();
+
+    assertThat(completedConnectLatch.await(100, MILLISECONDS)).isFalse();
+    shadowOf(bluetoothSocket).unblockConnect();
+    assertThat(completedConnectLatch.await(100, MILLISECONDS)).isTrue();
+    assertThat(connectCompletedExceptionally.get()).isFalse();
+    assertThat(bluetoothSocket.isConnected()).isTrue();
+  }
+
+  @Test
+  public void connect_whileBlocked_blockedUntilCloseAndThrows() throws Exception {
+    shadowOf(bluetoothSocket).blockConnect();
+
+    AtomicBoolean connectCompletedExceptionally = new AtomicBoolean(false);
+    CountDownLatch aboutToConnectLatch = new CountDownLatch(1);
+    CountDownLatch completedConnectLatch = new CountDownLatch(1);
+    Future<?> unused =
+        Executors.newSingleThreadExecutor()
+            .submit(
+                () -> {
+                  aboutToConnectLatch.countDown();
+                  try {
+                    bluetoothSocket.connect();
+                  } catch (IOException e) {
+                    connectCompletedExceptionally.set(true);
+                  }
+                  completedConnectLatch.countDown();
+                });
+    aboutToConnectLatch.await();
+
+    assertThat(completedConnectLatch.await(100, MILLISECONDS)).isFalse();
+    bluetoothSocket.close();
+    assertThat(completedConnectLatch.await(100, MILLISECONDS)).isTrue();
+    assertThat(connectCompletedExceptionally.get()).isTrue();
     assertThat(bluetoothSocket.isConnected()).isFalse();
   }
 }
