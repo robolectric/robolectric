696bcc8f3b276b39418317c543aa4a218419b85d
Googler <noreply@google.com>
2025-06-20 08:46:58 -0700

    Add ability to specify is80211azNtbMeasurement to ShadowRangingResult
    Add ability to specify is80211azNtbMeasurement to ShadowRangingResult.
    This is available since Android V.
    
    PiperOrigin-RevId: 773719513

--- robolectric/src/test/java/org/robolectric/shadows/ShadowRangingResultTest.java
+++ robolectric/src/test/java/org/robolectric/shadows/ShadowRangingResultTest.java
@@ -3,6 +3,7 @@ package org.robolectric.shadows;
 import static android.os.Build.VERSION_CODES.P;
 import static android.os.Build.VERSION_CODES.R;
 import static android.os.Build.VERSION_CODES.S;
+import static android.os.Build.VERSION_CODES.VANILLA_ICE_CREAM;
 import static com.google.common.truth.Truth.assertThat;
 
 import android.net.MacAddress;
@@ -41,8 +42,8 @@ public class ShadowRangingResultTest {
   }
 
   @Test
-  @Config(minSdk = S)
-  public void testBuilder_sPlus() {
+  @Config(minSdk = S, maxSdk = VANILLA_ICE_CREAM)
+  public void testBuilder_sThroughV() {
     RangingResult result =
         new ShadowRangingResult.Builder(
                 RangingResult.STATUS_SUCCESS,
@@ -65,5 +66,33 @@ public class ShadowRangingResultTest {
     assertThat(result.getPeerHandle()).isNull();
     assertThat(result.getUnverifiedResponderLocation()).isNull();
     assertThat(result.is80211mcMeasurement()).isTrue();
+  }
+
+  @Test
+  @Config(minSdk = VANILLA_ICE_CREAM)
+  public void testBuilder_vPlus() {
+    RangingResult result =
+        new ShadowRangingResult.Builder(
+                RangingResult.STATUS_SUCCESS,
+                MacAddress.fromString("00:0a:95:9d:68:16"),
+                1548290142, // Timestamp
+                10000) // DistanceMm
+            .setRssi(100)
+            .setDistanceStandardDeviation(200)
+            .setNumAttemptedMeasurements(3)
+            .setNumSuccessfulMeasurements(5)
+            .setUnverifiedResponderLocation(null)
+            .setIs80211mcMeasurement(false)
+            .setIs80211azNtbMeasurement(true)
+            .build();
+    assertThat(result.getMacAddress()).isEqualTo(MacAddress.fromString("00:0a:95:9d:68:16"));
+    assertThat(result.getDistanceMm()).isEqualTo(10000);
+    assertThat(result.getRssi()).isEqualTo(100);
+    assertThat(result.getDistanceStdDevMm()).isEqualTo(200);
+    assertThat(result.getNumAttemptedMeasurements()).isEqualTo(3);
+    assertThat(result.getNumSuccessfulMeasurements()).isEqualTo(5);
+    assertThat(result.getPeerHandle()).isNull();
+    assertThat(result.getUnverifiedResponderLocation()).isNull();
+    assertThat(result.is80211azNtbMeasurement()).isTrue();
   }
 }
