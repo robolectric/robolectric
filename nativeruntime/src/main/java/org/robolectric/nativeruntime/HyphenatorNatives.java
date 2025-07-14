5dfa9db8adb215e67203a53f4a3be1f05cad9efd
Michael Hoisie <hoisie@google.com>
2025-07-14 11:14:13 -0700

    Add HyphenatorNatives
    The android_text_hyphenator.cpp JNI will bind to HyphenatorNatives.
    
    Note this isn't being invoked right now as the nativeruntime jars will need to
    be updated to contain the hyphenation data.
    
    For #10402
    
    PiperOrigin-RevId: 782974137

---
+++ nativeruntime/src/main/java/org/robolectric/nativeruntime/HyphenatorNatives.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.robolectric.nativeruntime;
+
+/**
+ * Native methods for {@link android.text.Hyphenator} JNI registration.
+ *
+ * <p>Native method signatures are derived from
+ * https://cs.android.com/android/platform/superproject/+/android-12.0.0_r1:frameworks/base/core/java/android/text/Hyphenator.java
+ */
+public final class HyphenatorNatives {
+  public static native void nInit();
+
+  private HyphenatorNatives() {}
+}
