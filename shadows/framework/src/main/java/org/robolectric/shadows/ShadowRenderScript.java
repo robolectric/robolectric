c034573c4d21ac5c0d721c2e3b1b7a3385ad8791
Michael Hoisie <hoisie@google.com>
2025-09-24 22:46:00 -0700

    Add a public API to check if a RenderScript is destroyed
    Also, add support for another Allocation method to avoid crashing.
    
    PiperOrigin-RevId: 811190484

--- shadows/framework/src/main/java/org/robolectric/shadows/ShadowRenderScript.java
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowRenderScript.java
@@ -3,14 +3,19 @@ package org.robolectric.shadows;
 import static android.os.Build.VERSION_CODES.P;
 import static android.os.Build.VERSION_CODES.Q;
 import static android.os.Build.VERSION_CODES.R;
+import static org.robolectric.util.reflector.Reflector.reflector;
 
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.renderscript.RenderScript;
 import org.robolectric.annotation.Implementation;
 import org.robolectric.annotation.Implements;
+import org.robolectric.annotation.RealObject;
+import org.robolectric.shadow.api.Shadow;
 import org.robolectric.util.ReflectionHelpers;
 import org.robolectric.util.ReflectionHelpers.ClassParameter;
+import org.robolectric.util.reflector.Direct;
+import org.robolectric.util.reflector.ForType;
 
 /**
  * Shadow for {@link RenderScript}. The main purpose of this shadow is to allow creation of
@@ -19,6 +24,9 @@ import org.robolectric.util.ReflectionHelpers.ClassParameter;
  */
 @Implements(RenderScript.class)
 public class ShadowRenderScript {
+
+  @RealObject RenderScript realRenderScript;
+  private boolean isDestroyed;
 
   /**
    * The real implementation of this method spawns a new background thread, so we shadow at this
@@ -31,6 +39,17 @@ public class ShadowRenderScript {
         ReflectionHelpers.callConstructor(
             RenderScript.class, ClassParameter.from(Context.class, ctx));
     ReflectionHelpers.setField(rs, "mContext", 1L);
+
+    // Initialize the `mMessageThread` field to ensure that the RenderScript.destroy doesn't throw
+    // an NPE.
+    Class<?> messageThreadClass =
+        ReflectionHelpers.loadClass(
+            Shadow.class.getClassLoader(), "android.renderscript.RenderScript$MessageThread");
+    ReflectionHelpers.setField(
+        rs,
+        "mMessageThread",
+        ReflectionHelpers.callConstructor(
+            messageThreadClass, ClassParameter.from(RenderScript.class, rs)));
     return rs;
   }
 
@@ -84,5 +103,27 @@ public class ShadowRenderScript {
   protected long rsnTypeCreate(
       long con, long eid, int x, int y, int z, boolean mips, boolean faces, int yuv) {
     return 1;
+  }
+
+  @Implementation
+  protected long rsnAllocationCreateTyped(long con, long type, int mip, int usage, long pointer) {
+    return 1;
+  }
+
+  @Implementation
+  protected void destroy() {
+    isDestroyed = true;
+
+    reflector(RenderScriptReflector.class, realRenderScript).destroy();
+  }
+
+  public boolean isDestroyed() {
+    return isDestroyed;
+  }
+
+  @ForType(RenderScript.class)
+  interface RenderScriptReflector {
+    @Direct
+    void destroy();
   }
 }
