d08c7bd519d70d8db093b04550104033940e30bd
Googler <noreply@google.com>
2025-10-01 16:57:00 -0700

    Change in RangingManager roboelectric shadow
    Allows to capture RangingPreference used for session start.
    
    PiperOrigin-RevId: 813972457

--- shadows/framework/src/main/java/org/robolectric/shadows/ShadowRangingManager.java
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowRangingManager.java
@@ -7,6 +7,8 @@ import android.os.RemoteException;
 import android.ranging.IRangingAdapter;
 import android.ranging.RangingCapabilities;
 import android.ranging.RangingManager;
+import android.ranging.RangingPreference;
+import javax.annotation.Nullable;
 import org.robolectric.annotation.Implements;
 import org.robolectric.annotation.RealObject;
 import org.robolectric.util.reflector.Accessor;
@@ -27,6 +29,20 @@ public class ShadowRangingManager {
         (FakeRangingAdapter)
             reflector(RangingManagerReflector.class, realRangingManager).getRangingAdapter();
     rangingAdapter.updateRangingCapabilities(rangingCapabilities);
+  }
+
+  /**
+   * Returns the ranging preference of the open ranging session.
+   *
+   * @return the ranging preference of the open ranging session, or null if there is no open
+   *     session.
+   */
+  @Nullable
+  public RangingPreference getOpenSessionRangingPreference() throws RemoteException {
+    FakeRangingAdapter rangingAdapter =
+        (FakeRangingAdapter)
+            reflector(RangingManagerReflector.class, realRangingManager).getRangingAdapter();
+    return rangingAdapter.getOpenSessionRangingPreference();
   }
 
   @ForType(RangingManager.class)
