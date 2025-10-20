75bb5465b4c97114e45343d208d36e9c28f5d7e3
Googler <noreply@google.com>
2025-10-20 13:16:16 -0700

    Add shadow for AttributionSource
    Add following shadow APIs so we could mark whether a specific AttributionSource is
    trusted.This should work for both standalone AttributionSource, as well as the
    chained AttributionSources
    - addTrustedAttributionSource
    - clearTrustedAttributionSource
    
    This is probably easiest solution for testing purpose, since the actual call goes
    into real system service, which is a bit difficult to shadow.
    
    PiperOrigin-RevId: 821775075

---
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowAttributionSource.java
@@ -0,0 +1,30 @@
+package org.robolectric.shadows;
+
+import android.content.AttributionSource;
+import android.content.Context;
+import android.os.Build.VERSION_CODES;
+import java.util.HashSet;
+import java.util.Set;
+import org.robolectric.annotation.Implementation;
+import org.robolectric.annotation.Implements;
+import org.robolectric.annotation.RealObject;
+
+/** Shadow for {@link AttributionSource}. */
+@Implements(value = AttributionSource.class, minSdk = VERSION_CODES.S, isInAndroidSdk = false)
+public final class ShadowAttributionSource {
+  @RealObject AttributionSource realAttributionSource;
+  private static final Set<AttributionSource> trustedAttributionSources = new HashSet<>();
+
+  public static void addTrustedAttributionSource(AttributionSource attributionSource) {
+    trustedAttributionSources.add(attributionSource);
+  }
+
+  public static void clearTrustedAttributionSources() {
+    trustedAttributionSources.clear();
+  }
+
+  @Implementation
+  protected boolean isTrusted(Context context) {
+    return trustedAttributionSources.contains(realAttributionSource);
+  }
+}
