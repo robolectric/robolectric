9c04acd3a22b4f6090981c50700a11d3af3df56f
Googler <noreply@google.com>
2025-10-21 11:28:51 -0700

    Use concurrent HashSet in ShadowAttributionSource
    So it can be thread safe.
    
    PiperOrigin-RevId: 822205447

--- shadows/framework/src/main/java/org/robolectric/shadows/ShadowAttributionSource.java
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowAttributionSource.java
@@ -3,7 +3,7 @@ package org.robolectric.shadows;
 import android.content.AttributionSource;
 import android.content.Context;
 import android.os.Build.VERSION_CODES;
-import java.util.HashSet;
+import com.google.common.collect.Sets;
 import java.util.Set;
 import org.robolectric.annotation.Implementation;
 import org.robolectric.annotation.Implements;
@@ -13,7 +13,8 @@ import org.robolectric.annotation.RealObject;
 @Implements(value = AttributionSource.class, minSdk = VERSION_CODES.S, isInAndroidSdk = false)
 public final class ShadowAttributionSource {
   @RealObject AttributionSource realAttributionSource;
-  private static final Set<AttributionSource> trustedAttributionSources = new HashSet<>();
+  private static final Set<AttributionSource> trustedAttributionSources =
+      Sets.newConcurrentHashSet();
 
   public static void addTrustedAttributionSource(AttributionSource attributionSource) {
     trustedAttributionSources.add(attributionSource);
