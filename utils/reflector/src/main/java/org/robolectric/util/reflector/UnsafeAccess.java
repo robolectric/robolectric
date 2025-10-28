bf632f4923e2f87b6b77a49877b9667cdadb6478
Michael Hoisie <hoisie@google.com>
2025-10-28 15:38:39 -0700

    Remove pre-11 logic in UnsafeAccess
    Robolectric now requires Java 11 or above.
    
    PiperOrigin-RevId: 825234669

--- utils/reflector/src/main/java/org/robolectric/util/reflector/UnsafeAccess.java
+++ utils/reflector/src/main/java/org/robolectric/util/reflector/UnsafeAccess.java
@@ -2,76 +2,16 @@ package org.robolectric.util.reflector;
 
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodHandles.Lookup;
-import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.security.ProtectionDomain;
-import org.robolectric.util.Util;
-import sun.misc.Unsafe;
 
 /** Access to sun.misc.Unsafe and the various scary things within. */
 @SuppressWarnings("NewApi")
 public class UnsafeAccess {
+  private static final Method privateLookupInMethod;
+  private static final Method defineClassMethod;
 
-  private static final Danger DANGER =
-      Util.getJavaVersion() < 11 ? new DangerPre11() : new Danger11Plus();
-
-  interface Danger {
-    <T> Class<?> defineClass(Class<T> iClass, String reflectorClassName, byte[] bytecode);
-  }
-
-  static <T> Class<?> defineClass(Class<T> iClass, String reflectorClassName, byte[] bytecode) {
-    return DANGER.defineClass(iClass, reflectorClassName, bytecode);
-  }
-
-  @SuppressWarnings("RethrowReflectiveOperationExceptionAsLinkageError")
-  private static class DangerPre11 implements Danger {
-    private final Unsafe unsafe;
-    private final Method defineClassMethod;
-
-    {
-      try {
-        Field unsafeField = Unsafe.class.getDeclaredField("theUnsafe");
-        unsafeField.setAccessible(true);
-        unsafe = (Unsafe) unsafeField.get(null);
-        defineClassMethod =
-            Unsafe.class.getMethod(
-                "defineClass",
-                String.class,
-                byte[].class,
-                int.class,
-                int.class,
-                ClassLoader.class,
-                ProtectionDomain.class);
-      } catch (NoSuchFieldException | IllegalAccessException | NoSuchMethodException e) {
-        throw new AssertionError(e);
-      }
-    }
-
-    @Override
-    public <T> Class<?> defineClass(Class<T> iClass, String reflectorClassName, byte[] bytecode) {
-      // use reflection to call since this method does not exist on JDK11
-      try {
-        return (Class<?>)
-            defineClassMethod.invoke(
-                unsafe,
-                reflectorClassName,
-                bytecode,
-                0,
-                bytecode.length,
-                iClass.getClassLoader(),
-                null);
-      } catch (IllegalAccessException | InvocationTargetException e) {
-        throw new AssertionError(e);
-      }
-    }
-  }
-
-  private static class Danger11Plus implements Danger {
-    private final Method privateLookupInMethod;
-    private final Method defineClassMethod;
-
-    {
+  static {
       try {
         privateLookupInMethod =
             MethodHandles.class.getMethod(
@@ -82,8 +22,8 @@ public class UnsafeAccess {
       }
     }
 
-    @Override
-    public <T> Class<?> defineClass(Class<T> iClass, String reflectorClassName, byte[] bytecode) {
+  public static <T> Class<?> defineClass(
+      Class<T> iClass, String reflectorClassName, byte[] bytecode) {
       MethodHandles.Lookup lookup = MethodHandles.lookup();
 
       try {
@@ -94,5 +34,4 @@ public class UnsafeAccess {
         throw new AssertionError(e);
       }
     }
-  }
 }
