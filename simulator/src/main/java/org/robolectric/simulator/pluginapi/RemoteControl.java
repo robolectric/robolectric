0dd6ff91b978241230db4d767a37f211d8b3b23e
Googler <noreply@google.com>
2025-10-31 16:22:11 -0700

    Updating the RemoteControl protocol to be notified every time the Simulator goes through a cycle of its main loop.
    The RemoteControl may need to wait for at least once cycle before it starts
    up.
    
    PiperOrigin-RevId: 826666889

--- simulator/src/main/java/org/robolectric/simulator/pluginapi/RemoteControl.java
+++ simulator/src/main/java/org/robolectric/simulator/pluginapi/RemoteControl.java
@@ -6,7 +6,18 @@ import java.time.Duration;
 
 /** Connects a Robolectric process to a remote control. */
 public interface RemoteControl {
+  /** Connects the RemoteControl to the Simulator. */
   void connect(UiAutomation uiAutomation, Looper looper);
 
+  /**
+   * Informs the RemoteControl that the Simulator has gone through another cycle.
+   *
+   * <p>Since this is called in every cycle, it needs to be *fast*. Use it to signal other threads.
+   *
+   * @param uptimeNanos Current uptime.
+   */
+  void onCycle(long uptimeNanos);
+
+  /** Disconnects the RemoteControl, shutting down any running jobs within the timeout. */
   void disconnect(Duration timeout);
 }
