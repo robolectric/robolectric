7e1ef1192e346d17fa2e37484ad241e8158eb364
Michael Hoisie <hoisie@google.com>
2025-11-14 16:10:09 -0800

    Refactor shadows for CaptureRequest.Builder
    Previously, CaptureRequest.Builder had a shadow to get and set camera
    characteristic properties. However, the problem was that the CaptureRequest
    object that was built did not have the same characteristics. Add a shadow at
    the CameraMetadataNative object that can be transferred between
    CaptureRequest.Builder and CaptureRequest
    
    PiperOrigin-RevId: 832492535

--- shadows/framework/src/main/java/org/robolectric/shadows/ShadowCameraMetadataNative.java
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowCameraMetadataNative.java
@@ -2,14 +2,34 @@ package org.robolectric.shadows;
 
 import static android.os.Build.VERSION_CODES.Q;
 import static android.os.Build.VERSION_CODES.R;
+import static org.robolectric.util.reflector.Reflector.reflector;
 
 import android.hardware.camera2.impl.CameraMetadataNative;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import org.robolectric.annotation.Implementation;
 import org.robolectric.annotation.Implements;
+import org.robolectric.annotation.RealObject;
+import org.robolectric.shadow.api.Shadow;
+import org.robolectric.util.reflector.Direct;
+import org.robolectric.util.reflector.ForType;
 
 /** Shadow class for {@link CameraMetadataNative} */
 @Implements(value = CameraMetadataNative.class, isInAndroidSdk = false)
 public class ShadowCameraMetadataNative {
+  @RealObject private CameraMetadataNative realObject;
+
+  private final Map<CameraMetadataNative.Key<?>, Object> characteristics =
+      Collections.synchronizedMap(new HashMap<>());
+
+  @Implementation
+  protected void __constructor__(CameraMetadataNative other) {
+    reflector(CameraMetadataNativeReflector.class, realObject).__constructor__(other);
+    ShadowCameraMetadataNative otherShadow = Shadow.extract(other);
+    this.characteristics.putAll(otherShadow.characteristics);
+  }
+
   @Implementation(minSdk = R)
   protected static long nativeAllocate() {
     return 1L;
@@ -28,5 +48,21 @@ public class ShadowCameraMetadataNative {
   @Implementation(maxSdk = Q)
   protected long nativeAllocateCopy(CameraMetadataNative other) {
     return 1L;
+  }
+
+  @Implementation
+  protected <T> void set(CameraMetadataNative.Key<T> key, T value) {
+    characteristics.put(key, value);
+  }
+
+  @Implementation
+  protected <T> T get(CameraMetadataNative.Key<T> key) {
+    return (T) characteristics.get(key);
+  }
+
+  @ForType(CameraMetadataNative.class)
+  interface CameraMetadataNativeReflector {
+    @Direct
+    void __constructor__(CameraMetadataNative other);
   }
 }
