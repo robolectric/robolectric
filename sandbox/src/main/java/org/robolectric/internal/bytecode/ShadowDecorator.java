c0449b05b15ee5a07f74cb11c126bc07d95d294c
Michael Hoisie <hoisie@google.com>
2025-12-10 10:44:25 -0800

    Fix ShadowCaptureRequestBuilderTest in Gradle
    For Android SDK levels B and below, there is an Android framework bug in the
    reflection logic in CameraMetadata.getKeys, in which the __robo_data__ fields
    get included in the field iteration. This will be fixed in the upcoming Android
    release. Add a temporary workaround by shadowing CameraMetadata.getKeys. Once
    the preinstrumented jars are updated, this shadow can be removed.
    
    PiperOrigin-RevId: 842789433

--- sandbox/src/main/java/org/robolectric/internal/bytecode/ShadowDecorator.java
+++ sandbox/src/main/java/org/robolectric/internal/bytecode/ShadowDecorator.java
@@ -1,6 +1,7 @@
 package org.robolectric.internal.bytecode;
 
 import com.google.auto.service.AutoService;
+import com.google.common.collect.ImmutableSet;
 import javax.annotation.Priority;
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.Type;
@@ -15,18 +16,30 @@ public class ShadowDecorator implements ClassInstrumentor.Decorator {
   private static final Type OBJECT_TYPE = Type.getType(Object.class);
   private static final String GET_ROBO_DATA_SIGNATURE = "()Ljava/lang/Object;";
 
+  // The Android framework uses reflection to iterate over fields on these camera2 classes. There
+  // was an Android framework bug that included __robo_data__ fields when iterating. This framework
+  // has been fixed in Android C (37), but it's simpler to make these fields private so it doesn't
+  // interfere with this reflection. Note that we don't want to make all __robo_data__ fields
+  // private for performance reasons.
+  private static final ImmutableSet<String> CLASSES_WITH_PRIVATE_ROBO_DATA_FIELDS =
+      ImmutableSet.of(
+          "android.hardware.camera2.CameraCharacteristics",
+          "android.hardware.camera2.CaptureRequest",
+          "android.hardware.camera2.CaptureResult");
+
   @Override
   public void decorate(MutableClass mutableClass) {
     mutableClass.addInterface(Type.getInternalName(ShadowedObject.class));
-
+    int access = Opcodes.ACC_SYNTHETIC | Opcodes.ACC_TRANSIENT;
+    if (CLASSES_WITH_PRIVATE_ROBO_DATA_FIELDS.contains(mutableClass.getName())) {
+      access |= Opcodes.ACC_PRIVATE;
+    } else {
+      access |= Opcodes.ACC_PUBLIC;
+    }
     mutableClass.addField(
         0,
         new FieldNode(
-            Opcodes.ACC_PUBLIC | Opcodes.ACC_SYNTHETIC | Opcodes.ACC_TRANSIENT,
-            ShadowConstants.CLASS_HANDLER_DATA_FIELD_NAME,
-            OBJECT_DESC,
-            OBJECT_DESC,
-            null));
+            access, ShadowConstants.CLASS_HANDLER_DATA_FIELD_NAME, OBJECT_DESC, OBJECT_DESC, null));
 
     addRoboGetDataMethod(mutableClass);
   }
