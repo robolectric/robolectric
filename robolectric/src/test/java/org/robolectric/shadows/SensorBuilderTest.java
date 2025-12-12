8b47e6a22c8a8d7bc0c42cab8c8bcadac7493dbe
Michael Hoisie <hoisie@google.com>
2025-12-11 16:21:18 -0800

    Add a Robolectric Builder for android.hardware.Sensor
    Sensor is pure Java code and should not need to
    be shadowed. Also, deprecate all public methods on ShadowSensor.
    
    PiperOrigin-RevId: 843417524

---
+++ robolectric/src/test/java/org/robolectric/shadows/SensorBuilderTest.java
@@ -0,0 +1,48 @@
+package org.robolectric.shadows;
+
+import static android.hardware.Sensor.TYPE_ACCELEROMETER;
+import static com.google.common.truth.Truth.assertThat;
+
+import android.hardware.Sensor;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/** Tests for {@link SensorBuilder}. */
+@RunWith(AndroidJUnit4.class)
+public class SensorBuilderTest {
+
+  @Test
+  public void createSensor_defaults() {
+    Sensor testSensor = SensorBuilder.newBuilder().build();
+    assertThat(testSensor.getType()).isEqualTo(0);
+    assertThat(testSensor.isWakeUpSensor()).isEqualTo(false);
+    assertThat(testSensor.getMaximumRange()).isEqualTo(0f);
+    assertThat(testSensor.getMinDelay()).isEqualTo(0);
+    assertThat(testSensor.getName()).isEqualTo(null);
+  }
+
+  @Test
+  public void createSensor() {
+    Sensor testSensor =
+        SensorBuilder.newBuilder()
+            .setType(TYPE_ACCELEROMETER)
+            .setWakeUpFlag(true)
+            .setMaximumRange(10f)
+            .setMinDelay(100)
+            .setName("test_sensor")
+            .build();
+
+    assertThat(testSensor.getType()).isEqualTo(TYPE_ACCELEROMETER);
+    assertThat(testSensor.isWakeUpSensor()).isEqualTo(true);
+    assertThat(testSensor.getMaximumRange()).isEqualTo(10f);
+    assertThat(testSensor.getMinDelay()).isEqualTo(100);
+    assertThat(testSensor.getName()).isEqualTo("test_sensor");
+  }
+
+  @Test
+  public void createSensor_falseWakeUpFlag() {
+    Sensor testSensor = SensorBuilder.newBuilder().setWakeUpFlag(true).setWakeUpFlag(false).build();
+    assertThat(testSensor.isWakeUpSensor()).isEqualTo(false);
+  }
+}
