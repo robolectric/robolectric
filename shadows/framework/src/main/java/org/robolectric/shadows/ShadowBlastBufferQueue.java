0714a65229cc154716fddf38b3f7720c6b5a0e87
Brett Chabot <brettchabot@google.com>
2026-01-23 12:29:07 -0800

    Make PixelCopy reuse the Surface's ImageReader in real view draw traversal mode.
    When useRealViewDrawTraversals are enabled, drawing will already be performed to
    an Surface backed by an ImageReader.
    
    This commit modifies ShadowPixelCopy to use ImageReader directly from the Surface
    in this mode.
    And fix the ImageReader dimensions to use the surface dimensions
    (which may be offset by surface insets) instead of the Display dimensions.
    
    PiperOrigin-RevId: 860211289

--- shadows/framework/src/main/java/org/robolectric/shadows/ShadowBlastBufferQueue.java
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowBlastBufferQueue.java
@@ -1,18 +1,28 @@
 package org.robolectric.shadows;
 
 import static android.os.Build.VERSION_CODES.TIRAMISU;
+import static com.google.common.base.Preconditions.checkState;
 
 import android.graphics.BLASTBufferQueue;
 import android.graphics.PixelFormat;
 import android.media.ImageReader;
-import android.view.Display;
 import android.view.Surface;
+import android.view.SurfaceControl;
 import org.robolectric.annotation.Implementation;
 import org.robolectric.annotation.Implements;
 import org.robolectric.shadow.api.Shadow;
 
 @Implements(value = BLASTBufferQueue.class, isInAndroidSdk = false, minSdk = TIRAMISU)
 public class ShadowBlastBufferQueue {
+
+  private int width;
+  private int height;
+
+  @Implementation
+  protected void update(SurfaceControl sc, int width, int height, int format) {
+    this.width = width;
+    this.height = height;
+  }
 
   /**
    * The real Android implementation is backed by native code which is currently unsupported on host
@@ -24,13 +34,11 @@ public class ShadowBlastBufferQueue {
   @Implementation
   protected Surface createSurfaceWithHandle() {
     if (ShadowView.useRealDrawTraversals()) {
-      Display display = ShadowDisplay.getDefaultDisplay();
-
-      int width = display.getWidth();
-      int height = display.getHeight();
-
+      checkState(width > 0);
+      checkState(height > 0);
       ImageReader imageReader = ImageReader.newInstance(width, height, PixelFormat.RGBA_8888, 1);
       Surface surface = imageReader.getSurface();
+
       ShadowNativeSurface shadowSurface = Shadow.extract(surface);
       shadowSurface.setContainerImageReader(imageReader);
       return surface;
