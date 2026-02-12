dc478067170bc0b0c4c95afbbdd83243103b5abe
utzcoz <utzcoz@outlook.com>
2026-02-13 00:46:31 +0800

    chore: Add hint and integration tests of rap for AGP 9.0
    Signed-off-by: utzcoz <utzcoz@outlook.com>

---
+++ integration_tests/rap-kotlin/src/test/java/org/robolectric/rap/kotlin/ExtendedShadowApplicationTest.java
@@ -0,0 +1,21 @@
+package org.robolectric.rap.kotlin;
+
+import static android.os.Build.VERSION_CODES.S;
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
+import org.robolectric.annotation.Config;
+import org.robolectric.shadow.api.Shadow;
+
+@RunWith(RobolectricTestRunner.class)
+public final class ExtendedShadowApplicationTest {
+  @Config(sdk = S)
+  @Test
+  public void behaviorBeingTested_expectedResult() {
+    ExtendedShadowApplication application = Shadow.extract(RuntimeEnvironment.getApplication());
+    assertThat(application).isNotNull();
+  }
+}
