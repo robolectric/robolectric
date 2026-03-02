61b87d44f11dbac45072e89edd2adcd48f9fd0cd
Googler <noreply@google.com>
2026-03-02 15:21:24 -0800

    Add support for setNetworkValidationStatus (API 35) in PreciseDataConnectionState
    Add shadow implementation for PreciseDataConnectionState.setNetworkValidationStatus(int)
    
    PiperOrigin-RevId: 877602474

--- shadows/framework/src/main/java/org/robolectric/shadows/PreciseDataConnectionStateBuilder.java
+++ shadows/framework/src/main/java/org/robolectric/shadows/PreciseDataConnectionStateBuilder.java
@@ -4,6 +4,7 @@ import android.net.LinkProperties;
 import android.os.Build.VERSION_CODES;
 import android.telephony.PreciseDataConnectionState;
 import android.telephony.data.ApnSetting;
+import com.google.errorprone.annotations.CanIgnoreReturnValue;
 import org.robolectric.RuntimeEnvironment;
 import org.robolectric.util.ReflectionHelpers;
 import org.robolectric.util.ReflectionHelpers.ClassParameter;
@@ -18,6 +19,7 @@ public class PreciseDataConnectionStateBuilder {
   private LinkProperties linkProperties;
   private ApnSetting apnSetting;
   private int dataFailCause;
+  private int networkValidationStatus;
 
   private PreciseDataConnectionStateBuilder() {}
 
@@ -25,38 +27,51 @@ public class PreciseDataConnectionStateBuilder {
     return new PreciseDataConnectionStateBuilder();
   }
 
+  @CanIgnoreReturnValue
   public PreciseDataConnectionStateBuilder setDataState(int dataState) {
     this.dataState = dataState;
     return this;
   }
 
+  @CanIgnoreReturnValue
   public PreciseDataConnectionStateBuilder setNetworkType(int networkType) {
     this.networkType = networkType;
     return this;
   }
 
+  @CanIgnoreReturnValue
   public PreciseDataConnectionStateBuilder setTransportType(int transportType) {
     this.transportType = transportType;
     return this;
   }
 
+  @CanIgnoreReturnValue
   public PreciseDataConnectionStateBuilder setLinkProperties(LinkProperties linkProperties) {
     this.linkProperties = linkProperties;
     return this;
   }
 
+  @CanIgnoreReturnValue
   public PreciseDataConnectionStateBuilder setId(int id) {
     this.id = id;
     return this;
   }
 
+  @CanIgnoreReturnValue
   public PreciseDataConnectionStateBuilder setApnSetting(ApnSetting apnSetting) {
     this.apnSetting = apnSetting;
     return this;
   }
 
+  @CanIgnoreReturnValue
   public PreciseDataConnectionStateBuilder setDataFailCause(int dataFailCause) {
     this.dataFailCause = dataFailCause;
+    return this;
+  }
+
+  @CanIgnoreReturnValue
+  public PreciseDataConnectionStateBuilder setNetworkValidationStatus(int networkValidationStatus) {
+    this.networkValidationStatus = networkValidationStatus;
     return this;
   }
 
@@ -75,15 +90,21 @@ public class PreciseDataConnectionStateBuilder {
           ClassParameter.from(int.class, dataFailCause),
           ClassParameter.from(ApnSetting.class, apnSetting));
     } else {
-      return new PreciseDataConnectionState.Builder()
-          .setTransportType(transportType)
-          .setId(id)
-          .setState(dataState)
-          .setNetworkType(networkType)
-          .setLinkProperties(linkProperties)
-          .setFailCause(dataFailCause)
-          .setApnSetting(apnSetting)
-          .build();
+      PreciseDataConnectionState.Builder builder =
+          new PreciseDataConnectionState.Builder()
+              .setTransportType(transportType)
+              .setId(id)
+              .setState(dataState)
+              .setNetworkType(networkType)
+              .setLinkProperties(linkProperties)
+              .setFailCause(dataFailCause)
+              .setApnSetting(apnSetting);
+
+      if (apiLevel >= VERSION_CODES.VANILLA_ICE_CREAM) {
+        builder = builder.setNetworkValidationStatus(networkValidationStatus);
+      }
+
+      return builder.build();
     }
   }
 }
