aa2358910a3507c2f31cbbea840da3ce6d6f005e
Googler <noreply@google.com>
2026-03-25 13:50:47 -0700

    ShadowAppFunctionManager: add setAppFunctionExists.
    This helps emulates scenarios where the app function is
    not found in system server.
    
    PiperOrigin-RevId: 889419181

--- robolectric/src/test/java/org/robolectric/shadows/ShadowAppFunctionManagerTest.java
+++ robolectric/src/test/java/org/robolectric/shadows/ShadowAppFunctionManagerTest.java
@@ -4,14 +4,18 @@ import static android.os.Build.VERSION_CODES.BAKLAVA;
 import static androidx.test.core.app.ApplicationProvider.getApplicationContext;
 import static com.google.common.truth.Truth.assertThat;
 import static com.google.common.util.concurrent.MoreExecutors.directExecutor;
+import static org.junit.Assert.assertThrows;
 
 import android.app.appfunctions.AppFunctionManager;
 import android.content.Context;
+import android.os.OutcomeReceiver;
 import com.google.common.util.concurrent.SettableFuture;
+import java.util.concurrent.ExecutionException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
+import org.robolectric.shadow.api.Shadow;
 
 @RunWith(RobolectricTestRunner.class)
 @Config(minSdk = BAKLAVA)
@@ -39,11 +43,10 @@ public class ShadowAppFunctionManagerTest {
         Context.APP_FUNCTION_SERVICE, /* available= */ true);
     AppFunctionManager appFunctionManager = getManager();
 
-    SettableFuture<Boolean> future = SettableFuture.create();
-    appFunctionManager.isAppFunctionEnabled(
-        "function_identifier", "com.android.packagename", directExecutor(), future::set);
+    boolean isAppFunctionEnabled =
+        isAppFunctionEnabled(appFunctionManager, "function_identifier", "com.android.packagename");
 
-    assertThat(future.get()).isTrue();
+    assertThat(isAppFunctionEnabled).isTrue();
   }
 
   @Test
@@ -52,18 +55,11 @@ public class ShadowAppFunctionManagerTest {
         Context.APP_FUNCTION_SERVICE, /* available= */ true);
     AppFunctionManager appFunctionManager = getManager();
 
-    SettableFuture<Void> setFuture = SettableFuture.create();
-    appFunctionManager.setAppFunctionEnabled(
-        "function_identifier",
-        AppFunctionManager.APP_FUNCTION_STATE_ENABLED,
-        directExecutor(),
-        setFuture::set);
-    setFuture.get();
+    setAppFunctionEnabled(
+        appFunctionManager, "function_identifier", AppFunctionManager.APP_FUNCTION_STATE_ENABLED);
 
-    SettableFuture<Boolean> getFuture = SettableFuture.create();
-    appFunctionManager.isAppFunctionEnabled(
-        "function_identifier", directExecutor(), getFuture::set);
-    assertThat(getFuture.get()).isTrue();
+    boolean isAppFunctionEnabled = isAppFunctionEnabled(appFunctionManager, "function_identifier");
+    assertThat(isAppFunctionEnabled).isTrue();
   }
 
   @Test
@@ -72,22 +68,138 @@ public class ShadowAppFunctionManagerTest {
         Context.APP_FUNCTION_SERVICE, /* available= */ true);
     AppFunctionManager appFunctionManager = getManager();
 
-    SettableFuture<Void> setFuture = SettableFuture.create();
-    appFunctionManager.setAppFunctionEnabled(
-        "function_identifier",
-        AppFunctionManager.APP_FUNCTION_STATE_DISABLED,
-        directExecutor(),
-        setFuture::set);
-    setFuture.get();
+    setAppFunctionEnabled(
+        appFunctionManager, "function_identifier", AppFunctionManager.APP_FUNCTION_STATE_DISABLED);
 
-    SettableFuture<Boolean> getFuture = SettableFuture.create();
-    appFunctionManager.isAppFunctionEnabled(
-        "function_identifier", directExecutor(), getFuture::set);
-    assertThat(getFuture.get()).isFalse();
+    boolean isAppFunctionEnabled = isAppFunctionEnabled(appFunctionManager, "function_identifier");
+    assertThat(isAppFunctionEnabled).isFalse();
   }
 
-  private AppFunctionManager getManager() {
+  @Test
+  public void setAppFunctionEnabled_invalidState_throwsIllegalArgumentException() throws Exception {
+    ShadowServiceManager.setServiceAvailability(
+        Context.APP_FUNCTION_SERVICE, /* available= */ true);
+    AppFunctionManager appFunctionManager = getManager();
+
+    IllegalArgumentException e =
+        assertThrows(
+            IllegalArgumentException.class,
+            () -> setAppFunctionEnabled(appFunctionManager, "function_identifier", 12348124));
+    assertThat(e).hasMessageThat().contains("Invalid state: 12348124");
+  }
+
+  @Test
+  public void setAppFunctionExists_false_setsIllegalArgumentExceptionInCallbacks()
+      throws Exception {
+    ShadowServiceManager.setServiceAvailability(
+        Context.APP_FUNCTION_SERVICE, /* available= */ true);
+    AppFunctionManager appFunctionManager = getManager();
+
+    Shadow.<ShadowAppFunctionManager>extract(appFunctionManager)
+        .setAppFunctionExists("function_identifier", false);
+
+    ExecutionException isAppFunctionEnabledException =
+        assertThrows(
+            ExecutionException.class,
+            () -> isAppFunctionEnabled(appFunctionManager, "function_identifier"));
+    assertThat(isAppFunctionEnabledException)
+        .hasCauseThat()
+        .isInstanceOf(IllegalArgumentException.class);
+    assertThat(isAppFunctionEnabledException)
+        .hasCauseThat()
+        .hasMessageThat()
+        .contains("App function not found.");
+    ExecutionException setAppFunctionEnabledException =
+        assertThrows(
+            ExecutionException.class,
+            () ->
+                setAppFunctionEnabled(
+                    appFunctionManager,
+                    "function_identifier",
+                    AppFunctionManager.APP_FUNCTION_STATE_ENABLED));
+    assertThat(setAppFunctionEnabledException)
+        .hasCauseThat()
+        .isInstanceOf(IllegalArgumentException.class);
+    assertThat(setAppFunctionEnabledException)
+        .hasCauseThat()
+        .hasMessageThat()
+        .contains("Function function_identifier does not exist");
+  }
+
+  @Test
+  public void setAppFunctionExists_didNotExist_resetsState() throws Exception {
+    ShadowServiceManager.setServiceAvailability(
+        Context.APP_FUNCTION_SERVICE, /* available= */ true);
+    AppFunctionManager appFunctionManager = getManager();
+    setAppFunctionEnabled(
+        appFunctionManager, "function_identifier", AppFunctionManager.APP_FUNCTION_STATE_DISABLED);
+
+    Shadow.<ShadowAppFunctionManager>extract(appFunctionManager)
+        .setAppFunctionExists("function_identifier", false);
+    Shadow.<ShadowAppFunctionManager>extract(appFunctionManager)
+        .setAppFunctionExists("function_identifier", true);
+
+    boolean isAppFunctionEnabled = isAppFunctionEnabled(appFunctionManager, "function_identifier");
+    assertThat(isAppFunctionEnabled).isTrue();
+  }
+
+  @Test
+  public void setAppFunctionExists_alreadyExists_doesNotOverrideState() throws Exception {
+    ShadowServiceManager.setServiceAvailability(
+        Context.APP_FUNCTION_SERVICE, /* available= */ true);
+    AppFunctionManager appFunctionManager = getManager();
+    setAppFunctionEnabled(
+        appFunctionManager, "function_identifier", AppFunctionManager.APP_FUNCTION_STATE_DISABLED);
+
+    Shadow.<ShadowAppFunctionManager>extract(appFunctionManager)
+        .setAppFunctionExists("function_identifier", true);
+
+    boolean isAppFunctionEnabled = isAppFunctionEnabled(appFunctionManager, "function_identifier");
+    assertThat(isAppFunctionEnabled).isFalse();
+  }
+
+  private static AppFunctionManager getManager() {
     return (AppFunctionManager)
         getApplicationContext().getSystemService(Context.APP_FUNCTION_SERVICE);
+  }
+
+  private static boolean isAppFunctionEnabled(
+      AppFunctionManager appFunctionManager, String functionIdentifier, String targetPackage)
+      throws Exception {
+    SettableFuture<Boolean> future = SettableFuture.create();
+    appFunctionManager.isAppFunctionEnabled(
+        functionIdentifier, targetPackage, directExecutor(), toOutcomeReceiver(future));
+    return future.get();
+  }
+
+  private static boolean isAppFunctionEnabled(
+      AppFunctionManager appFunctionManager, String functionIdentifier) throws Exception {
+    SettableFuture<Boolean> future = SettableFuture.create();
+    appFunctionManager.isAppFunctionEnabled(
+        functionIdentifier, directExecutor(), toOutcomeReceiver(future));
+    return future.get();
+  }
+
+  private static void setAppFunctionEnabled(
+      AppFunctionManager appFunctionManager, String functionIdentifier, int state)
+      throws Exception {
+    SettableFuture<Void> future = SettableFuture.create();
+    appFunctionManager.setAppFunctionEnabled(
+        functionIdentifier, state, directExecutor(), toOutcomeReceiver(future));
+    future.get();
+  }
+
+  private static <T> OutcomeReceiver<T, Exception> toOutcomeReceiver(SettableFuture<T> future) {
+    return new OutcomeReceiver<T, Exception>() {
+      @Override
+      public void onResult(T result) {
+        future.set(result);
+      }
+
+      @Override
+      public void onError(Exception e) {
+        future.setException(e);
+      }
+    };
   }
 }
--- shadows/framework/src/main/java/org/robolectric/shadows/ShadowAppFunctionManager.java
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowAppFunctionManager.java
@@ -1,6 +1,7 @@
 package org.robolectric.shadows;
 
 import static android.os.Build.VERSION_CODES.BAKLAVA;
+import static com.google.common.base.Preconditions.checkArgument;
 import static org.robolectric.util.reflector.Reflector.reflector;
 
 import android.app.appfunctions.AppFunctionManager;
@@ -16,8 +17,10 @@ import org.robolectric.util.reflector.Accessor;
 import org.robolectric.util.reflector.ForType;
 
 /** Shadow for the {@link AppFunctionManager} framework class. */
-@Implements(value = AppFunctionManager.class, minSdk = BAKLAVA)
+@Implements(value = AppFunctionManager.class, minSdk = BAKLAVA, isInAndroidSdk = false)
 public class ShadowAppFunctionManager {
+
+  private static final int APP_FUNCTION_STATE_DOES_NOT_EXIST = -1234;
 
   @RealObject private AppFunctionManager realAppFunctionManager;
 
@@ -30,12 +33,17 @@ public class ShadowAppFunctionManager {
       Executor executor,
       OutcomeReceiver<Boolean, Exception> callback) {
     String functionIdKey = functionIdKey(targetPackage, functionIdentifier);
-    // TODO: Better handling of default state.
-    boolean enabled =
+    int state =
         appFunctionEnabledMap.getOrDefault(
-                functionIdKey, AppFunctionManager.APP_FUNCTION_STATE_DEFAULT)
-            != AppFunctionManager.APP_FUNCTION_STATE_DISABLED;
-    executor.execute(() -> callback.onResult(enabled));
+            functionIdKey, AppFunctionManager.APP_FUNCTION_STATE_DEFAULT);
+    if (state == APP_FUNCTION_STATE_DOES_NOT_EXIST) {
+      executor.execute(
+          () -> callback.onError(new IllegalArgumentException("App function not found.")));
+    } else {
+      // TODO: Better handling of default state.
+      executor.execute(
+          () -> callback.onResult(state != AppFunctionManager.APP_FUNCTION_STATE_DISABLED));
+    }
   }
 
   @Implementation
@@ -44,15 +52,61 @@ public class ShadowAppFunctionManager {
       int state,
       Executor executor,
       OutcomeReceiver<Void, Exception> callback) {
+    checkValidAppFunctionState(state);
     String functionIdKey =
         functionIdKey(
             reflector(AppFunctionManagerReflector.class, realAppFunctionManager)
                 .getContext()
                 .getPackageName(),
             functionIdentifier);
-    // TODO: Validate state.
-    appFunctionEnabledMap.put(functionIdKey, state);
-    executor.execute(() -> callback.onResult(null));
+    int newState =
+        appFunctionEnabledMap.compute(
+            functionIdKey,
+            (key, oldState) -> {
+              if (oldState != null && oldState == APP_FUNCTION_STATE_DOES_NOT_EXIST) {
+                return oldState;
+              }
+              return state;
+            });
+    if (newState == APP_FUNCTION_STATE_DOES_NOT_EXIST) {
+      executor.execute(
+          () ->
+              callback.onError(
+                  new IllegalArgumentException(
+                      String.format("Function %s does not exist", functionIdentifier))));
+    } else {
+      executor.execute(() -> callback.onResult(null));
+    }
+  }
+
+  /**
+   * Allows simulating the app function not existing in system server by making the {@link
+   * AppFunctionManager} return an {@link IllegalArgumentException} in the associated error
+   * callbacks if {@code exists} is {@code false}.
+   */
+  public void setAppFunctionExists(String functionIdentifier, boolean exists) {
+    String functionIdKey =
+        functionIdKey(
+            reflector(AppFunctionManagerReflector.class, realAppFunctionManager)
+                .getContext()
+                .getPackageName(),
+            functionIdentifier);
+    if (exists) {
+      appFunctionEnabledMap.computeIfPresent(
+          functionIdKey,
+          (key, oldState) -> oldState == APP_FUNCTION_STATE_DOES_NOT_EXIST ? null : oldState);
+    } else {
+      appFunctionEnabledMap.put(functionIdKey, APP_FUNCTION_STATE_DOES_NOT_EXIST);
+    }
+  }
+
+  private static void checkValidAppFunctionState(int state) {
+    checkArgument(
+        state == AppFunctionManager.APP_FUNCTION_STATE_DISABLED
+            || state == AppFunctionManager.APP_FUNCTION_STATE_ENABLED
+            || state == AppFunctionManager.APP_FUNCTION_STATE_DEFAULT,
+        "Invalid state: %s",
+        state);
   }
 
   private static String functionIdKey(String targetPackage, String functionIdentifier) {
