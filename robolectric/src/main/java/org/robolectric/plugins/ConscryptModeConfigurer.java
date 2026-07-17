282c2e2a1a4b40dbb361d4039d34f8e82b5d38f9
Michael Hoisie <hoisie@google.com>
2026-07-17 11:29:55 -0700

    Enable Conscrypt for Mac arm64 by default
    As of Conscrypt 2.6.0, there are prebuilt libraries for Mac arm64 contained in
    the UberJar. Update the ConscryptModeConfigurer to default to ON for
    all platforms.
    
    PiperOrigin-RevId: 949673707

--- robolectric/src/main/java/org/robolectric/plugins/ConscryptModeConfigurer.java
+++ robolectric/src/main/java/org/robolectric/plugins/ConscryptModeConfigurer.java
@@ -1,15 +1,12 @@
 package org.robolectric.plugins;
 
-import static com.google.common.base.StandardSystemProperty.OS_ARCH;
 
 import com.google.auto.service.AutoService;
-import java.util.Locale;
 import java.util.Properties;
 import org.robolectric.annotation.ConscryptMode;
 import org.robolectric.annotation.ConscryptMode.Mode;
 import org.robolectric.pluginapi.config.Configurer;
 import org.robolectric.plugins.config.SingleValueConfigurer;
-import org.robolectric.util.OsUtil;
 
 /** Provides configuration to Robolectric for its @{@link ConscryptMode} annotation. */
 @AutoService(Configurer.class)
@@ -21,16 +18,8 @@ public class ConscryptModeConfigurer
     super(
         ConscryptMode.class,
         ConscryptMode.Mode.class,
-        defaultValue(systemProperties),
+        Mode.ON,
         propertyFileLoader,
         systemProperties);
-  }
-
-  private static ConscryptMode.Mode defaultValue(Properties properties) {
-    String arch = properties.getProperty(OS_ARCH.key(), "").toLowerCase(Locale.ROOT);
-    if (OsUtil.isMac() && arch.equals("aarch64")) {
-      return Mode.OFF;
-    }
-    return Mode.ON;
   }
 }
