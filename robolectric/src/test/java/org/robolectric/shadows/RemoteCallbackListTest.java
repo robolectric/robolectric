9aa987e6464d64f8d6851c737d6fe46c2ed34e04
Googler <noreply@google.com>
2026-07-22 11:30:22 -0700

    Delete ShadowRemoteCallbackList
    Renamed ShadowRemoteCallbackListTest to RemoteCallbackListTest
    It continues to pass even with the shadow deleted, which means we don't
    need the shadow.
    
    PiperOrigin-RevId: 952237056

--- robolectric/src/test/java/org/robolectric/shadows/RemoteCallbackListTest.java
+++ robolectric/src/test/java/org/robolectric/shadows/RemoteCallbackListTest.java
@@ -1,5 +1,6 @@
 package org.robolectric.shadows;
 
+import static android.os.Build.VERSION_CODES.BAKLAVA;
 import static com.google.common.truth.Truth.assertThat;
 
 import android.os.Binder;
@@ -7,12 +8,20 @@ import android.os.IBinder;
 import android.os.IInterface;
 import android.os.RemoteCallbackList;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.robolectric.annotation.Config;
 
+/**
+ * Unit test which verifies that RemoteCallbackList works as expected in robolectric.
+ * ShadowRemoteCallback list used to exist, but this test proves that everything works without the
+ * shadow.
+ */
 @RunWith(AndroidJUnit4.class)
-public class ShadowRemoteCallbackListTest {
+public class RemoteCallbackListTest {
 
   private RemoteCallbackList<Foo> fooRemoteCallbackList;
 
@@ -36,6 +45,29 @@ public class ShadowRemoteCallbackListTest {
     fooRemoteCallbackList.register(new Foo());
 
     assertThat(fooRemoteCallbackList.getRegisteredCallbackCount()).isEqualTo(1);
+  }
+
+  @Test
+  @Config(minSdk = BAKLAVA)
+  public void broadcast_registeredCallbacks_invokesConsumer() {
+    Foo callback1 = new Foo();
+    Foo callback2 = new Foo();
+    fooRemoteCallbackList.register(callback1);
+    fooRemoteCallbackList.register(callback2);
+
+    List<Foo> calledCallbacks = new ArrayList<>();
+    fooRemoteCallbackList.broadcast((Foo cb) -> calledCallbacks.add(cb));
+
+    assertThat(calledCallbacks).containsExactly(callback1, callback2);
+  }
+
+  @Test
+  @Config(minSdk = BAKLAVA)
+  public void broadcast_emptyList_doesNotInvokeConsumer() {
+    List<Foo> calledCallbacks = new ArrayList<>();
+    fooRemoteCallbackList.broadcast((Foo cb) -> calledCallbacks.add(cb));
+
+    assertThat(calledCallbacks).isEmpty();
   }
 
   private static class Foo implements IInterface {
